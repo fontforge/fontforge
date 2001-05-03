0b06042f78b3f4c60fea8ddc7bd61d12d635e187
George Williams <pfaedit@users.sourceforge.net>
2001-05-03 20:09:27 +0000

    Set the date stamp properly

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,4 +1,4 @@
 #include <time.h>
 
-const time_t link_time = 988492233;
-const char *link_time_str = "14:10 28-4-2001";
+const time_t link_time = 988918140;
+const char *link_time_str = "12:29 3-5-2001";
