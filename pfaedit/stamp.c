ab90c7dc8f2ddd56990ee3ece46fc54494e7e870
George Williams <pfaedit@users.sourceforge.net>
2001-09-05 16:21:29 +0000

    Fix yesterday's install problems (I hope)

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,4 +1,4 @@
 #include <time.h>
 
-const time_t link_time = 999671451;
-const char *link_time_str = "23:30 4-9-2001";
+const time_t link_time = 999704779;
+const char *link_time_str = "8:46 5-9-2001";
