2f8631cfa900607c11510003f699acf709f91619
George Williams <pfaedit@users.sourceforge.net>
2002-03-06 23:41:26 +0000

    Support for the type1 files produced by GNU type1utils 	(vaguely similar to .gsf files)

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1015398123;
-const char *source_modtime_str = "23:02 5-3-2002";
-const char *source_version_str = "020305";
+const time_t source_modtime = 1015445464;
+const char *source_modtime_str = "12:11 6-3-2002";
+const char *source_version_str = "020306";
