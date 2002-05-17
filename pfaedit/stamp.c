65a1dcf91411904074ad85a93d1b0f7ee87ebe14
George Williams <pfaedit@users.sourceforge.net>
2002-05-17 05:45:37 +0000

    Use psili rather than koronis

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1021523829;
-const char *source_modtime_str = "21:37 15-5-2002";
-const char *source_version_str = "020515";
+const time_t source_modtime = 1021613840;
+const char *source_modtime_str = "22:37 16-5-2002";
+const char *source_version_str = "020516";
