5a5be48921d1f65d4c08b2ffe6da5c7bf4c19b6f
George Williams <pfaedit@users.sourceforge.net>
2002-05-18 22:14:04 +0000

    Various configuration problems that showed up on Mac (with no dynamic libs 	that I can understand).

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1021613840;
-const char *source_modtime_str = "22:37 16-5-2002";
-const char *source_version_str = "020516";
+const time_t source_modtime = 1021758735;
+const char *source_modtime_str = "14:52 18-5-2002";
+const char *source_version_str = "020518";
