dbf5616c3ff37875a20033cf62c1ff440852d4be
George Williams <pfaedit@users.sourceforge.net>
2002-06-26 15:00:37 +0000

    Final openvms change from Jouk.

--- gdraw/gxdraw.c
+++ gdraw/gxdraw.c
@@ -24,6 +24,9 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#ifdef __VMS
+# include <vms_x_fix.h>
+#endif
 #include <stdlib.h>
 #include <math.h>
 
