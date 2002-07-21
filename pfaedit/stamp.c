9f636725901847c68ae9078142e9a2ecb3f4cd9a
George Williams <pfaedit@users.sourceforge.net>
2002-07-21 15:36:12 +0000

    Sync up.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1027219494;
-const char *source_modtime_str = "19:44 20-7-2002";
-const char *source_version_str = "020720";
+const time_t source_modtime = 1027264688;
+const char *source_modtime_str = "8:18 21-7-2002";
+const char *source_version_str = "020721";
