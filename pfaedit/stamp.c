70efd231c7d6574437fea4f1e3d57c58a6e8e1aa
George Williams <pfaedit@users.sourceforge.net>
2002-08-08 06:15:09 +0000

    Textfields within Pfaedit did not respond to the numeric keypad.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1028765467;
-const char *source_modtime_str = "17:11 7-8-2002";
+const time_t source_modtime = 1028786346;
+const char *source_modtime_str = "22:59 7-8-2002";
 const char *source_version_str = "020807";
