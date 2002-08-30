d4f03d5b2cd36d3bf526fcc56025a58f79c7dc06
George Williams <pfaedit@users.sourceforge.net>
2002-08-30 05:39:26 +0000

    bdf fonts were mistakenly being loaded into the background.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1030665684;
-const char *source_modtime_str = "17:01 29-8-2002";
+const time_t source_modtime = 1030684809;
+const char *source_modtime_str = "22:20 29-8-2002";
 const char *source_version_str = "020829";
