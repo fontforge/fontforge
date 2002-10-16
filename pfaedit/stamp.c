2127cf8354cd01863e9d5adca304973d3dc6e9d1
George Williams <pfaedit@users.sourceforge.net>
2002-10-16 18:46:26 +0000

    Some of Luc Devroye's fonts contain a "div" in the FontMatrix. ATM does not 	support this, but we might as well. Still don't support a full 	expression. We could read in a gsf font, but if we generated a postscript font from that 	we weren't able to read in the resulting pfa/b.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1034787994;
-const char *source_modtime_str = "10:06 16-10-2002";
+const time_t source_modtime = 1034793465;
+const char *source_modtime_str = "11:37 16-10-2002";
 const char *source_version_str = "021016";
