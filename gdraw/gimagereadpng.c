7bb80be575730ea76fe2e07a12de669d502d7fc1
George Williams <pfaedit@users.sourceforge.net>
2002-11-03 03:51:14 +0000

    Cleanup png reading so we don't have seperate cases for static and dynamic libs. Make it read mono images as mono. Make it do something reasonable with Grey scale images with an alpha channel.

--- gdraw/gimagereadpng.c
+++ gdraw/gimagereadpng.c
@@ -36,20 +36,24 @@
 /*  libpng, so it is best just to compile it in (set NODYNAMIC) */
 
 #ifdef _NO_LIBPNG
-static int a_file_must_define_something=0;	/* ANSI says so */
-#elif !defined(_STATIC_LIBPNG) && !defined(NODYNAMIC)	/* I don't know how to deal with dynamic libs on mac OS/X, hence this */
-#include <dlfcn.h>
-#include <png.h>
+static void *a_file_must_define_something=(void *) &a_file_must_define_something;
+		/* ANSI says so */
+#else
+# if !defined(_STATIC_LIBPNG) && !defined(NODYNAMIC)	/* I don't know how to deal with dynamic libs on mac OS/X, hence this */
+#  include <dlfcn.h>
+# endif
+# include <png.h>
 
-#define int32 _int32
-#define uint32 _uint32
-#define int16 _int16
-#define uint16 _uint16
-#define int8 _int8
-#define uint8 _uint8
+# define int32 _int32
+# define uint32 _uint32
+# define int16 _int16
+# define uint16 _uint16
+# define int8 _int8
+# define uint8 _uint8
 
-#include "gdraw.h"
+# include "gdraw.h"
 
+# if !defined(_STATIC_LIBPNG) && !defined(NODYNAMIC)
 static void *libpng=NULL;
 static png_structp (*_png_create_read_struct)(char *, png_voidp, png_error_ptr, png_error_ptr);
 static png_infop (*_png_create_info_struct)(png_structp);
@@ -57,9 +61,9 @@ static void (*_png_destroy_read_struct)(png_structpp, png_infopp, png_infopp);
 static void (*_png_init_io)(png_structp, FILE *);
 static void (*_png_read_info)(png_structp, png_infop);
 static void (*_png_set_strip_16)(png_structp);
+static void (*_png_set_strip_alpha)(png_structp);
 static void (*_png_set_packing)(png_structp);
 static void (*_png_set_filler)(png_structp,png_uint_32,int);
-static void (*_png_set_gray_to_rgb)(png_structp);
 static void (*_png_read_image)(png_structp,png_bytep*);
 static void (*_png_read_end)(png_structp,png_infop);
 
@@ -77,17 +81,34 @@ return( 0 );
     _png_set_strip_16 = dlsym(libpng,"png_set_strip_16");
     _png_set_packing = dlsym(libpng,"png_set_packing");
     _png_set_filler = dlsym(libpng,"png_set_filler");
-    _png_set_gray_to_rgb = dlsym(libpng,"png_set_gray_to_rgb");
     _png_read_image = dlsym(libpng,"png_read_image");
     _png_read_end = dlsym(libpng,"png_read_end");
+    _png_set_strip_alpha = dlsym(libpng,"png_set_strip_alpha");
     if ( _png_create_read_struct && _png_create_info_struct && _png_destroy_read_struct &&
 	    _png_init_io && _png_read_info && _png_set_strip_16 && _png_set_packing &&
-	    _png_set_filler && _png_set_gray_to_rgb && _png_read_image && _png_read_end)
+	    _png_set_filler && _png_read_image && _png_read_end &&
+	    _png_set_strip_alpha )
 return( 1 );
     dlclose(libpng);
     GDrawIError("%s", dlerror());
 return( 0 );
 }
+# else
+#  define _png_create_read_struct png_create_read_struct
+#  define _png_create_info_struct png_create_info_struct
+#  define _png_destroy_read_struct png_destroy_read_struct
+#  define _png_init_io png_init_io
+#  define _png_read_info png_read_info
+#  define _png_set_strip_16 png_set_strip_16
+#  define _png_set_strip_alpha png_set_strip_alpha
+#  define _png_set_packing png_set_packing
+#  define _png_set_filler png_set_filler
+#  define _png_read_image png_read_image
+#  define _png_read_end png_read_end
+static void *libpng=(void *) 1;
+
+static int loadpng() { return true; }
+# endif
  
 static void user_error_fn(png_structp png_ptr, png_const_charp error_msg) {
     GDrawError( error_msg );
@@ -135,19 +156,20 @@ return( NULL );
 
     _png_init_io(png_ptr, fp);
     _png_read_info(png_ptr, info_ptr);
-    if (info_ptr->bit_depth == 16)
-	_png_set_strip_16(png_ptr);
-    else if (info_ptr->bit_depth < 8)
+    _png_set_strip_16(png_ptr);
+    if ( (info_ptr->color_type==PNG_COLOR_TYPE_GRAY || info_ptr->color_type==PNG_COLOR_TYPE_PALETTE ) &&
+	    info_ptr->bit_depth == 1 )
+	/* Leave bitmaps packed */;
+    else
 	_png_set_packing(png_ptr);
+    _png_set_strip_alpha(png_ptr);
     if (info_ptr->color_type == PNG_COLOR_TYPE_RGB)
 	_png_set_filler(png_ptr, '\0', PNG_FILLER_BEFORE);
-    if (info_ptr->color_type == PNG_COLOR_TYPE_GRAY_ALPHA )
-         _png_set_gray_to_rgb(png_ptr);	
-    /* Can I remove alpha channels somehow? */
-    /* I hope I've stuffed them into the 4th (unused) colour byte where they */
-    /*  can happily be ignored */
 
-    if ( info_ptr->color_type==PNG_COLOR_TYPE_GRAY || info_ptr->color_type==PNG_COLOR_TYPE_GRAY_ALPHA ) {
+    if ( (info_ptr->color_type==PNG_COLOR_TYPE_GRAY || info_ptr->color_type==PNG_COLOR_TYPE_PALETTE ) &&
+	    info_ptr->bit_depth == 1 ) {
+	ret = GImageCreate(it_mono,info_ptr->width,info_ptr->height);
+    } else if ( info_ptr->color_type==PNG_COLOR_TYPE_GRAY || info_ptr->color_type==PNG_COLOR_TYPE_GRAY_ALPHA ) {
 	GClut *clut;
 	ret = GImageCreate(it_index,info_ptr->width,info_ptr->height);
 	clut = ret->u.image->clut;
@@ -169,20 +191,21 @@ return( NULL );
 			info_ptr->palette[i].blue);
     }
     base = ret->u.image;
-    if ( info_ptr->color_type==PNG_COLOR_TYPE_RGB || info_ptr->color_type==PNG_COLOR_TYPE_RGB_ALPHA ) {
-	if ( (info_ptr->valid&PNG_INFO_tRNS) && info_ptr->num_trans>0 )
+    if ( (info_ptr->valid&PNG_INFO_tRNS) && info_ptr->num_trans>0 ) {
+	if ( info_ptr->color_type==PNG_COLOR_TYPE_RGB || info_ptr->color_type==PNG_COLOR_TYPE_RGB_ALPHA )
 	    base->trans = COLOR_CREATE(
 		    (info_ptr->trans_values.red>>8),
 		    (info_ptr->trans_values.green>>8),
 		    (info_ptr->trans_values.blue>>8));
-    } else {
-	if ( (info_ptr->valid&PNG_INFO_tRNS) && info_ptr->num_trans>0 )
+	else if ( base->image_type == it_mono )
+	    base->trans = info_ptr->trans[0];
+	else
 	    base->clut->trans_index = base->trans = info_ptr->trans[0];
     }
 
     row_pointers = galloc(info_ptr->height*sizeof(png_bytep));
     for ( i=0; i<info_ptr->height; ++i )
-	row_pointers[i] = (unsigned char *) (base->data + i*base->bytes_per_line);
+	row_pointers[i] = (png_bytep) (base->data + i*base->bytes_per_line);
 
     /* Ignore progressive loads for now */
     /* libpng wants me to do it with callbacks, but that doesn't sit well */
@@ -198,140 +221,6 @@ return( NULL );
     }
 
     _png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-    gfree(row_pointers);
-    /* Note png b&w images come out as indexed */
-return( ret );
-}
-
-GImage *GImageReadPng(char *filename) {
-    GImage *ret=NULL;
-    FILE *fp;
-
-    fp = fopen(filename, "rb");
-    if (!fp)
-return( NULL );
-
-    ret = GImageRead_Png(fp);
-    fclose(fp);
-return( ret );
-}
-#else
-#include <png.h>
-
-#define int32 _int32
-#define uint32 _uint32
-#define int16 _int16
-#define uint16 _uint16
-#define int8 _int8
-#define uint8 _uint8
-
-#include "gdraw.h"
-
-static void user_error_fn(png_structp png_ptr, png_const_charp error_msg) {
-    GDrawError( error_msg );
-    longjmp(png_ptr->jmpbuf,1);
-}
-
-static void user_warning_fn(png_structp png_ptr, png_const_charp warning_msg) {
-    fprintf(stderr,"%s\n", warning_msg);
-}
-
-GImage *GImageRead_Png(FILE *fp) {
-    GImage *ret=NULL;
-    struct _GImage *base;
-    png_structp png_ptr;
-    png_infop info_ptr;
-    png_bytep *row_pointers=NULL;
-    int i;
-
-   png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
-      (void *)NULL, user_error_fn, user_warning_fn);
-
-   if (!png_ptr)
-return( NULL );
-
-    info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr) {
-      png_destroy_read_struct(&png_ptr,  (png_infopp)NULL, (png_infopp)NULL);
-return( NULL );
-    }
-
-    if (setjmp(png_ptr->jmpbuf)) {
-      /* Free all of the memory associated with the png_ptr and info_ptr */
-      png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-      if ( ret!=NULL ) {
-	  GImageDestroy(ret);
-	  gfree(row_pointers);
-      }
-      /* If we get here, we had a problem reading the file */
-return( NULL );
-    }
-
-    png_init_io(png_ptr, fp);
-    png_read_info(png_ptr, info_ptr);
-    if (info_ptr->bit_depth == 16)
-	png_set_strip_16(png_ptr);
-    else if (info_ptr->bit_depth < 8)
-	png_set_packing(png_ptr);
-    if (info_ptr->color_type == PNG_COLOR_TYPE_RGB)
-	png_set_filler(png_ptr, '\0', PNG_FILLER_BEFORE);
-    if (info_ptr->color_type == PNG_COLOR_TYPE_GRAY_ALPHA )
-         png_set_gray_to_rgb(png_ptr);	
-    /* Can I remove alpha channels somehow? */
-    /* I hope I've stuffed them into the 4th (unused) colour byte where they */
-    /*  can happily be ignored */
-
-    if ( info_ptr->color_type==PNG_COLOR_TYPE_GRAY || info_ptr->color_type==PNG_COLOR_TYPE_GRAY_ALPHA ) {
-	GClut *clut;
-	ret = GImageCreate(it_index,info_ptr->width,info_ptr->height);
-	clut = ret->u.image->clut;
-	clut->is_grey = true;
-	clut->clut_len = 256;
-	for ( i=0; i<256; ++i )
-	    clut->clut[i] = COLOR_CREATE(i,i,i);
-    } else if ( info_ptr->color_type==PNG_COLOR_TYPE_RGB || info_ptr->color_type==PNG_COLOR_TYPE_RGB_ALPHA )
-	ret = GImageCreate(it_true,info_ptr->width,info_ptr->height);
-    else {
-	GClut *clut;
-	ret = GImageCreate(it_index,info_ptr->width,info_ptr->height);
-	clut = ret->u.image->clut;
-	clut->is_grey = true;
-	clut->clut_len = info_ptr->num_palette;
-	for ( i=0; i<info_ptr->num_palette; ++i )
-	    clut->clut[i] = COLOR_CREATE(info_ptr->palette[i].red,
-			info_ptr->palette[i].green,
-			info_ptr->palette[i].blue);
-    }
-    base = ret->u.image;
-    if ( info_ptr->color_type==PNG_COLOR_TYPE_RGB || info_ptr->color_type==PNG_COLOR_TYPE_RGB_ALPHA ) {
-	if ( (info_ptr->valid&PNG_INFO_tRNS) && info_ptr->num_trans>0 )
-	    base->trans = COLOR_CREATE(
-		    (info_ptr->trans_values.red>>8),
-		    (info_ptr->trans_values.green>>8),
-		    (info_ptr->trans_values.blue>>8));
-    } else {
-	if ( (info_ptr->valid&PNG_INFO_tRNS) && info_ptr->num_trans>0 )
-	    base->clut->trans_index = base->trans = info_ptr->trans[0];
-    }
-
-    row_pointers = galloc(info_ptr->height*sizeof(png_bytep));
-    for ( i=0; i<info_ptr->height; ++i )
-	row_pointers[i] = (unsigned char *) (base->data + i*base->bytes_per_line);
-
-    /* Ignore progressive loads for now */
-    /* libpng wants me to do it with callbacks, but that doesn't sit well */
-    /*  with our asynchronous cruds... */
-    png_read_image(png_ptr,row_pointers);
-    png_read_end(png_ptr, NULL);
-
-    if ( info_ptr->color_type==PNG_COLOR_TYPE_RGB || info_ptr->color_type==PNG_COLOR_TYPE_RGB_ALPHA ) {
-	/* PNG orders its bytes as BBGGRRAA instead of 00RRGGBB */
-	uint32 *ipt, *iend;
-	for ( ipt = (uint32 *) (base->data), iend=ipt+base->width*base->height; ipt<iend; ++ipt )
-	    *ipt = COLOR_CREATE( ((*ipt>>8)&0xff) , ((*ipt>>16)&0xff) , (*ipt>>24) );
-    }
-
-    png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
     gfree(row_pointers);
     /* Note png b&w images come out as indexed */
 return( ret );
