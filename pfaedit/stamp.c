1cef47f3eff445c6075565d21ec690c990d03207
George Williams <pfaedit@users.sourceforge.net>
2002-11-04 19:44:14 +0000

    Add code for a hand tool (to scroll screen). No access to it yet.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1036432612;
-const char *source_modtime_str = "9:56 4-11-2002";
+const time_t source_modtime = 1036438805;
+const char *source_modtime_str = "11:40 4-11-2002";
 const char *source_version_str = "021104";
