04469c4ed7fb3aaf5bf560801b6f1de13817514a
George Williams <pfaedit@users.sourceforge.net>
2002-11-11 19:43:00 +0000

    Provide access to the hand tool. Provide hooks for the freehand tool.

---
+++ pfaedit/cvfreehand.c
@@ -0,0 +1,43 @@
+/* Copyright (C) 2002 by George Williams */
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+
+ * The name of the author may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "pfaeditui.h"
+#include <math.h>
+
+
+void CVMouseDownFreeHand(CharView *cv, GEvent *event) {
+}
+
+void CVMouseMoveFreeHand(CharView *cv, GEvent *event) {
+    cv->xoff += event->u.mouse.x-cv->p.x; cv->p.x = event->u.mouse.x;
+    cv->yoff -= event->u.mouse.y-cv->p.y; cv->p.y = event->u.mouse.y;
+    GScrollBarSetPos(cv->hsb,-cv->xoff);
+    GScrollBarSetPos(cv->vsb,cv->yoff-cv->height);
+    GDrawRequestExpose(cv->v,NULL,false);
+}
+
+void CVMouseUpFreeHand(CharView *cv) {
+}
