5b6bee493cf6e2e7fe598a35ed9ab4470877b4e6
George Williams <pfaedit@users.sourceforge.net>
2003-02-25 00:12:20 +0000

    Add Italian translations for styles in ttfnames.

--- pfaedit/fontinfo.c
+++ pfaedit/fontinfo.c
@@ -396,6 +396,20 @@ static const unichar_t regularspan[] = { 'N','o','r','m','a','l',  '\0' };
 static const unichar_t boldspan[] = { 'N','i','g','r','i','t','a',  '\0' };
 static const unichar_t italicspan[] = { 'C','u','r','s','i','v','a',  '\0' };
 
+static const unichar_t regularital[] = { 'N','o','r','m','a','l','e',  '\0' };
+static const unichar_t demiboldital[] = { 'N','e','r','r','e','t','t','o',  '\0' };
+static const unichar_t boldital[] = { 'N','e','r','o',  '\0' };
+static const unichar_t thinital[] = { 'F','i','n','e',  '\0' };
+static const unichar_t lightital[] = { 'C','h','i','a','r','o',  '\0' };
+static const unichar_t mediumital[] = { 'M','e','d','i','o',  '\0' };
+static const unichar_t bookital[] = { 'L','i','b','r','o',  '\0' };
+static const unichar_t heavyital[] = { 'N','e','r','i','s','s','i','m','o',  '\0' };
+static const unichar_t blackital[] = { 'E','x','t','r','a','N','e','r','o',  '\0' };
+static const unichar_t italicital[] = { 'C','u','r','s','i','v','o',  '\0' };
+static const unichar_t obliqueital[] = { 'O','b','l','i','q','u','o',  '\0' };
+static const unichar_t condensedital[] = { 'C','o','n','d','e','n','s','a','t','o',  '\0' };
+static const unichar_t expandedital[] = { 'A','l','l','a','r','g','a','t','o',  '\0' };
+
 static const unichar_t regularru[] = { 0x41e, 0x431, 0x44b, 0x447, 0x43d, 0x44b, 0x439,  '\0' };
 static const unichar_t demiboldru[] = { 0x41f, 0x43e, 0x43b, 0x443, 0x436, 0x438, 0x440, 0x43d, 0x44b, 0x439,  0 };
 static const unichar_t boldru[] = { 0x41e, 0x431, 0x44b, 0x447, 0x43d, 0x44b, 0x439,  0 };
@@ -408,21 +422,21 @@ static const unichar_t obliqueru[] = { 0x41d, 0x430, 0x43a, 0x43b, 0x43e, 0x43d/
 static const unichar_t condensedru[] = { 0x423, 0x437, 0x43a, 0x438, 0x439,  '\0' };
 static const unichar_t expandedru[] = { 0x428, 0x438, 0x440, 0x43e, 0x43a, 0x438, 0x439,  '\0' };
 
-static struct langstyle regs[] = { {0x409, regulareng}, { 0x40c, regularfren }, { 0x407, regulargerm }, { 0x40a, regularspan }, { 0x419, regularru }, { 0 }};
-static struct langstyle meds[] = { {0x409, mediumeng}, { 0x40c, mediumfren }, { 0x407, mediumgerm }, { 0 }};
-static struct langstyle books[] = { {0x409, bookeng}, { 0x407, bookgerm }, { 0 }};
-static struct langstyle bolds[] = { {0x409, boldeng}, { 0x40c, boldfren }, { 0x407, boldgerm }, { 0x407, boldgerm2 }, { 0x40a, boldspan}, { 0x419, boldru }, { 0 }};
-static struct langstyle italics[] = { {0x409, italiceng}, { 0x40c, italicfren }, { 0x407, italicgerm }, { 0x40a, italicspan}, { 0x419, italicru }, { 0 }};
-static struct langstyle obliques[] = { {0x409, obliqueeng}, { 0x40c, obliquefren }, { 0x407, obliquegerm }, { 0x419, obliqueru }, { 0 }};
+static struct langstyle regs[] = { {0x409, regulareng}, { 0x40c, regularfren }, { 0x410, regularital }, { 0x407, regulargerm }, { 0x40a, regularspan }, { 0x419, regularru }, { 0 }};
+static struct langstyle meds[] = { {0x409, mediumeng}, { 0x410, mediumital }, { 0x40c, mediumfren }, { 0x407, mediumgerm }, { 0 }};
+static struct langstyle books[] = { {0x409, bookeng}, { 0x410, bookital }, { 0x407, bookgerm }, { 0 }};
+static struct langstyle bolds[] = { {0x409, boldeng}, { 0x410, boldital }, { 0x40c, boldfren }, { 0x407, boldgerm }, { 0x407, boldgerm2 }, { 0x40a, boldspan}, { 0x419, boldru }, { 0 }};
+static struct langstyle italics[] = { {0x409, italiceng}, { 0x410, italicital }, { 0x40c, italicfren }, { 0x407, italicgerm }, { 0x40a, italicspan}, { 0x419, italicru }, { 0 }};
+static struct langstyle obliques[] = { {0x409, obliqueeng}, { 0x410, obliqueital }, { 0x40c, obliquefren }, { 0x407, obliquegerm }, { 0x419, obliqueru }, { 0 }};
 static struct langstyle demibolds[] = { {0x409, demiboldeng}, {0x409, demiboldeng3}, {0x409, demiboldeng5},
-	{ 0x40c, demiboldfren }, { 0x40c, demiboldfren2 }, { 0x407, demiboldgerm }, { 0x407, demiboldgerm2 },
+	{ 0x410, demiboldital }, { 0x40c, demiboldfren }, { 0x40c, demiboldfren2 }, { 0x407, demiboldgerm }, { 0x407, demiboldgerm2 },
 	{ 0x419, demiboldru }, { 0 }};
-static struct langstyle heavys[] = { {0x409, heavyeng}, { 0x419, heavyru }, { 0 }};
-static struct langstyle blacks[] = { {0x409, blackeng}, { 0x40c, blackfren }, { 0x407, blackgerm }, { 0x419, blackru }, { 0 }};
-static struct langstyle thins[] = { {0x409, thineng}, { 0x419, thinru }, { 0 }};
-static struct langstyle lights[] = { {0x409, lighteng}, {0x40c, lightfren}, {0x407, lightgerm}, { 0x419, lightru }, { 0 }};
-static struct langstyle condenseds[] = { {0x409, condensedeng}, {0x40c, condensedfren}, {0x407, condensedgerm}, { 0x419, condensedru }, { 0 }};
-static struct langstyle expandeds[] = { {0x409, expandedeng}, {0x40c, expandedfren}, {0x407, expandedgerm}, { 0x419, expandedru }, { 0 }};
+static struct langstyle heavys[] = { {0x409, heavyeng}, { 0x410, heavyital }, { 0x419, heavyru }, { 0 }};
+static struct langstyle blacks[] = { {0x409, blackeng}, { 0x410, blackital }, { 0x40c, blackfren }, { 0x407, blackgerm }, { 0x419, blackru }, { 0 }};
+static struct langstyle thins[] = { {0x409, thineng}, { 0x410, thinital }, { 0x419, thinru }, { 0 }};
+static struct langstyle lights[] = { {0x409, lighteng}, {0x410, lightital}, {0x40c, lightfren}, {0x407, lightgerm}, { 0x419, lightru }, { 0 }};
+static struct langstyle condenseds[] = { {0x409, condensedeng}, {0x410, condensedital}, {0x40c, condensedfren}, {0x407, condensedgerm}, { 0x419, condensedru }, { 0 }};
+static struct langstyle expandeds[] = { {0x409, expandedeng}, {0x410, expandedital}, {0x40c, expandedfren}, {0x407, expandedgerm}, { 0x419, expandedru }, { 0 }};
 static struct langstyle outlines[] = { {0x409, outlineeng}, {0x40c, outlinefren}, {0x407, outlinegerm}, { 0 }};
 static struct langstyle *stylelist[] = {regs, meds, books, demibolds, bolds, heavys, blacks,
 	lights, thins, italics, obliques, condenseds, expandeds, outlines, NULL };
--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1046107077;
-const char *source_modtime_str = "9:17 24-2-2003";
+const time_t source_modtime = 1046108186;
+const char *source_modtime_str = "9:36 24-2-2003";
 const char *source_version_str = "030224";
