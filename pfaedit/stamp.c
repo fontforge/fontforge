2c0d3ffb872abed574878c3a2d8ad22e805a9423
George Williams <pfaedit@users.sourceforge.net>
2003-02-25 05:53:31 +0000

    I've found the other MicroSoft Languages.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1046133054;
-const char *source_modtime_str = "16:30 24-2-2003";
+const time_t source_modtime = 1046152243;
+const char *source_modtime_str = "21:50 24-2-2003";
 const char *source_version_str = "030224";
