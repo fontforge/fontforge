6c727dde2ac7ae756762eef06bcb92744ea47fd0
George Williams <pfaedit@users.sourceforge.net>
2003-06-03 21:32:42 +0000

    A bunch of new script names taken from ISO 15924. Also Unicode now provides a Scripts.txt file which contains some info we didn't have. Use that.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1054660991;
-const char *source_modtime_str = "10:23 3-6-2003";
+const time_t source_modtime = 1054672105;
+const char *source_modtime_str = "13:28 3-6-2003";
 const char *source_version_str = "030603";
