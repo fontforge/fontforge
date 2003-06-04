4d3fbcd2954a9920df9aa0b6c2b0c253cfe7b93e
George Williams <pfaedit@users.sourceforge.net>
2003-06-04 23:23:38 +0000

    Sync

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1054741272;
-const char *source_modtime_str = "08:41 4-Jun-2003";
+const time_t source_modtime = 1054745213;
+const char *source_modtime_str = "09:46 4-Jun-2003";
 const char *source_version_str = "030604";
