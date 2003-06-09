387e18f4d1ff2e1fa2d866422b5c4c40e56b04e2
George Williams <pfaedit@users.sourceforge.net>
2003-06-09 16:27:46 +0000

    Improve the comment that says pfaedit made this in bdf files.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1055175867;
-const char *source_modtime_str = "09:24 9-Jun-2003";
+const time_t source_modtime = 1055176090;
+const char *source_modtime_str = "09:28 9-Jun-2003";
 const char *source_version_str = "030609";
