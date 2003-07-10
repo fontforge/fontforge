f502bd3f50a7c7d4eb4ca3120187364f36fd3564
George Williams <pfaedit@users.sourceforge.net>
2003-07-10 03:28:27 +0000

    Arabic print sample, courtesy of Thomas Milo.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1057729868;
-const char *source_modtime_str = "22:51 8-Jul-2003";
-const char *source_version_str = "030708";
+const time_t source_modtime = 1057807257;
+const char *source_modtime_str = "20:20 9-Jul-2003";
+const char *source_version_str = "030709";
