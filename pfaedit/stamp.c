39311d7baf0153e106204329967dfd9d094eedce
George Williams <pfaedit@users.sourceforge.net>
2003-08-25 17:29:55 +0000

    Kanou's patch to add encodings to scripting (that are present in font info but not in scripting).

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1061831569;
-const char *source_modtime_str = "10:12 25-Aug-2003";
+const time_t source_modtime = 1061832194;
+const char *source_modtime_str = "10:23 25-Aug-2003";
 const char *source_version_str = "030825";
