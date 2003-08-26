366562b367eba3d418673b28e2e248f21c1bdd18
George Williams <pfaedit@users.sourceforge.net>
2003-08-26 22:18:19 +0000

    Updated french UI.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1061936012;
-const char *source_modtime_str = "15:13 26-Aug-2003";
+const time_t source_modtime = 1061936307;
+const char *source_modtime_str = "15:18 26-Aug-2003";
 const char *source_version_str = "030826";
