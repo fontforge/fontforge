8748ac4e29460d9c277bb883088928e22270304d
George Williams <pfaedit@users.sourceforge.net>
2003-09-30 04:22:44 +0000

    Update to Adobe's new glyph list (well, new as of March). This means losing many glyph names, so it may not be a good thing to do. I don't understand Adobe's logic...

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1064885716;
-const char *source_modtime_str = "18:35 29-Sep-2003";
+const time_t source_modtime = 1064895675;
+const char *source_modtime_str = "21:21 29-Sep-2003";
 const char *source_version_str = "030929";
