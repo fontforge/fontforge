78b32f45c4fbec4f7a64bdaeff2ea19294f5eb31
George Williams <pfaedit@users.sourceforge.net>
2003-10-03 02:23:49 +0000

    Typo in range for hebrew.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1065138315;
-const char *source_modtime_str = "16:45 2-Oct-2003";
+const time_t source_modtime = 1065147789;
+const char *source_modtime_str = "19:23 2-Oct-2003";
 const char *source_version_str = "031002";
