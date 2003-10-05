f981e620fad94b560d0d31340ffa5972eb411ce9
George Williams <pfaedit@users.sourceforge.net>
2003-10-05 01:59:19 +0000

    Get proper french names for the mac feature/settings.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1065316787;
-const char *source_modtime_str = "18:19 4-Oct-2003";
+const time_t source_modtime = 1065319177;
+const char *source_modtime_str = "18:59 4-Oct-2003";
 const char *source_version_str = "031004";
