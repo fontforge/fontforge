6e6ed9a56ef00b3147e6e1c57b1dcc6545c0af6f
George Williams <pfaedit@users.sourceforge.net>
2003-10-05 02:45:43 +0000

    Kanou's greekfixup patch really should depend on the unicode encoding rather than the current encoding.

--- pfaedit/fvimportbdf.c
+++ pfaedit/fvimportbdf.c
@@ -146,9 +146,9 @@ static int figureProperEncoding(SplineFont *sf,BDFFont *b, int enc,char *name,
 		(strcmp(sf->chars[enc]->name,name)==0 || UniFromName(name)==enc ))
 	    i = enc;
 	else if ( enc<sf->charcnt && sf->chars[enc]!=NULL &&
-		  ((enc==0x2206 && strcmp(name,"Delta")==0) ||
-		   (enc==0x2126 && strcmp(name,"Omega")==0) ||
-		   (enc==0xb5 && strcmp(name,"mu")==0) ))
+		  ((sf->chars[enc]->unicodeenc==0x2206 && strcmp(name,"Delta")==0) ||
+		   (sf->chars[enc]->unicodeenc==0x2126 && strcmp(name,"Omega")==0) ||
+		   (sf->chars[enc]->unicodeenc==0xb5 && strcmp(name,"mu")==0) ))
 	    i = enc;
 	else for ( i=sf->charcnt-1; i>=0 ; --i ) if ( sf->chars[i]!=NULL ) {
 	    if ( strcmp(name,sf->chars[i]->name)==0 )
