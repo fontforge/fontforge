7fe96fe7cb99f430e8b1013f01505ee3ebb897a3
George Williams <pfaedit@users.sourceforge.net>
2003-10-13 16:40:38 +0000

    New japanese from kanou.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1065987524;
-const char *source_modtime_str = "12:38 12-Oct-2003";
-const char *source_version_str = "031012";
+const time_t source_modtime = 1066063239;
+const char *source_modtime_str = "09:40 13-Oct-2003";
+const char *source_version_str = "031013";
