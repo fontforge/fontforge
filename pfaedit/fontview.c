21ae853d383ff900fd2ebe39626fa05103e23880
George Williams <pfaedit@users.sourceforge.net>
2003-11-06 00:11:41 +0000

    When importing a bitmap font into an empty font, we might as well change the display to show the bitmap rather than the empty characters.

--- pfaedit/fontview.c
+++ pfaedit/fontview.c
@@ -791,7 +791,13 @@ void MenuAbout(GWindow base,struct gmenuitem *mi,GEvent *e) {
 
 static void FVMenuImport(GWindow gw,struct gmenuitem *mi,GEvent *e) {
     FontView *fv = (FontView *) GDrawGetUserData(gw);
+    int empty = fv->sf->onlybitmaps && fv->sf->bitmaps==NULL;
+    BDFFont *bdf;
     FVImport(fv);
+    if ( empty && fv->sf->bitmaps!=NULL ) {
+	for ( bdf= fv->sf->bitmaps; bdf->next!=NULL; bdf = bdf->next );
+	FVChangeDisplayBitmap(fv,bdf);
+    }
 }
 
 static int FVSelCount(FontView *fv) {
--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1068063682;
-const char *source_modtime_str = "12:21 5-Nov-2003";
+const time_t source_modtime = 1068076340;
+const char *source_modtime_str = "15:52 5-Nov-2003";
 const char *source_version_str = "031105";
