60610e2bb39809b0f09bb85a4a92d276c4e07e66
George Williams <pfaedit@users.sourceforge.net>
2003-12-03 19:12:50 +0000

    New French translation by Pierre Hanser

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1070478354;
-const char *source_modtime_str = "11:05 3-Dec-2003";
+const time_t source_modtime = 1070478627;
+const char *source_modtime_str = "11:10 3-Dec-2003";
 const char *source_version_str = "031203";
