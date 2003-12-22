bb7d33d5aca348063dceae4b997db0efc59d89de
George Williams <pfaedit@users.sourceforge.net>
2003-12-22 18:57:37 +0000

    Patch to the Italian UI by Giuseppe Ghibò.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1072119118;
-const char *source_modtime_str = "10:51 22-Dec-2003";
+const time_t source_modtime = 1072119381;
+const char *source_modtime_str = "10:56 22-Dec-2003";
 const char *source_version_str = "031222";
