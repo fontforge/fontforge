f7350faed3ecf9741fd5ba72dc3d22f73d4cf9dc
George Williams <pfaedit@users.sourceforge.net>
2004-01-01 17:21:03 +0000

    Update copyright notices (and splash screen) for 2004.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1072944141;
-const char *source_modtime_str = "00:02 1-Jan-2004";
+const time_t source_modtime = 1072976078;
+const char *source_modtime_str = "08:54 1-Jan-2004";
 const char *source_version_str = "040101";
