a07885d0cb90b97f62f70f47e0bd42311823645b
George Williams <pfaedit@users.sourceforge.net>
2004-02-10 05:52:50 +0000

    I've finally figured out why PfaEdit kept crashing when charview windows were closed (and bitmap view too, but I never noticed that). If palettes are docked, if user clicks on a gadget in palette, that gadget becomes the windows focus gadget. Then user goes to another charview window, palette follows (but old window now incorrectly keeps same focus gadget). New window gets destroyed. Palette (which is still inside it) gets destroyed too. Focus Gadget gets destroyed. Original window now has a pointer to a dead gadget, and sooner or later it goes blewy. Fix: When reparenting, null out focus gadget.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1076354745;
-const char *source_modtime_str = "11:25 9-Feb-2004";
+const time_t source_modtime = 1076391707;
+const char *source_modtime_str = "21:41 9-Feb-2004";
 const char *source_version_str = "040209";
