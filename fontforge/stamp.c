588be7438c001aa8f11a4f14186f1e4e6a169db3
George Williams <pfaedit@users.sourceforge.net>
2004-03-07 01:11:23 +0000

    Oops. The change the other day to fix the winascent actually broke it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1078621774;
-const char *source_modtime_str = "17:09 6-Mar-2004";
+const time_t source_modtime = 1078622084;
+const char *source_modtime_str = "17:14 6-Mar-2004";
 const char *source_version_str = "20040306";
