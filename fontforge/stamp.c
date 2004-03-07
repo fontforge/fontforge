9f6a6090928af31d6d3b7dc1aeb1ae1bebc7ff3c
George Williams <pfaedit@users.sourceforge.net>
2004-03-07 19:13:22 +0000

    When parsing a mac contextual substitution statemachine we made an eggregious error in guessing what classes could be active for mark substitutions. Be much more liberal in what we take now. (Now we have far too many subs, but that's better than missing some).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1078622084;
-const char *source_modtime_str = "17:14 6-Mar-2004";
-const char *source_version_str = "20040306";
+const time_t source_modtime = 1078686517;
+const char *source_modtime_str = "11:08 7-Mar-2004";
+const char *source_version_str = "20040307";
