28c191a9f8d9298dcd914a4a90f596cda74beaf0
George Williams <pfaedit@users.sourceforge.net>
2004-03-09 02:57:23 +0000

    We would occasionally get internal errors when moving points to far in order2 editing.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1078700886;
-const char *source_modtime_str = "15:08 7-Mar-2004";
+const time_t source_modtime = 1078706159;
+const char *source_modtime_str = "16:35 7-Mar-2004";
 const char *source_version_str = "20040307";
