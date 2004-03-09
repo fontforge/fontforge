328eab19a70fec7769728974d19758bf98d9d2d4
George Williams <pfaedit@users.sourceforge.net>
2004-03-09 05:59:27 +0000

    Make Generate fonts use a more realistic extension than ".mult". We use %s.pfb now (the %s can be anywhere in the filename, and we'll accept .pfa too).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1078810276;
-const char *source_modtime_str = "21:31 8-Mar-2004";
+const time_t source_modtime = 1078812208;
+const char *source_modtime_str = "22:03 8-Mar-2004";
 const char *source_version_str = "20040308";
