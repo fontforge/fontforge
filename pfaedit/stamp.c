059d8668306a713a568f1e32974c863e1dec271e
George Williams <pfaedit@users.sourceforge.net>
2004-03-10 20:27:04 +0000

    Still getting the windows ascent/descent wrong (now for cid fonts).

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1078930353;
-const char *source_modtime_str = "06:52 10-Mar-2004";
+const time_t source_modtime = 1078950773;
+const char *source_modtime_str = "12:32 10-Mar-2004";
 const char *source_version_str = "040310";
