cdc4cf7697ce1e001d60be6d7bd7bc0f6c0f86e9
George Williams <pfaedit@users.sourceforge.net>
2004-03-10 06:28:30 +0000

    I broke auto hint when Werner pointed out that hintmasks ran backwards. I tried to correct this in autohint, and ended up reversing the direction of some contours.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1078882501;
-const char *source_modtime_str = "17:35 9-Mar-2004";
+const time_t source_modtime = 1078900311;
+const char *source_modtime_str = "22:31 9-Mar-2004";
 const char *source_version_str = "20040309";
