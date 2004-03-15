905d06ade4ff10bddb72d195a49774f8813602b3
George Williams <pfaedit@users.sourceforge.net>
2004-03-15 20:04:59 +0000

    Dragging and dropping a character from the fontview to the char info dlg (to create a substitution) created a crash instead.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1079317804;
-const char *source_modtime_str = "18:30 14-Mar-2004";
-const char *source_version_str = "20040314";
+const time_t source_modtime = 1079381357;
+const char *source_modtime_str = "12:09 15-Mar-2004";
+const char *source_version_str = "040315";
