389b79abfb9025bd652f4542e6061d0656078f8f
pfaedit <pfaedit>
2004-03-21 19:12:58 +0000

    Take care of the case where the post table has more names than there are glyphs in the file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1079890106;
-const char *source_modtime_str = "09:28 21-Mar-2004";
+const time_t source_modtime = 1079893872;
+const char *source_modtime_str = "10:31 21-Mar-2004";
 const char *source_version_str = "20040321";
