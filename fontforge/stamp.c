deb1eedeeed7b37a1b37127f83cf4d2eee7f4703
George Williams <pfaedit@users.sourceforge.net>
2004-03-22 17:14:55 +0000

    Patch from R.L. Horn to fix stack overflows in type3 fonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1079910939;
-const char *source_modtime_str = "15:15 21-Mar-2004";
-const char *source_version_str = "20040321";
+const time_t source_modtime = 1079976328;
+const char *source_modtime_str = "09:25 22-Mar-2004";
+const char *source_version_str = "20040322";
