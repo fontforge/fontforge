410c706519b26abaf57558004cd1608c37ea23b1
George Williams <pfaedit@users.sourceforge.net>
2004-03-24 06:49:15 +0000

    PfaEdit would crash on bdf files with bad bounding boxes.

--- pfaedit/stamp.c
+++ pfaedit/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1080005503;
-const char *source_modtime_str = "17:31 22-Mar-2004";
-const char *source_version_str = "20040322";
+const time_t source_modtime = 1080111177;
+const char *source_modtime_str = "22:52 23-Mar-2004";
+const char *source_version_str = "20040323";
