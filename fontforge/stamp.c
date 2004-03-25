ee87f937a946c33e4c539d2eefd1ec5c70a708e4
pfaedit <pfaedit>
2004-03-25 18:27:21 +0000

    Oops. Typo in cvt editor. If there were a 'cvt ' table, editor created a blank one. If there weren't, editor crashed. Cases mixed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1080187672;
-const char *source_modtime_str = "20:07 24-Mar-2004";
-const char *source_version_str = "20040324";
+const time_t source_modtime = 1080239837;
+const char *source_modtime_str = "10:37 25-Mar-2004";
+const char *source_version_str = "20040325";
