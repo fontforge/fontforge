ba325abb9cdd97517eb8f0c0207e366feaf565f6
George Williams <pfaedit@users.sourceforge.net>
2004-03-27 04:47:21 +0000

    Display the phantom points, and be prepared for either 2 or 4 of them.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1080360827;
-const char *source_modtime_str = "20:13 26-Mar-2004";
+const time_t source_modtime = 1080362123;
+const char *source_modtime_str = "20:35 26-Mar-2004";
 const char *source_version_str = "20040326";
