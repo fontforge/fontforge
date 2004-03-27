dd10d9f45ff2d4f7c292c1d00abfe5a215a00a1a
George Williams <pfaedit@users.sourceforge.net>
2004-03-27 03:51:35 +0000

    Wasn't numbering points properly when it added new ones.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1080259704;
-const char *source_modtime_str = "16:08 25-Mar-2004";
-const char *source_version_str = "20040325";
+const time_t source_modtime = 1080359484;
+const char *source_modtime_str = "19:51 26-Mar-2004";
+const char *source_version_str = "20040326";
