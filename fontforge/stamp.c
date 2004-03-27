937e3dda4234257d252a2862b0d37bc7e4d6eb9c
pfaedit <pfaedit>
2004-03-27 03:53:43 +0000

    Fix a couple of memory problems with closing debugger.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1080359484;
-const char *source_modtime_str = "19:51 26-Mar-2004";
+const time_t source_modtime = 1080360246;
+const char *source_modtime_str = "20:04 26-Mar-2004";
 const char *source_version_str = "20040326";
