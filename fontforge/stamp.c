4655c71a1ecf29ed74ab5c5eebd5ad71df74bcbe
pfaedit <pfaedit>
2004-03-27 19:29:27 +0000

    Reset the typoAscender (if set) when scaling a font from the font view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1080362123;
-const char *source_modtime_str = "20:35 26-Mar-2004";
-const char *source_version_str = "20040326";
+const time_t source_modtime = 1080414640;
+const char *source_modtime_str = "11:10 27-Mar-2004";
+const char *source_version_str = "20040327";
