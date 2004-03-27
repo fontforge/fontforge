250e6ae618ca04ea21832eabb84a4d433e4b0f56
George Williams <pfaedit@users.sourceforge.net>
2004-03-27 04:45:22 +0000

    Drawing a rectangle from center out didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1080360246;
-const char *source_modtime_str = "20:04 26-Mar-2004";
+const time_t source_modtime = 1080360827;
+const char *source_modtime_str = "20:13 26-Mar-2004";
 const char *source_version_str = "20040326";
