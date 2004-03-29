ae98887fc97ca6dedaa4f1d48a44e81f838412a8
George Williams <pfaedit@users.sourceforge.net>
2004-03-29 23:47:34 +0000

    If a glyph was used as a reference, and we decided to put it in a subroutine (type1 output) and the glyph had hint conflicts, then it never got its first group of hints set.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1080542974;
-const char *source_modtime_str = "22:49 28-Mar-2004";
-const char *source_version_str = "20040328";
+const time_t source_modtime = 1080582570;
+const char *source_modtime_str = "09:49 29-Mar-2004";
+const char *source_version_str = "20040329";
