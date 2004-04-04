8687c097487d20955932efaffc5e3bfba905cde9
George Williams <pfaedit@users.sourceforge.net>
2004-04-04 19:33:18 +0000

    Oops, typo in hintmask patch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1081105630;
-const char *source_modtime_str = "12:07 4-Apr-2004";
+const time_t source_modtime = 1081106952;
+const char *source_modtime_str = "12:29 4-Apr-2004";
 const char *source_version_str = "20040404";
