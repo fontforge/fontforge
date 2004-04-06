90e5064304f87277c4d3fab75e5cfa6ebdc7758e
George Williams <pfaedit@users.sourceforge.net>
2004-04-06 06:03:19 +0000

    Check for a reference to a null char when pasting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1081106952;
-const char *source_modtime_str = "12:29 4-Apr-2004";
-const char *source_version_str = "20040404";
+const time_t source_modtime = 1081230587;
+const char *source_modtime_str = "22:49 5-Apr-2004";
+const char *source_version_str = "20040405";
