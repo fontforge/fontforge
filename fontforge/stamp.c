6705b8206d03b16bb4c6b53c80c3ea4954053b11
George Williams <pfaedit@users.sourceforge.net>
2004-04-09 02:36:27 +0000

    The rangeShift field of the 'kern' table had the wrong sign.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1081230587;
-const char *source_modtime_str = "22:49 5-Apr-2004";
-const char *source_version_str = "20040405";
+const time_t source_modtime = 1081373912;
+const char *source_modtime_str = "14:38 7-Apr-2004";
+const char *source_version_str = "20040407";
