6783fe5468bd8b9072d840de1562c9fa1eb56ab5
George Williams <pfaedit@users.sourceforge.net>
2004-04-09 02:39:30 +0000

    The flatten commands didn't presever the 'name' or 'OS/2' tables.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1081478187;
-const char *source_modtime_str = "19:36 8-Apr-2004";
+const time_t source_modtime = 1081478437;
+const char *source_modtime_str = "19:40 8-Apr-2004";
 const char *source_version_str = "20040408";
