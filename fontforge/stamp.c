e6a1efbeb355128aae1bc900569d89550fc001a5
pfaedit <pfaedit>
2004-04-11 05:50:32 +0000

    Markus Schwarzenberg points out that the ScaleToEm scripting command sometimes doesn't.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1081662723;
-const char *source_modtime_str = "22:52 10-Apr-2004";
+const time_t source_modtime = 1081663355;
+const char *source_modtime_str = "23:02 10-Apr-2004";
 const char *source_version_str = "20040410";
