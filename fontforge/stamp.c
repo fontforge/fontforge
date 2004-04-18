743e61bca14abb5e37ad2d5e2ba2929da904b9c4
pfaedit <pfaedit>
2004-04-18 05:23:14 +0000

    Fix a couple of bugs in dealing with references in multi-layered editing.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1081696072;
-const char *source_modtime_str = "08:07 11-Apr-2004";
-const char *source_version_str = "20040411";
+const time_t source_modtime = 1082244282;
+const char *source_modtime_str = "16:24 17-Apr-2004";
+const char *source_version_str = "20040417";
