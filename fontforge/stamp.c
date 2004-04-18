a6149174f2034701a5e7c0d84673943b2f0871f4
pfaedit <pfaedit>
2004-04-18 06:19:31 +0000

    'hhea'.caretSlopeRun should match tan(italicAngle).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1082264438;
-const char *source_modtime_str = "22:00 17-Apr-2004";
+const time_t source_modtime = 1082268993;
+const char *source_modtime_str = "23:16 17-Apr-2004";
 const char *source_version_str = "20040417";
