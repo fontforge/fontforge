ad9ad87ce7f641dc61079802ab1c6321b1828002
pfaedit <pfaedit>
2004-04-18 19:05:14 +0000

    FontForge did not calculate the MaxCompositePointCount field of 'maxp' properly (if the source was a set of cubic splines. It returned the point count in the cubic world.)

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1082305019;
-const char *source_modtime_str = "09:16 18-Apr-2004";
+const time_t source_modtime = 1082305073;
+const char *source_modtime_str = "09:17 18-Apr-2004";
 const char *source_version_str = "20040418";
