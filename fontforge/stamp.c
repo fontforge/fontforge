a041938e6c1332cbdb15c0046b37d0652ab0c95b
pfaedit <pfaedit>
2004-04-18 07:24:32 +0000

    Our cubic->quadratic conversion could yield spline segements whose endpoints were <1 unit apart. Since ttf rounds all coords to ints, this meant the endpoints would appear on top of each other (font val. complains about this, and it is stupid).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1082271330;
-const char *source_modtime_str = "23:55 17-Apr-2004";
-const char *source_version_str = "20040417";
+const time_t source_modtime = 1082272818;
+const char *source_modtime_str = "00:20 18-Apr-2004";
+const char *source_version_str = "20040418";
