8d64f41f125a5aec8ee56b5c990587de3140ddfc
pfaedit <pfaedit>
2004-04-20 01:05:41 +0000

    Typo in code for adding ghost hints at curves.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1082314236;
-const char *source_modtime_str = "11:50 18-Apr-2004";
-const char *source_version_str = "20040418";
+const time_t source_modtime = 1082421662;
+const char *source_modtime_str = "17:41 19-Apr-2004";
+const char *source_version_str = "20040419";
