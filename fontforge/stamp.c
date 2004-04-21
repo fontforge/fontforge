4bc098fa61d4c334bd03223bcdb4f2ade64826a0
pfaedit <pfaedit>
2004-04-21 17:18:44 +0000

    Patch by E.J. Neafsey, misspelled svg keyword.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1082566778;
-const char *source_modtime_str = "09:59 21-Apr-2004";
+const time_t source_modtime = 1082567701;
+const char *source_modtime_str = "10:15 21-Apr-2004";
 const char *source_version_str = "20040421";
