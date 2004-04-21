f65b26375e92c0568adb27806f78d40fc3d5a44a
pfaedit <pfaedit>
2004-04-21 16:55:53 +0000

    Yesterday's ghost hnt patch had a typo.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1082426336;
-const char *source_modtime_str = "18:58 19-Apr-2004";
-const char *source_version_str = "20040419";
+const time_t source_modtime = 1082566432;
+const char *source_modtime_str = "09:53 21-Apr-2004";
+const char *source_version_str = "20040421";
