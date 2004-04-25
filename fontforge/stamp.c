79d0e89ec128670b179daefa7ed2b06052083964
George Williams <pfaedit@users.sourceforge.net>
2004-04-25 00:18:21 +0000

    More autohint fixes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1082567701;
-const char *source_modtime_str = "10:15 21-Apr-2004";
-const char *source_version_str = "20040421";
+const time_t source_modtime = 1082848009;
+const char *source_modtime_str = "16:06 24-Apr-2004";
+const char *source_version_str = "20040424";
