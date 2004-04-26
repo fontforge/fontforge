1c7831e8c01d11d4dc99bf5b2cc58458ee6db94e
George Williams <pfaedit@users.sourceforge.net>
2004-04-26 03:19:12 +0000

    Oops, was a crash here.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1082935789;
-const char *source_modtime_str = "16:29 25-Apr-2004";
+const time_t source_modtime = 1082947770;
+const char *source_modtime_str = "19:49 25-Apr-2004";
 const char *source_version_str = "20040425";
