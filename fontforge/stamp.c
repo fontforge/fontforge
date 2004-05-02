c7811b7753e7283b66116bdca18493d355612e5b
pfaedit <pfaedit>
2004-05-02 07:13:29 +0000

    Various fixes to tfm input/output.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1083104346;
-const char *source_modtime_str = "15:19 27-Apr-2004";
-const char *source_version_str = "20040427";
+const time_t source_modtime = 1083480933;
+const char *source_modtime_str = "23:55 1-May-2004";
+const char *source_version_str = "20040501";
