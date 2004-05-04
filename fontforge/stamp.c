76c1160db0013ad7f830735577d6440dc2b983e1
George Williams <pfaedit@users.sourceforge.net>
2004-05-04 00:02:15 +0000

    Tfm files have an extension mechanism for when the lig/kern sub-table needs to be big. FontForge did not support it. It should have.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1083560447;
-const char *source_modtime_str = "22:00 2-May-2004";
-const char *source_version_str = "20040502";
+const time_t source_modtime = 1083626548;
+const char *source_modtime_str = "16:22 3-May-2004";
+const char *source_version_str = "20040503";
