2d849c41022f343e8cddc913b327b5cec8bf5a96
pfaedit <pfaedit>
2004-05-06 03:02:52 +0000

    More typos in the ghost hint code.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1083808103;
-const char *source_modtime_str = "18:48 5-May-2004";
+const time_t source_modtime = 1083812197;
+const char *source_modtime_str = "19:56 5-May-2004";
 const char *source_version_str = "20040505";
