31ca70e19ed87ab7742c22c1d55de7903745fa11
George Williams <pfaedit@users.sourceforge.net>
2004-05-07 01:17:58 +0000

    Fix some bugs in transforming OT contextual substitutions to Mac State Machines.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1083818784;
-const char *source_modtime_str = "21:46 5-May-2004";
-const char *source_version_str = "20040505";
+const time_t source_modtime = 1083892465;
+const char *source_modtime_str = "18:14 6-May-2004";
+const char *source_version_str = "20040506";
