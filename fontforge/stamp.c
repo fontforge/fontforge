e8b633c68ab9eb21162b11d5805998158ba60e8e
pfaedit <pfaedit>
2004-05-09 15:13:46 +0000

    Back out a bit of the GSUB->state machine conversion changes that I added last week. A bit too extreme I think.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1084084679;
-const char *source_modtime_str = "23:37 8-May-2004";
-const char *source_version_str = "20040508";
+const time_t source_modtime = 1084115582;
+const char *source_modtime_str = "08:13 9-May-2004";
+const char *source_version_str = "20040509";
