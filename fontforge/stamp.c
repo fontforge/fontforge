dbb2aaa3e1b39bc9afa7ceb440498a86df42d3c5
George Williams <pfaedit@users.sourceforge.net>
2004-05-11 16:45:21 +0000

    New Japanese UI from KANOU.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1084116358;
-const char *source_modtime_str = "08:25 9-May-2004";
-const char *source_version_str = "20040509";
+const time_t source_modtime = 1084292177;
+const char *source_modtime_str = "09:16 11-May-2004";
+const char *source_version_str = "20040511";
