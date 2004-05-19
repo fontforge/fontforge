1189dd5ed2abe5a0ad4ef077026922c4accf682a
George Williams <pfaedit@users.sourceforge.net>
2004-05-19 17:06:12 +0000

    Typo in saving an AMFM file (AFM for MultiMaster).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1084977971;
-const char *source_modtime_str = "07:46 19-May-2004";
+const time_t source_modtime = 1084985969;
+const char *source_modtime_str = "09:59 19-May-2004";
 const char *source_version_str = "20040519";
