fc98ebc065bb52d958146ca8793af1ae095f9762
pfaedit <pfaedit>
2004-05-20 15:47:55 +0000

    Scripting wasn't tweaked enough to support the new FOND interface.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1085029646;
-const char *source_modtime_str = "22:07 19-May-2004";
-const char *source_version_str = "20040519";
+const time_t source_modtime = 1085068008;
+const char *source_modtime_str = "08:46 20-May-2004";
+const char *source_version_str = "20040520";
