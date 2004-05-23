c93c631aa85a98de45b9dee5101d5d46e27aee5d
pfaedit <pfaedit>
2004-05-23 19:55:52 +0000

    Reorder choice of accent variants.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1085068008;
-const char *source_modtime_str = "08:46 20-May-2004";
-const char *source_version_str = "20040520";
+const time_t source_modtime = 1085342153;
+const char *source_modtime_str = "12:55 23-May-2004";
+const char *source_version_str = "20040523";
