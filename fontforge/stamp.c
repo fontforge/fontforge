56e1481f47fc656f80e1663d16dec9a64b296a2a
George Williams <pfaedit@users.sourceforge.net>
2004-05-25 15:33:39 +0000

    FF was confused by initial (or multiple) spaces in OTF class lists.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1085498545;
-const char *source_modtime_str = "08:22 25-May-2004";
+const time_t source_modtime = 1085499213;
+const char *source_modtime_str = "08:33 25-May-2004";
 const char *source_version_str = "20040525";
