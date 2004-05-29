d931d69b3a03c6eaf335ad6b47df9c10712d71a3
pfaedit <pfaedit>
2004-05-29 14:28:49 +0000

    Improve order2 merges a bit.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1085811668;
-const char *source_modtime_str = "23:21 28-May-2004";
-const char *source_version_str = "20040528";
+const time_t source_modtime = 1085840856;
+const char *source_modtime_str = "07:27 29-May-2004";
+const char *source_version_str = "20040529";
