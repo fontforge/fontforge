4fb3d666f1fb227a50379ea30db2b4bb42cca609
George Williams <pfaedit@users.sourceforge.net>
2004-05-29 00:27:57 +0000

    And a warning about POST resources too.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1085779341;
-const char *source_modtime_str = "14:22 28-May-2004";
+const time_t source_modtime = 1085788297;
+const char *source_modtime_str = "16:51 28-May-2004";
 const char *source_version_str = "20040528";
