33a91b7cffd462f5aca4d126267f6518a92fb022
George Williams <pfaedit@users.sourceforge.net>
2004-05-29 06:24:40 +0000

    Proplem with parallel lines in expand stroke caused nans which later caused a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1085788297;
-const char *source_modtime_str = "16:51 28-May-2004";
+const time_t source_modtime = 1085811668;
+const char *source_modtime_str = "23:21 28-May-2004";
 const char *source_version_str = "20040528";
