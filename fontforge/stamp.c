f23e22e006512ce8088103ad42e382bdabc7d9e1
George Williams <pfaedit@users.sourceforge.net>
2004-06-01 03:38:59 +0000

    On 13-May (build=19-May) I introduced a bug whereby control points could be defaulted to NaN values.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1085840856;
-const char *source_modtime_str = "07:27 29-May-2004";
-const char *source_version_str = "20040529";
+const time_t source_modtime = 1086060901;
+const char *source_modtime_str = "20:35 31-May-2004";
+const char *source_version_str = "20040531";
