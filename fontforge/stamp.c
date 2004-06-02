8842f6e730bc32e6bfacea6958b5a3bf388ab650
pfaedit <pfaedit>
2004-06-02 14:33:06 +0000

    Rename a variable called "strlen". It confuses gcc.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1086124673;
-const char *source_modtime_str = "14:17 1-Jun-2004";
-const char *source_version_str = "20040601";
+const time_t source_modtime = 1086186792;
+const char *source_modtime_str = "07:33 2-Jun-2004";
+const char *source_version_str = "20040602";
