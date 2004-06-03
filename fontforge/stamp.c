6ead0d2e37df84a112b7579920c9cb2c274571a6
George Williams <pfaedit@users.sourceforge.net>
2004-06-03 22:52:46 +0000

    The preference items controlling accent positioning were not accessable from scripts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1086186792;
-const char *source_modtime_str = "07:33 2-Jun-2004";
-const char *source_version_str = "20040602";
+const time_t source_modtime = 1086303103;
+const char *source_modtime_str = "15:51 3-Jun-2004";
+const char *source_version_str = "20040603";
