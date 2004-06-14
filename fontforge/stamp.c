5d8173ed0cb9b5a365bdae6312fe767bdb340874
pfaedit <pfaedit>
2004-06-14 15:41:48 +0000

    Improve ItalicConstrained, allow verticals too.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1086993203;
-const char *source_modtime_str = "15:33 11-Jun-2004";
-const char *source_version_str = "20040611";
+const time_t source_modtime = 1087227614;
+const char *source_modtime_str = "08:40 14-Jun-2004";
+const char *source_version_str = "20040614";
