5ecb5ede337bf64fb7cf1ff5edc08111f036c165
George Williams <pfaedit@users.sourceforge.net>
2004-06-14 22:55:14 +0000

    Apply the recommended ordering of ttf/otf tables specified at http://partners.adobe.com/asn/tech/type/opentype/recom.jsp, though I'm blessed if I can see how this makes a difference.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1087227614;
-const char *source_modtime_str = "08:40 14-Jun-2004";
+const time_t source_modtime = 1087253620;
+const char *source_modtime_str = "15:53 14-Jun-2004";
 const char *source_version_str = "20040614";
