1fa3c61a7a629b90d8139562aa092437ed963cda
pfaedit <pfaedit>
2004-06-16 22:03:55 +0000

    Can no longer blindly rotate a splineset to avoid an initial flex. If the point has a hintmask (in a type2 font) then we must insure that whatever point we rotate to also has a hintmask. Easier just to turn off flex.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1087328107;
-const char *source_modtime_str = "12:35 15-Jun-2004";
-const char *source_version_str = "20040615";
+const time_t source_modtime = 1087422582;
+const char *source_modtime_str = "14:49 16-Jun-2004";
+const char *source_version_str = "20040616";
