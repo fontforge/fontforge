1b47c717665e282f1fa654501dc57ce603dae22f
George Williams <pfaedit@users.sourceforge.net>
2004-06-18 00:12:41 +0000

    Do a better job fixing up for Greek fonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1087422582;
-const char *source_modtime_str = "14:49 16-Jun-2004";
-const char *source_version_str = "20040616";
+const time_t source_modtime = 1087515207;
+const char *source_modtime_str = "16:33 17-Jun-2004";
+const char *source_version_str = "20040617";
