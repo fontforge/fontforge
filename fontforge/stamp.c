effc2103e68a4ca61c0ec50146da0fad4d18cca9
George Williams <pfaedit@users.sourceforge.net>
2004-07-01 21:58:11 +0000

    FontForge didn't realize that tonos should be treated as an accent in BuildAccented (it is spacing).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1088717447;
-const char *source_modtime_str = "14:30 1-Jul-2004";
+const time_t source_modtime = 1088719012;
+const char *source_modtime_str = "14:56 1-Jul-2004";
 const char *source_version_str = "20040701";
