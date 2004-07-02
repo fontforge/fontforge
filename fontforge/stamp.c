ac23a6897cb4e5f38162bc91095ff7a571587941
George Williams <pfaedit@users.sourceforge.net>
2004-07-02 13:24:49 +0000

    KANOU provides a patch which lets windows access embedded bitmaps in ttf files. Windows, he says, needs a modification to the gasp table for this to work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1088747419;
-const char *source_modtime_str = "22:50 1-Jul-2004";
-const char *source_version_str = "20040701";
+const time_t source_modtime = 1088774639;
+const char *source_modtime_str = "06:23 2-Jul-2004";
+const char *source_version_str = "20040702";
