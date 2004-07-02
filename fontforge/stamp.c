2ef3382be0b7f9f4670d277fc3cf9112ede9eb3a
pfaedit <pfaedit>
2004-07-02 13:38:10 +0000

    KANOU's last patch also included a patch to set the average character width in the OS/2 table in a weighted fashion.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1088774639;
-const char *source_modtime_str = "06:23 2-Jul-2004";
+const time_t source_modtime = 1088775533;
+const char *source_modtime_str = "06:38 2-Jul-2004";
 const char *source_version_str = "20040702";
