2d5e80582eec12c63282aa969852612a0a4b5715
George Williams <pfaedit@users.sourceforge.net>
2004-07-02 05:53:14 +0000

    Add support for Adobe's variant accents 	Grave and grave.cap	(for grave accents over capital letters) 	breve.cyr		(for breve accents over cyrillic letters) 	breve.cyrcap

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1088741207;
-const char *source_modtime_str = "21:06 1-Jul-2004";
+const time_t source_modtime = 1088747419;
+const char *source_modtime_str = "22:50 1-Jul-2004";
 const char *source_version_str = "20040701";
