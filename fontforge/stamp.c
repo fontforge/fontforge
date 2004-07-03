503be82c161c156be2494bfa9580a06af71badbd
George Williams <pfaedit@users.sourceforge.net>
2004-07-03 15:13:08 +0000

    Undoes in the guide layer crashed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1088832859;
-const char *source_modtime_str = "22:34 2-Jul-2004";
-const char *source_version_str = "20040702";
+const time_t source_modtime = 1088867509;
+const char *source_modtime_str = "08:11 3-Jul-2004";
+const char *source_version_str = "20040703";
