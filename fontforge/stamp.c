a05f2ec9d0173a1dd755745136b90b4c150b07d1
pfaedit <pfaedit>
2004-07-07 21:36:29 +0000

    The libuninameslist library now comes in localized versions (currently only French is available), change the thing which dlopens the lib to check for the one which is right for the current locale.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1088867509;
-const char *source_modtime_str = "08:11 3-Jul-2004";
-const char *source_version_str = "20040703";
+const time_t source_modtime = 1089235144;
+const char *source_modtime_str = "14:19 7-Jul-2004";
+const char *source_version_str = "20040707";
