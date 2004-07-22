1aca4b5a12c009ebbac28f6912a125d65abe1844
pfaedit <pfaedit>
2004-07-22 20:42:09 +0000

    Make sure the ttf unique font id entry (in 'name' table) gets checked when user changes fontname, just as we check all the other unique id type entries.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1090523204;
-const char *source_modtime_str = "12:06 22-Jul-2004";
+const time_t source_modtime = 1090528157;
+const char *source_modtime_str = "13:29 22-Jul-2004";
 const char *source_version_str = "20040722";
