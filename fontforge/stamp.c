97b443d1c3acec01850771e0bf743829831ea79d
George Williams <pfaedit@users.sourceforge.net>
2004-07-23 00:26:33 +0000

    Thomas Phinney of Adobe confirms that truetype Unique Font IDs should be unique (one for apple, one for ms, no multiple langs allowed). Check for this.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1090528157;
-const char *source_modtime_str = "13:29 22-Jul-2004";
+const time_t source_modtime = 1090542153;
+const char *source_modtime_str = "17:22 22-Jul-2004";
 const char *source_version_str = "20040722";
