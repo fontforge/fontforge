66015ca84c28e925683f1fdac3bd2e0e1faf668b
George Williams <pfaedit@users.sourceforge.net>
2004-07-25 04:43:25 +0000

    Give CopyGLyphFeatures a ... style argument. It will either accept an array, or an arbetrary list of strings/numbers.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1090728616;
-const char *source_modtime_str = "21:10 24-Jul-2004";
+const time_t source_modtime = 1090730442;
+const char *source_modtime_str = "21:40 24-Jul-2004";
 const char *source_version_str = "20040724";
