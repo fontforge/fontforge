4162720015564513cf8051733cb19ca4b80d3906
George Williams <pfaedit@users.sourceforge.net>
2004-07-29 13:47:08 +0000

    The fondname patch broke $fontversion and $copyright in scripts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1090988148;
-const char *source_modtime_str = "21:15 27-Jul-2004";
-const char *source_version_str = "20040727";
+const time_t source_modtime = 1091108619;
+const char *source_modtime_str = "06:43 29-Jul-2004";
+const char *source_version_str = "20040729";
