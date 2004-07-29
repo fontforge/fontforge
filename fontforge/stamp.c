9f94992f113f050d9322f8c27eb6182b41d03163
pfaedit <pfaedit>
2004-07-29 14:21:20 +0000

    Still problems with tfm files.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1091108619;
-const char *source_modtime_str = "06:43 29-Jul-2004";
+const time_t source_modtime = 1091110511;
+const char *source_modtime_str = "07:15 29-Jul-2004";
 const char *source_version_str = "20040729";
