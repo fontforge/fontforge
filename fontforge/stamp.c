ed5135d4f725a8cc763d607c7549ad1b28cc3170
George Williams <pfaedit@users.sourceforge.net>
2004-07-30 23:04:29 +0000

    Added a scripting command to get unix environment variables.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1091218747;
-const char *source_modtime_str = "13:19 30-Jul-2004";
+const time_t source_modtime = 1091228476;
+const char *source_modtime_str = "16:01 30-Jul-2004";
 const char *source_version_str = "20040730";
