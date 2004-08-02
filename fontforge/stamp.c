b306078b4ae038b22af9732e67d7dd4ade7ee78e
pfaedit <pfaedit>
2004-08-02 20:54:21 +0000

    I got the locale wrong for Hong Kong.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1091478842;
-const char *source_modtime_str = "13:34 2-Aug-2004";
+const time_t source_modtime = 1091479784;
+const char *source_modtime_str = "13:49 2-Aug-2004";
 const char *source_version_str = "20040802";
