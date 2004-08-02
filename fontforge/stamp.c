7c635b682b130a70c33626394d6c8fcc8df568fc
George Williams <pfaedit@users.sourceforge.net>
2004-08-02 18:39:46 +0000

    In spanish, the kern classes' dlg's buttons are too squinched.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1091470089;
-const char *source_modtime_str = "11:08 2-Aug-2004";
+const time_t source_modtime = 1091471366;
+const char *source_modtime_str = "11:29 2-Aug-2004";
 const char *source_version_str = "20040802";
