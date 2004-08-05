dd7524f75dbf219ecba5840db4bfc6aa6b6e0493
George Williams <pfaedit@users.sourceforge.net>
2004-08-05 14:25:07 +0000

    Oops, got the gender wrong on kachide'.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1091652489;
-const char *source_modtime_str = "13:48 4-Aug-2004";
-const char *source_version_str = "20040804";
+const time_t source_modtime = 1091715803;
+const char *source_modtime_str = "07:23 5-Aug-2004";
+const char *source_version_str = "20040805";
