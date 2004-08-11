dcba0c2d49a7b1bda8fb90a81d101d2119d9c1ab
George Williams <pfaedit@users.sourceforge.net>
2004-08-11 13:21:01 +0000

    MURAOKA Taro provides a patch to fix the SetPanose scripting command.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1092176979;
-const char *source_modtime_str = "15:29 10-Aug-2004";
-const char *source_version_str = "20040810";
+const time_t source_modtime = 1092230308;
+const char *source_modtime_str = "06:18 11-Aug-2004";
+const char *source_version_str = "20040811";
