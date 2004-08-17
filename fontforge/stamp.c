02c2455096e845a2fd5c529f130af16aa8c521a6
George Williams <pfaedit@users.sourceforge.net>
2004-08-17 22:49:38 +0000

    There was a path through LoadEncoding file where it would silently fail in a script. I hope I've plugged it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1092782050;
-const char *source_modtime_str = "15:34 17-Aug-2004";
+const time_t source_modtime = 1092782510;
+const char *source_modtime_str = "15:41 17-Aug-2004";
 const char *source_version_str = "20040817";
