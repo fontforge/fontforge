b1a9a843a5667635ce4428956e89d2490dc979fc
George Williams <pfaedit@users.sourceforge.net>
2004-09-22 05:06:30 +0000

    If we delete a char view window with docked palettes and then change the cursor in another char view, we will have killed the cvtools palette and the attempt to set a cursor in it crashes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1094383980;
-const char *source_modtime_str = "14:33 5-Sep-2004";
-const char *source_version_str = "20040905";
+const time_t source_modtime = 1094292418;
+const char *source_modtime_str = "13:06 4-Sep-2004";
+const char *source_version_str = "20040904";
