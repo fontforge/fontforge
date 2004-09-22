85178c07bbae419a9861ee057340f730c5d166aa
pfaedit <pfaedit>
2004-09-22 04:55:14 +0000

    Use a different approach to approximating a set of points with a spline (holding end slopes fixed). Used to do successive least squares, now do one least squares, fix up the slopes and then vary the lengths of the slope vectors until we get a good match. (perturbations).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1094292055;
-const char *source_modtime_str = "13:00 4-Sep-2004";
-const char *source_version_str = "20040904";
+const time_t source_modtime = 1094229902;
+const char *source_modtime_str = "19:45 3-Sep-2004";
+const char *source_version_str = "20040903";
