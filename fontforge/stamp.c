93e66ce8546c226424fc6fb9198dc09581df897d
George Williams <pfaedit@users.sourceforge.net>
2004-09-22 23:00:18 +0000

    Patch by Michael Gährken to allow the scripting Skew command to take numerator/denominator style angles.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1094292418;
-const char *source_modtime_str = "13:06 4-Sep-2004";
-const char *source_version_str = "20040904";
+const time_t source_modtime = 1095889519;
+const char *source_modtime_str = "14:45 22-Sep-2004";
+const char *source_version_str = "20040922";
