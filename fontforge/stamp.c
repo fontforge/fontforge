41cb644f0da5e06b66eb7ab139a73d65c90e33ae
George Williams <pfaedit@users.sourceforge.net>
2004-09-26 22:50:36 +0000

    We used to clear the info line in the charview when the cursor left the window. But some of the info remains relevant, so continue to display those bits.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1095892327;
-const char *source_modtime_str = "15:32 22-Sep-2004";
-const char *source_version_str = "20040922";
+const time_t source_modtime = 1096238734;
+const char *source_modtime_str = "15:45 26-Sep-2004";
+const char *source_version_str = "20040926";
