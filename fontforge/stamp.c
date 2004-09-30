1fa9e2d474a9329de4e6363ccf2f38b6894b3145
George Williams <pfaedit@users.sourceforge.net>
2004-09-30 04:56:51 +0000

    ff would crash at some random point after loading a font with multiple versions of some of the greek letters.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1096516171;
-const char *source_modtime_str = "20:49 29-Sep-2004";
+const time_t source_modtime = 1096519405;
+const char *source_modtime_str = "21:43 29-Sep-2004";
 const char *source_version_str = "20040929";
