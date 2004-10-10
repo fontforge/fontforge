96c6eae633e48530486cb66cc95df4ee78207935
George Williams <pfaedit@users.sourceforge.net>
2004-10-10 22:35:56 +0000

    Fix various problems with multi layered mode (particularly turning it off in fontinfo, also rasterizing it).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1096923822;
-const char *source_modtime_str = "14:03 4-Oct-2004";
-const char *source_version_str = "20041004";
+const time_t source_modtime = 1097447559;
+const char *source_modtime_str = "15:32 10-Oct-2004";
+const char *source_version_str = "20041010";
