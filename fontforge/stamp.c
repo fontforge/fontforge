3369fbff1421c7f90e51da880a98b95b8064584b
pfaedit <pfaedit>
2004-10-12 04:28:36 +0000

    First real version of nomen-es.c

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1097554907;
-const char *source_modtime_str = "21:21 11-Oct-2004";
+const time_t source_modtime = 1097555258;
+const char *source_modtime_str = "21:27 11-Oct-2004";
 const char *source_version_str = "20041011";
