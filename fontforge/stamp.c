e326acc16180fead9ec7cdcf9b5729f64e697028
pfaedit <pfaedit>
2004-10-14 04:03:12 +0000

    Window title of bitmap view was missing the glyph name.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1097703032;
-const char *source_modtime_str = "14:30 13-Oct-2004";
+const time_t source_modtime = 1097708728;
+const char *source_modtime_str = "16:05 13-Oct-2004";
 const char *source_version_str = "20041013";
