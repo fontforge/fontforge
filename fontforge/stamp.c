86025df0864164505a536d2651b4bb06b9fe8042
George Williams <pfaedit@users.sourceforge.net>
2004-10-14 04:02:27 +0000

    Write glyph names in sans-serif type in the fontview.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1097700957;
-const char *source_modtime_str = "13:55 13-Oct-2004";
+const time_t source_modtime = 1097703032;
+const char *source_modtime_str = "14:30 13-Oct-2004";
 const char *source_version_str = "20041013";
