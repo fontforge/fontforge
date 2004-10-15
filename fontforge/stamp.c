80ac4b55d789205c51449be3f01e5d175e50da69
George Williams <pfaedit@users.sourceforge.net>
2004-10-15 20:40:03 +0000

    Oops. memory problem in new spline overlap.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1097776373;
-const char *source_modtime_str = "10:52 14-Oct-2004";
-const char *source_version_str = "20041014";
+const time_t source_modtime = 1097872409;
+const char *source_modtime_str = "13:33 15-Oct-2004";
+const char *source_version_str = "20041015";
