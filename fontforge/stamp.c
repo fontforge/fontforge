5c67bfcb85b57a32f8b28d633f8f47edde3d42b4
George Williams <pfaedit@users.sourceforge.net>
2004-10-17 22:32:33 +0000

    The ruler code was based on the assumption that people were dealing with reasonable splines, and that showing 1 decimal point was accurate enough. Well Andrey V. Panov keeps giving me nasty splines where I need to know distances on the order of 1e-5. So if the distance is tiny, use %g rather than %.1f.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1097937488;
-const char *source_modtime_str = "07:38 16-Oct-2004";
-const char *source_version_str = "20041016";
+const time_t source_modtime = 1098049913;
+const char *source_modtime_str = "14:51 17-Oct-2004";
+const char *source_version_str = "20041017";
