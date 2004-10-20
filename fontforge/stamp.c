abd8419c7870e9f824261f3016a61cc3b1527d1d
pfaedit <pfaedit>
2004-10-20 20:48:32 +0000

    When creating a new glyph in a greymap font, most pathways actually created a bitmap glyph which immediately caused problems.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1098121998;
-const char *source_modtime_str = "10:53 18-Oct-2004";
-const char *source_version_str = "20041018";
+const time_t source_modtime = 1098299248;
+const char *source_modtime_str = "12:07 20-Oct-2004";
+const char *source_version_str = "20041020";
