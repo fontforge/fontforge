98da297fbe0f86b5785801f1d8b9330efe8c7a18
pfaedit <pfaedit>
2004-10-21 22:38:11 +0000

    Simplify sometimes failed to recognize extrema (ie. it would sometimes remove one).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1098332757;
-const char *source_modtime_str = "21:25 20-Oct-2004";
-const char *source_version_str = "20041020";
+const time_t source_modtime = 1098392005;
+const char *source_modtime_str = "13:53 21-Oct-2004";
+const char *source_version_str = "20041021";
