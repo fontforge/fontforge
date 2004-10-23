a94ae630b0b62d5eaad6f18483e5691b2ab52819
pfaedit <pfaedit>
2004-10-23 13:52:32 +0000

    Yet more problems with simplify introduced by recent change.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1098484658;
-const char *source_modtime_str = "15:37 22-Oct-2004";
-const char *source_version_str = "20041022";
+const time_t source_modtime = 1098538149;
+const char *source_modtime_str = "06:29 23-Oct-2004";
+const char *source_version_str = "20041023";
