5941bb0c3e55d81939d180a832080b99e0500938
George Williams <pfaedit@users.sourceforge.net>
2004-10-28 14:02:56 +0000

    Extremum test was still wrong.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1098971133;
-const char *source_modtime_str = "06:45 28-Oct-2004";
+const time_t source_modtime = 1098971797;
+const char *source_modtime_str = "06:56 28-Oct-2004";
 const char *source_version_str = "20041028";
