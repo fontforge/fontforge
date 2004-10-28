c3af1812cfbf9de7e9b8ddaf5e74a827bbcdaf84
George Williams <pfaedit@users.sourceforge.net>
2004-10-28 17:05:40 +0000

    More rounding error fixes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1098980045;
-const char *source_modtime_str = "09:14 28-Oct-2004";
+const time_t source_modtime = 1098982457;
+const char *source_modtime_str = "09:54 28-Oct-2004";
 const char *source_version_str = "20041028";
