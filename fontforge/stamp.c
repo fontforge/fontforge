9d406e842d8fd4a7a1bd43bcd9d7bda1db5fead7
pfaedit <pfaedit>
2004-10-28 15:41:27 +0000

    Remove Overlap also gets confused if a control point stretchs beyond the end point of a spline (so that there is a backtrack at the other end-point).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1098971797;
-const char *source_modtime_str = "06:56 28-Oct-2004";
+const time_t source_modtime = 1098977384;
+const char *source_modtime_str = "08:29 28-Oct-2004";
 const char *source_version_str = "20041028";
