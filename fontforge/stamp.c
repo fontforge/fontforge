812e338787959bd76a3ed28598d713e090e9db22
George Williams <pfaedit@users.sourceforge.net>
2004-11-04 04:02:15 +0000

    Martin Giese corrects Pippi Longstocking.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1099508655;
-const char *source_modtime_str = "11:04 3-Nov-2004";
+const time_t source_modtime = 1099540188;
+const char *source_modtime_str = "19:49 3-Nov-2004";
 const char *source_version_str = "20041103";
