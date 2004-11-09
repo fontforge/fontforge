9a0895f264fd4eb3f4cfdb6cf98a9081e780d904
pfaedit <pfaedit>
2004-11-09 05:54:05 +0000

    More overlap fixes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1099923169;
-const char *source_modtime_str = "06:12 8-Nov-2004";
+const time_t source_modtime = 1099979047;
+const char *source_modtime_str = "21:44 8-Nov-2004";
 const char *source_version_str = "20041108";
