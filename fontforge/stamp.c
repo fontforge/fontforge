6cf796f773df4f727fe5483dd2e21ce85209a230
George Williams <pfaedit@users.sourceforge.net>
2004-11-09 23:09:11 +0000

    When the glyph window was scaled so that the vertical distance from top to bottom was less than one em-unit, then the vertical ruler was drawn with the top and bottom labels reversed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1100011706;
-const char *source_modtime_str = "06:48 9-Nov-2004";
+const time_t source_modtime = 1100041407;
+const char *source_modtime_str = "15:03 9-Nov-2004";
 const char *source_version_str = "20041109";
