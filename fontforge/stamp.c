d0e3979526d4b59b6ef366ef4b5ddeaa37e4ff2c
pfaedit <pfaedit>
2004-11-12 20:36:13 +0000

    ff had a limit on ttf names of 1024 characters. Remove that limit.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1100041407;
-const char *source_modtime_str = "15:03 9-Nov-2004";
-const char *source_version_str = "20041109";
+const time_t source_modtime = 1100290848;
+const char *source_modtime_str = "12:20 12-Nov-2004";
+const char *source_version_str = "20041112";
