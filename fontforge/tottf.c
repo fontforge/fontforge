654816c108fc732562fc5ca8ba463050abb146e3
pfaedit <pfaedit>
2004-11-15 23:57:56 +0000

    The ascent and descent fields of hhea were usually wrong (0).

--- fontforge/tottf.c
+++ fontforge/tottf.c
@@ -2863,6 +2863,7 @@ void SFDefaultOS2Simple(struct pfminfo *pfminfo,SplineFont *sf) {
     pfminfo->width = 5;
     pfminfo->panose[3] = 3;
     pfminfo->winascent_add = pfminfo->windescent_add = true;
+    pfminfo->hheadascent_add = pfminfo->hheaddescent_add = true;
     pfminfo->os2_winascent = pfminfo->os2_windescent = 0;
 
     if ( sf->subfonts!=NULL ) sf = sf->subfonts[0];
