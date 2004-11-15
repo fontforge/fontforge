3210ab0fbd9042185433009d3e8c4439ba113682
George Williams <pfaedit@users.sourceforge.net>
2004-11-15 14:57:09 +0000

    If user set glyph comment/color but nothing else, glyph was not preserved in sfd file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1100290848;
-const char *source_modtime_str = "12:20 12-Nov-2004";
-const char *source_version_str = "20041112";
+const time_t source_modtime = 1100530210;
+const char *source_modtime_str = "06:50 15-Nov-2004";
+const char *source_version_str = "20041115";
