8bc2484ea72f8352695f2a90a79ac9ffb5529904
pfaedit <pfaedit>
2004-11-15 17:35:23 +0000

    When saving a utf7 string into an sfd file (ttf names, anchor names, etc.) If a hyphen followed directly after a character which needed to be expressed in base64, then that hyphen would vanish.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1100532949;
-const char *source_modtime_str = "07:35 15-Nov-2004";
+const time_t source_modtime = 1100539757;
+const char *source_modtime_str = "09:29 15-Nov-2004";
 const char *source_version_str = "20041115";
