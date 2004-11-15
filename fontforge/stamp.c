8319de170658276bcb9aa503e96d926217ac3c2c
George Williams <pfaedit@users.sourceforge.net>
2004-11-15 23:56:40 +0000

    New version of nomen-fr from Pierre.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1100539757;
-const char *source_modtime_str = "09:29 15-Nov-2004";
+const time_t source_modtime = 1100561666;
+const char *source_modtime_str = "15:34 15-Nov-2004";
 const char *source_version_str = "20041115";
