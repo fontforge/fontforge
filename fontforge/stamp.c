c1937fad9a2dcd8b38dccc35691b170d40cae50f
pfaedit <pfaedit>
2004-11-19 15:31:43 +0000

    Further work on default language for ttf names. Moved locale names out of the language name strings and put into a separate table. Added some more languages.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1100795363;
-const char *source_modtime_str = "08:29 18-Nov-2004";
-const char *source_version_str = "20041118";
+const time_t source_modtime = 1100877086;
+const char *source_modtime_str = "07:11 19-Nov-2004";
+const char *source_version_str = "20041119";
