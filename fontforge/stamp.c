89abe3fa9ce23985cd6f097683dfcbc54464ed3a
pfaedit <pfaedit>
2004-11-20 15:39:24 +0000

    Kevin Schoedel points out that it is perfectly legal to have a NUL in a postscript string. While ff isn't going to support this, we should at least read such files without error.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1100963162;
-const char *source_modtime_str = "07:06 20-Nov-2004";
+const time_t source_modtime = 1100964630;
+const char *source_modtime_str = "07:30 20-Nov-2004";
 const char *source_version_str = "20041120";
