57603a9be4754645104b30688e0a42d4dc50a2dc
pfaedit <pfaedit>
2004-11-22 04:27:21 +0000

    Kevin Schoedel points out that type1 fonts in a mac resource fork are stored in resource id order rather than file order (normally the two will prove the same) and provides a patch to read them that way.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1101088619;
-const char *source_modtime_str = "17:56 21-Nov-2004";
+const time_t source_modtime = 1101096162;
+const char *source_modtime_str = "20:02 21-Nov-2004";
 const char *source_version_str = "20041121";
