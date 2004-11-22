96fc6cf1bc39afdf45cfa3152a7eff1d07f5f5e4
pfaedit <pfaedit>
2004-11-22 17:07:10 +0000

    Fonts without a FontName string did not go through ff's checks to validate the fontname.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1101140687;
-const char *source_modtime_str = "08:24 22-Nov-2004";
+const time_t source_modtime = 1101142774;
+const char *source_modtime_str = "08:59 22-Nov-2004";
 const char *source_version_str = "20041122";
