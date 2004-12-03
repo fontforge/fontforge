fe6e8ab4f15499012713ab5d260a369e9210023a
pfaedit <pfaedit>
2004-12-03 23:36:30 +0000

    Need to add a stub in case the debugger isn't available.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102115690;
-const char *source_modtime_str = "15:14 3-Dec-2004";
+const time_t source_modtime = 1102116579;
+const char *source_modtime_str = "15:29 3-Dec-2004";
 const char *source_version_str = "20041203";
