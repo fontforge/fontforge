1a66bc9c0811bbaa33dbc6912f6c25db2e39790f
George Williams <pfaedit@users.sourceforge.net>
2004-12-03 23:25:48 +0000

    Oops, typo in preserved table stuff.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102115029;
-const char *source_modtime_str = "15:03 3-Dec-2004";
+const time_t source_modtime = 1102115690;
+const char *source_modtime_str = "15:14 3-Dec-2004";
 const char *source_version_str = "20041203";
