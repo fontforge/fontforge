add1732edf6f97bf53eec5bb27d6bd59ab094f2e
pfaedit <pfaedit>
2004-12-03 23:09:23 +0000

    Oops. Windows users probably want the binary flag on preserved tables.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102112523;
-const char *source_modtime_str = "14:22 3-Dec-2004";
+const time_t source_modtime = 1102115029;
+const char *source_modtime_str = "15:03 3-Dec-2004";
 const char *source_version_str = "20041203";
