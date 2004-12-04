83892d589b8e13b3344addb44be07478f5044d61
George Williams <pfaedit@users.sourceforge.net>
2004-12-04 23:52:46 +0000

    I took a short cut to make the raster comparison easier. The result was that it sometimes refused to compare.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102116579;
-const char *source_modtime_str = "15:29 3-Dec-2004";
-const char *source_version_str = "20041203";
+const time_t source_modtime = 1102203914;
+const char *source_modtime_str = "15:45 4-Dec-2004";
+const char *source_version_str = "20041204";
