b7c96cb8a7297b7adcf02ad79d6d6a77a11356b6
George Williams <pfaedit@users.sourceforge.net>
2004-12-05 15:00:38 +0000

    Clean up a problem with the optimizer

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102203914;
-const char *source_modtime_str = "15:45 4-Dec-2004";
-const char *source_version_str = "20041204";
+const time_t source_modtime = 1102258360;
+const char *source_modtime_str = "06:52 5-Dec-2004";
+const char *source_version_str = "20041205";
