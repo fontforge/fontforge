63b95cbf14ff1c20acfe843234d69fdbc8c18cda
George Williams <pfaedit@users.sourceforge.net>
2004-12-05 22:39:38 +0000

    Some error messages seem to have crept back into remove overlap. Get rid of them.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102281375;
-const char *source_modtime_str = "13:16 5-Dec-2004";
+const time_t source_modtime = 1102285588;
+const char *source_modtime_str = "14:26 5-Dec-2004";
 const char *source_version_str = "20041205";
