8b57108654775b6daf919dc33b1a0e0546e131dd
George Williams <pfaedit@users.sourceforge.net>
2004-12-11 19:49:19 +0000

    In some places I assumed freetype gave me 2 phantom pts, in other cases 4. Now I check the library version # to determine which.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102779929;
-const char *source_modtime_str = "07:45 11-Dec-2004";
+const time_t source_modtime = 1102793906;
+const char *source_modtime_str = "11:38 11-Dec-2004";
 const char *source_version_str = "20041211";
