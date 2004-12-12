8bd8d8ebb029c81312111733568e8b7b7b317c44
pfaedit <pfaedit>
2004-12-12 18:41:45 +0000

    Got the supplementary PUA bounds wrong.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102875414;
-const char *source_modtime_str = "10:16 12-Dec-2004";
+const time_t source_modtime = 1102876621;
+const char *source_modtime_str = "10:37 12-Dec-2004";
 const char *source_version_str = "20041212";
