0febd6ba688f61cc32ee73937a5ce793da7af8d0
George Williams <pfaedit@users.sourceforge.net>
2004-12-12 21:31:58 +0000

    Redo backslash newline so it is ignored rather than a space.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102876621;
-const char *source_modtime_str = "10:37 12-Dec-2004";
+const time_t source_modtime = 1102886734;
+const char *source_modtime_str = "13:25 12-Dec-2004";
 const char *source_version_str = "20041212";
