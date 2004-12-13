b429c11fb4e364bd5d19281182e1772a346d37b1
George Williams <pfaedit@users.sourceforge.net>
2004-12-13 05:11:15 +0000

    Printing a font display at 140pt tried to print 4 glyphs per line when there was only room for 3

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1102913962;
-const char *source_modtime_str = "20:59 12-Dec-2004";
+const time_t source_modtime = 1102914169;
+const char *source_modtime_str = "21:02 12-Dec-2004";
 const char *source_version_str = "20041212";
