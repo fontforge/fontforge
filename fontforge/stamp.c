aaaeaecbd87b0579bebcea65a9bf46657dd59a8e
George Williams <pfaedit@users.sourceforge.net>
2004-12-15 05:17:28 +0000

    If a glyph was used for two encodings, and an opentype (cff, not ttf) font was generated, then the metrics tables would contain an extra entry and every glyph after the second encoding would have the wrong width.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1103047725;
-const char *source_modtime_str = "10:08 14-Dec-2004";
+const time_t source_modtime = 1103087163;
+const char *source_modtime_str = "21:06 14-Dec-2004";
 const char *source_version_str = "20041214";
