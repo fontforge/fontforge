aab29c48e1d377284ade57ee03ca6d0c625e6050
George Williams <pfaedit@users.sourceforge.net>
2004-12-16 16:14:54 +0000

    Fix some unprotected isalnum/isdigit calls

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1103087163;
-const char *source_modtime_str = "21:06 14-Dec-2004";
-const char *source_version_str = "20041214";
+const time_t source_modtime = 1103212665;
+const char *source_modtime_str = "07:57 16-Dec-2004";
+const char *source_version_str = "20041216";
