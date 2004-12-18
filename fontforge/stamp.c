39e13607b7f199bd044aa1b4d148c3a4f9079f58
George Williams <pfaedit@users.sourceforge.net>
2004-12-18 23:31:33 +0000

    A better way to find the intersection of two splines. Use an approach based on the gradient.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1103292712;
-const char *source_modtime_str = "06:11 17-Dec-2004";
-const char *source_version_str = "20041217";
+const time_t source_modtime = 1103407780;
+const char *source_modtime_str = "14:09 18-Dec-2004";
+const char *source_version_str = "20041218";
