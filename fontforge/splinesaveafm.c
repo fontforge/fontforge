b729d2d49ade3f72b452575b7c4de663e8e44f9b
George Williams <pfaedit@users.sourceforge.net>
2004-12-20 03:39:20 +0000

    Oh. Windows pfm files use Win Latin encoding, not the encoding in the pfb file. (actually there are several different possibilities for encoding, but as I don'tknow what any of them mean aside from win latin, I shall ignore the other cases).

--- fontforge/splinesaveafm.c
+++ fontforge/splinesaveafm.c
@@ -1367,22 +1367,105 @@ static void putlint(int val,FILE *pfm) {
     putc((val>>24)&0xff,pfm);
 }
 
+static const unsigned short unicode_from_win[] = {
+  0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007,
+  0x0008, 0x0009, 0x000a, 0x000b, 0x000c, 0x000d, 0x000e, 0x000f,
+  0x0010, 0x0011, 0x0012, 0x0013, 0x0014, 0x0015, 0x0016, 0x0017,
+  0x0018, 0x0019, 0x001a, 0x001b, 0x001c, 0x001d, 0x001e, 0x001f,
+  0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027,
+  0x0028, 0x0029, 0x002a, 0x002b, 0x002c, 0x002d, 0x002e, 0x002f,
+  0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037,
+  0x0038, 0x0039, 0x003a, 0x003b, 0x003c, 0x003d, 0x003e, 0x003f,
+  0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0x0045, 0x0046, 0x0047,
+  0x0048, 0x0049, 0x004a, 0x004b, 0x004c, 0x004d, 0x004e, 0x004f,
+  0x0050, 0x0051, 0x0052, 0x0053, 0x0054, 0x0055, 0x0056, 0x0057,
+  0x0058, 0x0059, 0x005a, 0x005b, 0x005c, 0x005d, 0x005e, 0x005f,
+  0x0060, 0x0061, 0x0062, 0x0063, 0x0064, 0x0065, 0x0066, 0x0067,
+  0x0068, 0x0069, 0x006a, 0x006b, 0x006c, 0x006d, 0x006e, 0x006f,
+  0x0070, 0x0071, 0x0072, 0x0073, 0x0074, 0x0075, 0x0076, 0x0077,
+  0x0078, 0x0079, 0x007a, 0x007b, 0x007c, 0x007d, 0x007e, 0x007f,
+  0x20ac, 0x0081, 0x201a, 0x0192, 0x201e, 0x2026, 0x2020, 0x2021,
+  0x02c6, 0x2030, 0x0160, 0x2039, 0x0152, 0x008d, 0x008e, 0x008f,
+  0x0090, 0x2018, 0x2019, 0x201c, 0x201d, 0x2022, 0x2013, 0x2014,
+  0x02dc, 0x2122, 0x0161, 0x203a, 0x0153, 0x009d, 0x009e, 0x0178,
+  0x00a0, 0x00a1, 0x00a2, 0x00a3, 0x00a4, 0x00a5, 0x00a6, 0x00a7,
+  0x00a8, 0x00a9, 0x00aa, 0x00ab, 0x00ac, 0x00ad, 0x00ae, 0x00af,
+  0x00b0, 0x00b1, 0x00b2, 0x00b3, 0x00b4, 0x00b5, 0x00b6, 0x00b7,
+  0x00b8, 0x00b9, 0x00ba, 0x00bb, 0x00bc, 0x00bd, 0x00be, 0x00bf,
+  0x00c0, 0x00c1, 0x00c2, 0x00c3, 0x00c4, 0x00c5, 0x00c6, 0x00c7,
+  0x00c8, 0x00c9, 0x00ca, 0x00cb, 0x00cc, 0x00cd, 0x00ce, 0x00cf,
+  0x00d0, 0x00d1, 0x00d2, 0x00d3, 0x00d4, 0x00d5, 0x00d6, 0x00d7,
+  0x00d8, 0x00d9, 0x00da, 0x00db, 0x00dc, 0x00dd, 0x00de, 0x00df,
+  0x00e0, 0x00e1, 0x00e2, 0x00e3, 0x00e4, 0x00e5, 0x00e6, 0x00e7,
+  0x00e8, 0x00e9, 0x00ea, 0x00eb, 0x00ec, 0x00ed, 0x00ee, 0x00ef,
+  0x00f0, 0x00f1, 0x00f2, 0x00f3, 0x00f4, 0x00f5, 0x00f6, 0x00f7,
+  0x00f8, 0x00f9, 0x00fa, 0x00fb, 0x00fc, 0x00fd, 0x00fe, 0x00ff
+};
+
+static void inwin(SplineFont *sf, int winmap[256]) {
+    int i, j;
+
+    memset(winmap,-1,sizeof(int[256]));
+    for ( i=0; i<sf->charcnt; ++i ) if ( sf->chars[i]!=NULL ) {
+	int unienc = sf->chars[i]->unicodeenc;
+	if ( unienc==-1 || unienc>0x3000 )
+    continue;
+	for ( j=255; j>=0; --j ) {
+	    if ( unicode_from_win[j]==unienc ) {
+		winmap[j] = i;
+	break;
+	    }
+	}
+    }
+}
+
+static int revwinmap(int winmap[256], int enc) {
+    int i;
+    for ( i=255; i>=0; --i )
+	if ( winmap[i]==enc )
+    break;
+return( i );
+}
+
 int PfmSplineFont(FILE *pfm, SplineFont *sf, int type0) {
     int caph=0, xh=0, ash=0, dsh=0, cnt=0, first=-1, samewid=-1, maxwid= -1, last=0, wid=0, ymax=0, ymin=0;
     int kerncnt=0, spacepos=0x20;
-    int i;
+    int i, ii;
     char *pt;
     KernPair *kp;
+    int winmap[256];
     /* my docs imply that pfm files can only handle 1byte fonts */
+    /* I think they must be output as if the font were encoded in winansi */
     /* Ah, but Adobe's technical note 5178.PFM.PDF gives the two byte format */
     long size, devname, facename, extmetrics, exttable, driverinfo, kernpairs, pos;
     DBounds b;
     int style;
+    int windows_encoding;
+
+    if ( sf->encoding_name->is_japanese ||
+	    (sf->cidmaster!=NULL && strnmatch(sf->cidmaster->ordering,"Japan",5)==0 ))
+	windows_encoding = 128;
+    else if ( sf->encoding_name->is_korean ||
+	    (sf->cidmaster!=NULL && strnmatch(sf->cidmaster->ordering,"Korea",5)==0 ))
+	windows_encoding = 129;
+    else if ( sf->encoding_name->is_tradchinese ||
+	    (sf->cidmaster!=NULL && strnmatch(sf->cidmaster->ordering,"CNS",3)==0 ))
+	windows_encoding = 136;
+    else if ( sf->encoding_name->is_simplechinese ||
+	    (sf->cidmaster!=NULL && strnmatch(sf->cidmaster->ordering,"GB",2)==0 ))
+	windows_encoding = 134;
+    else
+	windows_encoding = strmatch(sf->encoding_name->enc_name,"symbol")==0?2:0;
+    if ( windows_encoding==0 )
+	inwin(sf,winmap);
+    else
+	for ( i=0; i<256; ++i )
+	    winmap[i] = i;
 
     SFKernPrepare(sf,false);
-    for ( i=0; i<sf->charcnt; ++i ) if ( sf->chars[i]!=NULL ) {
+    for ( ii=0; ii<256; ++ii ) if ( (i=winmap[ii])!=-1 ) {
 	if ( sf->chars[i]!=NULL && sf->chars[i]->unicodeenc==' ' )
-	    spacepos = i;
+	    spacepos = ii;
 	if ( SCWorthOutputting(sf->chars[i]) ) {
 	    ++cnt;
 	    if ( sf->chars[i]->unicodeenc=='I' || sf->chars[i]->unicodeenc=='x' ||
@@ -1407,11 +1490,11 @@ int PfmSplineFont(FILE *pfm, SplineFont *sf, int type0) {
 	    if ( maxwid<sf->chars[i]->width )
 		maxwid = sf->chars[i]->width;
 	    if ( first==-1 )
-		first = i;
+		first = ii;
 	    wid += sf->chars[i]->width;
-	    last = i;
-	    if ( i<256 ) for ( kp=sf->chars[i]->kerns; kp!=NULL; kp = kp->next )
-		if ( kp->sc->enc<256 )
+	    last = ii;
+	    for ( kp=sf->chars[i]->kerns; kp!=NULL; kp = kp->next )
+		if ( revwinmap(winmap,kp->sc->enc)!=-1 )
 		    ++kerncnt;
 	}
     }
@@ -1447,20 +1530,7 @@ int PfmSplineFont(FILE *pfm, SplineFont *sf, int type0) {
     putc(0,pfm);			/* underline */
     putc(0,pfm);			/* strikeout */
     putlshort(sf->pfminfo.weight,pfm);	/* weight */
-    if ( sf->encoding_name->is_japanese ||
-	    (sf->cidmaster!=NULL && strnmatch(sf->cidmaster->ordering,"Japan",5)==0 ))
-	putc(128,pfm);
-    else if ( sf->encoding_name->is_korean ||
-	    (sf->cidmaster!=NULL && strnmatch(sf->cidmaster->ordering,"Korea",5)==0 ))
-	putc(129,pfm);
-    else if ( sf->encoding_name->is_tradchinese ||
-	    (sf->cidmaster!=NULL && strnmatch(sf->cidmaster->ordering,"CNS",3)==0 ))
-	putc(136,pfm);
-    else if ( sf->encoding_name->is_simplechinese ||
-	    (sf->cidmaster!=NULL && strnmatch(sf->cidmaster->ordering,"GB",2)==0 ))
-	putc(134,pfm);
-    else
-	putc(strmatch(sf->encoding_name->enc_name,"symbol")==0?2:0,pfm);	/* charset. I'm always saying windows roman (ANSI) or symbol because I don't know the other choices */
+    putc(windows_encoding,pfm);		/* Some indication of the encoding */
     putlshort(/*samewid<0?sf->ascent+sf->descent:samewid*/0,pfm);	/* width */
     putlshort(sf->ascent+sf->descent,pfm);	/* height */
     putc(sf->pfminfo.pfmfamily,pfm);	/* family */
@@ -1563,11 +1633,12 @@ int PfmSplineFont(FILE *pfm, SplineFont *sf, int type0) {
     fseek(pfm,exttable,SEEK_SET);
     putlint(pos,pfm);
     fseek(pfm,pos,SEEK_SET);
-    for ( i=first; i<=last; ++i )
-	if ( sf->chars[i]==NULL )
+    for ( ii=first; ii<=last; ++ii ) {
+	if ( (i=winmap[ii])==-1 || sf->chars[i]==NULL )
 	    putlshort(0,pfm);
 	else
 	    putlshort(sf->chars[i]->width,pfm);
+    }
 
 /* driver info */ /* == ps font name */
     pos = ftell(pfm);
@@ -1586,12 +1657,12 @@ int PfmSplineFont(FILE *pfm, SplineFont *sf, int type0) {
 	fseek(pfm,pos,SEEK_SET);
 	putlshort(kerncnt,pfm);		/* number of kerning pairs <= 512 */
 	kerncnt = 0;
-	for ( i=first; i<last; ++i ) if ( sf->chars[i]!=NULL ) {
+	for ( ii=first; ii<last; ++ii ) if ( (i=winmap[ii])!=-1 && sf->chars[i]!=NULL ) {
 	    if ( SCWorthOutputting(sf->chars[i]) ) {
 		for ( kp=sf->chars[i]->kerns; kp!=NULL; kp = kp->next )
-		    if ( kp->sc->enc<256 && kerncnt<512 ) {
-			putc(i,pfm);
-			putc(kp->sc->enc,pfm);
+		    if ( kerncnt<512 && revwinmap(winmap,kp->sc->enc)!=-1 ) {
+			putc(ii,pfm);
+			putc(revwinmap(winmap,kp->sc->enc),pfm);
 			putlshort(kp->off,pfm);
 			++kerncnt;
 		    }
@@ -1616,6 +1687,8 @@ int LoadKerningDataFromPfm(SplineFont *sf, char *filename) {
     FILE *file = fopen(filename,"rb");
     int widthbytes, kernoff, i, kerncnt;
     int ch1, ch2, offset;
+    int winmap[256];
+    int encoding;
 
     if ( file==NULL )
 return( 0 );
@@ -1636,7 +1709,7 @@ return( false );
     /* underline = */	getc(file);
     /* strikeout = */	getc(file);
     /* weight = */	getlshort(file);
-    /* encoding = */	getc(file);
+    encoding =		getc(file);
     /* width = */	getlshort(file);
     /* height = */	getlshort(file);
     /* family = */	getc(file);
@@ -1662,8 +1735,13 @@ return( false );
 	/* origin table = */	getlint(file);
 	kernoff =		getlint(file);
     }
-    if ( kernoff!=0 ) {
+    if ( kernoff!=0 && !feof(file) ) {
 	fseek(file,kernoff,SEEK_SET);
+	if ( encoding==0 )
+	    inwin(sf,winmap);
+	else
+	    for ( i=0; i<256; ++i )
+		winmap[i] = i;
 	kerncnt = getlshort(file);
 	/* Docs say at most 512 kerning pairs. Not worth posting a progress */
 	/*  dlg */
@@ -1671,7 +1749,9 @@ return( false );
 	    ch1 = getc(file);
 	    ch2 = getc(file);
 	    offset = (short) getlshort(file);
-	    KPInsert(sf->chars[ch1],sf->chars[ch2],offset,false);	/* No support for vertical kerning that I'm aware of */
+	    if ( !feof(file))
+		KPInsert(sf->chars[winmap[ch1]],sf->chars[winmap[ch2]],offset,false);
+		/* No support for vertical kerning that I'm aware of */
 	}
     }
     fclose(file);
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1103407780;
-const char *source_modtime_str = "14:09 18-Dec-2004";
-const char *source_version_str = "20041218";
+const time_t source_modtime = 1103513351;
+const char *source_modtime_str = "19:29 19-Dec-2004";
+const char *source_version_str = "20041219";
