7e6eaf63f5ee740c64a26aa987403da1ac950563
George Williams <pfaedit@users.sourceforge.net>
2004-12-21 21:28:32 +0000

    Improvements to Type1 reference output.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1103663152;
-const char *source_modtime_str = "13:05 21-Dec-2004";
+const time_t source_modtime = 1103664127;
+const char *source_modtime_str = "13:22 21-Dec-2004";
 const char *source_version_str = "20041221";
