02b22eecfb31cd127121c440ee3edf1a87200f47
pfaedit <pfaedit>
2004-12-23 01:30:06 +0000

    Yoshiki Hayashi points out that in SVG export I had a transform attribute which is not legal. Inserted a nested <g> to contain it instead. He also points out that my DTD pointer is obsolete, updated it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1103664127;
-const char *source_modtime_str = "13:22 21-Dec-2004";
-const char *source_version_str = "20041221";
+const time_t source_modtime = 1103764589;
+const char *source_modtime_str = "17:16 22-Dec-2004";
+const char *source_version_str = "20041222";
