faed93babc9456b8135b71da93ba5f6d0c705051
pfaedit <pfaedit>
2004-12-23 02:38:39 +0000

    More quibbles about ghost hints.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1103767898;
-const char *source_modtime_str = "18:11 22-Dec-2004";
+const time_t source_modtime = 1103769183;
+const char *source_modtime_str = "18:33 22-Dec-2004";
 const char *source_version_str = "20041222";
