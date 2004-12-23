9f7ed06a89ef41a44ee22940fe4bacc7674a8d63
George Williams <pfaedit@users.sourceforge.net>
2004-12-23 04:10:58 +0000

    Tweak the bounding box of exported svg chars a little. Remove the advance width from exported svg chars (didn't have it in exported eps files).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1103769183;
-const char *source_modtime_str = "18:33 22-Dec-2004";
+const time_t source_modtime = 1103774672;
+const char *source_modtime_str = "20:04 22-Dec-2004";
 const char *source_version_str = "20041222";
