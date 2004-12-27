98107c00ec46c2d8908b943292b5165b55407706
pfaedit <pfaedit>
2004-12-27 05:31:05 +0000

    Pasting a reference in multi-layer mode produced a very odd layer.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104033708;
-const char *source_modtime_str = "20:01 25-Dec-2004";
-const char *source_version_str = "20041225";
+const time_t source_modtime = 1104125186;
+const char *source_modtime_str = "21:26 26-Dec-2004";
+const char *source_version_str = "20041226";
