94c4a01f001352751be0ef42aac6bcb1c8c2db46
George Williams <pfaedit@users.sourceforge.net>
2004-12-28 05:47:01 +0000

    If a character were not in the current encoding (but was in the font) then the character could not be found in the font. This meant that the metrics view would not display it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104179861;
-const char *source_modtime_str = "12:37 27-Dec-2004";
+const time_t source_modtime = 1104212366;
+const char *source_modtime_str = "21:39 27-Dec-2004";
 const char *source_version_str = "20041227";
