941928eb5f1356872f0b7e3c7d41234ba3600f1f
pfaedit <pfaedit>
2004-12-31 15:22:35 +0000

    Fix some problems caused by unicode accent unification when building accented letters. Many chars which are said to use cedilla actually use something that looks like a comma. Similarly for caron and comma. Make U+0145 be buildable as an accented letter.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104463717;
-const char *source_modtime_str = "19:28 30-Dec-2004";
-const char *source_version_str = "20041230";
+const time_t source_modtime = 1104506209;
+const char *source_modtime_str = "07:16 31-Dec-2004";
+const char *source_version_str = "20041231";
