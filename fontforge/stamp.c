9103f58456d8acd78dee746cd7927fc0a310dd53
pfaedit <pfaedit>
2005-01-01 15:05:54 +0000

    Update copyright notices to 2004. Remove reference to PfaEdit (no more "used to be named PfaEdit" on startup).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104550012;
-const char *source_modtime_str = "19:26 31-Dec-2004";
-const char *source_version_str = "20041231";
+const time_t source_modtime = 1104590664;
+const char *source_modtime_str = "06:44 1-Jan-2005";
+const char *source_version_str = "20050101";
