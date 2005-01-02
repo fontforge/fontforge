a808061dd95dda49dd33114a0faa1c09a49083b7
pfaedit <pfaedit>
2005-01-02 19:42:45 +0000

    More pointtype fixes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104608213;
-const char *source_modtime_str = "11:36 1-Jan-2005";
-const char *source_version_str = "20050101";
+const time_t source_modtime = 1104694137;
+const char *source_modtime_str = "11:28 2-Jan-2005";
+const char *source_version_str = "20050102";
