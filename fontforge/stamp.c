e1ef5752b5a1feae9a3ad3192428f57c2ba9433d
George Williams <pfaedit@users.sourceforge.net>
2005-01-02 20:31:21 +0000

    Add a warning when two external contours with differing orientations intersect in remove overlap.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104694572;
-const char *source_modtime_str = "11:36 2-Jan-2005";
+const time_t source_modtime = 1104697261;
+const char *source_modtime_str = "12:21 2-Jan-2005";
 const char *source_version_str = "20050102";
