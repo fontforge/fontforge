f1ef9cc2c18f25410d1076d44681bd26d3c8f627
George Williams <pfaedit@users.sourceforge.net>
2005-01-03 02:54:16 +0000

    A simple idiom for producing dots in a type3 font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104697261;
-const char *source_modtime_str = "12:21 2-Jan-2005";
+const time_t source_modtime = 1104720480;
+const char *source_modtime_str = "18:48 2-Jan-2005";
 const char *source_version_str = "20050102";
