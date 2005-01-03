1dbc20460830402e598918345a9c8f1444c9f7a2
George Williams <pfaedit@users.sourceforge.net>
2005-01-03 14:39:27 +0000

    Was not prepared to deal with a gradiant whose length was 0 in the intersection improver of spline overlap.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104720480;
-const char *source_modtime_str = "18:48 2-Jan-2005";
-const char *source_version_str = "20050102";
+const time_t source_modtime = 1104762923;
+const char *source_modtime_str = "06:35 3-Jan-2005";
+const char *source_version_str = "20050103";
