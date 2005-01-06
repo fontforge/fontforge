1744c57839507eb13e23c9d9500017ba4793c0c0
George Williams <pfaedit@users.sourceforge.net>
2005-01-06 03:04:56 +0000

    Add a check for a null pointer (I can't duplicate the reported crash, but the check can't hurt).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1104979874;
-const char *source_modtime_str = "18:51 5-Jan-2005";
+const time_t source_modtime = 1104980538;
+const char *source_modtime_str = "19:02 5-Jan-2005";
 const char *source_version_str = "20050105";
