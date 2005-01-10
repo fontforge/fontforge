7a9539bac2d28f2a719182eda4f084c221bb16e9
George Williams <pfaedit@users.sourceforge.net>
2005-01-10 04:21:28 +0000

    And there was a similar confusion about writing device tables for anchors.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105329613;
-const char *source_modtime_str = "20:00 9-Jan-2005";
+const time_t source_modtime = 1105330683;
+const char *source_modtime_str = "20:18 9-Jan-2005";
 const char *source_version_str = "20050109";
