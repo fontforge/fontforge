65247d0c3bc2af9ae0a28b0490fa7a15c7ff6134
pfaedit <pfaedit>
2005-01-11 15:11:55 +0000

    Use freetype more efficiently in the anchor dlg.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105418112;
-const char *source_modtime_str = "20:35 10-Jan-2005";
-const char *source_version_str = "20050110";
+const time_t source_modtime = 1105455382;
+const char *source_modtime_str = "06:56 11-Jan-2005";
+const char *source_version_str = "20050111";
