809eef9053d35c8bb977c163418ed9724678f846
pfaedit <pfaedit>
2005-01-12 23:17:52 +0000

    If we have two glyphs, for example bullet and openbullet where the outer circle of openbullet matches the only circle of bullet, then that appears to be a match and ReplaceWithReference wants to replace it. But that would leave us with a reference and a counter-clockwise path... which makes no sense. So look for this very particular case, and don't allow it to happen. I don't see how to detect more complex cases without huge amounts of processing.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105571575;
-const char *source_modtime_str = "15:12 12-Jan-2005";
+const time_t source_modtime = 1105571641;
+const char *source_modtime_str = "15:14 12-Jan-2005";
 const char *source_version_str = "20050112";
