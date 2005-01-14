e4ba558f9dc1e01a0a5895b90000363d157c1d85
pfaedit <pfaedit>
2005-01-14 01:39:49 +0000

    Make the behavior of simplify in fontview match that in charview. FontView used to use the values specified in last simplify more command.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105663910;
-const char *source_modtime_str = "16:51 13-Jan-2005";
+const time_t source_modtime = 1105666568;
+const char *source_modtime_str = "17:36 13-Jan-2005";
 const char *source_version_str = "20050113";
