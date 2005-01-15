6fdca128c13eb9c1d68ff994cae216adbf8c4025
pfaedit <pfaedit>
2005-01-15 15:20:50 +0000

    Make the last values given in the Simplify More dlg be the default for the next simplify (usually).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105763142;
-const char *source_modtime_str = "20:25 14-Jan-2005";
-const char *source_version_str = "20050114";
+const time_t source_modtime = 1105801815;
+const char *source_modtime_str = "07:10 15-Jan-2005";
+const char *source_version_str = "20050115";
