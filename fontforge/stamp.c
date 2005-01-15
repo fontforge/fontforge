a4268f4c4b0c698164ca7fbb5bef46baf6314df7
pfaedit <pfaedit>
2005-01-15 23:30:45 +0000

    Simplify was too willing to throw out lines as "not-parallel". Many rounding errors when end points are forced to integers.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105801815;
-const char *source_modtime_str = "07:10 15-Jan-2005";
+const time_t source_modtime = 1105831394;
+const char *source_modtime_str = "15:23 15-Jan-2005";
 const char *source_version_str = "20050115";
