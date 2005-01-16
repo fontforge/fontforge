71e656c4f09ef75b6eeace075fd2a010622f6bcd
George Williams <pfaedit@users.sourceforge.net>
2005-01-16 14:29:51 +0000

    Typo in sfd routine to read size data.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105845521;
-const char *source_modtime_str = "19:18 15-Jan-2005";
-const char *source_version_str = "20050115";
+const time_t source_modtime = 1105884270;
+const char *source_modtime_str = "06:04 16-Jan-2005";
+const char *source_version_str = "20050116";
