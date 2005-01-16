c13aed3b847b7a65751ffa1a4a384e7d3e2d5c26
George Williams <pfaedit@users.sourceforge.net>
2005-01-16 19:01:56 +0000

    KANOU provides a patch to:   1) fixup glyph names if they aren't legal postscript name tokens   2) fix a crash when a non-latin1 bdf font is imported into a new font as background.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105884270;
-const char *source_modtime_str = "06:04 16-Jan-2005";
+const time_t source_modtime = 1105901932;
+const char *source_modtime_str = "10:58 16-Jan-2005";
 const char *source_version_str = "20050116";
