2aba21a789a092a36c3b1954c2a7b1940b3c91e0
George Williams <pfaedit@users.sourceforge.net>
2005-01-16 02:47:59 +0000

    Further attempts to improve spline approximation, try to avoid local minima.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105831394;
-const char *source_modtime_str = "15:23 15-Jan-2005";
+const time_t source_modtime = 1105843442;
+const char *source_modtime_str = "18:44 15-Jan-2005";
 const char *source_version_str = "20050115";
