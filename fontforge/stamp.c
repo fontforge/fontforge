77ec2b5e070af784a902edffe1e562a23ed8d40c
George Williams <pfaedit@users.sourceforge.net>
2005-01-17 03:30:15 +0000

    Make the arrows scroll when the current tool is the scroll hand.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105929044;
-const char *source_modtime_str = "18:30 16-Jan-2005";
+const time_t source_modtime = 1105932017;
+const char *source_modtime_str = "19:20 16-Jan-2005";
 const char *source_version_str = "20050116";
