93efb3b47856989b52ba21281211dbb92e2f5fef
George Williams <pfaedit@users.sourceforge.net>
2005-01-17 02:30:33 +0000

    Reencoding something to "Original" encoding wrote memory 1 beyond what was allocated.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1105902846;
-const char *source_modtime_str = "11:14 16-Jan-2005";
+const time_t source_modtime = 1105928189;
+const char *source_modtime_str = "18:16 16-Jan-2005";
 const char *source_version_str = "20050116";
