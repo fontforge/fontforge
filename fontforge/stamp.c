23469d6c8114fc1f355e579dd75f4ceed657e783
pfaedit <pfaedit>
2005-01-18 14:53:35 +0000

    Werner suggests adding a trailing newline to my type1 fonts. Sounds reasonable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1106014871;
-const char *source_modtime_str = "18:21 17-Jan-2005";
-const char *source_version_str = "20050117";
+const time_t source_modtime = 1106059899;
+const char *source_modtime_str = "06:51 18-Jan-2005";
+const char *source_version_str = "20050118";
