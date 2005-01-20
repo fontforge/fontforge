f03330ada776dfad05890a4364e1196c739f6cb8
George Williams <pfaedit@users.sourceforge.net>
2005-01-20 17:15:28 +0000

    Changing control points with get info had problems in quadratic splines.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1106200361;
-const char *source_modtime_str = "21:52 19-Jan-2005";
-const char *source_version_str = "20050119";
+const time_t source_modtime = 1106241042;
+const char *source_modtime_str = "09:10 20-Jan-2005";
+const char *source_version_str = "20050120";
