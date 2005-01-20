6b3fb51a24fdfcb2a8428587426d061686b79dbb
pfaedit <pfaedit>
2005-01-20 04:00:09 +0000

    Take out the warning I put in that was supposed to detect overlaps between contours of different orientations. It actually tested something else, closely related, because I couldn't figure out how to do the real test in an efficient manner, but the test I used returned too many false positives.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1106059899;
-const char *source_modtime_str = "06:51 18-Jan-2005";
-const char *source_version_str = "20050118";
+const time_t source_modtime = 1106193444;
+const char *source_modtime_str = "19:57 19-Jan-2005";
+const char *source_version_str = "20050119";
