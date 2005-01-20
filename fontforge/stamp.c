42a60142c7698f24b2c831ab6b1b9046fa24b4f7
pfaedit <pfaedit>
2005-01-20 19:02:50 +0000

    Don't apply transformations to glyphs which aren't worth outputting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1106241042;
-const char *source_modtime_str = "09:10 20-Jan-2005";
+const time_t source_modtime = 1106247455;
+const char *source_modtime_str = "10:57 20-Jan-2005";
 const char *source_version_str = "20050120";
