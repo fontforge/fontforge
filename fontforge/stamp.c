4f5895eac7a4629c60b45709075c47c6ab197403
pfaedit <pfaedit>
2005-01-21 02:04:18 +0000

    Change the arguments of the RoundToContext scripting command. They didn't make much sense before.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1106265761;
-const char *source_modtime_str = "16:02 20-Jan-2005";
+const time_t source_modtime = 1106271570;
+const char *source_modtime_str = "17:39 20-Jan-2005";
 const char *source_version_str = "20050120";
