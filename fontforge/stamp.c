1234f44ca347603bb9b795266baa031100424a45
George Williams <pfaedit@users.sourceforge.net>
2005-01-25 15:22:21 +0000

    Also didn't work without multilayer.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1106665662;
-const char *source_modtime_str = "07:07 25-Jan-2005";
+const time_t source_modtime = 1106666067;
+const char *source_modtime_str = "07:14 25-Jan-2005";
 const char *source_version_str = "20050125";
