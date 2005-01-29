e4d6a72d31633b2cf67208041b9227251413345b
George Williams <pfaedit@users.sourceforge.net>
2005-01-29 05:33:02 +0000

    Just in case iconv returns NULL on failure (instead of -1).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1106975641;
-const char *source_modtime_str = "21:14 28-Jan-2005";
+const time_t source_modtime = 1106976360;
+const char *source_modtime_str = "21:26 28-Jan-2005";
 const char *source_version_str = "20050128";
