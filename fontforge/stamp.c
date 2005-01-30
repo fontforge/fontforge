32d1766ffaacbc7278b22171cd82c3684f2d6a0f
pfaedit <pfaedit>
2005-01-30 15:52:29 +0000

    Patch by Ralf Stubner. Fonts without UniqueID had a bad syntax.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1106976360;
-const char *source_modtime_str = "21:26 28-Jan-2005";
-const char *source_version_str = "20050128";
+const time_t source_modtime = 1107100130;
+const char *source_modtime_str = "07:48 30-Jan-2005";
+const char *source_version_str = "20050130";
