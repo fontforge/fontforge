6b8a223581c92b74549c101146b85ece1cf4f8c8
pfaedit <pfaedit>
2005-02-02 20:51:35 +0000

    Oops. Code to handle loading relative filespecs for fonts didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1107136841;
-const char *source_modtime_str = "18:00 30-Jan-2005";
-const char *source_version_str = "20050130";
+const time_t source_modtime = 1107377255;
+const char *source_modtime_str = "12:47 2-Feb-2005";
+const char *source_version_str = "20050202";
