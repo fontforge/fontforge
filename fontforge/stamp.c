433f3947ff80f78a091cc0fff99fb579950303a4
pfaedit <pfaedit>
2005-02-03 16:29:03 +0000

    I was wrong. Solaris doesn't have a braindead iconv, some user has screwed up his libraries to the point they don't work. Add a work around and a small check.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1107404635;
-const char *source_modtime_str = "20:23 2-Feb-2005";
-const char *source_version_str = "20050202";
+const time_t source_modtime = 1107447178;
+const char *source_modtime_str = "08:12 3-Feb-2005";
+const char *source_version_str = "20050203";
