7c8be5163e399cb030ad372c4be1534db81d60f3
George Williams <pfaedit@users.sourceforge.net>
2005-02-06 22:51:25 +0000

    Missing an include file for the bitmap stuff (when compiling freetype.o against a static library).

--- fontforge/freetype.c
+++ fontforge/freetype.c
@@ -84,6 +84,7 @@ return( NULL );
 #else
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include FT_OUTLINE_H
 #include <unistd.h>
 #include <sys/mman.h>
 
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1107447178;
-const char *source_modtime_str = "08:12 3-Feb-2005";
-const char *source_version_str = "20050203";
+const time_t source_modtime = 1107730084;
+const char *source_modtime_str = "14:48 6-Feb-2005";
+const char *source_version_str = "20050206";
