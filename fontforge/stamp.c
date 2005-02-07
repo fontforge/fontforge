0ed8126c1c6e095f4946b318c8c8f98aaf2cfa54
George Williams <pfaedit@users.sourceforge.net>
2005-02-07 21:10:21 +0000

    There's yet another ASCII map in UNICODE, at 0xe0000 for specifying language names.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1107730084;
-const char *source_modtime_str = "14:48 6-Feb-2005";
-const char *source_version_str = "20050206";
+const time_t source_modtime = 1107810371;
+const char *source_modtime_str = "13:06 7-Feb-2005";
+const char *source_version_str = "20050207";
