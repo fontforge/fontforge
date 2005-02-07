bdc74c623ce7aaaca96c483a2e39167ccdcf4489
George Williams <pfaedit@users.sourceforge.net>
2005-02-07 22:22:18 +0000

    There is no guarantee that iconv(3) supports all the encodings we need to read a ttf file properly. Be prepared to handled a NULL return from FindOrMakeEncoding when reading 'cmap'.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1107810371;
-const char *source_modtime_str = "13:06 7-Feb-2005";
+const time_t source_modtime = 1107814227;
+const char *source_modtime_str = "14:10 7-Feb-2005";
 const char *source_version_str = "20050207";
