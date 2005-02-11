8013f0bf72c1f7af93471faf3ae1059510e22859
George Williams <pfaedit@users.sourceforge.net>
2005-02-11 05:00:27 +0000

    The font type detector would fail to notice a svg file as such if it began with the byte-order character (didn't matter much because we'd usually work it out from the extension).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1108046457;
-const char *source_modtime_str = "06:40 10-Feb-2005";
+const time_t source_modtime = 1108090621;
+const char *source_modtime_str = "18:57 10-Feb-2005";
 const char *source_version_str = "20050210";
