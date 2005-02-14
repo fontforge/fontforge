96d9b5676d1b3296a81be0a0afe842d61970b0a6
George Williams <pfaedit@users.sourceforge.net>
2005-02-14 03:33:28 +0000

    ff had a bug when outputting otf contextual ligatures.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1108270525;
-const char *source_modtime_str = "20:55 12-Feb-2005";
-const char *source_version_str = "20050212";
+const time_t source_modtime = 1108328349;
+const char *source_modtime_str = "12:59 13-Feb-2005";
+const char *source_version_str = "20050213";
