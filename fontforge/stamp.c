ddc86c031a957ec74842e987956812f55154bf62
George Williams <pfaedit@users.sourceforge.net>
2005-02-17 02:19:04 +0000

    Some tweaks to autohint to make it work better with metafont (work in progress).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1108440058;
-const char *source_modtime_str = "20:00 14-Feb-2005";
-const char *source_version_str = "20050214";
+const time_t source_modtime = 1108606665;
+const char *source_modtime_str = "18:17 16-Feb-2005";
+const char *source_version_str = "20050216";
