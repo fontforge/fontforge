c39775d034cc4633be152e26b95343407e100f93
pfaedit <pfaedit>
2005-02-17 19:10:05 +0000

    More parsing protection.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1108618644;
-const char *source_modtime_str = "21:37 16-Feb-2005";
-const char *source_version_str = "20050216";
+const time_t source_modtime = 1108665378;
+const char *source_modtime_str = "10:36 17-Feb-2005";
+const char *source_version_str = "20050217";
