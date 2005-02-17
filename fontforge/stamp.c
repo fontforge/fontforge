1a8e505a95305ca2d07d3f5ef2eae43451189955
George Williams <pfaedit@users.sourceforge.net>
2005-02-17 03:32:43 +0000

    Allow the scripting Export() command to take a format spec to give user control over filename.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1108607855;
-const char *source_modtime_str = "18:37 16-Feb-2005";
+const time_t source_modtime = 1108610415;
+const char *source_modtime_str = "19:20 16-Feb-2005";
 const char *source_version_str = "20050216";
