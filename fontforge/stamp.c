7ded6cde6412b934caa84373b28c1b037677d638
pfaedit <pfaedit>
2005-02-18 20:49:34 +0000

    KANOU provides a MakeLine command.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1108705124;
-const char *source_modtime_str = "21:38 17-Feb-2005";
-const char *source_version_str = "20050217";
+const time_t source_modtime = 1108759400;
+const char *source_modtime_str = "12:43 18-Feb-2005";
+const char *source_version_str = "20050218";
