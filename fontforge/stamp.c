3281d50d9901dddeea0878f910b7c563c347e620
pfaedit <pfaedit>
2005-02-18 05:10:13 +0000

    Oops, if TYPE3 was not enabled there was a flow of control through a function which did not return a value.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1108702146;
-const char *source_modtime_str = "20:49 17-Feb-2005";
+const time_t source_modtime = 1108703294;
+const char *source_modtime_str = "21:08 17-Feb-2005";
 const char *source_version_str = "20050217";
