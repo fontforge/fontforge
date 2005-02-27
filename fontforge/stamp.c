0a1390e20848545bb87006b381d5f7f6bb33bc17
pfaedit <pfaedit>
2005-02-27 03:12:13 +0000

    KANOU points out that the stroked file import glyph command only worked if multilayer set.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1109473390;
-const char *source_modtime_str = "19:03 26-Feb-2005";
+const time_t source_modtime = 1109473789;
+const char *source_modtime_str = "19:09 26-Feb-2005";
 const char *source_version_str = "20050226";
