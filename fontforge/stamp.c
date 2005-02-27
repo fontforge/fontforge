202238c1a7e3a438bd22b0403a10821770c93b3c
George Williams <pfaedit@users.sourceforge.net>
2005-02-27 03:10:45 +0000

    Add a bitmap preview for eps output.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1109376563;
-const char *source_modtime_str = "16:09 25-Feb-2005";
-const char *source_version_str = "20050225";
+const time_t source_modtime = 1109473390;
+const char *source_modtime_str = "19:03 26-Feb-2005";
+const char *source_version_str = "20050226";
