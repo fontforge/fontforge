1b5bc78877067d2d245195a2624398c22f990e10
Werner Lemberg <wl@gnu.org>
2005-02-28 14:12:20 +0000

    s/[vV]ender/[vV]endor/

--- fontforge/splinefont.h
+++ fontforge/splinefont.h
@@ -893,7 +893,7 @@ typedef struct splinechar {
 
 enum ttfnames { ttf_copyright=0, ttf_family, ttf_subfamily, ttf_uniqueid,
     ttf_fullname, ttf_version, ttf_postscriptname, ttf_trademark,
-    ttf_manufacturer, ttf_designer, ttf_descriptor, ttf_venderurl,
+    ttf_manufacturer, ttf_designer, ttf_descriptor, ttf_vendorurl,
     ttf_designerurl, ttf_license, ttf_licenseurl, ttf_idontknow, ttf_preffamilyname,
     ttf_prefmodifiers, ttf_compatfull, ttf_sampletext, ttf_namemax };
 struct ttflangname {
