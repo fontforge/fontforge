98af4f4b7e826b3c4d1c3070378107b01b0fb229
pfaedit <pfaedit>
2005-03-04 21:55:39 +0000

    Warn if a font contains no useful glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1109971267;
-const char *source_modtime_str = "13:21 4-Mar-2005";
+const time_t source_modtime = 1109972822;
+const char *source_modtime_str = "13:47 4-Mar-2005";
 const char *source_version_str = "20050304";
