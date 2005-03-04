092d6db83ea0421a41850b6a32d7748802e9daf7
George Williams <pfaedit@users.sourceforge.net>
2005-03-04 02:50:11 +0000

    Someone corrected the spelling of "Vendor" in many of the nomen files, but not in the English one, nor in the place that used it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1109903852;
-const char *source_modtime_str = "18:37 3-Mar-2005";
+const time_t source_modtime = 1109904460;
+const char *source_modtime_str = "18:47 3-Mar-2005";
 const char *source_version_str = "20050303";
