864715780a905275e9860bd02b684b43f2c26eb8
pfaedit <pfaedit>
2005-03-04 02:05:17 +0000

    When generating a font from a script using the default flag setting, we would always generate a tfm/pfm/afm file. In some cases we'd do it twice. Also uninit reference in tfm generation.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1109558736;
-const char *source_modtime_str = "18:45 27-Feb-2005";
-const char *source_version_str = "20050227";
+const time_t source_modtime = 1109901714;
+const char *source_modtime_str = "18:01 3-Mar-2005";
+const char *source_version_str = "20050303";
