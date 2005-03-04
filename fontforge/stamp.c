d28ca6fb19a6d2e2981fe9b60020020d664b8f65
George Williams <pfaedit@users.sourceforge.net>
2005-03-04 21:28:39 +0000

    The Options dlg (of Generate fonts) behaved oddly with respect to Apple mode & OpenType mode. This patch should make things clearer. Also there was a typo and the setting for PS Glyph Name option was incorrectly controled by the truetype hints flag.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1109952605;
-const char *source_modtime_str = "08:10 4-Mar-2005";
+const time_t source_modtime = 1109971267;
+const char *source_modtime_str = "13:21 4-Mar-2005";
 const char *source_version_str = "20050304";
