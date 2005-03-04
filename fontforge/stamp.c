382927b9f9e3b2e487532ac5a0f647f117aa4f61
pfaedit <pfaedit>
2005-03-04 20:48:57 +0000

    When given a glyph with conflicting hints where the first contour of that glyph needs no hints, then in type2 output the first hintmask operator would be omitted, which means that the location and width of the first vertical hint would be used as the position for the first point. This meant that the glyph would be oddly offset from where it should have been.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1109904460;
-const char *source_modtime_str = "18:47 3-Mar-2005";
-const char *source_version_str = "20050303";
+const time_t source_modtime = 1109952605;
+const char *source_modtime_str = "08:10 4-Mar-2005";
+const char *source_version_str = "20050304";
