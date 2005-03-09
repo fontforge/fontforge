3c88195396d693e38201dc097886f09a59769038
pfaedit <pfaedit>
2005-03-09 00:53:05 +0000

    deleting a class within a kerning by classes object caused a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1110320519;
-const char *source_modtime_str = "14:21 8-Mar-2005";
+const time_t source_modtime = 1110329335;
+const char *source_modtime_str = "16:48 8-Mar-2005";
 const char *source_version_str = "20050308";
