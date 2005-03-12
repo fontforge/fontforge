d0effc2b8fcb1a985304e1709f4aaffa93184970
George Williams <pfaedit@users.sourceforge.net>
2005-03-12 04:42:57 +0000

    Add scripting access to standard math functions (trig, exp, log, pow, sqrt, isnan, finite)

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1110549770;
-const char *source_modtime_str = "06:02 11-Mar-2005";
+const time_t source_modtime = 1110601866;
+const char *source_modtime_str = "20:31 11-Mar-2005";
 const char *source_version_str = "20050311";
