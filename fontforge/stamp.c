bab94ecbf5103aa65d7f098e3ec5d84e547d0ae1
George Williams <pfaedit@users.sourceforge.net>
2005-03-18 23:37:50 +0000

    Tavmjong Bah points out that now that scripting supports reals I shoud return real preference items as real values.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1111185154;
-const char *source_modtime_str = "14:32 18-Mar-2005";
+const time_t source_modtime = 1111188895;
+const char *source_modtime_str = "15:34 18-Mar-2005";
 const char *source_version_str = "20050318";
