53f15c840b68583a0c62f7723e7b682c000e9d01
pfaedit <pfaedit>
2005-03-24 16:53:52 +0000

    When reencoding with Adobe Standard, or some other encoding where glyph names are more important that unicode code points, make sure that we force the glyph names to be correct after doing the encoding.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1111676551;
-const char *source_modtime_str = "07:02 24-Mar-2005";
+const time_t source_modtime = 1111683126;
+const char *source_modtime_str = "08:52 24-Mar-2005";
 const char *source_version_str = "20050324";
