c69c0001d6c4201c2230db545b9b45b93729ad40
George Williams <pfaedit@users.sourceforge.net>
2005-03-31 03:52:10 +0000

    Make the gloss window a bit bigger.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1112240998;
-const char *source_modtime_str = "19:49 30-Mar-2005";
+const time_t source_modtime = 1112241011;
+const char *source_modtime_str = "19:50 30-Mar-2005";
 const char *source_version_str = "20050330";
