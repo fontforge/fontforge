4d216bfa3b0113b3874a66fd24fb3ef7229f8d8b
pfaedit <pfaedit>
2005-03-31 03:51:14 +0000

    Recent change to give stack window a scrollbar caused us to crash if displaying stack window when executing the "end of glyph" psuedo-return instruction.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1112240877;
-const char *source_modtime_str = "19:47 30-Mar-2005";
+const time_t source_modtime = 1112240998;
+const char *source_modtime_str = "19:49 30-Mar-2005";
 const char *source_version_str = "20050330";
