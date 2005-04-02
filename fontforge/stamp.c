0e7f5dc49d1360abd6cd0201f8f3e15d824b60c4
pfaedit <pfaedit>
2005-04-02 04:00:51 +0000

    The point info dlg was quite unusable if you typed something which was not a valid number.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1112393279;
-const char *source_modtime_str = "14:07 1-Apr-2005";
+const time_t source_modtime = 1112414183;
+const char *source_modtime_str = "19:56 1-Apr-2005";
 const char *source_version_str = "20050401";
