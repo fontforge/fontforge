24e186a5e97801842f11d63d768377a265e84238
pfaedit <pfaedit>
2005-04-10 02:27:04 +0000

    Support '\r' and '\r\n' as continuation lines after backslash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1112825970;
-const char *source_modtime_str = "15:19 6-Apr-2005";
-const char *source_version_str = "20050406";
+const time_t source_modtime = 1113099689;
+const char *source_modtime_str = "19:21 9-Apr-2005";
+const char *source_version_str = "20050409";
