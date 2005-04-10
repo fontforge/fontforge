101ee4a0ab15e730e5b2bb8ab5aef10411d5acee
pfaedit <pfaedit>
2005-04-10 02:48:37 +0000

    Add a select fewer command to allow users to turn off selection on certain chars.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1113100058;
-const char *source_modtime_str = "19:27 9-Apr-2005";
+const time_t source_modtime = 1113101190;
+const char *source_modtime_str = "19:46 9-Apr-2005";
 const char *source_version_str = "20050409";
