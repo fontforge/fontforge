b3aff5c1e3e12c8752247edb358cb2596cdcef6b
George Williams <pfaedit@users.sourceforge.net>
2005-04-11 13:18:11 +0000

    Previous scripting crlf patch didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1113104368;
-const char *source_modtime_str = "20:39 9-Apr-2005";
-const char *source_version_str = "20050409";
+const time_t source_modtime = 1113225442;
+const char *source_modtime_str = "06:17 11-Apr-2005";
+const char *source_version_str = "20050411";
