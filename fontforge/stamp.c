0536c45aa95bfc97adbf1530f6d6fb57d5e4022d
pfaedit <pfaedit>
2005-04-14 03:48:32 +0000

    The various "FindBounds" routines didn't work for stroked fonts (nor take into account type3 fonts in some cases).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1113340729;
-const char *source_modtime_str = "14:18 12-Apr-2005";
-const char *source_version_str = "20050412";
+const time_t source_modtime = 1113450415;
+const char *source_modtime_str = "20:46 13-Apr-2005";
+const char *source_version_str = "20050413";
