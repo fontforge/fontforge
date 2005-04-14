3445dbe29e028e9117cb6740eb016dbe00bd3f2b
George Williams <pfaedit@users.sourceforge.net>
2005-04-14 22:53:45 +0000

    When using freetype to rasterize in the fontview, don't do an automatic close of open paths (allow user to see broken paths.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1113518664;
-const char *source_modtime_str = "15:44 14-Apr-2005";
+const time_t source_modtime = 1113519136;
+const char *source_modtime_str = "15:52 14-Apr-2005";
 const char *source_version_str = "20050414";
