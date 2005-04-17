61ebccda986da9cad1c9c799ab0185f366860006
pfaedit <pfaedit>
2005-04-17 04:56:20 +0000

    Problems parsing bare cff cid-keyed fonts (attempted to give them an encoding and treat as cid.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
 const time_t source_modtime = 1113606770;
-const char *source_modtime_str = "16:12 15-Apr-2005";
-const char *source_version_str = "20050415";
+const char *source_modtime_str = "06:36 16-Apr-2005";
+const char *source_version_str = "20050416";
