0f212542349cc4965201dc79aebcb8500f54823a
George Williams <pfaedit@users.sourceforge.net>
2005-05-03 15:26:36 +0000

    Bad argument type checking on the AddAccent scripting command.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1114731634;
-const char *source_modtime_str = "16:40 28-Apr-2005";
+const time_t source_modtime = 1114731841;
+const char *source_modtime_str = "16:44 28-Apr-2005";
 const char *source_version_str = "20050428";
