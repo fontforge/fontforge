e7cdd3e6f2f0261810cddeff5729081138c0847e
pfaedit <pfaedit>
2005-05-04 20:06:03 +0000

    When interpolating to quadratic fonts where the two designs didn't match then the various possible errors gave us bad splines: 	Different numbers of points on the paths meant that the control points at the end (after we reached the end of the contour in one design but not the other) didn't match. 	Different designs meant that interpolating a line (with no control points) to a curved spline again produced nasties.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1115063340;
-const char *source_modtime_str = "12:49 2-May-2005";
-const char *source_version_str = "20050502";
+const time_t source_modtime = 1115236954;
+const char *source_modtime_str = "13:02 4-May-2005";
+const char *source_version_str = "20050504";
