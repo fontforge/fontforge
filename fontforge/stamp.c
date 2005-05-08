77aedcded4782b9dc40a4685b1ba693efd0e895f
George Williams <pfaedit@users.sourceforge.net>
2005-05-08 13:35:28 +0000

    Remove a NaN that occurred when moving quadratic splines.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1115384450;
-const char *source_modtime_str = "06:00 6-May-2005";
-const char *source_version_str = "20050506";
+const time_t source_modtime = 1115559286;
+const char *source_modtime_str = "06:34 8-May-2005";
+const char *source_version_str = "20050508";
