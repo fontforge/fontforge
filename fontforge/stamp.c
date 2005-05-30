54b339bba7e851d2ad3f1ad8d3b31b21a8938934
George Williams <pfaedit@users.sourceforge.net>
2005-05-30 21:33:54 +0000

    Won-kyu Park points out that ff's internal utf7 parser (deep in the guts of sfd.c) only parses utf7 strings as produced by ff. He took a utf7 string produced by python and inserted it and ff could not read it. He provides a patch to fix the problem.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1117487884;
-const char *source_modtime_str = "14:18 30-May-2005";
+const time_t source_modtime = 1117488664;
+const char *source_modtime_str = "14:31 30-May-2005";
 const char *source_version_str = "20050530";
