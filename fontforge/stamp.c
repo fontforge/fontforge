f7885d065db9979b75c3df35924824c960e0ab93
George Williams <pfaedit@users.sourceforge.net>
2005-05-30 21:18:59 +0000

    If we got an invalid second order spline, let's fix it up into some vaguely reasonable form, so the poor user doesn't keep getting errors.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1117486265;
-const char *source_modtime_str = "13:51 30-May-2005";
+const time_t source_modtime = 1117487884;
+const char *source_modtime_str = "14:18 30-May-2005";
 const char *source_version_str = "20050530";
