9a9cba05315a90619a9ca81388eb43156e9170af
George Williams <pfaedit@users.sourceforge.net>
2005-05-30 19:42:30 +0000

    Make page Up/Down work in Show ATT

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1117478816;
-const char *source_modtime_str = "11:46 30-May-2005";
+const time_t source_modtime = 1117482107;
+const char *source_modtime_str = "12:41 30-May-2005";
 const char *source_version_str = "20050530";
