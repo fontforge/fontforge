2fea2cea151aba15c48d37ef3fb390bde5544259
George Williams <pfaedit@users.sourceforge.net>
2005-05-30 18:48:06 +0000

    Sometimes the simplify algorithem fails to converge.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1115612355;
-const char *source_modtime_str = "21:19 8-May-2005";
-const char *source_version_str = "20050508";
+const time_t source_modtime = 1117478816;
+const char *source_modtime_str = "11:46 30-May-2005";
+const char *source_version_str = "20050530";
