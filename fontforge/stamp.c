29916a53ed3e9b302b205be8fa3362c663096213
George Williams <pfaedit@users.sourceforge.net>
2005-05-30 20:10:15 +0000

    The test in show att that all components of a substitution existed failed to handle a trailing space.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1117482107;
-const char *source_modtime_str = "12:41 30-May-2005";
+const time_t source_modtime = 1117483702;
+const char *source_modtime_str = "13:08 30-May-2005";
 const char *source_version_str = "20050530";
