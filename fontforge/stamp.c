fac9ae13210c6b512e6ea9ace8198efcbfe45a66
pfaedit <pfaedit>
2005-06-08 19:11:58 +0000

    Kerning (and Anchor positioning) didn't work when printing to pdf.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1117488664;
-const char *source_modtime_str = "14:31 30-May-2005";
-const char *source_version_str = "20050530";
+const time_t source_modtime = 1118257672;
+const char *source_modtime_str = "12:07 8-Jun-2005";
+const char *source_version_str = "20050608";
