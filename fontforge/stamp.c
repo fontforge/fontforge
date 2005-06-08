a880981c08017d668796bb6a3cd17af596bb7292
pfaedit <pfaedit>
2005-06-08 19:44:15 +0000

    Problem with cubic to quadratic spline approximation. Fixed by a more careful comparison between original and resulting spline.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1118257672;
-const char *source_modtime_str = "12:07 8-Jun-2005";
+const time_t source_modtime = 1118259735;
+const char *source_modtime_str = "12:42 8-Jun-2005";
 const char *source_version_str = "20050608";
