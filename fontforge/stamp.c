02a4a28f7f72437723560c75a5304281b80d1f89
George Williams <pfaedit@users.sourceforge.net>
2005-06-13 17:59:42 +0000

    AddAccent behaved differently when passed a glyphname and a unicode code point. In the first case it used the glyph specified. In the second it performed an arcane search which would not always use the specified glyph (left over from the days when it was important to use a glyph in Adobe Standard so you could do a seac).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1118681867;
-const char *source_modtime_str = "09:57 13-Jun-2005";
+const time_t source_modtime = 1118685427;
+const char *source_modtime_str = "10:57 13-Jun-2005";
 const char *source_version_str = "20050613";
