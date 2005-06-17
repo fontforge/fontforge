4044d434205ff4bd699c10b6084d5305e9ee35f8
George Williams <pfaedit@users.sourceforge.net>
2005-06-17 21:15:34 +0000

    vhea & vmtx tables were generally wrong in otf fonts (unless the last full vmetric happened to be the last full hmetric). Also fix problems with cid hmtx output.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1118960310;
-const char *source_modtime_str = "15:18 16-Jun-2005";
-const char *source_version_str = "20050616";
+const time_t source_modtime = 1119042592;
+const char *source_modtime_str = "14:09 17-Jun-2005";
+const char *source_version_str = "20050617";
