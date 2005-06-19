611328e9bd87a922c2193072b94f0e3542a36c07
pfaedit <pfaedit>
2005-06-19 02:30:48 +0000

    Add some code to protect against badly generated bitmap strikes in sfnts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119042592;
-const char *source_modtime_str = "14:09 17-Jun-2005";
-const char *source_version_str = "20050617";
+const time_t source_modtime = 1119147849;
+const char *source_modtime_str = "19:24 18-Jun-2005";
+const char *source_version_str = "20050618";
