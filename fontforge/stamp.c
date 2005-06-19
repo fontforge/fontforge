a9dbf1474fab816a3e082850abb5829acceb8060
George Williams <pfaedit@users.sourceforge.net>
2005-06-19 20:56:58 +0000

    The default (notdef) glyph generated by default for palm fonts was a little wonky.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119212677;
-const char *source_modtime_str = "13:24 19-Jun-2005";
+const time_t source_modtime = 1119214250;
+const char *source_modtime_str = "13:50 19-Jun-2005";
 const char *source_version_str = "20050619";
