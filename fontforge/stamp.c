630e4acba4e7358a3ca9a2bae2821d3ed0f412d9
George Williams <pfaedit@users.sourceforge.net>
2005-06-20 17:29:06 +0000

    We didn't get DSC page numbers properly when printing a CID keyed font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119214250;
-const char *source_modtime_str = "13:50 19-Jun-2005";
-const char *source_version_str = "20050619";
+const time_t source_modtime = 1119288319;
+const char *source_modtime_str = "10:25 20-Jun-2005";
+const char *source_version_str = "20050620";
