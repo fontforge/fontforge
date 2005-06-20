1bda2a6894a0508e9a784fa1e9df323903504138
George Williams <pfaedit@users.sourceforge.net>
2005-06-20 19:59:28 +0000

    When I fixed pdf output for t42 cid fonts I broke it for ps t42 cid fonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119288319;
-const char *source_modtime_str = "10:25 20-Jun-2005";
+const time_t source_modtime = 1119297291;
+const char *source_modtime_str = "12:54 20-Jun-2005";
 const char *source_version_str = "20050620";
