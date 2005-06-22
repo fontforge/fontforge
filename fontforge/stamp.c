ad2822e0dd5408e83cb8ed556b1d5cb54233ce5f
George Williams <pfaedit@users.sourceforge.net>
2005-06-22 21:09:44 +0000

    There is a misleading statement in Adobe's Technical Note: 5176.cff.pdf: 	A Private DICT is required, but may be specified as having a 	length of 0 if there are no non-default values to be stored. I took this litterally and inserted an empty private dict into the top dict of a cid cff font. Acrobat was ok with it, but ghostscript got annoyed. If I remove the required private entry then gs is happy.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119302295;
-const char *source_modtime_str = "14:18 21-Jun-2005";
-const char *source_version_str = "20050621";
+const time_t source_modtime = 1119470963;
+const char *source_modtime_str = "13:09 22-Jun-2005";
+const char *source_version_str = "20050622";
