d1540ef27586674b70a57cfaabaa01be5d0388ca
pfaedit <pfaedit>
2005-06-27 16:51:44 +0000

    Fix more crashes with user defined encodings.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119888891;
-const char *source_modtime_str = "09:14 27-Jun-2005";
+const time_t source_modtime = 1119889585;
+const char *source_modtime_str = "09:26 27-Jun-2005";
 const char *source_version_str = "20050627";
