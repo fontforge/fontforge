a4a6a473afc2f05dfea17b1b47efe07ef0b51e22
George Williams <pfaedit@users.sourceforge.net>
2005-06-27 14:26:43 +0000

    Only half of the backslash newline patch got installed and windows newlines didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119845534;
-const char *source_modtime_str = "21:12 26-Jun-2005";
-const char *source_version_str = "20050626";
+const time_t source_modtime = 1119882286;
+const char *source_modtime_str = "07:24 27-Jun-2005";
+const char *source_version_str = "20050627";
