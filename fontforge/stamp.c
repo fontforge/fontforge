910705c5a141307441db112f17e5d20628606c39
George Williams <pfaedit@users.sourceforge.net>
2005-06-27 17:11:28 +0000

    Another place where glyphmax wasn't set propperly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119891555;
-const char *source_modtime_str = "09:59 27-Jun-2005";
+const time_t source_modtime = 1119892104;
+const char *source_modtime_str = "10:08 27-Jun-2005";
 const char *source_version_str = "20050627";
