6080a1624ebaf824099c2bf7995484d21f21cf87
pfaedit <pfaedit>
2005-06-28 04:27:35 +0000

    Oops, the unique checkbox didn't move when the dlg was resized.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1119930801;
-const char *source_modtime_str = "20:53 27-Jun-2005";
+const time_t source_modtime = 1119932834;
+const char *source_modtime_str = "21:27 27-Jun-2005";
 const char *source_version_str = "20050627";
