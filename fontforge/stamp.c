4ce7caec7c25957deac85839a48b6b47f959ce54
George Williams <pfaedit@users.sourceforge.net>
2005-06-30 17:32:28 +0000

    Werner points out that my afm files still claim to be made by pfaedit. Oops.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1120102222;
-const char *source_modtime_str = "20:30 29-Jun-2005";
-const char *source_version_str = "20050629";
+const time_t source_modtime = 1120152584;
+const char *source_modtime_str = "10:29 30-Jun-2005";
+const char *source_version_str = "20050630";
