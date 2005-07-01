3eca060cea9d1449be536eb4aa56bd3d302bfd10
George Williams <pfaedit@users.sourceforge.net>
2005-07-01 00:01:50 +0000

    Add a check in case sfd files contain unreasonable values for pixelsize, etc.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1120171904;
-const char *source_modtime_str = "15:51 30-Jun-2005";
+const time_t source_modtime = 1120174920;
+const char *source_modtime_str = "16:42 30-Jun-2005";
 const char *source_version_str = "20050630";
