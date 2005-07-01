48a7f519beb2d8e51b0da4e7a6dd5617e556f43c
George Williams <pfaedit@users.sourceforge.net>
2005-07-01 00:28:43 +0000

    Once again starting a browser on windows is broken.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1120176146;
-const char *source_modtime_str = "17:02 30-Jun-2005";
+const time_t source_modtime = 1120177020;
+const char *source_modtime_str = "17:17 30-Jun-2005";
 const char *source_version_str = "20050630";
