1168d45d6a5a38067202622e11a838453a75f0b5
George Williams <pfaedit@users.sourceforge.net>
2005-07-03 22:49:52 +0000

    Werner wants an Invert Selection command.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1120427785;
-const char *source_modtime_str = "14:56 3-Jul-2005";
+const time_t source_modtime = 1120430683;
+const char *source_modtime_str = "15:44 3-Jul-2005";
 const char *source_version_str = "20050703";
