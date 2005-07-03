139348aa8bb1b4f7883af8cd1c4124e93c48bb64
George Williams <pfaedit@users.sourceforge.net>
2005-07-03 22:00:08 +0000

    Deleting glyph classes from the kerning by classes dlg didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1120427431;
-const char *source_modtime_str = "14:50 3-Jul-2005";
+const time_t source_modtime = 1120427785;
+const char *source_modtime_str = "14:56 3-Jul-2005";
 const char *source_version_str = "20050703";
