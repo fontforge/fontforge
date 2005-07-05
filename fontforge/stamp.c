d4bb197572ce27a6ab4c8f9682ea8468ce158f18
pfaedit <pfaedit>
2005-07-05 22:29:52 +0000

    Try harder to guard against absurd cases in simplify.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1120432581;
-const char *source_modtime_str = "16:16 3-Jul-2005";
-const char *source_version_str = "20050703";
+const time_t source_modtime = 1120602399;
+const char *source_modtime_str = "15:26 5-Jul-2005";
+const char *source_version_str = "20050705";
