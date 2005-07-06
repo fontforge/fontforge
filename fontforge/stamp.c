bd69f4c82c5e5609f72a988b3a57c14fbe9b2ec2
pfaedit <pfaedit>
2005-07-06 18:48:12 +0000

    Add scripting routines to detach glyphs from the encoding and to remove them from the font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1120602399;
-const char *source_modtime_str = "15:26 5-Jul-2005";
-const char *source_version_str = "20050705";
+const time_t source_modtime = 1120675562;
+const char *source_modtime_str = "11:46 6-Jul-2005";
+const char *source_version_str = "20050706";
