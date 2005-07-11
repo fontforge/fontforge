8971ccaa7337a99bb76fd682ea96410398e43194
pfaedit <pfaedit>
2005-07-11 05:21:15 +0000

    When we had: a glyph which contained refs where that glyph was itself used as a reference in another glyph and all glyphs (except the last) lived in subroutines then we got multiple declarations of hints and the middle glyph was translated from where it should have been.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121040865;
-const char *source_modtime_str = "17:14 10-Jul-2005";
+const time_t source_modtime = 1121058725;
+const char *source_modtime_str = "22:12 10-Jul-2005";
 const char *source_version_str = "20050710";
