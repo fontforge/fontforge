5a1e74300b18e2f07864e78064667977ca10715c
pfaedit <pfaedit>
2005-07-11 21:11:22 +0000

    Werner suggests indicating multi-layer in the version string.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121114659;
-const char *source_modtime_str = "13:44 11-Jul-2005";
+const time_t source_modtime = 1121116275;
+const char *source_modtime_str = "14:11 11-Jul-2005";
 const char *source_version_str = "20050711";
