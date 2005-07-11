5e12a7e8bb6b7ad2b9310d66688657b206dc0d46
George Williams <pfaedit@users.sourceforge.net>
2005-07-11 00:19:15 +0000

    When changing a list we always used to snap back to the beginning of the list (instead of continuing to show the line changed).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121039351;
-const char *source_modtime_str = "16:49 10-Jul-2005";
+const time_t source_modtime = 1121040865;
+const char *source_modtime_str = "17:14 10-Jul-2005";
 const char *source_version_str = "20050710";
