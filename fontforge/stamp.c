9c984f082b860f7cf65793398e290e038bf7c959
George Williams <pfaedit@users.sourceforge.net>
2005-07-12 22:43:22 +0000

    Bad loop bounds check.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121139749;
-const char *source_modtime_str = "20:42 11-Jul-2005";
-const char *source_version_str = "20050711";
+const time_t source_modtime = 1121208170;
+const char *source_modtime_str = "15:42 12-Jul-2005";
+const char *source_version_str = "20050712";
