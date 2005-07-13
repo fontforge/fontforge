89075a862eb3e9dc0ca9ab33594e396cbda6b2c2
pfaedit <pfaedit>
2005-07-13 18:56:14 +0000

    Fix yet another crash caused be referencing by encoding rather than gid.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121279909;
-const char *source_modtime_str = "11:38 13-Jul-2005";
+const time_t source_modtime = 1121280732;
+const char *source_modtime_str = "11:52 13-Jul-2005";
 const char *source_version_str = "20050713";
