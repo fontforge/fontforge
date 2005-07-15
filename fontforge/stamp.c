9d7f6ee100f8bba82884b5d663db8fb979febed3
George Williams <pfaedit@users.sourceforge.net>
2005-07-15 20:59:21 +0000

    Context chain dlg had problems with empty patterns (no terminal NUL in empty str ing used to represent them).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121296704;
-const char *source_modtime_str = "16:18 13-Jul-2005";
-const char *source_version_str = "20050713";
+const time_t source_modtime = 1121460649;
+const char *source_modtime_str = "13:50 15-Jul-2005";
+const char *source_version_str = "20050715";
