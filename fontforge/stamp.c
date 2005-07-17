9359f95d7ba9e7ead3f459d6882a6e4a94fa4260
pfaedit <pfaedit>
2005-07-17 20:17:34 +0000

    Selection of kern classes didn't take into account scrolling.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121629840;
-const char *source_modtime_str = "12:50 17-Jul-2005";
+const time_t source_modtime = 1121631176;
+const char *source_modtime_str = "13:12 17-Jul-2005";
 const char *source_version_str = "20050717";
