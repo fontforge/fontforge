eef1d3479166c1a832bf3c9efbec4fc3382ced1e
George Williams <pfaedit@users.sourceforge.net>
2005-07-18 22:36:12 +0000

    A bit of extranious code snuck in to the kern class dlg in the selection work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121633383;
-const char *source_modtime_str = "13:49 17-Jul-2005";
-const char *source_version_str = "20050717";
+const time_t source_modtime = 1121725644;
+const char *source_modtime_str = "15:27 18-Jul-2005";
+const char *source_version_str = "20050718";
