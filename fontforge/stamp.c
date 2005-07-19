e2073ea564bf8178bc6aa151105133d911159ab0
George Williams <pfaedit@users.sourceforge.net>
2005-07-19 13:41:01 +0000

    When saving multiple, make sure the afm,tfm files don't contain kerns to glyphs outside of the subfonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121739765;
-const char *source_modtime_str = "19:22 18-Jul-2005";
-const char *source_version_str = "20050718";
+const time_t source_modtime = 1121779566;
+const char *source_modtime_str = "06:26 19-Jul-2005";
+const char *source_version_str = "20050719";
