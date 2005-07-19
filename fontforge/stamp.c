ec8f941c6a62da5e21aeabaf241ef3e47d9d025b
George Williams <pfaedit@users.sourceforge.net>
2005-07-19 22:44:58 +0000

    Still problems with detecting where a subroutine begins.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121808153;
-const char *source_modtime_str = "14:22 19-Jul-2005";
+const time_t source_modtime = 1121810871;
+const char *source_modtime_str = "15:07 19-Jul-2005";
 const char *source_version_str = "20050719";
