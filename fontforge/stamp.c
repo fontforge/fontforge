81d55f7a428aae4fea22935acea6e89fe59e5666
pfaedit <pfaedit>
2005-07-19 21:23:05 +0000

    Generating a statemachine from a FPST had problems introduced by the encoding change.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121779566;
-const char *source_modtime_str = "06:26 19-Jul-2005";
+const time_t source_modtime = 1121807876;
+const char *source_modtime_str = "14:17 19-Jul-2005";
 const char *source_version_str = "20050719";
