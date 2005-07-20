55ac65331d784f2fa1ba77d701ee06bd6ded752e
George Williams <pfaedit@users.sourceforge.net>
2005-07-20 20:09:19 +0000

    Add some new scripting commands to access the TeX per-glyph fields

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121887315;
-const char *source_modtime_str = "12:21 20-Jul-2005";
+const time_t source_modtime = 1121889868;
+const char *source_modtime_str = "13:04 20-Jul-2005";
 const char *source_version_str = "20050720";
