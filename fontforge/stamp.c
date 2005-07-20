03fb3b9bbc14edec9b4f7166d435c7dae02d27e7
George Williams <pfaedit@users.sourceforge.net>
2005-07-20 21:21:20 +0000

    GetFontNames returned an uninitialized value on bad ttf files.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121889868;
-const char *source_modtime_str = "13:04 20-Jul-2005";
+const time_t source_modtime = 1121894397;
+const char *source_modtime_str = "14:19 20-Jul-2005";
 const char *source_version_str = "20050720";
