36a15069ab63007010e4c05ca6da489ca1a3b96d
George Williams <pfaedit@users.sourceforge.net>
2005-07-20 03:05:58 +0000

    I was attaching script/langs to lookups. That wasn't good enough. Each sub-table and have it's own set, so attach script/langs to subtables as well. Still a bug in that sub-tables could be referenced through severl extension sub-tables or directly and I don't unravel that complexity until too late.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121828277;
-const char *source_modtime_str = "19:57 19-Jul-2005";
+const time_t source_modtime = 1121828319;
+const char *source_modtime_str = "19:58 19-Jul-2005";
 const char *source_version_str = "20050719";
