b0617390a15cca24d69c4eaf277c9a08f6acca28
pfaedit <pfaedit>
2005-07-20 03:01:55 +0000

    ATM which handles some kerning for otf fonts for Word, does not handle 'kern' features where the feature contains more than one lookup. So whenever we have a feature with multiple lookups try to compress them into one lookup with multiple sub-tables.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121827822;
-const char *source_modtime_str = "19:50 19-Jul-2005";
+const time_t source_modtime = 1121828277;
+const char *source_modtime_str = "19:57 19-Jul-2005";
 const char *source_version_str = "20050719";
