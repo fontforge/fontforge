c430c0a3ace09769a3cb29053cf613e316d44b4b
pfaedit <pfaedit>
2005-07-22 04:39:45 +0000

    Revert Werner's request for special handling of CJK glyphs in tfm files.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1121986189;
-const char *source_modtime_str = "15:49 21-Jul-2005";
+const time_t source_modtime = 1122007141;
+const char *source_modtime_str = "21:39 21-Jul-2005";
 const char *source_version_str = "20050721";
