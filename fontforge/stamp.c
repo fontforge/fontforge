388fa2653e2e05df9a6ba8af63afccf3e389f9f6
pfaedit <pfaedit>
2005-07-24 22:33:44 +0000

    We were getting annoying "No outline" marks for ".notdef".

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122243499;
-const char *source_modtime_str = "15:18 24-Jul-2005";
+const time_t source_modtime = 1122244261;
+const char *source_modtime_str = "15:31 24-Jul-2005";
 const char *source_version_str = "20050724";
