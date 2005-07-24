1676a5cbeedc9ce4556e993fa44c5d11725d805b
George Williams <pfaedit@users.sourceforge.net>
2005-07-24 21:36:30 +0000

    Detach Glyphs was dis/enabled in the menu exactly the wrong times.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122234171;
-const char *source_modtime_str = "12:42 24-Jul-2005";
+const time_t source_modtime = 1122240823;
+const char *source_modtime_str = "14:33 24-Jul-2005";
 const char *source_version_str = "20050724";
