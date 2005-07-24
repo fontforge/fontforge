39566e4bf24454eda3d51dbef4d0ed1b0fe919a7
George Williams <pfaedit@users.sourceforge.net>
2005-07-24 22:11:06 +0000

    FF still didn't let the user create two ligatures for one glyph.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122242050;
-const char *source_modtime_str = "14:54 24-Jul-2005";
+const time_t source_modtime = 1122242951;
+const char *source_modtime_str = "15:09 24-Jul-2005";
 const char *source_version_str = "20050724";
