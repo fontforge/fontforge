dd27d16fc8cd4577ebf0c529890b74c4d087d109
pfaedit <pfaedit>
2005-07-24 17:55:31 +0000

    Add a "TypeOf" routine to scripting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122127398;
-const char *source_modtime_str = "07:03 23-Jul-2005";
-const char *source_version_str = "20050723";
+const time_t source_modtime = 1122227639;
+const char *source_modtime_str = "10:53 24-Jul-2005";
+const char *source_version_str = "20050724";
