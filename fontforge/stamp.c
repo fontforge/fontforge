fc47fde7f7e79f58396f0a0e24950dd80e58e751
George Williams <pfaedit@users.sourceforge.net>
2005-07-27 20:19:31 +0000

    Fix another encoding related crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122436314;
-const char *source_modtime_str = "20:51 26-Jul-2005";
-const char *source_version_str = "20050726";
+const time_t source_modtime = 1122495332;
+const char *source_modtime_str = "13:15 27-Jul-2005";
+const char *source_version_str = "20050727";
