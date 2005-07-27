cce7b717c5fd0aba7b2483ceeaff3131c3a5070c
pfaedit <pfaedit>
2005-07-27 01:06:20 +0000

    :-) Generate Multiple gave incorrect filenames if: 	1) The %s was in the middle of the filename (without extension) 	2) The sub-font was identified by a single character.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122416223;
-const char *source_modtime_str = "15:17 26-Jul-2005";
+const time_t source_modtime = 1122426045;
+const char *source_modtime_str = "18:00 26-Jul-2005";
 const char *source_version_str = "20050726";
