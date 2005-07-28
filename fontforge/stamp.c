80f3a41359158696f1964ddd3ec3dac4136a9818
pfaedit <pfaedit>
2005-07-28 17:00:12 +0000

    If we create a bitmap strike in an empty font, then change the fontview to look at the strike.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122569786;
-const char *source_modtime_str = "09:56 28-Jul-2005";
+const time_t source_modtime = 1122570006;
+const char *source_modtime_str = "10:00 28-Jul-2005";
 const char *source_version_str = "20050728";
