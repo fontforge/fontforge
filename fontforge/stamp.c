d08dabeb223ba7fb9aa0780f56e7fa4faa102017
pfaedit <pfaedit>
2005-07-28 16:43:42 +0000

    FontView Set Width (and friends) produced a garbage value for the default width if the font had no glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122496287;
-const char *source_modtime_str = "13:31 27-Jul-2005";
-const char *source_version_str = "20050727";
+const time_t source_modtime = 1122569009;
+const char *source_modtime_str = "09:43 28-Jul-2005";
+const char *source_version_str = "20050728";
