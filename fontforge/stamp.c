c574313f5fc182ed903a9fa5fda6ad040d79c2d5
George Williams <pfaedit@users.sourceforge.net>
2005-07-30 19:51:26 +0000

    Scripting didn't have a way to generate a font with neither Apple nor OpenType tables. Add one.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122670798;
-const char *source_modtime_str = "13:59 29-Jul-2005";
-const char *source_version_str = "20050729";
+const time_t source_modtime = 1122752615;
+const char *source_modtime_str = "12:43 30-Jul-2005";
+const char *source_version_str = "20050730";
