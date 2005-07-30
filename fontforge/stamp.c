bac202c6234f9cff5426f9019c45148c8367ebbd
pfaedit <pfaedit>
2005-07-30 20:14:37 +0000

    If neither Apple mode nor OpenType mode were set, then the presence of a kerning class would force a new Apple-style 'kern' table. And it should not have. Instead decompose all kerning classes into kern pairs as we do for AFM files and generate an old style table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122752615;
-const char *source_modtime_str = "12:43 30-Jul-2005";
+const time_t source_modtime = 1122754085;
+const char *source_modtime_str = "13:08 30-Jul-2005";
 const char *source_version_str = "20050730";
