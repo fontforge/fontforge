eaf02bd7d61553fcbe803543273b6a97e8f5b0c0
George Williams <pfaedit@users.sourceforge.net>
2005-07-31 19:23:10 +0000

    Make class be the default format for contextual/chaining substitutions rather than coverage tables.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122835173;
-const char *source_modtime_str = "11:39 31-Jul-2005";
+const time_t source_modtime = 1122837551;
+const char *source_modtime_str = "12:19 31-Jul-2005";
 const char *source_version_str = "20050731";
