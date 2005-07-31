4cf7e634db4fb6ed45a3d1d6d42caf433d47d132
George Williams <pfaedit@users.sourceforge.net>
2005-07-31 17:34:30 +0000

    Indicate which gadget gets focus in kern class dlg when creating a new class.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122825205;
-const char *source_modtime_str = "08:53 31-Jul-2005";
+const time_t source_modtime = 1122831179;
+const char *source_modtime_str = "10:32 31-Jul-2005";
 const char *source_version_str = "20050731";
