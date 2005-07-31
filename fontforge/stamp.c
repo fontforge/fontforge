737b0c25668c53226ca45f1d2230c553a38780fd
George Williams <pfaedit@users.sourceforge.net>
2005-07-31 17:57:46 +0000

    Reference to bad memory when creating a popup in combinations list.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122831179;
-const char *source_modtime_str = "10:32 31-Jul-2005";
+const time_t source_modtime = 1122832190;
+const char *source_modtime_str = "10:49 31-Jul-2005";
 const char *source_version_str = "20050731";
