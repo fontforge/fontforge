dbeda743d32bcd73de8e4c822537e1c1f8c88f4a
pfaedit <pfaedit>
2005-08-01 14:12:42 +0000

    If one loaded a Unicode style encoding and then cancelled the dlg which asked for a name for that encoding, then ff would crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122847134;
-const char *source_modtime_str = "14:58 31-Jul-2005";
+const time_t source_modtime = 1122866795;
+const char *source_modtime_str = "20:26 31-Jul-2005";
 const char *source_version_str = "20050731";
