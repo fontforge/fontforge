58817f903a0113495b158b617d2cb1412be0e6d3
George Williams <pfaedit@users.sourceforge.net>
2005-08-01 19:42:24 +0000

    Enter some new language/locale codes for ms.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122907394;
-const char *source_modtime_str = "07:43 1-Aug-2005";
+const time_t source_modtime = 1122925127;
+const char *source_modtime_str = "12:38 1-Aug-2005";
 const char *source_version_str = "20050801";
