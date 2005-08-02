91dabc6305525990efecc22eb7fd03ded602cea3
George Williams <pfaedit@users.sourceforge.net>
2005-08-02 21:50:19 +0000

    I was failing to detect ASCII in the code which generated the OS/2 coderanges. Which meant none of the latin code ranges were set.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1122931116;
-const char *source_modtime_str = "14:18 1-Aug-2005";
-const char *source_version_str = "20050801";
+const time_t source_modtime = 1123016935;
+const char *source_modtime_str = "14:08 2-Aug-2005";
+const char *source_version_str = "20050802";
