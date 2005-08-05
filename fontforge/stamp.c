7facd71eca65fb227b34b26f700a035a26b1ebad
pfaedit <pfaedit>
2005-08-05 20:07:50 +0000

    FindExistingSlot should understand altuni.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1123271191;
-const char *source_modtime_str = "12:46 5-Aug-2005";
+const time_t source_modtime = 1123272370;
+const char *source_modtime_str = "13:06 5-Aug-2005";
 const char *source_version_str = "20050805";
