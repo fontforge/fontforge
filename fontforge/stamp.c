bbc9de87c512d2b86999f9cc99b4b0589a9aa35b
pfaedit <pfaedit>
2005-08-06 03:07:43 +0000

    Preserve altinate unicode values for multiply encoded glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1123281071;
-const char *source_modtime_str = "15:31 5-Aug-2005";
+const time_t source_modtime = 1123297114;
+const char *source_modtime_str = "19:58 5-Aug-2005";
 const char *source_version_str = "20050805";
