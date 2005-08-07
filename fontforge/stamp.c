57440356b5574f6933a4f8780441d01d733c11a4
pfaedit <pfaedit>
2005-08-07 15:25:07 +0000

    Using the X clipboard to paste a glyph into a glyph that contained stuff caused a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1123426735;
-const char *source_modtime_str = "07:58 7-Aug-2005";
+const time_t source_modtime = 1123428156;
+const char *source_modtime_str = "08:22 7-Aug-2005";
 const char *source_version_str = "20050807";
