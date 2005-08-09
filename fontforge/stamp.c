6c9b962604b4248326ae3211b6098478031df5b6
pfaedit <pfaedit>
2005-08-09 15:07:37 +0000

    Oops, I need to set the xim bit for ttf name text fields.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1123561667;
-const char *source_modtime_str = "21:27 8-Aug-2005";
-const char *source_version_str = "20050808";
+const time_t source_modtime = 1123599901;
+const char *source_modtime_str = "08:05 9-Aug-2005";
+const char *source_version_str = "20050809";
