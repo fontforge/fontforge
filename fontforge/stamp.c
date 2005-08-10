d1cfba08021162318065efcb22ab2d8bfc2f275d
George Williams <pfaedit@users.sourceforge.net>
2005-08-10 12:25:16 +0000

    Didn't set the length of the format4 'cmap' sub-table, so the offset to the form at12 (32bit unicode) table was wrong.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1123599901;
-const char *source_modtime_str = "08:05 9-Aug-2005";
+const time_t source_modtime = 1123635047;
+const char *source_modtime_str = "17:50 9-Aug-2005";
 const char *source_version_str = "20050809";
