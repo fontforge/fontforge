51df78e900df784dbc37dad25cfa78d8c945785c
George Williams <pfaedit@users.sourceforge.net>
2005-08-10 12:55:47 +0000

    ff would crash if it had two windows open on the same font and one got reencoded.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1123635047;
-const char *source_modtime_str = "17:50 9-Aug-2005";
+const time_t source_modtime = 1123640575;
+const char *source_modtime_str = "19:22 9-Aug-2005";
 const char *source_version_str = "20050809";
