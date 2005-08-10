9068551ed194db0a00420bc3e559ee7be9d65ad7
George Williams <pfaedit@users.sourceforge.net>
2005-08-10 13:04:46 +0000

    The code for creating an 8/16 cmap subtable didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1123640575;
-const char *source_modtime_str = "19:22 9-Aug-2005";
+const time_t source_modtime = 1123643720;
+const char *source_modtime_str = "20:15 9-Aug-2005";
 const char *source_version_str = "20050809";
