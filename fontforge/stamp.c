f056ed4daa2c16cdc195409920bdede8288b5949
George Williams <pfaedit@users.sourceforge.net>
2005-08-13 20:15:50 +0000

    I find the prev control point hard to distinguish against the grey of a back ground image. Make it a bit darker.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1123789682;
-const char *source_modtime_str = "12:48 11-Aug-2005";
-const char *source_version_str = "20050811";
+const time_t source_modtime = 1123963933;
+const char *source_modtime_str = "13:12 13-Aug-2005";
+const char *source_version_str = "20050813";
