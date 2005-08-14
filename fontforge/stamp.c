e7a46936e8d130257daf0bc97adf7b58c4c9a388
pfaedit <pfaedit>
2005-08-14 20:42:01 +0000

    Only coalesce multiple subtables into one lookup of the feature is kern.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124051650;
-const char *source_modtime_str = "13:34 14-Aug-2005";
-const char *source_version_str = "20050814";
+const time_t source_modtime = 1123771499;
+const char *source_modtime_str = "07:44 11-Aug-2005";
+const char *source_version_str = "20050811";
