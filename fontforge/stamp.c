85dde659b84252111d8eb9e63cd2b9ec89ef77ce
pfaedit <pfaedit>
2005-08-14 20:37:41 +0000

    When generating nested contextual lookups, ff did not reset the lookup count to zero between gpos/gsub. So if there were contextuals in both gpos & gsub, ff got confused when doing gsub.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124050693;
-const char *source_modtime_str = "13:18 14-Aug-2005";
+const time_t source_modtime = 1124051650;
+const char *source_modtime_str = "13:34 14-Aug-2005";
 const char *source_version_str = "20050814";
