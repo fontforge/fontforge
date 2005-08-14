8c96123930a21f092257d356448326b16af4a869
George Williams <pfaedit@users.sourceforge.net>
2005-08-14 20:21:14 +0000

    ff would occasionally generate a format 4 cmap subtable which mapped glyph 0xffff to an encoding. This is an undocumented ability of the cmap table, so the encodings should work, but ff should not be generating them. This patch gets rid of them.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124048970;
-const char *source_modtime_str = "12:49 14-Aug-2005";
+const time_t source_modtime = 1124050693;
+const char *source_modtime_str = "13:18 14-Aug-2005";
 const char *source_version_str = "20050814";
