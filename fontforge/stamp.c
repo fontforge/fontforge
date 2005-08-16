7a29f1f54eacd28595f9ecbd5f2d58cde01e45dd
pfaedit <pfaedit>
2005-08-16 15:02:15 +0000

    When we read a pfb file, and seac-ed glyph will contain no hints. So we should mark it as in need of autohinting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124114600;
-const char *source_modtime_str = "07:03 15-Aug-2005";
-const char *source_version_str = "20050815";
+const time_t source_modtime = 1124204174;
+const char *source_modtime_str = "07:56 16-Aug-2005";
+const char *source_version_str = "20050816";
