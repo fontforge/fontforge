d744495f39630842f2607b07e7a6e53e9dd2c36f
pfaedit <pfaedit>
2005-08-17 20:12:38 +0000

    When rasterizing a multilayer font we would sometimes get warnings from the stroker about how the stroke self-intersected. Get rid of those warnings.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124308806;
-const char *source_modtime_str = "13:00 17-Aug-2005";
+const time_t source_modtime = 1124309196;
+const char *source_modtime_str = "13:06 17-Aug-2005";
 const char *source_version_str = "20050817";
