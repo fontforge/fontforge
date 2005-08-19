0ffe1a9878781d40a83bb8aed5f1b73aed3466cb
pfaedit <pfaedit>
2005-08-19 19:52:54 +0000

    ff couldn't undo changing the lig carets (if it weren't compiled for multilayer)

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124424943;
-const char *source_modtime_str = "21:15 18-Aug-2005";
-const char *source_version_str = "20050818";
+const time_t source_modtime = 1124479054;
+const char *source_modtime_str = "12:17 19-Aug-2005";
+const char *source_version_str = "20050819";
