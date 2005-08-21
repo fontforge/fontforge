0fa4c35e6a58308aa0443760144f41c409efa32c
pfaedit <pfaedit>
2005-08-21 16:00:39 +0000

    Add a configure option to allow users to generate mac only fonts (those with version number 'true').

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124569909;
-const char *source_modtime_str = "13:31 20-Aug-2005";
-const char *source_version_str = "20050820";
+const time_t source_modtime = 1124638025;
+const char *source_modtime_str = "08:27 21-Aug-2005";
+const char *source_version_str = "20050821";
