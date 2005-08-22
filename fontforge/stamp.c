2903e771619bdfc286b18b17f5efc2ddb61b1acb
George Williams <pfaedit@users.sourceforge.net>
2005-08-22 03:02:30 +0000

    Fixed an uninitialized variable in the new type1 reference code.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124677676;
-const char *source_modtime_str = "19:27 21-Aug-2005";
+const time_t source_modtime = 1124679762;
+const char *source_modtime_str = "20:02 21-Aug-2005";
 const char *source_version_str = "20050821";
