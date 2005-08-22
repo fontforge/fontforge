5acfd21c10c979914ddc759f08273a62d69b7948
George Williams <pfaedit@users.sourceforge.net>
2005-08-22 03:00:25 +0000

    If a font contained mac feature setting names, then when ff generated it, ff wou ld produce duplicate entries in the 'name' table for these guys. One for the nam e in the font itself, and one for the name ff thought that feature setting shoul d have.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124641958;
-const char *source_modtime_str = "09:32 21-Aug-2005";
+const time_t source_modtime = 1124677676;
+const char *source_modtime_str = "19:27 21-Aug-2005";
 const char *source_version_str = "20050821";
