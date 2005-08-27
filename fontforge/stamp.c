41d9795ae345b56d8d18a6edba70e10147621db1
pfaedit <pfaedit>
2005-08-27 03:49:38 +0000

    Do the same thing for create hint that we did for add hint.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1124978293;
-const char *source_modtime_str = "06:58 25-Aug-2005";
-const char *source_version_str = "20050825";
+const time_t source_modtime = 1125114156;
+const char *source_modtime_str = "20:42 26-Aug-2005";
+const char *source_version_str = "20050826";
