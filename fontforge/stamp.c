68e14dcc8dd078879d4b738876528618005e06b5
George Williams <pfaedit@users.sourceforge.net>
2005-08-27 14:17:13 +0000

    Cleanup pasting references from one font to another (gave some very strange results when pasting refs with no unicode encodings).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1125114156;
-const char *source_modtime_str = "20:42 26-Aug-2005";
+const time_t source_modtime = 1125115981;
+const char *source_modtime_str = "21:13 26-Aug-2005";
 const char *source_version_str = "20050826";
