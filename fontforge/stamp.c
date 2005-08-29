d24ad52f3700462801d55836979d783d28796535
pfaedit <pfaedit>
2005-08-29 20:47:05 +0000

    Importing an eps file (or the X clipboard) into a glyph made use of an uninitialzed variable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1125171014;
-const char *source_modtime_str = "12:30 27-Aug-2005";
-const char *source_version_str = "20050827";
+const time_t source_modtime = 1125348443;
+const char *source_modtime_str = "13:47 29-Aug-2005";
+const char *source_version_str = "20050829";
