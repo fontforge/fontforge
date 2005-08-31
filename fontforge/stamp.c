338df4f64114ebbf12cc98d0010c7a532079a9c8
pfaedit <pfaedit>
2005-08-31 17:55:56 +0000

    Oops, fix a typo in problems.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1125510612;
-const char *source_modtime_str = "10:50 31-Aug-2005";
+const time_t source_modtime = 1125510906;
+const char *source_modtime_str = "10:55 31-Aug-2005";
 const char *source_version_str = "20050831";
