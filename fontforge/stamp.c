834d2e3b26992167a356292f63745a228ccfe70d
pfaedit <pfaedit>
2005-09-02 19:53:51 +0000

    Scripting SetCharCnt command has been broken since the encoding change.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1125587340;
-const char *source_modtime_str = "08:09 1-Sep-2005";
-const char *source_version_str = "20050901";
+const time_t source_modtime = 1125690065;
+const char *source_modtime_str = "12:41 2-Sep-2005";
+const char *source_version_str = "20050902";
