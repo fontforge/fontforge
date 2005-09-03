d9ca84343b81ea2999b8fec9f47ccb66c405c9e6
George Williams <pfaedit@users.sourceforge.net>
2005-09-03 21:04:09 +0000

    Fix some uninitialized values in fontview.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1125780677;
-const char *source_modtime_str = "13:51 3-Sep-2005";
+const time_t source_modtime = 1125781176;
+const char *source_modtime_str = "13:59 3-Sep-2005";
 const char *source_version_str = "20050903";
