d20dfc2cfb0bb8e4165c7611df0b22d13a090360
George Williams <pfaedit@users.sourceforge.net>
2005-09-06 14:37:29 +0000

    If a font contained fewer than 256 glyphs, then ff failed to load a format0 cmap subtable properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1125871072;
-const char *source_modtime_str = "14:57 4-Sep-2005";
-const char *source_version_str = "20050904";
+const time_t source_modtime = 1126017228;
+const char *source_modtime_str = "07:33 6-Sep-2005";
+const char *source_version_str = "20050906";
