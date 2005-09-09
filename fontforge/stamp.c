ee5a63ea2cc0a4bbccd94be0124be70201c26cca
pfaedit <pfaedit>
2005-09-09 18:26:50 +0000

    Couple of tweaks to the warning window.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1126237775;
-const char *source_modtime_str = "20:49 8-Sep-2005";
-const char *source_version_str = "20050908";
+const time_t source_modtime = 1126290439;
+const char *source_modtime_str = "11:27 9-Sep-2005";
+const char *source_version_str = "20050909";
