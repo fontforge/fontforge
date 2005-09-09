7de578de7bc88e41ff24f5d01fd1ea850423be01
pfaedit <pfaedit>
2005-09-09 21:43:37 +0000

    If we have a bdf font with two glyphs with the same name, then ff would crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1126299297;
-const char *source_modtime_str = "13:54 9-Sep-2005";
+const time_t source_modtime = 1126301794;
+const char *source_modtime_str = "14:36 9-Sep-2005";
 const char *source_version_str = "20050909";
