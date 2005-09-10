97903f92e3a0eeb7399e8ee770c73d14d5c8c1f9
pfaedit <pfaedit>
2005-09-10 03:44:18 +0000

    The save fontdlg wasn't quite right (with respect to the new bitmap type: PS Type3).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1126322682;
-const char *source_modtime_str = "20:24 9-Sep-2005";
+const time_t source_modtime = 1126323778;
+const char *source_modtime_str = "20:42 9-Sep-2005";
 const char *source_version_str = "20050909";
