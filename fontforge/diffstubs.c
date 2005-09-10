e31df0e4a8091b5bae43117d586561571efe9a27
George Williams <pfaedit@users.sourceforge.net>
2005-09-10 04:01:09 +0000

    As usual, sfddiff is broken.

--- fontforge/diffstubs.c
+++ fontforge/diffstubs.c
@@ -369,6 +369,14 @@ void IError(const char *format, ... ) {
     va_end(ap);
 }
 
+void LogError(const char *format,...) {
+    va_list ap;
+
+    va_start(ap,format);
+    vfprintf(stderr,format,ap);
+    va_end(ap);
+}
+
 int RealNear(real a,real b) {
     real d;
 
