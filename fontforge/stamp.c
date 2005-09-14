f006fbc888fbb39914a959576275cf42403a947a
pfaedit <pfaedit>
2005-09-14 19:34:24 +0000

    In quadratic fonts the Remove Overlap menu item is disabled, but if you use the hotkey the command is invoked (with disasterous results).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1126721738;
-const char *source_modtime_str = "11:15 14-Sep-2005";
+const time_t source_modtime = 1126726238;
+const char *source_modtime_str = "12:30 14-Sep-2005";
 const char *source_version_str = "20050914";
