454cda20a6fd807be88bf088e1cda0b2d27ec658
George Williams <pfaedit@users.sourceforge.net>
2005-09-14 18:19:04 +0000

    Typo in point info code to determine whether a hint mask contains conflicts (matched vertical hints against horizontal ons)

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1126534761;
-const char *source_modtime_str = "07:19 12-Sep-2005";
-const char *source_version_str = "20050912";
+const time_t source_modtime = 1126721738;
+const char *source_modtime_str = "11:15 14-Sep-2005";
+const char *source_version_str = "20050914";
