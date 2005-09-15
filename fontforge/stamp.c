6004e58f2a76749f17b6b150b545357770eb6370
pfaedit <pfaedit>
2005-09-15 15:26:06 +0000

    Hide another problem with rounding errors in remove overlap.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1126729909;
-const char *source_modtime_str = "13:31 14-Sep-2005";
-const char *source_version_str = "20050914";
+const time_t source_modtime = 1126797825;
+const char *source_modtime_str = "08:23 15-Sep-2005";
+const char *source_version_str = "20050915";
