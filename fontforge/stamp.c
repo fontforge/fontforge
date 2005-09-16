f00f462d060e0ba2b4a3307c3c4c5e0631be40ce
pfaedit <pfaedit>
2005-09-16 21:25:59 +0000

    Some Unicode code blocks have moved (probably those which only in the pipeline or some such), and some others have been added to the pipeline.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1126803071;
-const char *source_modtime_str = "09:51 15-Sep-2005";
-const char *source_version_str = "20050915";
+const time_t source_modtime = 1126905922;
+const char *source_modtime_str = "14:25 16-Sep-2005";
+const char *source_version_str = "20050916";
