a14909b5b5e3a050e483a67a2d269942bff907fd
pfaedit <pfaedit>
2005-09-20 21:03:34 +0000

    Fix a couple of problems with cid font display that "gai" fonts expose.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1127230579;
-const char *source_modtime_str = "08:36 20-Sep-2005";
+const time_t source_modtime = 1127249897;
+const char *source_modtime_str = "13:58 20-Sep-2005";
 const char *source_version_str = "20050920";
