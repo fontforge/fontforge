01cb5b6d92db04bc983d3ec2d521b327cd4b769f
George Williams <pfaedit@users.sourceforge.net>
2005-09-22 03:37:57 +0000

    Provide two new scripting functions: 	MoveReference(xoff,yoff,[ref-name/ref-unicode]+) 	PositionReference(x,y,[ref-name/ref-unicode]+) These search all selected glyphs, looking for any references in those glyphs with the given name/unicode value, and then translating the reference by (xoff,yoff) or positioning the reference absolutely at (x,y).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1127331277;
-const char *source_modtime_str = "12:34 21-Sep-2005";
+const time_t source_modtime = 1127359914;
+const char *source_modtime_str = "20:31 21-Sep-2005";
 const char *source_version_str = "20050921";
