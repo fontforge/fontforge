1459d68dbd6954c04853716f981154750074896d
pfaedit <pfaedit>
2005-09-25 18:45:31 +0000

    Notice 'gai' files in the open dlg.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1127623554;
-const char *source_modtime_str = "21:45 24-Sep-2005";
-const char *source_version_str = "20050924";
+const time_t source_modtime = 1127673799;
+const char *source_modtime_str = "11:43 25-Sep-2005";
+const char *source_version_str = "20050925";
