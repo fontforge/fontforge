d412a8437f3c6f977321493e78a41f6365fa27ff
George Williams <pfaedit@users.sourceforge.net>
2005-09-26 19:45:05 +0000

    os2 typo linegap was not read out of sfd files.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1127673799;
-const char *source_modtime_str = "11:43 25-Sep-2005";
-const char *source_version_str = "20050925";
+const time_t source_modtime = 1127762720;
+const char *source_modtime_str = "12:25 26-Sep-2005";
+const char *source_version_str = "20050926";
