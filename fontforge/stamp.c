aa14dfdd1433ffdee1de860c901d896e9930aa0c
George Williams <pfaedit@users.sourceforge.net>
2005-09-29 22:44:50 +0000

    Patch from Michal Nowakowski to improve memory usage in auto instr.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128033511;
-const char *source_modtime_str = "15:38 29-Sep-2005";
+const time_t source_modtime = 1128033885;
+const char *source_modtime_str = "15:44 29-Sep-2005";
 const char *source_version_str = "20050929";
