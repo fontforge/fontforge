48267587c42c2ebd4363d44d356ae1b959cf4c5d
George Williams <pfaedit@users.sourceforge.net>
2005-09-30 02:46:32 +0000

    Oops, left this out of the massive utf8 patch. (not really part of it, but some other changes got mixed in and this is now required).

---
+++ inc/intl.h
@@ -0,0 +1,43 @@
+/* Copyright (C) 2005 by George Williams */
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+
+ * The name of the author may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef _INTL_H
+#define _INTL_H
+
+#if HAVE_LIBINTL_H
+# include <libintl.h>
+# define _(str)  gettext(str)
+# define N_(str) gettext_noop(str)
+#else
+# define _(str)  (str)
+# define N_(str) (str)
+#endif
+#define S_(str) sgettext(str)
+
+extern void GResourceUseGetText(void);
+char *sgettext(const char *msgid);
+
+#endif	/* _INTL_H */
