69c182e170621716e2254ecf825ca8d4c7aa3d6e
George Williams <pfaedit@users.sourceforge.net>
2005-09-30 13:23:54 +0000

    Michal Nowakowski points out that negative values are duplicated in cvt.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128086404;
-const char *source_modtime_str = "06:20 30-Sep-2005";
+const time_t source_modtime = 1128086659;
+const char *source_modtime_str = "06:24 30-Sep-2005";
 const char *source_version_str = "20050930";
