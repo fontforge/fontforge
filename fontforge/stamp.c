7b7a50854d943efea3a207c50c5da86a26bdd530
pfaedit <pfaedit>
2005-09-30 13:20:36 +0000

    Patch from Michal Nowakowski fixing crash when setting size of cvt table to 0.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128043621;
-const char *source_modtime_str = "18:27 29-Sep-2005";
-const char *source_version_str = "20050929";
+const time_t source_modtime = 1128086404;
+const char *source_modtime_str = "06:20 30-Sep-2005";
+const char *source_version_str = "20050930";
