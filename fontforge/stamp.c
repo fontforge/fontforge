55c7de9e634f7ca7aba7828f2e0db3c357dc328a
pfaedit <pfaedit>
2005-10-02 23:18:22 +0000

    Stepan Roh points out that in a monospace font, combining glyphs should default to the mono-width while in a proportional font they should default to 0 width (or perhaps 1 em-unit for compatability with old idiotic windows systems). Use the panose field to determine if a font is monospace.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128292607;
-const char *source_modtime_str = "15:36 2-Oct-2005";
+const time_t source_modtime = 1128294663;
+const char *source_modtime_str = "16:11 2-Oct-2005";
 const char *source_version_str = "20051002";
