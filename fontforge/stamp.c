fff31cc40821e5bf67d15bdaac9cf993f0d64f59
pfaedit <pfaedit>
2005-10-02 15:31:35 +0000

    For no reason I can think of this patch seems to make transparent images work on cygwin.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128086659;
-const char *source_modtime_str = "06:24 30-Sep-2005";
-const char *source_version_str = "20050930";
+const time_t source_modtime = 1128267167;
+const char *source_modtime_str = "08:32 2-Oct-2005";
+const char *source_version_str = "20051002";
