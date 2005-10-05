04877d69a78779c1e79e131fd2c22fd1250ac294
George Williams <pfaedit@users.sourceforge.net>
2005-10-05 18:20:01 +0000

    Stefan Wanger gave me a font containing the utf8 byte order mark in a fontname (in a CFF Name Index). PostScript says FontNames are printable ASCII. But it's easy enough to skip over it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128516904;
-const char *source_modtime_str = "05:55 5-Oct-2005";
+const time_t source_modtime = 1128535702;
+const char *source_modtime_str = "11:08 5-Oct-2005";
 const char *source_version_str = "20051005";
