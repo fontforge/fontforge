4c7ccd40237505b15579442ec0922a53ebcefdb6
pfaedit <pfaedit>
2005-10-07 03:02:54 +0000

    Add some support for reading fonts out of a pdf file. 	* No support for pdf type3 	* Only support (Hex, 85, Flate, RLE)Decode filters 		(no support for lzw, fax, JBIG, DCT)

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128550971;
-const char *source_modtime_str = "15:22 5-Oct-2005";
-const char *source_version_str = "20051005";
+const time_t source_modtime = 1128653041;
+const char *source_modtime_str = "19:44 6-Oct-2005";
+const char *source_version_str = "20051006";
