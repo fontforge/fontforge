c3bb6887602f3092f76913f955c8326bba4ee167
pfaedit <pfaedit>
2005-10-09 04:59:12 +0000

    Oops. The create monospace glyph patch was wrong.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128732174;
-const char *source_modtime_str = "17:42 7-Oct-2005";
-const char *source_version_str = "20051007";
+const time_t source_modtime = 1128833053;
+const char *source_modtime_str = "21:44 8-Oct-2005";
+const char *source_version_str = "20051008";
