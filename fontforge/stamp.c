0bea7d573216324c0f07d4ddf996dcb9fffb7e89
George Williams <pfaedit@users.sourceforge.net>
2005-10-10 20:45:19 +0000

    If I tried to generate a font from an absolutely empty fontdb (ie. a new one) then I'd get a stupid error about how .notdef was at encoding -1 and this was a bad idea.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128867709;
-const char *source_modtime_str = "07:21 9-Oct-2005";
-const char *source_version_str = "20051009";
+const time_t source_modtime = 1128976949;
+const char *source_modtime_str = "13:42 10-Oct-2005";
+const char *source_version_str = "20051010";
