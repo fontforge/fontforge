35bf1209a3c6fb35d90abd2136a32da5154a2522
pfaedit <pfaedit>
2005-10-11 20:08:42 +0000

    Entry for the warnings window is dimmed even if window is invisible.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1128976949;
-const char *source_modtime_str = "13:42 10-Oct-2005";
-const char *source_version_str = "20051010";
+const time_t source_modtime = 1129061359;
+const char *source_modtime_str = "13:09 11-Oct-2005";
+const char *source_version_str = "20051011";
