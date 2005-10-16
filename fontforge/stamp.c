c21ee3347e049199d8e7f1cc4bfc21c37bcaa44b
George Williams <pfaedit@users.sourceforge.net>
2005-10-16 15:33:09 +0000

    More tweaks to ofm files, but ofm2opl still crashes on our attempts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1129419784;
-const char *source_modtime_str = "16:43 15-Oct-2005";
-const char *source_version_str = "20051015";
+const time_t source_modtime = 1129473546;
+const char *source_modtime_str = "07:39 16-Oct-2005";
+const char *source_version_str = "20051016";
