f4ce0672a6c3290f6e6c42ff171a6765769bb529
pfaedit <pfaedit>
2005-10-16 15:47:44 +0000

    FontInfo had some display problems.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1129473546;
-const char *source_modtime_str = "07:39 16-Oct-2005";
+const time_t source_modtime = 1129477285;
+const char *source_modtime_str = "08:41 16-Oct-2005";
 const char *source_version_str = "20051016";
