aa3a6efd48db5b36059efbcad9f4ad8b77496247
pfaedit <pfaedit>
2005-10-17 04:42:12 +0000

    On the mac we need a special -I entry to find libintl.h (it's in /sw/include) but we can't use CFLAGS for splinerefigure (lest we turn on the optimizer), so we have to compile it with CPPFLAGS. Which we weren't doing.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
 const time_t source_modtime = 1129523738;
-const char *source_modtime_str = "21:35 16-Oct-2005";
+const char *source_modtime_str = "21:36 16-Oct-2005";
 const char *source_version_str = "20051016";
