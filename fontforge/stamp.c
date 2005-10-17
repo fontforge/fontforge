ddfefd6f55db671529638fa394d1a7b4718a19bf
George Williams <pfaedit@users.sourceforge.net>
2005-10-17 01:59:56 +0000

    I think I know what values to use for font_direction in an ofm file. I shall only worry about l2r & r2l.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1129486431;
-const char *source_modtime_str = "11:13 16-Oct-2005";
+const time_t source_modtime = 1129514234;
+const char *source_modtime_str = "18:57 16-Oct-2005";
 const char *source_version_str = "20051016";
