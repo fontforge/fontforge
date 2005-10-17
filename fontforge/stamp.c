732d731f2b104326476210c78038e15fc724426a
pfaedit <pfaedit>
2005-10-17 23:34:17 +0000

    Oh. It looks as though ofm files don't actually use the font direction field. All files I've looked at have this set to 0 (Top to bottom, Left to Right) even the arabic only ones.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1129591678;
-const char *source_modtime_str = "16:27 17-Oct-2005";
+const time_t source_modtime = 1129592020;
+const char *source_modtime_str = "16:33 17-Oct-2005";
 const char *source_version_str = "20051017";
