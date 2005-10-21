5c5faedb62dffdd381cf50e0f6b86ba38f43ffcc
pfaedit <pfaedit>
2005-10-21 03:22:36 +0000

    Daniel Gillmor points out there is no way to set the OS/2 Width field from a script. He then provides a script which does this. I realize there is no way to set any of the OS/2 values (except panose) or the PS private dictionary, and there should be (and to retrieve them too).
    the following new scripting functions:
            SetOS2Value("item-name", value)
            GetOS2Value("item-name")
            SetPrivateValue("item-key","item-value")
            GetPrivateValue("item-key")
            HasPrivateValue("item-key")
            RemovePrivateValue("item-key")
    
    SetPrivateValue
            Changes (or Adds) an entry to the font's private dictionary with
            the given key and given value. Both must be strings. The key may
            be anything, it is not limited to any set of expected keys.
    GetPrivateValue
            Returns the string in the private dictionary which matches the
            given key. If the key isn't in the dictionary it explodes.
    HasPrivateValue
            Returns whether a key is in the dictionary
    RemovePrivateValue
            Removes a key (and associated value) from the dictionary.
            Returns whether the key was in the dictionary (this is not a
            typo).
    SetOS2Value
            The first argument must be one of the following strings. The
            second argument is usually an integer, but VendorId takes a (4
            character) string, and panose takes a 10 element integer array.
            Weight, Width, FSType, IBMFamily, VendorID, Panose,
            WinAscent, WinAscentIsOffset
                    These are equivalent to the WinAscent field & []Offset
                    check box in Font Info->OS/2->Misc. Where
                    WinAscentOffset controls whether WinAscent is treated as
                    an absolute number or as relative to the expected
                    number.
            WinDescent, WinDescentIsOffset,
            TypoAscent, TypoAscentIsOffset,
            TypoDescent, TypoDescentIsOffset,
            HHeadAscent, HHeadAscentIsOffset,
            HHeadDescent, HHeadDescentIsOffset,
            TypoLineGap, HHeadLineGap, VHeadLineGap,
            SubXSize, SubYSize, SubXOffset, SubYOffset,
                    Sets the Subscript x/y sizes/offsets
            SupXSize, SupYSize, SupXOffset, SupYOffset,
                    Sets the Superscript x/y sizes/offsets
            StrikeOutSize, StrikeOutPos
    GetOS2Value
            The argument takes the same set of tag strings. VendorId returns
            a string, and Panose returns an array. The others return
            integers.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1129822264;
-const char *source_modtime_str = "08:31 20-Oct-2005";
+const time_t source_modtime = 1129864411;
+const char *source_modtime_str = "20:13 20-Oct-2005";
 const char *source_version_str = "20051020";
