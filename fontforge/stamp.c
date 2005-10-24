bbd8b53e45492c9f24f23b2a88e7babc04709c1d
George Williams <pfaedit@users.sourceforge.net>
2005-10-24 21:37:48 +0000

    FF gave alternate substitutions (and multiple subs?) a tag of 0.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1130186892;
-const char *source_modtime_str = "13:48 24-Oct-2005";
+const time_t source_modtime = 1130189485;
+const char *source_modtime_str = "14:31 24-Oct-2005";
 const char *source_version_str = "20051024";
