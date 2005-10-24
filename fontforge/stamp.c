a695260babe7f99a50ebaf4ccb8e5ddd3926ea66
pfaedit <pfaedit>
2005-10-24 15:54:41 +0000

    Add two scripting routines to allow people to read & write strings from and to files. One string per file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1130164100;
-const char *source_modtime_str = "07:28 24-Oct-2005";
+const time_t source_modtime = 1130168546;
+const char *source_modtime_str = "08:42 24-Oct-2005";
 const char *source_version_str = "20051024";
