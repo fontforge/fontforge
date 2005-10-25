348efbcec99fcf9bd9702044628924f7ac0af60f
pfaedit <pfaedit>
2005-10-25 21:15:12 +0000

    I was dumping all opentype ligatures into an ofm(tfm) file. But I should probably only add 'liga' and 'rlig' ligatures. Omega has a bug where it thinks 0 width glyphs do not exist. Which means it thinks none of the combining accents, etc. exist. Stupid. So force the width of any zero width glyphs to be the smallest positive width.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1130189485;
-const char *source_modtime_str = "14:31 24-Oct-2005";
-const char *source_version_str = "20051024";
+const time_t source_modtime = 1130274248;
+const char *source_modtime_str = "14:04 25-Oct-2005";
+const char *source_version_str = "20051025";
