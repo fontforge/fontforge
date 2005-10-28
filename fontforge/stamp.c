ff2ebb516453659cce76374b3ac8ab745ce51c72
pfaedit <pfaedit>
2005-10-28 13:02:57 +0000

    Didn't terminate an array of answer strings to a question dialog. Fixed a crash in stem database.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1130274248;
-const char *source_modtime_str = "14:04 25-Oct-2005";
-const char *source_version_str = "20051025";
+const time_t source_modtime = 1130504401;
+const char *source_modtime_str = "06:00 28-Oct-2005";
+const char *source_version_str = "20051028";
