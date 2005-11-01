6b70e68a82db435ce820e6c124e736933a334f2a
pfaedit <pfaedit>
2005-11-01 04:27:43 +0000

    Panov wants the offset cut off to be smaller.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1130537985;
-const char *source_modtime_str = "15:19 28-Oct-2005";
-const char *source_version_str = "20051028";
+const time_t source_modtime = 1130819265;
+const char *source_modtime_str = "20:27 31-Oct-2005";
+const char *source_version_str = "20051031";
