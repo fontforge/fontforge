f64ffd435db4ab6d871f9020b5bcb5ce32ff4c72
pfaedit <pfaedit>
2005-11-04 21:53:30 +0000

    Panov wants a way to build up arrays using easier syntax than: create array, assign each element.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1131077068;
-const char *source_modtime_str = "20:04 3-Nov-2005";
-const char *source_version_str = "20051103";
+const time_t source_modtime = 1131140991;
+const char *source_modtime_str = "13:49 4-Nov-2005";
+const char *source_version_str = "20051104";
