4263287c1f7465720d01310b12a504f6e381c233
George Williams <pfaedit@users.sourceforge.net>
2005-11-07 21:18:08 +0000

    Still some places in menus where the lines are marked as selected

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1131140991;
-const char *source_modtime_str = "13:49 4-Nov-2005";
-const char *source_version_str = "20051104";
+const time_t source_modtime = 1131396357;
+const char *source_modtime_str = "12:45 7-Nov-2005";
+const char *source_version_str = "20051107";
