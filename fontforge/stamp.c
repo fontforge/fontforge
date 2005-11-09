700a03c6982a0ebda5a1a9a2aa2be81632fce12b
George Williams <pfaedit@users.sourceforge.net>
2005-11-09 20:11:53 +0000

    Further tweaks to the simplify patch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1131490125;
-const char *source_modtime_str = "14:48 8-Nov-2005";
-const char *source_version_str = "20051108";
+const time_t source_modtime = 1131566950;
+const char *source_modtime_str = "12:09 9-Nov-2005";
+const char *source_version_str = "20051109";
