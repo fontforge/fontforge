e5f89022779ad11dc6bd9391df7bc5431665bb77
pfaedit <pfaedit>
2005-11-14 04:08:21 +0000

    Screwed up View->Show Grid Fit... in Mono mode.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1131939935;
-const char *source_modtime_str = "19:45 13-Nov-2005";
+const time_t source_modtime = 1131941193;
+const char *source_modtime_str = "20:06 13-Nov-2005";
 const char *source_version_str = "20051113";
