73f6a5cc1f47c7fc20eee53ada691978c5e80ded
pfaedit <pfaedit>
2005-11-16 01:09:29 +0000

    If a curved point had no control point in one direction, then ff thought it had no direction and felt free to change it. It should inherit the direction from the other side of the point. This was even more confusing if the point went through a two step process, first simplifying one side into a line (losing the cp) then simplifying the line away (losing the direction).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1131941626;
-const char *source_modtime_str = "20:13 13-Nov-2005";
-const char *source_version_str = "20051113";
+const time_t source_modtime = 1132103091;
+const char *source_modtime_str = "17:04 15-Nov-2005";
+const char *source_version_str = "20051115";
