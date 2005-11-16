81ecd312f240d84134547e6f21c3c45d30b0896c
George Williams <pfaedit@users.sourceforge.net>
2005-11-16 21:48:51 +0000

    Add popup info showing the level of greyness in a truetype font being debugged in anti-alias mode.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1132175416;
-const char *source_modtime_str = "13:10 16-Nov-2005";
+const time_t source_modtime = 1132177421;
+const char *source_modtime_str = "13:43 16-Nov-2005";
 const char *source_version_str = "20051116";
