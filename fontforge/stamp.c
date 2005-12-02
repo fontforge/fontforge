b0afdd59fa4c61ac82ec8e22401a73cc373edc02
George Williams <pfaedit@users.sourceforge.net>
2005-12-02 00:11:01 +0000

    I added a check in parsing tfm files to make sure I didn't read outside the bounds of the kern table. But the table size was expressed in ints (32 bit units), and my index was in bytes, so I frequently exceded it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1133479352;
-const char *source_modtime_str = "15:22 1-Dec-2005";
+const time_t source_modtime = 1133482123;
+const char *source_modtime_str = "16:08 1-Dec-2005";
 const char *source_version_str = "20051201";
