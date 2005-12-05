79f5e02e0f9016fb36fe38fcf19de539fae5691e
George Williams <pfaedit@users.sourceforge.net>
2005-12-05 00:39:06 +0000

    /* Jonathyn Bet'nct points out that once you edit in an outline window, even */ /*  if by mistake, your onlybitmaps status is gone for good */ /* Regenerate it when reading an sfd file if the font has no splines, refs, etc. */

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1133556606;
-const char *source_modtime_str = "12:50 2-Dec-2005";
-const char *source_version_str = "20051202";
+const time_t source_modtime = 1133742966;
+const char *source_modtime_str = "16:36 4-Dec-2005";
+const char *source_version_str = "20051204";
