ff9a1dc0feef136900653a9ac7cf16df09caaa08
pfaedit <pfaedit>
2005-12-09 21:28:27 +0000

    Another attempt to position the debugging bitmap properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134090997;
-const char *source_modtime_str = "17:16 8-Dec-2005";
-const char *source_version_str = "20051208";
+const time_t source_modtime = 1134163401;
+const char *source_modtime_str = "13:23 9-Dec-2005";
+const char *source_version_str = "20051209";
