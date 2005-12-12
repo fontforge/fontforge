c845ac38c76ce2ef9dde54563c467a0acb99fb97
George Williams <pfaedit@users.sourceforge.net>
2005-12-12 03:14:03 +0000

    Wasn't storing the vendor id in the OS/2 table properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134356570;
-const char *source_modtime_str = "19:02 11-Dec-2005";
+const time_t source_modtime = 1134357141;
+const char *source_modtime_str = "19:12 11-Dec-2005";
 const char *source_version_str = "20051211";
