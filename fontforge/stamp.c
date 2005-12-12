187d47e6fc28843fd3b14824747378f1a98a545b
pfaedit <pfaedit>
2005-12-12 22:19:13 +0000

    Michal Nowakowski points out that renumbering points in a glyph should turn off point matching in references.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134422428;
-const char *source_modtime_str = "13:20 12-Dec-2005";
+const time_t source_modtime = 1134425732;
+const char *source_modtime_str = "14:15 12-Dec-2005";
 const char *source_version_str = "20051212";
