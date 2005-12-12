0742f3c08fbe4f654cde2472c908390863b74164
George Williams <pfaedit@users.sourceforge.net>
2005-12-12 22:50:12 +0000

    Moving the control points of an implied point should move the implied point. And we should indicate which points are implied so this behavior doesn't surprise.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134425732;
-const char *source_modtime_str = "14:15 12-Dec-2005";
+const time_t source_modtime = 1134427423;
+const char *source_modtime_str = "14:43 12-Dec-2005";
 const char *source_version_str = "20051212";
