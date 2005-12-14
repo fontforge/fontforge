6de088414e34ccf22b19c62ea031f4f8f0c56bae
pfaedit <pfaedit>
2005-12-14 18:26:06 +0000

    A mouse click in an empty metrics view references garbage memory -- And crashed Kanou's session.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134534063;
-const char *source_modtime_str = "20:21 13-Dec-2005";
-const char *source_version_str = "20051213";
+const time_t source_modtime = 1134584656;
+const char *source_modtime_str = "10:24 14-Dec-2005";
+const char *source_version_str = "20051214";
