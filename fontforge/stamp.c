2c480106b4ba0fbc1f39e28818a368ee1190ec69
George Williams <pfaedit@users.sourceforge.net>
2005-12-14 22:12:14 +0000

    Kanou's patch for adding a new flag to scripting Import, to clear out the layer first.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134595530;
-const char *source_modtime_str = "13:25 14-Dec-2005";
+const time_t source_modtime = 1134598078;
+const char *source_modtime_str = "14:07 14-Dec-2005";
 const char *source_version_str = "20051214";
