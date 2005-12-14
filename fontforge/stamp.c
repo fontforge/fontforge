a971becb82be8c5dbe16c148982fe71fb9d68494
pfaedit <pfaedit>
2005-12-14 02:38:14 +0000

    Loading a large bitmap font left bits of the backmap of the encoding map uninitialized.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134428805;
-const char *source_modtime_str = "15:06 12-Dec-2005";
-const char *source_version_str = "20051212";
+const time_t source_modtime = 1134526823;
+const char *source_modtime_str = "18:20 13-Dec-2005";
+const char *source_version_str = "20051213";
