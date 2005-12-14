1269a0c556b3bc6268a7c44dabee4ef68ffcf637
pfaedit <pfaedit>
2005-12-14 19:34:58 +0000

    If an implied point became real, this fact was not reflected on the display until the user forced a point renumbering.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134584656;
-const char *source_modtime_str = "10:24 14-Dec-2005";
+const time_t source_modtime = 1134588731;
+const char *source_modtime_str = "11:32 14-Dec-2005";
 const char *source_version_str = "20051214";
