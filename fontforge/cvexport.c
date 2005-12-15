19551c766cbe1d22ede01bac96020a4ed10be0a6
pfaedit <pfaedit>
2005-12-15 05:47:51 +0000

    Hmm. Not all systems have tzset. Add a configuration option for it.

--- fontforge/cvexport.c
+++ fontforge/cvexport.c
@@ -193,14 +193,18 @@ int _ExportPDF(FILE *pdf,SplineChar *sc) {
     fprintf( pdf, "    /Creator (FontForge)\n" );
     time(&now);
     tm = localtime(&now);
-    tzset();
     fprintf( pdf, "    /CreationDate (D:%04d%02d%02d%02d%2d%02d",
 	    1900+tm->tm_year, tm->tm_mon+1, tm->tm_mday,
 	    tm->tm_hour, tm->tm_min, tm->tm_sec );
+#ifdef _NO_TZSET
+    fprintf( pdf, "Z)\n" );
+#else
+    tzset();
     if ( timezone==0 )
 	fprintf( pdf, "Z)\n" );
     else 
 	fprintf( pdf, "%+02d')\n", (int) timezone/3600 );	/* doesn't handle half-hour zones */
+#endif
     fprintf( pdf, "    /Title (%s from %s)\n", sc->name, sc->parent->fontname );
     if ( author!=NULL )
 	fprintf( pdf, "    /Author (%s)\n", author );
