3c94ae4c8d7c8fa9bf4a2a2ae349146c58412228
George Williams <pfaedit@users.sourceforge.net>
2005-12-15 05:47:51 +0000

    Hmm. Not all systems have tzset. Add a configuration option for it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1134621879;
-const char *source_modtime_str = "20:44 14-Dec-2005";
+const time_t source_modtime = 1134624758;
+const char *source_modtime_str = "21:32 14-Dec-2005";
 const char *source_version_str = "20051214";
