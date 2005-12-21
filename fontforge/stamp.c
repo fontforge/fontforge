ff4694af3a22ab41828817d3dadc494b3127a41c
George Williams <pfaedit@users.sourceforge.net>
2005-12-21 18:59:51 +0000

    Grr. I was generating vertical metrics info for bdf fonts even if they had no vertical metrics. (recent bug).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1135190765;
-const char *source_modtime_str = "10:46 21-Dec-2005";
+const time_t source_modtime = 1135191610;
+const char *source_modtime_str = "11:00 21-Dec-2005";
 const char *source_version_str = "20051221";
