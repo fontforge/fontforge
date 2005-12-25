849a0c513355470c660c623e70906cf1d284be87
George Williams <pfaedit@users.sourceforge.net>
2005-12-25 19:32:57 +0000

    KANOU points out that the origin pulldown list in the transform dlg was not being translated.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1135372597;
-const char *source_modtime_str = "13:16 23-Dec-2005";
-const char *source_version_str = "20051223";
+const time_t source_modtime = 1135538926;
+const char *source_modtime_str = "11:28 25-Dec-2005";
+const char *source_version_str = "20051225";
