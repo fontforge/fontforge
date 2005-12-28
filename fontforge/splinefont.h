1212fac4942789aad714ef2d5c2ec1dcc74e3a98
pfaedit <pfaedit>
2005-12-28 20:58:54 +0000

    Add a default anchor point type to the AddAnchorPoint scripting command. FF will try to guess an appropriate value.

--- fontforge/splinefont.h
+++ fontforge/splinefont.h
@@ -1291,6 +1291,7 @@ extern void AnchorPointsFree(AnchorPoint *kp);
 extern AnchorPoint *AnchorPointsCopy(AnchorPoint *alist);
 extern void SFRemoveAnchorClass(SplineFont *sf,AnchorClass *an);
 extern int AnchorClassesNextMerge(AnchorClass *ac);
+extern int IsAnchorClassUsed(SplineChar *sc,AnchorClass *an);
 extern AnchorPoint *APAnchorClassMerge(AnchorPoint *anchors,AnchorClass *into,AnchorClass *from);
 extern void AnchorClassMerge(SplineFont *sf,AnchorClass *into,AnchorClass *from);
 extern void AnchorClassesFree(AnchorClass *kp);
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1135636422;
-const char *source_modtime_str = "14:33 26-Dec-2005";
-const char *source_version_str = "20051226";
+const time_t source_modtime = 1135803357;
+const char *source_modtime_str = "12:55 28-Dec-2005";
+const char *source_version_str = "20051228";
