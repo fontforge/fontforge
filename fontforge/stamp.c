0a7920209cb11022f7b2a165b9b38255ca4443d5
George Williams <pfaedit@users.sourceforge.net>
2006-01-01 14:48:39 +0000

    New copyright notices for 2006

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1136091143;
-const char *source_modtime_str = "20:52 31-Dec-2005";
-const char *source_version_str = "20051231";
+const time_t source_modtime = 1136123066;
+const char *source_modtime_str = "05:44 1-Jan-2006";
+const char *source_version_str = "20060101";
