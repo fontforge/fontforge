d8e3a55d5eb532c8075dbfbf2c3aa4a216c5376d
pfaedit <pfaedit>
2006-01-01 03:36:25 +0000

    Order2 elipses were not drawn accurately.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1136084971;
-const char *source_modtime_str = "19:09 31-Dec-2005";
+const time_t source_modtime = 1136086563;
+const char *source_modtime_str = "19:36 31-Dec-2005";
 const char *source_version_str = "20051231";
