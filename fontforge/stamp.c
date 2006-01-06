efeeffd5945c65fc020a66867343380dead3c269
George Williams <pfaedit@users.sourceforge.net>
2006-01-06 02:05:04 +0000

    Gleep! Ever since the gettext code went in, the Generate fonts dlg has refused to generate "No Outline Font" and "No Bitmap Font".

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1136510652;
-const char *source_modtime_str = "17:24 5-Jan-2006";
+const time_t source_modtime = 1136512489;
+const char *source_modtime_str = "17:54 5-Jan-2006";
 const char *source_version_str = "20060105";
