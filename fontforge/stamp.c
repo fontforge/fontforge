f4e15784181abf795ae157cb878415687ca011df
George Williams <pfaedit@users.sourceforge.net>
2006-01-06 01:28:43 +0000

    Herbert Duerr from OpenOffice tells me that their OpenSymbol.ttf font when produced by fontforge does not work on Win98. This is because Win98 doesn't like the codepages bits ff produces and refuses to use any glyphs in the font. He submitted a patch to make ff produce a version 0 OS/2 table (with no codepage info) for this font. Win98 will accept it then. His patch was against an earlier version of ff and needed to be modified for the current version.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1136407049;
-const char *source_modtime_str = "12:37 4-Jan-2006";
-const char *source_version_str = "20060104";
+const time_t source_modtime = 1136510652;
+const char *source_modtime_str = "17:24 5-Jan-2006";
+const char *source_version_str = "20060105";
