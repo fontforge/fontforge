1f536b7a50d11e22645851042ee54d53decbab59
George Williams <pfaedit@users.sourceforge.net>
2006-01-08 21:07:34 +0000

    Wasn't numbering points in references properly. Shouldn't matter much, but it screwed up test119.pe

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1136731975;
-const char *source_modtime_str = "06:52 8-Jan-2006";
+const time_t source_modtime = 1136754115;
+const char *source_modtime_str = "13:01 8-Jan-2006";
 const char *source_version_str = "20060108";
