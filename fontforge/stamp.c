41a926c747f635b470aa57a1da4d08570efe94a3
George Williams <pfaedit@users.sourceforge.net>
2006-01-08 22:51:37 +0000

    When reading a GenTags: line from an sfd file stored with CRLF line terminators, the next line would be lost.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1136759017;
-const char *source_modtime_str = "14:23 8-Jan-2006";
+const time_t source_modtime = 1136760638;
+const char *source_modtime_str = "14:50 8-Jan-2006";
 const char *source_version_str = "20060108";
