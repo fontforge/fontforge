5f5b75922003fb2c2ff7a640c580b1d9fb2c5d62
pfaedit <pfaedit>
2006-01-10 02:05:52 +0000

    Oops. Remove compaction if we reencode.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1136855751;
-const char *source_modtime_str = "17:15 9-Jan-2006";
+const time_t source_modtime = 1136858607;
+const char *source_modtime_str = "18:03 9-Jan-2006";
 const char *source_version_str = "20060109";
