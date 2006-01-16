c0731dcfe21e382d8fcbc3b33ab32fa3ca6f1c2b
pfaedit <pfaedit>
2006-01-16 04:16:00 +0000

    Make the Import() scripting command handle compressed bdf/pcf fonts too.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1137375532;
-const char *source_modtime_str = "17:38 15-Jan-2006";
+const time_t source_modtime = 1137382980;
+const char *source_modtime_str = "19:43 15-Jan-2006";
 const char *source_version_str = "20060115";
