414a55bc0b7b62f2531776f0e10eb9455a7504cd
George Williams <pfaedit@users.sourceforge.net>
2006-01-17 15:33:59 +0000

    Oops. A patch with nothing to do with tilepath made a global function with the same name as a static func in tilepath (and used different arguments).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1137511863;
-const char *source_modtime_str = "07:31 17-Jan-2006";
+const time_t source_modtime = 1137512089;
+const char *source_modtime_str = "07:34 17-Jan-2006";
 const char *source_version_str = "20060117";
