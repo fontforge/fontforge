3e1c943f2b011507a12f52917210897dfb953e38
George Williams <pfaedit@users.sourceforge.net>
2006-01-20 04:18:56 +0000

    Add a homedir (and an updir) button to the file chooser dialogs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1137613185;
-const char *source_modtime_str = "11:39 18-Jan-2006";
-const char *source_version_str = "20060118";
+const time_t source_modtime = 1137728941;
+const char *source_modtime_str = "19:49 19-Jan-2006";
+const char *source_version_str = "20060119";
