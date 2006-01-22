394cd0737697ed3c5bea3d59d63127da56332ba6
George Williams <pfaedit@users.sourceforge.net>
2006-01-22 22:51:25 +0000

    In Gentium Roman the base line is such that "0" is outside one standard deviation from the mean. Tweak things (lie about the standard deviation) so that 0 is always within range.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1137969624;
-const char *source_modtime_str = "14:40 22-Jan-2006";
+const time_t source_modtime = 1137970205;
+const char *source_modtime_str = "14:50 22-Jan-2006";
 const char *source_version_str = "20060122";
