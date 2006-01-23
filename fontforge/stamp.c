cb0911c2c8b823c3fdac9a61ae2bf6832915199e
pfaedit <pfaedit>
2006-01-23 13:52:29 +0000

    Someone handed us a glyph with a point at 13,476,000 (and another near 0) and asked us to rasterize it. We ran out of memory.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1137983723;
-const char *source_modtime_str = "18:35 22-Jan-2006";
-const char *source_version_str = "20060122";
+const time_t source_modtime = 1138024273;
+const char *source_modtime_str = "05:51 23-Jan-2006";
+const char *source_version_str = "20060123";
