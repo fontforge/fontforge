7411187f47a77bc2233cacb466ca4da8e12338e1
pfaedit <pfaedit>
2006-01-23 20:43:36 +0000

    Chia points out that I should use the PIXEL_SIZE property to set a bdf font's pixelsize. I was using FONT_ASCENT+FONT_DESCENT. I think they should be the same, but sometimes they are not.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1138024273;
-const char *source_modtime_str = "05:51 23-Jan-2006";
+const time_t source_modtime = 1138048848;
+const char *source_modtime_str = "12:40 23-Jan-2006";
 const char *source_version_str = "20060123";
