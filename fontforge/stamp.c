1a0c10ca62f61cd6b7646bc933a6935277613b10
George Williams <pfaedit@users.sourceforge.net>
2006-01-25 01:12:01 +0000

    If you ask to create an otb font but provide no valid bitmap strikes, ff will crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1138048848;
-const char *source_modtime_str = "12:40 23-Jan-2006";
-const char *source_version_str = "20060123";
+const time_t source_modtime = 1138151464;
+const char *source_modtime_str = "17:11 24-Jan-2006";
+const char *source_version_str = "20060124";
