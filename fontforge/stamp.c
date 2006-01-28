4ef36d2c982391e269c014b33cc1f498f1f1a993
George Williams <pfaedit@users.sourceforge.net>
2006-01-28 00:04:04 +0000

    Typo in an encoding property chech that occurred when loading ZapfDingbats.pcf

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1138284554;
-const char *source_modtime_str = "06:09 26-Jan-2006";
-const char *source_version_str = "20060126";
+const time_t source_modtime = 1138402439;
+const char *source_modtime_str = "14:53 27-Jan-2006";
+const char *source_version_str = "20060127";
