b4ae9a49618d6b245b3f21ef81d86199c5ffad07
George Williams <pfaedit@users.sourceforge.net>
2006-02-01 21:41:53 +0000

    More work for Kanou's disambiguation between latin script and latin language. Mac languages broken since the gettext patch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1138826831;
-const char *source_modtime_str = "12:47 1-Feb-2006";
+const time_t source_modtime = 1138829168;
+const char *source_modtime_str = "13:26 1-Feb-2006";
 const char *source_version_str = "20060201";
