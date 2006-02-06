b9b8ebdb912cae75614d61e29c9e876f79c41b31
pfaedit <pfaedit>
2006-02-06 16:13:24 +0000

    Add script for N'Ko.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1139236906;
-const char *source_modtime_str = "06:41 6-Feb-2006";
+const time_t source_modtime = 1139242121;
+const char *source_modtime_str = "08:08 6-Feb-2006";
 const char *source_version_str = "20060206";
