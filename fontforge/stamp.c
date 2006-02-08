feca56ff833cf892f43e39b3bd19cc676182f501
pfaedit <pfaedit>
2006-02-08 19:33:35 +0000

    Last "Fix" for merge command, meant that we broke curve points (ie. they formed a corner rather than a curve). This fixes it properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1139242335;
-const char *source_modtime_str = "08:12 6-Feb-2006";
-const char *source_version_str = "20060206";
+const time_t source_modtime = 1139352691;
+const char *source_modtime_str = "14:51 7-Feb-2006";
+const char *source_version_str = "20060207";
