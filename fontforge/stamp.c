42aa6c7d61c97e806919856d95cd3af98b44b648
pfaedit <pfaedit>
2006-02-08 19:38:01 +0000

    If a copyright message were an exact multiple of 100 characters long, fontforge would screw up memory (and potentially crash) when reading it in from an sfd file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1139352691;
-const char *source_modtime_str = "14:51 7-Feb-2006";
+const time_t source_modtime = 1139356175;
+const char *source_modtime_str = "15:49 7-Feb-2006";
 const char *source_version_str = "20060207";
