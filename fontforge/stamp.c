6b7366e3c0521409d13f6f9729515544da38df85
pfaedit <pfaedit>
2006-02-08 20:05:58 +0000

    Once more we are numbering points badly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1139427891;
-const char *source_modtime_str = "11:44 8-Feb-2006";
+const time_t source_modtime = 1139428883;
+const char *source_modtime_str = "12:01 8-Feb-2006";
 const char *source_version_str = "20060208";
