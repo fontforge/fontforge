d87205d0f16a049f138c49e29ab8320846ca5d35
pfaedit <pfaedit>
2006-02-12 15:59:12 +0000

    gniibe reports that the scripting function Export() produces bitmap images with the wrong format and provides a patch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1139534972;
-const char *source_modtime_str = "17:29 9-Feb-2006";
-const char *source_version_str = "20060209";
+const time_t source_modtime = 1139760026;
+const char *source_modtime_str = "08:00 12-Feb-2006";
+const char *source_version_str = "20060212";
