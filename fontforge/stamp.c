72b3225270967d5a8b137e5946a958c0be3718e8
George Williams <pfaedit@users.sourceforge.net>
2006-02-13 01:36:32 +0000

    Make Merge/Simplify even more accurate.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1139760026;
-const char *source_modtime_str = "08:00 12-Feb-2006";
+const time_t source_modtime = 1139794250;
+const char *source_modtime_str = "17:30 12-Feb-2006";
 const char *source_version_str = "20060212";
