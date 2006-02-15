420c8e89f8f4fe711676b2ebf9f9a631dc588dad
pfaedit <pfaedit>
2006-02-15 14:12:02 +0000

    Glyph compare didn't work on bitmaps with depth>1

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1139893017;
-const char *source_modtime_str = "20:56 13-Feb-2006";
-const char *source_version_str = "20060213";
+const time_t source_modtime = 1140012653;
+const char *source_modtime_str = "06:10 15-Feb-2006";
+const char *source_version_str = "20060215";
