da03ef2d712f2afbc40729055c878b59af6a2947
pfaedit <pfaedit>
2006-02-15 22:21:48 +0000

    Fix simple crash in metafont.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140039069;
-const char *source_modtime_str = "13:31 15-Feb-2006";
+const time_t source_modtime = 1140042127;
+const char *source_modtime_str = "14:22 15-Feb-2006";
 const char *source_version_str = "20060215";
