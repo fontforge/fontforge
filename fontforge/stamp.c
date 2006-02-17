313dc34dfd19113ef9b3dc4a974910c9000d4d8a
George Williams <pfaedit@users.sourceforge.net>
2006-02-17 03:11:43 +0000

    When guessing points at which to attach hints in type1/2 read in we failed if the points were too widely spaced. Be a bit more lenient.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140139856;
-const char *source_modtime_str = "17:30 16-Feb-2006";
+const time_t source_modtime = 1140143634;
+const char *source_modtime_str = "18:33 16-Feb-2006";
 const char *source_version_str = "20060216";
