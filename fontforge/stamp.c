1c82f7645e340291dd4564cbb3ab27d70f51eecf
pfaedit <pfaedit>
2006-02-17 01:31:16 +0000

    SetCharName didn't check its flag value properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140066067;
-const char *source_modtime_str = "21:01 15-Feb-2006";
-const char *source_version_str = "20060215";
+const time_t source_modtime = 1140139856;
+const char *source_modtime_str = "17:30 16-Feb-2006";
+const char *source_version_str = "20060216";
