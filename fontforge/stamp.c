5b4483bdb1d9b627da3e736aa4e487bd8fe38f5b
George Williams <pfaedit@users.sourceforge.net>
2006-02-17 04:59:11 +0000

    When we've got one curved edge and one straight edge we have a hard time figuring if a hint is too big.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140150816;
-const char *source_modtime_str = "20:33 16-Feb-2006";
+const time_t source_modtime = 1140152405;
+const char *source_modtime_str = "21:00 16-Feb-2006";
 const char *source_version_str = "20060216";
