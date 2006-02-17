dfcd822174d5d38fa3e0ac8c35b08821f3d62523
George Williams <pfaedit@users.sourceforge.net>
2006-02-17 22:11:09 +0000

    When generating opentype tables in from a single sub-font of a cid-keyed font (ie. user asked for a ttf font generated from one sub-font) ff would crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140152405;
-const char *source_modtime_str = "21:00 16-Feb-2006";
-const char *source_version_str = "20060216";
+const time_t source_modtime = 1140213986;
+const char *source_modtime_str = "14:06 17-Feb-2006";
+const char *source_version_str = "20060217";
