9d1dc8e4bf5db0c73a57200c7db0057c3c867a17
George Williams <pfaedit@users.sourceforge.net>
2006-02-18 22:29:54 +0000

    Ralf Stubner points out that I produce type1 charstrings which end in "seac endchar" when a simple "seac" is sufficient. He's right.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140240379;
-const char *source_modtime_str = "21:26 17-Feb-2006";
-const char *source_version_str = "20060217";
+const time_t source_modtime = 1140301425;
+const char *source_modtime_str = "14:23 18-Feb-2006";
+const char *source_version_str = "20060218";
