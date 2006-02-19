419a185e9e87d1ea01a60bfa4c96b294a037bb9e
pfaedit <pfaedit>
2006-02-19 02:56:48 +0000

    Johan Winge suggests an improvement to converting cubic splines to quadratic: If we end up using a line when looking at the spline as a whole, try breaking it at the point of infection(s) first. It seems like a good idea.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140317704;
-const char *source_modtime_str = "18:55 18-Feb-2006";
+const time_t source_modtime = 1140317806;
+const char *source_modtime_str = "18:56 18-Feb-2006";
 const char *source_version_str = "20060218";
