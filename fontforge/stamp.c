0808babd3111a8c0dfde3351ed6d9e46c80ecd4f
George Williams <pfaedit@users.sourceforge.net>
2006-02-19 20:00:10 +0000

    Fix hint compare.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140364403;
-const char *source_modtime_str = "07:53 19-Feb-2006";
+const time_t source_modtime = 1140379308;
+const char *source_modtime_str = "12:01 19-Feb-2006";
 const char *source_version_str = "20060219";
