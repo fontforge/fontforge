830e2869fa0c0c327f1a2e052d35137a85671901
pfaedit <pfaedit>
2006-02-26 21:07:30 +0000

    There was still a way for parallel lines to sneak into the quadratic conversion leading to nans.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140964847;
-const char *source_modtime_str = "06:40 26-Feb-2006";
+const time_t source_modtime = 1140987525;
+const char *source_modtime_str = "12:58 26-Feb-2006";
 const char *source_version_str = "20060226";
