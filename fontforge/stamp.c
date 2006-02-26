4626d12bd3ab7bcc02bc19f4bf06da40b9765cc5
George Williams <pfaedit@users.sourceforge.net>
2006-02-26 14:39:33 +0000

    Alexey Kryukov's patch to fix an error message.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1140826512;
-const char *source_modtime_str = "16:15 24-Feb-2006";
-const char *source_version_str = "20060224";
+const time_t source_modtime = 1140964847;
+const char *source_modtime_str = "06:40 26-Feb-2006";
+const char *source_version_str = "20060226";
