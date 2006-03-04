6c2450a606b13a73c3c2e25e153c77ad49aae78f
pfaedit <pfaedit>
2006-03-04 00:43:41 +0000

    Some tweaks to cubic->quadratic

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1141429939;
-const char *source_modtime_str = "15:52 3-Mar-2006";
+const time_t source_modtime = 1141433062;
+const char *source_modtime_str = "16:44 3-Mar-2006";
 const char *source_version_str = "20060303";
