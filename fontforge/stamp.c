662e6b9c4b2930cec637f8278f56c58a736c975e
pfaedit <pfaedit>
2006-03-06 01:02:35 +0000

    Werner wants CC (composite character) entries in afm for GPOS mark-to-base features.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1141451120;
-const char *source_modtime_str = "21:45 3-Mar-2006";
-const char *source_version_str = "20060303";
+const time_t source_modtime = 1141570528;
+const char *source_modtime_str = "06:55 5-Mar-2006";
+const char *source_version_str = "20060305";
