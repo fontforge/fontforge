752ce9508ca28c2b4b823ee7abff7ce8245e5552
pfaedit <pfaedit>
2006-03-06 13:48:15 +0000

    Patch by Kanou. cvknife didn't compile in one mode.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1141622562;
-const char *source_modtime_str = "21:22 5-Mar-2006";
-const char *source_version_str = "20060305";
+const time_t source_modtime = 1141653010;
+const char *source_modtime_str = "05:50 6-Mar-2006";
+const char *source_version_str = "20060306";
