f0b59f8895ac07c004f364bf7613902bb57f9ff5
pfaedit <pfaedit>
2006-03-07 06:06:48 +0000

    In an effort to make simplify/merge work better, I decided to slow it down by a factor of two. (Start my binary search from both the best point and the second best). This helps some...

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1141689159;
-const char *source_modtime_str = "15:52 6-Mar-2006";
+const time_t source_modtime = 1141711622;
+const char *source_modtime_str = "22:07 6-Mar-2006";
 const char *source_version_str = "20060306";
