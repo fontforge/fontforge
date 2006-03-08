235c7115247b1fb6c67b573d36050c75cf8b3f18
pfaedit <pfaedit>
2006-03-08 21:05:18 +0000

    Simplify shouldn't simplify away corner points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1141781262;
-const char *source_modtime_str = "17:27 7-Mar-2006";
-const char *source_version_str = "20060307";
+const time_t source_modtime = 1141851302;
+const char *source_modtime_str = "12:55 8-Mar-2006";
+const char *source_version_str = "20060308";
