fd0c8ea479382a86495274179edd0b8a81d759e5
George Williams <pfaedit@users.sourceforge.net>
2006-03-08 22:34:56 +0000

    Barry.SCHWARTZ points out that my "cfg" files contain no newlines. Shows how much use they've received:-)

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1141857151;
-const char *source_modtime_str = "14:32 8-Mar-2006";
+const time_t source_modtime = 1141857369;
+const char *source_modtime_str = "14:36 8-Mar-2006";
 const char *source_version_str = "20060308";
