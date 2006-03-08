ee7d0125bf29ee1ebe8761c984c436ed6a5a5236
pfaedit <pfaedit>
2006-03-08 01:26:34 +0000

    Simplify is now too slow. Make it faster.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1141772264;
-const char *source_modtime_str = "14:57 7-Mar-2006";
+const time_t source_modtime = 1141781262;
+const char *source_modtime_str = "17:27 7-Mar-2006";
 const char *source_version_str = "20060307";
