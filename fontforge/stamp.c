93564c3b95ab82a8b661d28c152964f1b95788c9
George Williams <pfaedit@users.sourceforge.net>
2006-03-10 04:25:58 +0000

    Oops. Last patch was wrong.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1141963011;
-const char *source_modtime_str = "19:56 9-Mar-2006";
+const time_t source_modtime = 1141964771;
+const char *source_modtime_str = "20:26 9-Mar-2006";
 const char *source_version_str = "20060309";
