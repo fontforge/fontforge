539a34f47f16fb08981194363a8238c58b149537
pfaedit <pfaedit>
2006-03-12 04:17:46 +0000

    more on autohinting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142039298;
-const char *source_modtime_str = "17:08 10-Mar-2006";
-const char *source_version_str = "20060310";
+const time_t source_modtime = 1142132672;
+const char *source_modtime_str = "19:04 11-Mar-2006";
+const char *source_version_str = "20060311";
