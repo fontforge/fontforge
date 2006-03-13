a93af77da89a698c3c9b3803ebb8cbc09bb06722
pfaedit <pfaedit>
2006-03-13 15:54:59 +0000

    Include some synonems from Richard Kinch's TrueTeX list.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142132672;
-const char *source_modtime_str = "19:04 11-Mar-2006";
-const char *source_version_str = "20060311";
+const time_t source_modtime = 1142223145;
+const char *source_modtime_str = "20:12 12-Mar-2006";
+const char *source_version_str = "20060312";
