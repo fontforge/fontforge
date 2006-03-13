9651d9cac762dc7627b02edd2b780a0676663cee
George Williams <pfaedit@users.sourceforge.net>
2006-03-13 16:08:08 +0000

    Make ff aware of ExtraLight as a valid weight when setting a default value for the PS weight string.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142265256;
-const char *source_modtime_str = "07:54 13-Mar-2006";
-const char *source_version_str = "20060313";
+const time_t source_modtime = 1142225642;
+const char *source_modtime_str = "20:54 12-Mar-2006";
+const char *source_version_str = "20060312";
