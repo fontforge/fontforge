38c30569f0ace8e50b4d4036d395ab71e4786221
George Williams <pfaedit@users.sourceforge.net>
2006-03-13 16:09:48 +0000

    New version of the stem database routines. They seem to do a better job of finding hints.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142225642;
-const char *source_modtime_str = "20:54 12-Mar-2006";
-const char *source_version_str = "20060312";
+const time_t source_modtime = 1142266059;
+const char *source_modtime_str = "08:07 13-Mar-2006";
+const char *source_version_str = "20060313";
