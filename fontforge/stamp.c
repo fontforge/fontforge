f3dc76b6f7c04728fa53e6545aff116151cd877c
pfaedit <pfaedit>
2006-03-13 16:04:52 +0000

    When enter a textfield with attached pulldown list, ff would get an event saying the first entry on the list had been chosen. This meant that the Tag dialog (used for new ligatures/substitutions/etc. would show the first tag in the list instead of blank.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142223145;
-const char *source_modtime_str = "20:12 12-Mar-2006";
-const char *source_version_str = "20060312";
+const time_t source_modtime = 1142265256;
+const char *source_modtime_str = "07:54 13-Mar-2006";
+const char *source_version_str = "20060313";
