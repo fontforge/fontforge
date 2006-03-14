ae42e32b71bbe84154299395b99c6e8907796429
pfaedit <pfaedit>
2006-03-14 23:35:48 +0000

    In Find Problems, the Flipped Reference check could fail if the reference contained a contours which intersected (themselves or each other).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142266059;
-const char *source_modtime_str = "08:07 13-Mar-2006";
-const char *source_version_str = "20060313";
+const time_t source_modtime = 1142377472;
+const char *source_modtime_str = "15:04 14-Mar-2006";
+const char *source_version_str = "20060314";
