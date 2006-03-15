0a1490630ae39e956549e8ceac99e50b1027a0d5
pfaedit <pfaedit>
2006-03-15 01:30:28 +0000

    Another case where cubic->quadratic failed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142384756;
-const char *source_modtime_str = "17:05 14-Mar-2006";
+const time_t source_modtime = 1142386174;
+const char *source_modtime_str = "17:29 14-Mar-2006";
 const char *source_version_str = "20060314";
