ab2ed60ed38b27f0812a7ad33f5a0a3e520bd4b6
George Williams <pfaedit@users.sourceforge.net>
2006-03-15 17:29:07 +0000

    In some error messages I ended a sentence with a comma. Make it a full stop.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142442953;
-const char *source_modtime_str = "09:15 15-Mar-2006";
+const time_t source_modtime = 1142443717;
+const char *source_modtime_str = "09:28 15-Mar-2006";
 const char *source_version_str = "20060315";
