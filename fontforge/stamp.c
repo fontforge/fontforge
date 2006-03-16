b0de1c830b1bcd489b1a5ace94651971190729b2
George Williams <pfaedit@users.sourceforge.net>
2006-03-16 14:39:00 +0000

    Scripting command AskUser doesn't work unless FF_SCRIPT_IN_LATIN1 is set. Patch by Kanou.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142443717;
-const char *source_modtime_str = "09:28 15-Mar-2006";
-const char *source_version_str = "20060315";
+const time_t source_modtime = 1142519957;
+const char *source_modtime_str = "06:39 16-Mar-2006";
+const char *source_version_str = "20060316";
