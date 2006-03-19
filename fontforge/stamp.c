3c98447c8bdfb7c3c51d86132af7ba717f82a004
pfaedit <pfaedit>
2006-03-19 22:02:11 +0000

    Refresh font view(s) after changing glyph name/unicode in glyph info in case those are being displayed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142805418;
-const char *source_modtime_str = "13:56 19-Mar-2006";
+const time_t source_modtime = 1142805609;
+const char *source_modtime_str = "14:00 19-Mar-2006";
 const char *source_version_str = "20060319";
