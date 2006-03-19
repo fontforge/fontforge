cf9d22c3133d76154fa6415adbe5eb2798b908c2
George Williams <pfaedit@users.sourceforge.net>
2006-03-19 04:58:48 +0000

    More hinting fixes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142740764;
-const char *source_modtime_str = "19:59 18-Mar-2006";
+const time_t source_modtime = 1142744366;
+const char *source_modtime_str = "20:59 18-Mar-2006";
 const char *source_version_str = "20060318";
