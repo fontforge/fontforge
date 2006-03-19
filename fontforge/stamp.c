783e48f09220c2d0c592e80ff45904913c3edc39
pfaedit <pfaedit>
2006-03-19 21:51:31 +0000

    If a name entered in the goto dlg was a single unicode char, then goto would treat it as such. Normally this is fine, but a user might use the underscore character (for example) as a glyph name and not assign it to U+005F. So check for the name too.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142804503;
-const char *source_modtime_str = "13:41 19-Mar-2006";
+const time_t source_modtime = 1142804949;
+const char *source_modtime_str = "13:49 19-Mar-2006";
 const char *source_version_str = "20060319";
