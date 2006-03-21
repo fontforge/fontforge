38dc40ef8506a91dec483db327e999c902a55595
pfaedit <pfaedit>
2006-03-21 23:21:31 +0000

    More work on font compare.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1142957555;
-const char *source_modtime_str = "08:12 21-Mar-2006";
+const time_t source_modtime = 1142982903;
+const char *source_modtime_str = "15:15 21-Mar-2006";
 const char *source_version_str = "20060321";
