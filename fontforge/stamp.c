e3e46d1808c603ade1ebced67c9588f914d9616b
pfaedit <pfaedit>
2006-03-22 20:41:40 +0000

    Patch from Kanou to prevent the hash function from returning negative numbers.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143004732;
-const char *source_modtime_str = "21:18 21-Mar-2006";
-const char *source_version_str = "20060321";
+const time_t source_modtime = 1143055613;
+const char *source_modtime_str = "11:26 22-Mar-2006";
+const char *source_version_str = "20060322";
