d04dba31d05aaa36c44990b9c8ce3e4d553a2798
George Williams <pfaedit@users.sourceforge.net>
2006-03-23 04:03:33 +0000

    Add Kanou's "fontview" font to the list of fonts searched by default for the fontview window.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143072795;
-const char *source_modtime_str = "16:13 22-Mar-2006";
+const time_t source_modtime = 1143085596;
+const char *source_modtime_str = "19:46 22-Mar-2006";
 const char *source_version_str = "20060322";
