382e9806220d4265423de34f27a30a4dee6f19a6
George Williams <pfaedit@users.sourceforge.net>
2006-03-23 16:27:27 +0000

    When pasting a glyph with hints into an empty glyph, we shouldn't set the "in need of autohinting" flag. (Actually, we might should if the original copy were done in a glyph with out of date hints.... but that gets more complicated).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143124972;
-const char *source_modtime_str = "06:42 23-Mar-2006";
+const time_t source_modtime = 1143130906;
+const char *source_modtime_str = "08:21 23-Mar-2006";
 const char *source_version_str = "20060323";
