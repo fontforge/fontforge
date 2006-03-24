456554093901624206886e9df697806db3850a54
pfaedit <pfaedit>
2006-03-24 23:47:50 +0000

    A merge in a quadratic font produced a straight line. Even when it shouldn't have.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143233650;
-const char *source_modtime_str = "12:54 24-Mar-2006";
+const time_t source_modtime = 1143243913;
+const char *source_modtime_str = "15:45 24-Mar-2006";
 const char *source_version_str = "20060324";
