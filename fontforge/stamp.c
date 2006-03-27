bb2587a637de41655598e7cc840164b5ebdb4253
George Williams <pfaedit@users.sourceforge.net>
2006-03-27 16:04:02 +0000

    One more tweak to getinfo.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143472779;
-const char *source_modtime_str = "07:19 27-Mar-2006";
+const time_t source_modtime = 1143473862;
+const char *source_modtime_str = "07:37 27-Mar-2006";
 const char *source_version_str = "20060327";
