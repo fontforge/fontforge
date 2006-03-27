7939f6abe2332a490cad63f1af2e3db6c292f642
pfaedit <pfaedit>
2006-03-27 15:21:32 +0000

    Instead of generating a quadratic shape at the start, generate a cubic shape and convert to quadratic. The convert routine now knows about how to find integer control points and such.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143471667;
-const char *source_modtime_str = "07:01 27-Mar-2006";
+const time_t source_modtime = 1143472779;
+const char *source_modtime_str = "07:19 27-Mar-2006";
 const char *source_version_str = "20060327";
