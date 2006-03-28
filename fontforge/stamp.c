c46a22d2b1bea3a907e03acfdb167a8b25c56cf2
pfaedit <pfaedit>
2006-03-28 02:00:21 +0000

    Hmm. Sometimes least squares does give the answer we want for merge. So give it a try.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143508405;
-const char *source_modtime_str = "17:13 27-Mar-2006";
+const time_t source_modtime = 1143511018;
+const char *source_modtime_str = "17:56 27-Mar-2006";
 const char *source_version_str = "20060327";
