25a40fe31fb4d7c02216c6946502cb97e850978d
George Williams <pfaedit@users.sourceforge.net>
2006-03-30 05:26:13 +0000

    The pen tool didn't handle quadratic splines properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143692774;
-const char *source_modtime_str = "20:26 29-Mar-2006";
+const time_t source_modtime = 1143696404;
+const char *source_modtime_str = "21:26 29-Mar-2006";
 const char *source_version_str = "20060329";
