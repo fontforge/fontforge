b6878687acc8e9396b08bb8ddbe1d923ea1d40e5
pfaedit <pfaedit>
2006-04-05 03:56:02 +0000

    FF still has problems when glyphs contain contours which start with control points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143764366;
-const char *source_modtime_str = "16:19 30-Mar-2006";
+const time_t source_modtime = 1143780575;
+const char *source_modtime_str = "20:49 30-Mar-2006";
 const char *source_version_str = "20060330";
