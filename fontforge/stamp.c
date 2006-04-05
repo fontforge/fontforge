dc34b2125261b2ac9a0aeca80b111ee08be54522
pfaedit <pfaedit>
2006-04-05 03:52:48 +0000

    If a font contained a .notdef glyph the when generating a sfnt, ff would usually put the wrong value for its advance width into the 'hmtx' table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1143761486;
-const char *source_modtime_str = "15:31 30-Mar-2006";
+const time_t source_modtime = 1143764366;
+const char *source_modtime_str = "16:19 30-Mar-2006";
 const char *source_version_str = "20060330";
