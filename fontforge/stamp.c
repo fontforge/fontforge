0b18c19eb78ca74c5fb49745663898e1c14f32cb
pfaedit <pfaedit>
2006-04-06 17:15:08 +0000

    Interesting. White space is defined differently after eexec than it is in other areas of PostScript. White space after eexec does not include formfeed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1144332159;
-const char *source_modtime_str = "07:02 6-Apr-2006";
+const time_t source_modtime = 1144343547;
+const char *source_modtime_str = "10:12 6-Apr-2006";
 const char *source_version_str = "20060406";
