09eb7c9ad5429337f2746bd67c43020983d32ae8
pfaedit <pfaedit>
2006-04-06 05:01:35 +0000

    Grump. Windows won't install a type1 font if the fontname is longer than 31 characters.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1144186295;
-const char *source_modtime_str = "14:31 4-Apr-2006";
-const char *source_version_str = "20060404";
+const time_t source_modtime = 1144299506;
+const char *source_modtime_str = "21:58 5-Apr-2006";
+const char *source_version_str = "20060405";
