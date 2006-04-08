60b27fd4ee3449378ed456f2ef28a4f8e6118e53
George Williams <pfaedit@users.sourceforge.net>
2006-04-08 04:29:55 +0000

    Werner wants to see the raw numbers for point locations.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1144470509;
-const char *source_modtime_str = "21:28 7-Apr-2006";
+const time_t source_modtime = 1144470705;
+const char *source_modtime_str = "21:31 7-Apr-2006";
 const char *source_version_str = "20060407";
