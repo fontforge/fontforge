1313c7f1b1cc53f82751066516a7ff3b53818d3c
pfaedit <pfaedit>
2006-04-08 19:52:16 +0000

    People seem to want sfddiff. So rewrite it as a fontforge script and add it back into the install process.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1144470705;
-const char *source_modtime_str = "21:31 7-Apr-2006";
-const char *source_version_str = "20060407";
+const time_t source_modtime = 1144524331;
+const char *source_modtime_str = "12:25 8-Apr-2006";
+const char *source_version_str = "20060408";
