be22da9b926fee43b9cdbc24ed9371a322f51d0a
pfaedit <pfaedit>
2006-04-11 04:35:52 +0000

    Oops a scripting function was marked as not needing a font when it did (HasPreservedTable & friends).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1144560954;
-const char *source_modtime_str = "22:35 8-Apr-2006";
-const char *source_version_str = "20060408";
+const time_t source_modtime = 1144730132;
+const char *source_modtime_str = "21:35 10-Apr-2006";
+const char *source_version_str = "20060410";
