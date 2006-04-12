f0a8beb0809842ec82672bbe7e7aac55a4c0a16d
George Williams <pfaedit@users.sourceforge.net>
2006-04-12 15:24:17 +0000

    We crashed if we changed from one cid sub-font to another when compacted.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1144798706;
-const char *source_modtime_str = "16:38 11-Apr-2006";
-const char *source_version_str = "20060411";
+const time_t source_modtime = 1144855366;
+const char *source_modtime_str = "08:22 12-Apr-2006";
+const char *source_version_str = "20060412";
