953275cf11465c9692bb95145cab8a6289f8f5a0
George Williams <pfaedit@users.sourceforge.net>
2006-04-14 20:49:10 +0000

    Hmm. sizeof(point)==8 on AMD64, but structures are aligned to 4. This means our chunk alloc routines complain. So if we're given something that isn't in units of sizeof(pointer) then just round it up.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1144981127;
-const char *source_modtime_str = "19:18 13-Apr-2006";
-const char *source_version_str = "20060413";
+const time_t source_modtime = 1145047640;
+const char *source_modtime_str = "13:47 14-Apr-2006";
+const char *source_version_str = "20060414";
