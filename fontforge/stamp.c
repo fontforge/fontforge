b2ad2ece72a0e68b9a2ebe3b91b830bec45bd657
pfaedit <pfaedit>
2006-04-16 02:44:11 +0000

    Oops. When parsing mac state machines I was using sizeof(long) when I should have said sizeof(int32). It gave rather bad results on 64bit machines.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1145131321;
-const char *source_modtime_str = "13:02 15-Apr-2006";
+const time_t source_modtime = 1145155038;
+const char *source_modtime_str = "19:37 15-Apr-2006";
 const char *source_version_str = "20060415";
