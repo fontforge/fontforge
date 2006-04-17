a38c66d6199b4d7e900ec7dc352218702f9bb623
George Williams <pfaedit@users.sourceforge.net>
2006-04-17 21:46:17 +0000

    The stem database work failed when we had tiny splines. Splines smaller than the rounding error in solving a cubic for example.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1145281599;
-const char *source_modtime_str = "06:46 17-Apr-2006";
+const time_t source_modtime = 1145310081;
+const char *source_modtime_str = "14:41 17-Apr-2006";
 const char *source_version_str = "20060417";
