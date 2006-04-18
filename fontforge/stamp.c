eb9ab80dacf381f4c441747c0d5835fffa5a805f
George Williams <pfaedit@users.sourceforge.net>
2006-04-18 05:41:15 +0000

    Get rid of some more memory leaks.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1145313506;
-const char *source_modtime_str = "15:38 17-Apr-2006";
+const time_t source_modtime = 1145339007;
+const char *source_modtime_str = "22:43 17-Apr-2006";
 const char *source_version_str = "20060417";
