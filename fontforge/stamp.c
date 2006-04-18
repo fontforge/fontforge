4bb689e607ac9bf4da3dbaa933785a27d5d83532
George Williams <pfaedit@users.sourceforge.net>
2006-04-18 14:58:17 +0000

    More leaks.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1145339007;
-const char *source_modtime_str = "22:43 17-Apr-2006";
-const char *source_version_str = "20060417";
+const time_t source_modtime = 1145372297;
+const char *source_modtime_str = "07:58 18-Apr-2006";
+const char *source_version_str = "20060418";
