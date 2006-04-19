2dff4b36b80aafa7c1cff3d81b782f0c0cc75643
pfaedit <pfaedit>
2006-04-19 19:51:41 +0000

    Random cleanup. Another leak or two.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1145471908;
-const char *source_modtime_str = "11:38 19-Apr-2006";
+const time_t source_modtime = 1145475874;
+const char *source_modtime_str = "12:44 19-Apr-2006";
 const char *source_version_str = "20060419";
