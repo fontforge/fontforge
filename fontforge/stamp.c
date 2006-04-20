311094988f71b05b92b9bc65ab9407948398fa03
George Williams <pfaedit@users.sourceforge.net>
2006-04-20 04:48:47 +0000

    The 'TeX ' table was output incorrectly if glyphs order wasn't the same in the sf as in the ttf.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1145475874;
-const char *source_modtime_str = "12:44 19-Apr-2006";
+const time_t source_modtime = 1145506804;
+const char *source_modtime_str = "21:20 19-Apr-2006";
 const char *source_version_str = "20060419";
