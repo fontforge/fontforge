94b3bb77fdb2eb24f9da8f79d8223e3d733904d9
George Williams <pfaedit@users.sourceforge.net>
2006-04-25 00:21:39 +0000

    More hinting fixes. If the edges of a stem aren't quite parallel we had problems

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1145658154;
-const char *source_modtime_str = "15:22 21-Apr-2006";
-const char *source_version_str = "20060421";
+const time_t source_modtime = 1145924386;
+const char *source_modtime_str = "17:19 24-Apr-2006";
+const char *source_version_str = "20060424";
