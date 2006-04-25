273cadd207459cba361e0f8b8de9fb052116b600
pfaedit <pfaedit>
2006-04-25 21:58:40 +0000

    Add a pref to control whether we prefer to use spacing or combining accents when building accented glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1145973982;
-const char *source_modtime_str = "07:06 25-Apr-2006";
+const time_t source_modtime = 1146002017;
+const char *source_modtime_str = "14:53 25-Apr-2006";
 const char *source_version_str = "20060425";
