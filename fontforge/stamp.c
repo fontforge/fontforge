40ae8ea9ea4ad319ba0a0e3d75874c00f037dca8
George Williams <pfaedit@users.sourceforge.net>
2006-04-27 19:11:47 +0000

    Users expect ff to recognize a glyph name "Diaeresis", even though Adobe (following standard American, but not European spelling) spells it "Dieresis".

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1146163886;
-const char *source_modtime_str = "11:51 27-Apr-2006";
+const time_t source_modtime = 1146165114;
+const char *source_modtime_str = "12:11 27-Apr-2006";
 const char *source_version_str = "20060427";
