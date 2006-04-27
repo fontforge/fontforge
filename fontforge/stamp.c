79b02ba3d9118245e3a3d183e10fac3ca5332ad7
pfaedit <pfaedit>
2006-04-27 03:33:54 +0000

    Add scripting command: RemoveDetachedGlyphs I note that scripting commands: DetachGlyphs and DetachAndRemoveGlyphs are not documented. Document them.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1146101154;
-const char *source_modtime_str = "18:25 26-Apr-2006";
+const time_t source_modtime = 1146108678;
+const char *source_modtime_str = "20:31 26-Apr-2006";
 const char *source_version_str = "20060426";
