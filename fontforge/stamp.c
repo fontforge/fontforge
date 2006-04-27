8ca012b3142a04cfb4a9ee8de8c93abae1e91f22
pfaedit <pfaedit>
2006-04-27 00:57:53 +0000

    Change some niggling irrelevancies in fontforge's truetype output.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1146004139;
-const char *source_modtime_str = "15:28 25-Apr-2006";
-const char *source_version_str = "20060425";
+const time_t source_modtime = 1146098677;
+const char *source_modtime_str = "17:44 26-Apr-2006";
+const char *source_version_str = "20060426";
