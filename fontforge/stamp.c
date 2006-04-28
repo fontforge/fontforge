5ef2bd83c3bb2848d8d50025c44297ad6872d825
George Williams <pfaedit@users.sourceforge.net>
2006-04-28 03:47:04 +0000

    Doing a Close() from a script didn't work if the windowing system were open.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1146194533;
-const char *source_modtime_str = "20:22 27-Apr-2006";
+const time_t source_modtime = 1146195990;
+const char *source_modtime_str = "20:46 27-Apr-2006";
 const char *source_version_str = "20060427";
