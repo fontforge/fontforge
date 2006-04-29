c51980b885f8dee96ddd10802b04b5ec4f7312e4
pfaedit <pfaedit>
2006-04-29 05:08:41 +0000

    Add a mode where we are alerted immediately when we get a bad o2 spline.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1146271232;
-const char *source_modtime_str = "17:40 28-Apr-2006";
+const time_t source_modtime = 1146287369;
+const char *source_modtime_str = "22:09 28-Apr-2006";
 const char *source_version_str = "20060428";
