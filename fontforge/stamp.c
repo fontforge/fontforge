30f3ca324983af8c0e55422f603021cc6fe4b25e
pfaedit <pfaedit>
2006-04-30 21:17:21 +0000

    Make double clicking in the list of kern pair/anchor combinations bring up either Kern Pair Closeup dlg or Anchors Away.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1146428939;
-const char *source_modtime_str = "13:28 30-Apr-2006";
+const time_t source_modtime = 1146431752;
+const char *source_modtime_str = "14:15 30-Apr-2006";
 const char *source_version_str = "20060430";
