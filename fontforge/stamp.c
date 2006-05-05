95c6912863fd3d5bf9e80011ecee6569d0d77cbf
George Williams <pfaedit@users.sourceforge.net>
2006-05-05 00:12:43 +0000

    Autohinting crashed on glyphs in an order2 font if the glyph contained both an outline and a reference.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1146597860;
-const char *source_modtime_str = "12:24 2-May-2006";
-const char *source_version_str = "20060502";
+const time_t source_modtime = 1146788015;
+const char *source_modtime_str = "17:13 4-May-2006";
+const char *source_version_str = "20060504";
