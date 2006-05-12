98ea85b3b98a423e4312e7a3edc9dfc3f6f550e1
pfaedit <pfaedit>
2006-05-12 21:02:33 +0000

    Was sometimes outputting mkmk base-mark attachement points as the mark attachment point.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1147063624;
-const char *source_modtime_str = "21:47 7-May-2006";
-const char *source_version_str = "20060507";
+const time_t source_modtime = 1147446767;
+const char *source_modtime_str = "08:12 12-May-2006";
+const char *source_version_str = "20060512";
