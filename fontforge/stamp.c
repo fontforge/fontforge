3dd09eedf7d6e7379f0fe30bb3bad4809585798c
pfaedit <pfaedit>
2006-06-01 13:17:04 +0000

    If we had a contextual substution and the subs was by coverage table and the coverage table had the same glyph name twice then we'd get an IError. It shouldn't happen in the first place, but an IError is extreme.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1147470787;
-const char *source_modtime_str = "14:53 12-May-2006";
-const char *source_version_str = "20060512";
+const time_t source_modtime = 1149001796;
+const char *source_modtime_str = "08:09 30-May-2006";
+const char *source_version_str = "20060530";
