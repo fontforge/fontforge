0c2bce913042edfab6be0ab45dd321b00b11fbfa
pfaedit <pfaedit>
2006-06-01 13:21:48 +0000

    More bad hinting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1149002064;
-const char *source_modtime_str = "08:14 30-May-2006";
+const time_t source_modtime = 1149007013;
+const char *source_modtime_str = "09:36 30-May-2006";
 const char *source_version_str = "20060530";
