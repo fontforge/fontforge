839fe70bb31967562398ea7cbb0ad9ec0ea34a37
George Williams <pfaedit@users.sourceforge.net>
2006-06-05 03:59:47 +0000

    In GaramondPremrPro.otf (version 001.000) Adobe has coverage tables (for some context chaining subs) which have the glyphs mentioned multiple times. I suppose this is legal, but it is annoying. I want my strings to have each glyph appear at most once. So add a little thingy to remove duplicate glyphs when we generate a list of glyph names.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1149253661;
-const char *source_modtime_str = "06:07 2-Jun-2006";
-const char *source_version_str = "20060602";
+const time_t source_modtime = 1149479446;
+const char *source_modtime_str = "20:50 4-Jun-2006";
+const char *source_version_str = "20060604";
