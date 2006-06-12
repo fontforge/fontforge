2665b5f395563a90cd427d8b1864424b25195d69
pfaedit <pfaedit>
2006-06-12 19:00:55 +0000

    Add something to the font view's copy from menu to control whether truetype instructions are copied or not.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150124297;
-const char *source_modtime_str = "07:58 12-Jun-2006";
+const time_t source_modtime = 1150138691;
+const char *source_modtime_str = "11:58 12-Jun-2006";
 const char *source_version_str = "20060612";
