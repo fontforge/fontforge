1ef56c86b5a358cf176d238d17a6728fa9599c6d
George Williams <pfaedit@users.sourceforge.net>
2006-06-13 22:40:58 +0000

    FT_Outline and TT_ExecContext have different signedness now for the tags and contours arrays.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150210470;
-const char *source_modtime_str = "07:54 13-Jun-2006";
+const time_t source_modtime = 1150238539;
+const char *source_modtime_str = "15:42 13-Jun-2006";
 const char *source_version_str = "20060613";
