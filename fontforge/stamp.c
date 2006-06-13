a60f878b21109ea7e97e96306b3043d4d1d2c208
George Williams <pfaedit@users.sourceforge.net>
2006-06-13 14:55:30 +0000

    More tweaks for the glyph search in kerning classes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150208194;
-const char *source_modtime_str = "07:16 13-Jun-2006";
+const time_t source_modtime = 1150210470;
+const char *source_modtime_str = "07:54 13-Jun-2006";
 const char *source_version_str = "20060613";
