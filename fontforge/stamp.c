7e9c2d93e7985b82233377c363f4794f7f24a2c0
George Williams <pfaedit@users.sourceforge.net>
2006-06-13 13:24:33 +0000

    The sfd format must support class 0 of kerning classes too.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150204673;
-const char *source_modtime_str = "06:17 13-Jun-2006";
+const time_t source_modtime = 1150205081;
+const char *source_modtime_str = "06:24 13-Jun-2006";
 const char *source_version_str = "20060613";
