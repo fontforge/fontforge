23dc22730719c32eaac35d095b76d7bf593c0b0a
pfaedit <pfaedit>
2006-06-14 04:23:33 +0000

    Printing a multi-size page of a truetype font crashed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150239138;
-const char *source_modtime_str = "15:52 13-Jun-2006";
+const time_t source_modtime = 1150258270;
+const char *source_modtime_str = "21:11 13-Jun-2006";
 const char *source_version_str = "20060613";
