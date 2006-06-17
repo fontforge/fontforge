2a0cf0f40095ea16f686232a2acbeee9ce4e1b6b
pfaedit <pfaedit>
2006-06-17 14:51:12 +0000

    Add some warning messages about out of date instructions, and when we lose instructions, and when point matching goes out of date, etc.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150344159;
-const char *source_modtime_str = "21:02 14-Jun-2006";
-const char *source_version_str = "20060614";
+const time_t source_modtime = 1150555839;
+const char *source_modtime_str = "07:50 17-Jun-2006";
+const char *source_version_str = "20060617";
