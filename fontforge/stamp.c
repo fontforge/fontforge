3a695800a1d69400b2f09adec4706f94ba2874b4
pfaedit <pfaedit>
2006-06-18 15:06:38 +0000

    The View->Display Substitutions has been broken since the encoding change last summer.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150601369;
-const char *source_modtime_str = "20:29 17-Jun-2006";
-const char *source_version_str = "20060617";
+const time_t source_modtime = 1150643184;
+const char *source_modtime_str = "08:06 18-Jun-2006";
+const char *source_version_str = "20060618";
