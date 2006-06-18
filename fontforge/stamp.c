edcf93e1d98589bea07fcee8d52d215739340d8a
pfaedit <pfaedit>
2006-06-18 02:44:53 +0000

    The last change to the cvt table generally did not get made.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150573325;
-const char *source_modtime_str = "12:42 17-Jun-2006";
+const time_t source_modtime = 1150598691;
+const char *source_modtime_str = "19:44 17-Jun-2006";
 const char *source_version_str = "20060617";
