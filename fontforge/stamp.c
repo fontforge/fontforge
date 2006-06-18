5ccf5da8723bce2976be911bc17a56afca6f4eb8
George Williams <pfaedit@users.sourceforge.net>
2006-06-18 15:43:03 +0000

    Typo in scripting function DefaultATT.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150643184;
-const char *source_modtime_str = "08:06 18-Jun-2006";
+const time_t source_modtime = 1150645227;
+const char *source_modtime_str = "08:40 18-Jun-2006";
 const char *source_version_str = "20060618";
