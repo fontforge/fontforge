57252f5c3adad23b6cabb5a725037d1ed505b00a
George Williams <pfaedit@users.sourceforge.net>
2006-06-19 14:53:17 +0000

    Nathan Summers points out that if the XUID is garbage the ff can go into an infinite loop when generating an otf/cff font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150727873;
-const char *source_modtime_str = "07:37 19-Jun-2006";
+const time_t source_modtime = 1150728667;
+const char *source_modtime_str = "07:51 19-Jun-2006";
 const char *source_version_str = "20060619";
