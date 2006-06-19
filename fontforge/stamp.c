e5b1dbd7bc8a680e3efac0a90daf4f39bc1b6102
George Williams <pfaedit@users.sourceforge.net>
2006-06-19 14:49:58 +0000

    Somehow the XUID and UniqueID fields crept of the bottom of the fontinfo Names pane and weren't visible or modifyable. Give them their own pane now.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150692341;
-const char *source_modtime_str = "21:45 18-Jun-2006";
-const char *source_version_str = "20060618";
+const time_t source_modtime = 1150727873;
+const char *source_modtime_str = "07:37 19-Jun-2006";
+const char *source_version_str = "20060619";
