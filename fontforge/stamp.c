f487d6ad047f374c8307e6131bd9178fc33cb04c
pfaedit <pfaedit>
2006-06-20 22:17:46 +0000

    SPLCopyTranslatedHintMasks didn't work if there were more than one contour being copied.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1150728667;
-const char *source_modtime_str = "07:51 19-Jun-2006";
-const char *source_version_str = "20060619";
+const time_t source_modtime = 1150841885;
+const char *source_modtime_str = "15:18 20-Jun-2006";
+const char *source_version_str = "20060620";
