9620e7c2e661e8bf046b92a87edaa18e623b8710
George Williams <pfaedit@users.sourceforge.net>
2006-06-27 14:22:52 +0000

    When asking for default 'subs' features, every glyph gets assigned U+2080.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1151359806;
-const char *source_modtime_str = "15:10 26-Jun-2006";
-const char *source_version_str = "20060626";
+const time_t source_modtime = 1151418112;
+const char *source_modtime_str = "07:21 27-Jun-2006";
+const char *source_version_str = "20060627";
