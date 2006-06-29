0134526ab5ec9c027dfcd0124c2e9f2cac352c34
pfaedit <pfaedit>
2006-06-29 14:08:00 +0000

    Another bug in ExpandStroke.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1151539129;
-const char *source_modtime_str = "16:58 28-Jun-2006";
-const char *source_version_str = "20060628";
+const time_t source_modtime = 1151589556;
+const char *source_modtime_str = "06:59 29-Jun-2006";
+const char *source_version_str = "20060629";
