b14c43f5171f33711a694d8f883cdd46559f9603
pfaedit <pfaedit>
2006-06-30 03:07:15 +0000

    The names of kern class 0 were not retained. The kern pair sub-mode of the kerning class dlg didn't show any glyphs for a class 0 with glyphs in it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1151633290;
-const char *source_modtime_str = "19:08 29-Jun-2006";
+const time_t source_modtime = 1151636861;
+const char *source_modtime_str = "20:07 29-Jun-2006";
 const char *source_version_str = "20060629";
