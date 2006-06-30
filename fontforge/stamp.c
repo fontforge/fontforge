9cef96edfb20580fc530f4f1cda0d373bf1e0ea6
pfaedit <pfaedit>
2006-06-30 01:40:01 +0000

    Add Extrema only looked at how close a point was to the extrema in one dimension. While that dimension is more important, make a less stringent check on the other.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1151622678;
-const char *source_modtime_str = "16:11 29-Jun-2006";
+const time_t source_modtime = 1151631612;
+const char *source_modtime_str = "18:40 29-Jun-2006";
 const char *source_version_str = "20060629";
