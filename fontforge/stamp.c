3b5b3ffb19367d4895192a4ea5be1158ccac0fe5
pfaedit <pfaedit>
2006-07-01 04:34:10 +0000

    When generating a FOND for a bitmap only sfnt, we created dummy 'NFNT' resources with the wrong resource ids. Which meant we got garbage when the font was used.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1151701211;
-const char *source_modtime_str = "14:00 30-Jun-2006";
+const time_t source_modtime = 1151728531;
+const char *source_modtime_str = "21:35 30-Jun-2006";
 const char *source_version_str = "20060630";
