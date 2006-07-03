e5699118507b0102736fc9e26d09f8b5b9bce57d
pfaedit <pfaedit>
2006-07-03 22:50:15 +0000

    If we had a font consisting of one or two glyphs and we generated an otf from it then we'd not get a meaningful 'hmtx' table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1151965368;
-const char *source_modtime_str = "15:22 3-Jul-2006";
+const time_t source_modtime = 1151966886;
+const char *source_modtime_str = "15:48 3-Jul-2006";
 const char *source_version_str = "20060703";
