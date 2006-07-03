e7d8545991a3104d704bf20f7ad3ea2ec3bd270a
George Williams <pfaedit@users.sourceforge.net>
2006-07-03 17:35:20 +0000

    Canonical contours crashed on open contours.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1151942598;
-const char *source_modtime_str = "09:03 3-Jul-2006";
+const time_t source_modtime = 1151948117;
+const char *source_modtime_str = "10:35 3-Jul-2006";
 const char *source_version_str = "20060703";
