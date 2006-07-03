60f92947817eb40ee775214b2bc4f426d29baf73
pfaedit <pfaedit>
2006-07-03 18:46:11 +0000

    Type2 charstrings have no closepath operator. I used to think that meant I could not omit the final vector. In fact it means I can omit the final vector AND the closepath operator.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1151950953;
-const char *source_modtime_str = "11:22 3-Jul-2006";
+const time_t source_modtime = 1151952263;
+const char *source_modtime_str = "11:44 3-Jul-2006";
 const char *source_version_str = "20060703";
