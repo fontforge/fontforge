e2dcb1df62c37e7da405d61fc9d83382c1b37667
George Williams <pfaedit@users.sourceforge.net>
2006-07-14 13:08:54 +0000

    The SetPref() scripting function was misdocumented to be called SetPrefs() so accepte both names.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1152836964;
-const char *source_modtime_str = "17:29 13-Jul-2006";
-const char *source_version_str = "20060713";
+const time_t source_modtime = 1152882594;
+const char *source_modtime_str = "06:09 14-Jul-2006";
+const char *source_version_str = "20060714";
