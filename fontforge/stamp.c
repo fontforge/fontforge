78c02d5661aac2453932583d745bd014350818fe
pfaedit <pfaedit>
2006-07-16 00:56:40 +0000

    When loading an apple bitmap font (with no hmtx tables) we need to dummy up widths for the outline glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1153008082;
-const char *source_modtime_str = "17:01 15-Jul-2006";
+const time_t source_modtime = 1153011337;
+const char *source_modtime_str = "17:55 15-Jul-2006";
 const char *source_version_str = "20060715";
