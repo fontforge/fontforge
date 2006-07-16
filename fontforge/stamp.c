b30bc323bd4d3b363ece8d61ea13baf3744bad94
George Williams <pfaedit@users.sourceforge.net>
2006-07-16 19:23:03 +0000

    For mutually exclusive features there must be an "Off" feature, which generally does nothing.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1153011337;
-const char *source_modtime_str = "17:55 15-Jul-2006";
-const char *source_version_str = "20060715";
+const time_t source_modtime = 1153077476;
+const char *source_modtime_str = "12:17 16-Jul-2006";
+const char *source_version_str = "20060716";
