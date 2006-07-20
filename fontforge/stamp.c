1e79c8b843dde8c9af19377956b2d8058002b5ff
George Williams <pfaedit@users.sourceforge.net>
2006-07-20 15:11:50 +0000

    The cantremove bit on ttf names wasn't always initialized.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1153194626;
-const char *source_modtime_str = "20:50 17-Jul-2006";
-const char *source_version_str = "20060717";
+const time_t source_modtime = 1153407529;
+const char *source_modtime_str = "07:58 20-Jul-2006";
+const char *source_version_str = "20060720";
