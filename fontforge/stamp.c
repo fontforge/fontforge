3d7db72ce984c0c916127e66eb0a4f467f4545d2
George Williams <pfaedit@users.sourceforge.net>
2006-07-23 03:27:54 +0000

    Merge & Simplify would sometimes generate solutions using control points which were diametrically opposed to what they should be.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1153611218;
-const char *source_modtime_str = "16:33 22-Jul-2006";
+const time_t source_modtime = 1153625265;
+const char *source_modtime_str = "20:27 22-Jul-2006";
 const char *source_version_str = "20060722";
