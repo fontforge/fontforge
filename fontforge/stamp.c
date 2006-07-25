446d1db98f703b6ef3269f368f065381c86e4334
George Williams <pfaedit@users.sourceforge.net>
2006-07-25 15:21:25 +0000

    ff could crash on an sfd file containing erroneous anchors (such as might be generated by the merge fonts command).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1153708070;
-const char *source_modtime_str = "19:27 23-Jul-2006";
-const char *source_version_str = "20060723";
+const time_t source_modtime = 1153840728;
+const char *source_modtime_str = "08:18 25-Jul-2006";
+const char *source_version_str = "20060725";
