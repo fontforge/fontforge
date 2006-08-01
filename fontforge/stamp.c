86b763ff2ce8de2dedb21836818a101c2fbae6e2
pfaedit <pfaedit>
2006-08-01 14:59:42 +0000

    Kanou needs to disambiguate between "Points" (meaning a location on the screen) and "Points" (meaning 1/72.27 of an inch).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1154108398;
-const char *source_modtime_str = "10:39 28-Jul-2006";
-const char *source_version_str = "20060728";
+const time_t source_modtime = 1154444105;
+const char *source_modtime_str = "07:55 1-Aug-2006";
+const char *source_version_str = "20060801";
