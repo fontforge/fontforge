f2ede448a93af20cf85ba2b2529bfd38fb6d9651
George Williams <pfaedit@users.sourceforge.net>
2006-08-04 02:36:07 +0000

    Open contours caused the ttf point renumberer to crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1154554659;
-const char *source_modtime_str = "14:37 2-Aug-2006";
-const char *source_version_str = "20060802";
+const time_t source_modtime = 1154657441;
+const char *source_modtime_str = "19:10 3-Aug-2006";
+const char *source_version_str = "20060803";
