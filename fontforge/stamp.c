d276ce861bfc2d017c777e82e753fed1ed513d84
pfaedit <pfaedit>
2006-08-04 16:46:57 +0000

    Mark classes could not be set from the selection.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1154661196;
-const char *source_modtime_str = "20:13 3-Aug-2006";
-const char *source_version_str = "20060803";
+const time_t source_modtime = 1154709766;
+const char *source_modtime_str = "09:42 4-Aug-2006";
+const char *source_version_str = "20060804";
