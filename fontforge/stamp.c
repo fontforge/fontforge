b06bbbfd97f82a9b0d1a4173cce2ae2ff46769c2
pfaedit <pfaedit>
2006-08-04 02:59:24 +0000

    Empty glyphs weren't marked with red Xs any more.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1154658624;
-const char *source_modtime_str = "19:30 3-Aug-2006";
+const time_t source_modtime = 1154660065;
+const char *source_modtime_str = "19:54 3-Aug-2006";
 const char *source_version_str = "20060803";
