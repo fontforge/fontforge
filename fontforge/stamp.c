40426ee4e221a556ac8c624056a699e880e87683
George Williams <pfaedit@users.sourceforge.net>
2006-08-18 21:12:19 +0000

    CODE2000 contains an anchor class with only base glyphs (no mark glyphs) as part of a set of anchor classes. FF crashed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1155931539;
-const char *source_modtime_str = "13:05 18-Aug-2006";
+const time_t source_modtime = 1155934633;
+const char *source_modtime_str = "13:57 18-Aug-2006";
 const char *source_version_str = "20060818";
