17dff24f1d50843ce3dee1d2d5568f71c3f0f957
George Williams <pfaedit@users.sourceforge.net>
2006-08-20 19:23:42 +0000

    Was getting stems which weren't stems.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1155939102;
-const char *source_modtime_str = "15:11 18-Aug-2006";
-const char *source_version_str = "20060818";
+const time_t source_modtime = 1156049053;
+const char *source_modtime_str = "21:44 19-Aug-2006";
+const char *source_version_str = "20060819";
