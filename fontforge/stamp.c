15910d340eaf12869ee49abd6f650fa1ff530393
George Williams <pfaedit@users.sourceforge.net>
2006-08-21 23:03:26 +0000

    Produce a hint at the bottom of a circumflex.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1156105344;
-const char *source_modtime_str = "13:22 20-Aug-2006";
-const char *source_version_str = "20060820";
+const time_t source_modtime = 1156198265;
+const char *source_modtime_str = "15:11 21-Aug-2006";
+const char *source_version_str = "20060821";
