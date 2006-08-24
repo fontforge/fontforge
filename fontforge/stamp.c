90c6e8fcc47ec6b66c4cab44018aa44b8b91da1b
George Williams <pfaedit@users.sourceforge.net>
2006-08-24 23:53:13 +0000

    If applemode was not set and "bitmap only sfnt" was set then ff would probably crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1156259752;
-const char *source_modtime_str = "08:15 22-Aug-2006";
-const char *source_version_str = "20060822";
+const time_t source_modtime = 1156463313;
+const char *source_modtime_str = "16:48 24-Aug-2006";
+const char *source_version_str = "20060824";
