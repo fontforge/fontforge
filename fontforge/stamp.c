d3d82a21266bda3299695ca7d1dc9627a6a13800
George Williams <pfaedit@users.sourceforge.net>
2006-08-28 13:54:35 +0000

    further work on the cmap picker.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1156739109;
-const char *source_modtime_str = "21:25 27-Aug-2006";
-const char *source_version_str = "20060827";
+const time_t source_modtime = 1156772917;
+const char *source_modtime_str = "06:48 28-Aug-2006";
+const char *source_version_str = "20060828";
