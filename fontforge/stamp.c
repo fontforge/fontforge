a6302db0d9d8e553703c88a2e4770df16d984924
pfaedit <pfaedit>
2006-08-28 04:28:01 +0000

    Apple has deprecated some more useful functions. Their online docs even have the gall to say that this happened in 10.4. My 10.4.0 system does not think they are deprecated, but more recent systems do.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1156736423;
-const char *source_modtime_str = "20:40 27-Aug-2006";
+const time_t source_modtime = 1156739109;
+const char *source_modtime_str = "21:25 27-Aug-2006";
 const char *source_version_str = "20060827";
