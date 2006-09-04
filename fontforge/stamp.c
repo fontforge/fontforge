91962d016e6eb967a8add325a0261fbdd669d742
George Williams <pfaedit@users.sourceforge.net>
2006-09-04 03:45:04 +0000

    Oops. The vertical metrics bit wasn't preserved when flattening a cid font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1157207730;
-const char *source_modtime_str = "07:35 2-Sep-2006";
-const char *source_version_str = "20060902";
+const time_t source_modtime = 1157340988;
+const char *source_modtime_str = "20:36 3-Sep-2006";
+const char *source_version_str = "20060903";
