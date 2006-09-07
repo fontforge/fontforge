f69e8b2570b6b4f4a59bc8b1e056510e8ac75da9
pfaedit <pfaedit>
2006-09-07 03:13:00 +0000

    Again when adding accents to the left of a glyph we should shift the glyph over to give it a reasonable lbearing.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1157597182;
-const char *source_modtime_str = "19:46 6-Sep-2006";
+const time_t source_modtime = 1157598686;
+const char *source_modtime_str = "20:11 6-Sep-2006";
 const char *source_version_str = "20060906";
