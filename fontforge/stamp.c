5a1975d86bf577dbc4ee0b58ad401eabb8a0e349
George Williams <pfaedit@users.sourceforge.net>
2006-09-07 02:55:20 +0000

    When building an accented glyph, we generally want use_my_metrics set on the reference. But if we add an accent to right (horn) or left (psili/varia on cap greeks) we want to turn it off. We did turn it off on the right side, but not on the left.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1157573132;
-const char *source_modtime_str = "13:05 6-Sep-2006";
+const time_t source_modtime = 1157597182;
+const char *source_modtime_str = "19:46 6-Sep-2006";
 const char *source_version_str = "20060906";
