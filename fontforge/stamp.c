9cc7d99b8e7d74a90002fc878258e90a8a002c3f
pfaedit <pfaedit>
2006-09-10 03:33:22 +0000

    MS has changed their indic shaping engine. It still uses the same features but they are applied differently. To disambiguate this change they have created a parallel set of script tags for the indic writing system. FontForge should recognize these and use them when appropriate.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1157850514;
-const char *source_modtime_str = "18:08 9-Sep-2006";
+const time_t source_modtime = 1157858832;
+const char *source_modtime_str = "20:27 9-Sep-2006";
 const char *source_version_str = "20060909";
