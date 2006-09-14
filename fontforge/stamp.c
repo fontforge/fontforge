40385d0e630a067dace1171d9210d888a1be6dfd
pfaedit <pfaedit>
2006-09-14 13:21:32 +0000

    Ligature carets were not being scaled by scale to em.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158238488;
-const char *source_modtime_str = "05:54 14-Sep-2006";
+const time_t source_modtime = 1158239875;
+const char *source_modtime_str = "06:17 14-Sep-2006";
 const char *source_version_str = "20060914";
