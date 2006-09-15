ac54b4734adbc9a5dbad7b52286c8c15db00ba2e
George Williams <pfaedit@users.sourceforge.net>
2006-09-15 21:29:30 +0000

    Are we going outside array bounds?

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158290050;
-const char *source_modtime_str = "20:14 14-Sep-2006";
-const char *source_version_str = "20060914";
+const time_t source_modtime = 1158355492;
+const char *source_modtime_str = "14:24 15-Sep-2006";
+const char *source_version_str = "20060915";
