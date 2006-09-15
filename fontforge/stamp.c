35217591a1c2f3c55ec7a0c574129b7eaa7bddb3
pfaedit <pfaedit>
2006-09-15 00:00:53 +0000

    FF would crash if $HOME did not exist.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158276440;
-const char *source_modtime_str = "16:27 14-Sep-2006";
+const time_t source_modtime = 1158278204;
+const char *source_modtime_str = "16:56 14-Sep-2006";
 const char *source_version_str = "20060914";
