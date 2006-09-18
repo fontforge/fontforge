ef9ed9c126c4c3dfc8b524441a2a52e65856d70f
pfaedit <pfaedit>
2006-09-18 03:46:26 +0000

    The two preference items, AlwaysGenApple, and AlwaysGenOpenType, were a bad choice on my part and have lead to considerable confusion. I mentioned some time ago that I would remove them if there were no objection.
    There was no objection.
    
    I have now removed them.
    
    I also made the SetPrefs scripting function sort of work if the user
    tries to set one of these (it will set the appropriate flags in the
    default option set used by generate).
    
    The defaults of these flags were different on the Mac from everywhere
    else. This meant that a script could do different things on the Mac and
    on linux. That seems like a bad idea. I have changed things so that the
    default options setting for generate is always OpenType.
    
    THIS IS AN INCOMPATIBLE CHANGE AND SCRIPTS ON THE MAC COULD NOW BEHAVE
    DIFFERENTLY.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158549921;
-const char *source_modtime_str = "20:25 17-Sep-2006";
+const time_t source_modtime = 1158551118;
+const char *source_modtime_str = "20:45 17-Sep-2006";
 const char *source_version_str = "20060917";
