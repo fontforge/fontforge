35e3161baed0bf655951dceb39faadf7708aef47
pfaedit <pfaedit>
2006-09-20 03:11:58 +0000

    Didn't terminate a list.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158627277;
-const char *source_modtime_str = "17:54 18-Sep-2006";
-const char *source_version_str = "20060918";
+const time_t source_modtime = 1158721407;
+const char *source_modtime_str = "20:03 19-Sep-2006";
+const char *source_version_str = "20060919";
