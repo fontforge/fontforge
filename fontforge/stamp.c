7240abaa05ea98448a279cd9525ff28925ebf5fd
pfaedit <pfaedit>
2006-09-21 23:50:32 +0000

    Add scripting interfaces to many of the ctype functions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158874097;
-const char *source_modtime_str = "14:28 21-Sep-2006";
+const time_t source_modtime = 1158881717;
+const char *source_modtime_str = "16:35 21-Sep-2006";
 const char *source_version_str = "20060921";
