3a1c8bb49bebce53ddfdaf7adda3b1e2ed45707e
George Williams <pfaedit@users.sourceforge.net>
2006-09-21 22:18:25 +0000

    Add a scripting interface to the 'size' feature, now that Adobe has finally decided what to do about 'size'.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158811812;
-const char *source_modtime_str = "21:10 20-Sep-2006";
-const char *source_version_str = "20060920";
+const time_t source_modtime = 1158874097;
+const char *source_modtime_str = "14:28 21-Sep-2006";
+const char *source_version_str = "20060921";
