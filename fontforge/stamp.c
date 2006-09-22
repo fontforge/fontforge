b66ca3074f7a67e797497868a8f24570311e868d
pfaedit <pfaedit>
2006-09-22 12:40:07 +0000

    Various fixes for Michal N.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158881717;
-const char *source_modtime_str = "16:35 21-Sep-2006";
-const char *source_version_str = "20060921";
+const time_t source_modtime = 1158928501;
+const char *source_modtime_str = "05:35 22-Sep-2006";
+const char *source_version_str = "20060922";
