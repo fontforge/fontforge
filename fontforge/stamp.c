071cca3fb4e6ec93ccec1b6473496ff12f2dd41a
George Williams <pfaedit@users.sourceforge.net>
2006-09-22 20:54:31 +0000

    Boxify the maxp dialog and friends.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1158928501;
-const char *source_modtime_str = "05:35 22-Sep-2006";
+const time_t source_modtime = 1158958280;
+const char *source_modtime_str = "13:51 22-Sep-2006";
 const char *source_version_str = "20060922";
