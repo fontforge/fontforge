db31b35d26e5ee381aec321e426f01bac468a803
George Williams <pfaedit@users.sourceforge.net>
2006-09-27 15:34:17 +0000

    Parsing an empty list of ttf instructions crashed Michael Zedler's machine.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1159308408;
-const char *source_modtime_str = "15:06 26-Sep-2006";
-const char *source_version_str = "20060926";
+const time_t source_modtime = 1159371007;
+const char *source_modtime_str = "08:30 27-Sep-2006";
+const char *source_version_str = "20060927";
