637d466105d068056010f7f515edcddb36ede5b7
pfaedit <pfaedit>
2006-09-27 17:09:16 +0000

    Converted a utf8 string to unicode using latin1 conventions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1159371007;
-const char *source_modtime_str = "08:30 27-Sep-2006";
+const time_t source_modtime = 1159376545;
+const char *source_modtime_str = "10:02 27-Sep-2006";
 const char *source_version_str = "20060927";
