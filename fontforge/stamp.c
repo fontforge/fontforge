459cba56118509c57f813d5c4be708280db3645b
pfaedit <pfaedit>
2006-09-29 14:29:00 +0000

    Extra semicolon is illegal in declarations.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1159539854;
-const char *source_modtime_str = "07:24 29-Sep-2006";
+const time_t source_modtime = 1159540065;
+const char *source_modtime_str = "07:27 29-Sep-2006";
 const char *source_version_str = "20060929";
