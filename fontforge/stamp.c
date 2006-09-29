2db6bffd8455db368d463fe41c5cb1c20b290c76
pfaedit <pfaedit>
2006-09-29 14:25:30 +0000

    Extranious underscore.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1159499845;
-const char *source_modtime_str = "20:17 28-Sep-2006";
-const char *source_version_str = "20060928";
+const time_t source_modtime = 1159539854;
+const char *source_modtime_str = "07:24 29-Sep-2006";
+const char *source_version_str = "20060929";
