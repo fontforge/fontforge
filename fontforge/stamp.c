2c0396a505e8e1bef4f11a6bae82fa84391661cb
George Williams <pfaedit@users.sourceforge.net>
2006-09-29 03:23:44 +0000

    Rats. Ralph Levien's patch did not work. But maybe now I'm using FT_Outline_Get_Bitmap properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1159389960;
-const char *source_modtime_str = "13:46 27-Sep-2006";
-const char *source_version_str = "20060927";
+const time_t source_modtime = 1159499845;
+const char *source_modtime_str = "20:17 28-Sep-2006";
+const char *source_version_str = "20060928";
