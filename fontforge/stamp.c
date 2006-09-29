7582e8120c2df8599adc9ce19823cfa04eec696f
George Williams <pfaedit@users.sourceforge.net>
2006-09-29 21:57:45 +0000

    After editing instructions the debugger would incorrectly complain that instructions were out of date.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1159542339;
-const char *source_modtime_str = "08:05 29-Sep-2006";
+const time_t source_modtime = 1159566814;
+const char *source_modtime_str = "14:53 29-Sep-2006";
 const char *source_version_str = "20060929";
