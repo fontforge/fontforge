e12b17a54682ec24d01394beec512da25a7bee28
George Williams <pfaedit@users.sourceforge.net>
2006-10-02 20:13:42 +0000

    Disambiguate width here. Use boxes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1159566814;
-const char *source_modtime_str = "14:53 29-Sep-2006";
-const char *source_version_str = "20060929";
+const time_t source_modtime = 1159819877;
+const char *source_modtime_str = "13:11 2-Oct-2006";
+const char *source_version_str = "20061002";
