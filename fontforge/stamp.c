df66024428e96a8420cf9ce08c8e6624827a631a
pfaedit <pfaedit>
2006-10-03 13:36:56 +0000

    Add Polish styles to the translation list.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1159846531;
-const char *source_modtime_str = "20:35 2-Oct-2006";
-const char *source_version_str = "20061002";
+const time_t source_modtime = 1159882205;
+const char *source_modtime_str = "06:30 3-Oct-2006";
+const char *source_version_str = "20061003";
