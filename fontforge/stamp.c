5a81d797db9ead23f4a5c59d0294fb4f47f05fca
George Williams <pfaedit@users.sourceforge.net>
2006-10-05 14:34:44 +0000

    broke pairwise positioning dlg when I boxed it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1160023122;
-const char *source_modtime_str = "21:38 4-Oct-2006";
-const char *source_version_str = "20061004";
+const time_t source_modtime = 1160058584;
+const char *source_modtime_str = "07:29 5-Oct-2006";
+const char *source_version_str = "20061005";
