ce2b1e0e8012fa84e27af8509695b30904c1511e
pfaedit <pfaedit>
2006-10-06 22:03:45 +0000

    The encoding change of last summer broke tfm output with glyph lists or extension characters.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1160171355;
-const char *source_modtime_str = "14:49 6-Oct-2006";
+const time_t source_modtime = 1160171860;
+const char *source_modtime_str = "14:57 6-Oct-2006";
 const char *source_version_str = "20061006";
