c7381f9da95996edbcd72ac4de1e2ffe0ca5ce5f
George Williams <pfaedit@users.sourceforge.net>
2006-10-08 18:31:19 +0000

    Alexej Kryukov points out that reverting a glyph with anchors lost the anchors.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1160226225;
-const char *source_modtime_str = "06:03 7-Oct-2006";
-const char *source_version_str = "20061007";
+const time_t source_modtime = 1160332023;
+const char *source_modtime_str = "11:27 8-Oct-2006";
+const char *source_version_str = "20061008";
