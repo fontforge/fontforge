cb9dba239b9d502b0548b4c7ff96bfedacdb0239
George Williams <pfaedit@users.sourceforge.net>
2006-10-09 20:36:41 +0000

    Adding Greek translations for font styles. (Thanks to Apostolos)

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1160423317;
-const char *source_modtime_str = "12:48 9-Oct-2006";
+const time_t source_modtime = 1160425893;
+const char *source_modtime_str = "13:31 9-Oct-2006";
 const char *source_version_str = "20061009";
