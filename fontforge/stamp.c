c3aa06b912850d30ac6a21ce8db7ecda738333f4
pfaedit <pfaedit>
2006-10-09 19:53:19 +0000

    If we aren't dealing with a TeX math font, then the "Sub/super"script positioning fields of Glyph Info->TeX aren't meaningful. So grey them out.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1160362090;
-const char *source_modtime_str = "19:48 8-Oct-2006";
-const char *source_version_str = "20061008";
+const time_t source_modtime = 1160423317;
+const char *source_modtime_str = "12:48 9-Oct-2006";
+const char *source_version_str = "20061009";
