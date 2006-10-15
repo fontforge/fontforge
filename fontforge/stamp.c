ae1f2c49c96a3cfd8eabdb16c38f86209f2c464c
pfaedit <pfaedit>
2006-10-15 22:24:52 +0000

    Michal Nowakowski wants the 'gasp' titles disambiguated.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1160950083;
-const char *source_modtime_str = "15:08 15-Oct-2006";
+const time_t source_modtime = 1160950890;
+const char *source_modtime_str = "15:21 15-Oct-2006";
 const char *source_version_str = "20061015";
