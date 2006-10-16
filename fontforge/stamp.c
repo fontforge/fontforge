752e99c515b337fad0e35f8ae2c342d5c66207da
George Williams <pfaedit@users.sourceforge.net>
2006-10-16 19:54:32 +0000

    Disambiguation for Polish.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1160950890;
-const char *source_modtime_str = "15:21 15-Oct-2006";
-const char *source_version_str = "20061015";
+const time_t source_modtime = 1161028121;
+const char *source_modtime_str = "12:48 16-Oct-2006";
+const char *source_version_str = "20061016";
