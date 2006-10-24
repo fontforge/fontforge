53ad1528253ff4be9e63b7ea32779b1037f91d24
pfaedit <pfaedit>
2006-10-24 15:58:17 +0000

    Add scripting commands 	Revert 	RevertToBackup Make the "+" operator concatenate arrays   [1,2] + [3,4] == [1,2,3,4]   [1,2] + 3 == [1,2,3]

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1161703100;
-const char *source_modtime_str = "08:18 24-Oct-2006";
+const time_t source_modtime = 1161705168;
+const char *source_modtime_str = "08:52 24-Oct-2006";
 const char *source_version_str = "20061024";
