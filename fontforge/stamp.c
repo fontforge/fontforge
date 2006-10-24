23dfe53fbaa9c01e87b41ed15c4413bd04b37043
pfaedit <pfaedit>
2006-10-24 02:06:25 +0000

    Make it possible to save the Font Display dlg as an image.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1161648755;
-const char *source_modtime_str = "17:12 23-Oct-2006";
+const time_t source_modtime = 1161655354;
+const char *source_modtime_str = "19:02 23-Oct-2006";
 const char *source_version_str = "20061023";
