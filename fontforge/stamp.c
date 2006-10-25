4fc2e6a70087212a2a7648835cfc59072b91e76a
pfaedit <pfaedit>
2006-10-25 16:46:41 +0000

    Add a Ucs4 scripting command (reverses Utf8).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1161789364;
-const char *source_modtime_str = "08:16 25-Oct-2006";
+const time_t source_modtime = 1161792245;
+const char *source_modtime_str = "09:04 25-Oct-2006";
 const char *source_version_str = "20061025";
