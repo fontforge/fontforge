f1e29e67020f5bf4a61f8b60604267fcc0f78558
George Williams <pfaedit@users.sourceforge.net>
2006-10-26 22:53:16 +0000

    Unguarded use of isalpha caused a crash outside of BMP.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1161901730;
-const char *source_modtime_str = "15:28 26-Oct-2006";
+const time_t source_modtime = 1161902823;
+const char *source_modtime_str = "15:47 26-Oct-2006";
 const char *source_version_str = "20061026";
