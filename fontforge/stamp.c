ccb6c1e90ab539f3f5d714ae1be58b8b2419acd6
George Williams <pfaedit@users.sourceforge.net>
2006-10-26 22:31:22 +0000

    Patch from Raph Levien. FF mishandled seac if the accent were not translated.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1161831744;
-const char *source_modtime_str = "20:02 25-Oct-2006";
-const char *source_version_str = "20061025";
+const time_t source_modtime = 1161901730;
+const char *source_modtime_str = "15:28 26-Oct-2006";
+const char *source_version_str = "20061026";
