75edca048b75e86dc8361a13ac4fe3c318c7631b
pfaedit <pfaedit>
2006-10-27 22:25:55 +0000

    Tweak Raph's patch slightly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1161902823;
-const char *source_modtime_str = "15:47 26-Oct-2006";
-const char *source_version_str = "20061026";
+const time_t source_modtime = 1161987793;
+const char *source_modtime_str = "15:23 27-Oct-2006";
+const char *source_version_str = "20061027";
