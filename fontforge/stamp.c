fd591f4e48cf4ede9719dac0822e97919b665bed
George Williams <pfaedit@users.sourceforge.net>
2006-10-29 23:02:55 +0000

    Converting from the mac korean encoding didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1162159680;
-const char *source_modtime_str = "14:08 29-Oct-2006";
+const time_t source_modtime = 1162162735;
+const char *source_modtime_str = "14:58 29-Oct-2006";
 const char *source_version_str = "20061029";
