1bc8ea6f52013427a0f4bc38d1580e2df16bb66c
George Williams <pfaedit@users.sourceforge.net>
2006-10-30 15:29:21 +0000

    When examining a bitmap font, metrics view would crash when asked to look at a non-existant glyph.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1162163426;
-const char *source_modtime_str = "15:10 29-Oct-2006";
-const char *source_version_str = "20061029";
+const time_t source_modtime = 1162221966;
+const char *source_modtime_str = "07:26 30-Oct-2006";
+const char *source_version_str = "20061030";
