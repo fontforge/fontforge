a0584fc1f2b0d8c0d7194bc11dcf97f269d4eb3d
George Williams <pfaedit@users.sourceforge.net>
2006-11-14 03:33:56 +0000

    StrSplit & StrJoin scripting functions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1163467928;
-const char *source_modtime_str = "17:32 13-Nov-2006";
+const time_t source_modtime = 1163473678;
+const char *source_modtime_str = "19:07 13-Nov-2006";
 const char *source_version_str = "20061113";
