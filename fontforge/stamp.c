e6ab45a6a9e7cdc242663e53dba01fc44d08c53e
pfaedit <pfaedit>
2006-11-19 20:50:34 +0000

    Patch from Mike FABIAN. Rasterizing by freetype only initialized a variable if CONFIG_TYPE3 (multilayer) was set.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1163473678;
-const char *source_modtime_str = "19:07 13-Nov-2006";
-const char *source_version_str = "20061113";
+const time_t source_modtime = 1163969319;
+const char *source_modtime_str = "12:48 19-Nov-2006";
+const char *source_version_str = "20061119";
