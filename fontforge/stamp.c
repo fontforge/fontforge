84ce402ee1145b75622b0b4fac5fcb499a3f66e9
George Williams <pfaedit@users.sourceforge.net>
2006-11-19 21:18:32 +0000

    Apostolos points out that NAME_MAX isn't defined on solaris, but _POSIX_NAME_MAX is. But _POSIX_NAME_MAX doesn't seem to be defined on linux.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1163970121;
-const char *source_modtime_str = "13:02 19-Nov-2006";
+const time_t source_modtime = 1163970954;
+const char *source_modtime_str = "13:15 19-Nov-2006";
 const char *source_version_str = "20061119";
