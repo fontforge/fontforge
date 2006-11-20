402860f66ce8050b91b7d58297335b27ff9ccffc
pfaedit <pfaedit>
2006-11-20 21:13:30 +0000

    The pixel grid in truetype debugging (RasterGridColor) was invisible against the light blue background.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1164056102;
-const char *source_modtime_str = "12:55 20-Nov-2006";
+const time_t source_modtime = 1164056982;
+const char *source_modtime_str = "13:09 20-Nov-2006";
 const char *source_version_str = "20061120";
