c8d0242f288bf21699267a8b188e3cd57d45e0d9
pfaedit <pfaedit>
2006-11-20 22:01:00 +0000

    When debugging I provided popup information for anti-aliased rasters. But I didn't for mono rasters.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1164056982;
-const char *source_modtime_str = "13:09 20-Nov-2006";
+const time_t source_modtime = 1164059761;
+const char *source_modtime_str = "13:56 20-Nov-2006";
 const char *source_version_str = "20061120";
