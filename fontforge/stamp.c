767e0acc19b79dce6616e890c362096e9848079a
pfaedit <pfaedit>
2006-12-03 00:34:38 +0000

    Deal with the case where we have a sequence of control points before any indication of curve/qcurve.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1165023113;
-const char *source_modtime_str = "17:31 1-Dec-2006";
-const char *source_version_str = "20061201";
+const time_t source_modtime = 1165105858;
+const char *source_modtime_str = "16:30 2-Dec-2006";
+const char *source_version_str = "20061202";
