b7a47013c77561ed176880b21c1ca4150a766811
pfaedit <pfaedit>
2006-12-12 23:41:42 +0000

    Werner points out that when double clicking on a bitmap glyph in the font view, the glyph is created / rasterized without using freetype.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1165965418;
-const char *source_modtime_str = "15:16 12-Dec-2006";
+const time_t source_modtime = 1165966731;
+const char *source_modtime_str = "15:38 12-Dec-2006";
 const char *source_version_str = "20061212";
