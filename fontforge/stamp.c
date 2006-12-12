187f2461c1de56e90cf0de8ab663d4a734f58b61
George Williams <pfaedit@users.sourceforge.net>
2006-12-12 23:12:19 +0000

    FF was still setting firstcharindex in OS/2 incorrectly. The problem is that glyph 1 (or 2?) has encoding 0 in the mac cmap table while it is not referenced at all in the windows cmap.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1165944097;
-const char *source_modtime_str = "09:21 12-Dec-2006";
+const time_t source_modtime = 1165964652;
+const char *source_modtime_str = "15:04 12-Dec-2006";
 const char *source_version_str = "20061212";
