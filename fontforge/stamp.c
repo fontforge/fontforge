a8979aed2ed5af240eeac037b278c595c8db1baf
George Williams <pfaedit@users.sourceforge.net>
2006-12-13 01:57:47 +0000

    there was an uninitialized variable used in parsing contextual by coverage table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1165966731;
-const char *source_modtime_str = "15:38 12-Dec-2006";
+const time_t source_modtime = 1165974626;
+const char *source_modtime_str = "17:50 12-Dec-2006";
 const char *source_version_str = "20061212";
