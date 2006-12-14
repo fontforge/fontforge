228812f1a0f03e3054176c726553a982f2b5546d
pfaedit <pfaedit>
2006-12-14 21:33:04 +0000

    In a font without any LCG characters, fontforge set the OS/2 capheight and xheight fields to -1e10. It should have been 0. Patch by Roozbeh Pournader.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1166016553;
-const char *source_modtime_str = "05:29 13-Dec-2006";
-const char *source_version_str = "20061213";
+const time_t source_modtime = 1166131794;
+const char *source_modtime_str = "13:29 14-Dec-2006";
+const char *source_version_str = "20061214";
