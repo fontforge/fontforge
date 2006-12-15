44adebe8133a35e52993b26447de9ebdc87c70a2
pfaedit <pfaedit>
2006-12-15 01:23:52 +0000

    Make default char of OS/2 be space for otf and .notdef for ttf. This is a guess at what should be done.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1166139502;
-const char *source_modtime_str = "15:38 14-Dec-2006";
+const time_t source_modtime = 1166145431;
+const char *source_modtime_str = "17:17 14-Dec-2006";
 const char *source_version_str = "20061214";
