b8ceb0f0b55f75bed9726e3a35f1c7e1c29eaba3
George Williams <pfaedit@users.sourceforge.net>
2006-12-15 21:39:07 +0000

    Werner cared that the changed flag wasn't being set in Remove ATT.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1166218156;
-const char *source_modtime_str = "13:29 15-Dec-2006";
+const time_t source_modtime = 1166218591;
+const char *source_modtime_str = "13:36 15-Dec-2006";
 const char *source_version_str = "20061215";
