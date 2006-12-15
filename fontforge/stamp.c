a1c88a2aab519d6de5549c5676075c86e8e15161
George Williams <pfaedit@users.sourceforge.net>
2006-12-15 01:44:18 +0000

    GetPosSub scripting command trashed memory.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1166145431;
-const char *source_modtime_str = "17:17 14-Dec-2006";
+const time_t source_modtime = 1166146124;
+const char *source_modtime_str = "17:28 14-Dec-2006";
 const char *source_version_str = "20061214";
