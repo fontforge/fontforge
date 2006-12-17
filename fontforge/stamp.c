bf0fd6acc15505e6cc5948b681afb706cab916d5
George Williams <pfaedit@users.sourceforge.net>
2006-12-17 14:17:44 +0000

    Werner doesn't like that I always add dummy bitmaps for .notdef, .null & nonmarkingreturn. It seemed logical to me that if I were prepared to add those glyphs to the outline font I should be prepared to add them to the bitmap strike. But Werner (I think) sees bitmap strikes as containing exceptional glyphs which need special handling, whereas I just saw them as complete fonts. So in this context I guess there is no need to add these glyphs.
    As a compromise I have decided that I will add the glyphs to the bitmap strike if I added the glyphs to the outline font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1166219077;
-const char *source_modtime_str = "13:44 15-Dec-2006";
-const char *source_version_str = "20061215";
+const time_t source_modtime = 1166364743;
+const char *source_modtime_str = "06:12 17-Dec-2006";
+const char *source_version_str = "20061217";
