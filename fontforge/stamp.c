d890d90bce339fb86ab2bcce15d21d9e8312c439
pfaedit <pfaedit>
2006-12-18 03:08:17 +0000

    The docs don't mention this, but... mangal.ttf has a NULL offset for the lookahead class definition, so if that happens -- no classes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1166364743;
-const char *source_modtime_str = "06:12 17-Dec-2006";
+const time_t source_modtime = 1166411002;
+const char *source_modtime_str = "19:03 17-Dec-2006";
 const char *source_version_str = "20061217";
