6409ba985c5c734510015e62451c12df043a4797
pfaedit <pfaedit>
2006-12-20 19:23:17 +0000

    Uninitialized variable. Patch by Mike FABIAN of suse.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1166584209;
-const char *source_modtime_str = "19:10 19-Dec-2006";
-const char *source_version_str = "20061219";
+const time_t source_modtime = 1166642576;
+const char *source_modtime_str = "11:22 20-Dec-2006";
+const char *source_version_str = "20061220";
