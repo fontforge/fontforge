0ee482aaea6158951cfcbe171ca96eb0412561ea
pfaedit <pfaedit>
2006-12-21 16:26:09 +0000

    ReTag dlg would crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1166717486;
-const char *source_modtime_str = "08:11 21-Dec-2006";
+const time_t source_modtime = 1166718251;
+const char *source_modtime_str = "08:24 21-Dec-2006";
 const char *source_version_str = "20061221";
