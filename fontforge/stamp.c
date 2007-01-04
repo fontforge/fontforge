9e8acf9fac8d1db26c4031e5cc0190be0f9fa9d1
George Williams <pfaedit@users.sourceforge.net>
2007-01-04 22:48:59 +0000

    Oops, can only set the grayscale field of the freetype exc structure if version number is 2.1.10 or more (It didn't exist before then).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1167925837;
-const char *source_modtime_str = "07:50 4-Jan-2007";
+const time_t source_modtime = 1167926000;
+const char *source_modtime_str = "07:53 4-Jan-2007";
 const char *source_version_str = "20070104";
