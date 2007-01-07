db407051bc6aff13b565b42b0b86895caa3fe9ff
George Williams <pfaedit@users.sourceforge.net>
2007-01-07 13:45:10 +0000

    Werner points out that what I call "Type42 CID" should actually be called either "Type11" or "CID Type 2".

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1167954498;
-const char *source_modtime_str = "15:48 4-Jan-2007";
-const char *source_version_str = "20070104";
+const time_t source_modtime = 1168177416;
+const char *source_modtime_str = "05:43 7-Jan-2007";
+const char *source_version_str = "20070107";
