4d9a8190aa77bd5a6c23e02fd8e172b3ca7db95f
George Williams <pfaedit@users.sourceforge.net>
2007-01-12 00:02:59 +0000

    Force point numbers to be correct when changing glyphs in the charview.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1168557864;
-const char *source_modtime_str = "15:24 11-Jan-2007";
+const time_t source_modtime = 1168560074;
+const char *source_modtime_str = "16:01 11-Jan-2007";
 const char *source_version_str = "20070111";
