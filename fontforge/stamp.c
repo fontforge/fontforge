c212c095f658584a5d74d1f513d2e312ad33fded
pfaedit <pfaedit>
2007-01-18 05:16:09 +0000

    Gleep. Werner points out that when generating bitmap strikes, ff would always generate a postscript font to rasterize, so any truetype instructions would be ignored.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1168636441;
-const char *source_modtime_str = "13:14 12-Jan-2007";
-const char *source_version_str = "20070112";
+const time_t source_modtime = 1169097338;
+const char *source_modtime_str = "21:15 17-Jan-2007";
+const char *source_version_str = "20070117";
