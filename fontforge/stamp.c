ee6c5d40617ff60da5ddb7b2bcd0017761ee8897
George Williams <pfaedit@users.sourceforge.net>
2007-01-18 05:30:32 +0000

    Werner points out that removing a bitmap glyph from the bitmapview does not force a redisplay in the fontview.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1169097338;
-const char *source_modtime_str = "21:15 17-Jan-2007";
+const time_t source_modtime = 1169098161;
+const char *source_modtime_str = "21:29 17-Jan-2007";
 const char *source_version_str = "20070117";
