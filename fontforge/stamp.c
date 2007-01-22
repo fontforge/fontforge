8091eaca2094b7a751a7ce417c60d49495eff06e
George Williams <pfaedit@users.sourceforge.net>
2007-01-22 15:41:13 +0000

    Add hot key for Select->Points on selected contours.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1169478834;
-const char *source_modtime_str = "07:13 22-Jan-2007";
+const time_t source_modtime = 1169480363;
+const char *source_modtime_str = "07:39 22-Jan-2007";
 const char *source_version_str = "20070122";
