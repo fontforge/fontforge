25aa107329158c026d5829162918d5f280fed4da
George Williams <pfaedit@users.sourceforge.net>
2007-01-22 15:16:01 +0000

    Assign "dotlessj" to map to 0x237, the unicode code point, rather than to 0xf6be, the old Adobe private use area. !!!! THIS DOES NOT CONFORM TO THE AGL !!!!!

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1169259629;
-const char *source_modtime_str = "18:20 19-Jan-2007";
-const char *source_version_str = "20070119";
+const time_t source_modtime = 1169478834;
+const char *source_modtime_str = "07:13 22-Jan-2007";
+const char *source_version_str = "20070122";
