df093d5cd532eef8902412692e7e16da35fcf535
pfaedit <pfaedit>
2007-01-22 16:10:23 +0000

    Werner points out that if you have a series of the same glyph in a row in the encoding (generally .notdef) and you are in a char view, then pressing Next Glyph does nothing. You are stuck at the same code point. (same thing happens for Prev Glyph).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1169481316;
-const char *source_modtime_str = "07:55 22-Jan-2007";
+const time_t source_modtime = 1169482061;
+const char *source_modtime_str = "08:07 22-Jan-2007";
 const char *source_version_str = "20070122";
