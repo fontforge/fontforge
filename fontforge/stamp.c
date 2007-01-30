e6e0fdb96ebbcf9850eeb63dfd712ab5d01b5476
George Williams <pfaedit@users.sourceforge.net>
2007-01-30 03:40:36 +0000

    Typo gives us an unititialized variable when saving ufo.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1170116024;
-const char *source_modtime_str = "16:13 29-Jan-2007";
+const time_t source_modtime = 1170128305;
+const char *source_modtime_str = "19:38 29-Jan-2007";
 const char *source_version_str = "20070129";
