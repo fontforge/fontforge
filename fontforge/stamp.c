306967c8a8b2887af8a2469c0631f46fda4bee96
pfaedit <pfaedit>
2007-02-05 16:10:46 +0000

    Make the ClearHints scripting command take arguments: "Horizontal" or "Vertical" to allow users to choose to clear either horizontal or vertical hints (the old form with no args still works and clears both, of course).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1170128305;
-const char *source_modtime_str = "19:38 29-Jan-2007";
-const char *source_version_str = "20070129";
+const time_t source_modtime = 1170691091;
+const char *source_modtime_str = "07:58 5-Feb-2007";
+const char *source_version_str = "20070205";
