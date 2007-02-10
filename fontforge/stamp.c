e83c0b75cde10fdca56d6e99a239ac594cb50acc
George Williams <pfaedit@users.sourceforge.net>
2007-02-10 02:18:27 +0000

    The Export scripting command did not work as documented. It is documented to export all >selected< glyphs. Instead it exported >all< glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1170995788;
-const char *source_modtime_str = "20:36 8-Feb-2007";
-const char *source_version_str = "20070208";
+const time_t source_modtime = 1171073809;
+const char *source_modtime_str = "18:16 9-Feb-2007";
+const char *source_version_str = "20070209";
