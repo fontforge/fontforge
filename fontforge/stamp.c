2a1f36ea842023e76ae7f530f87884178d9d9f45
George Williams <pfaedit@users.sourceforge.net>
2007-02-16 20:04:38 +0000

    Code which I hope will handle German keyboards and make them do what the menu says they should for certain hot keys (brackets, curly brackets, brokets all get different modifier sequences on a german keyboard than an english).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1171653412;
-const char *source_modtime_str = "11:16 16-Feb-2007";
+const time_t source_modtime = 1171655924;
+const char *source_modtime_str = "11:58 16-Feb-2007";
 const char *source_version_str = "20070216";
