21e47293fb45bd9fc23f0832d1aeb03351c274e8
George Williams <pfaedit@users.sourceforge.net>
2007-02-16 04:19:54 +0000

    Patch by Michal Nowakowski to allow a nicer notation for delta instructions in the instruction editing window: Example: PUSHB_3 6@12 8 1 DELTAP2
    This moves point 8 by six delta-steps (that's clear) at 37 ppem (that's
    not clear at first glance - assuming delta_base is 9, DELTAP2 works
    from 25 ppem to 25+15 ppem, in this example it's 25+12 ppem).
    
    So it's just a way of constructing an argument byte for DELTAP/C.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1171594719;
-const char *source_modtime_str = "18:58 15-Feb-2007";
+const time_t source_modtime = 1171599544;
+const char *source_modtime_str = "20:19 15-Feb-2007";
 const char *source_version_str = "20070215";
