2df8ddaa3af05bdd6daea31136ebb4a243c8dd81
George Williams <pfaedit@users.sourceforge.net>
2007-03-01 05:06:54 +0000

    If a glyph's point numbers weren't ordered properly (and that should not happen, but did), then when adding ttf instructions to the glyph, the instructions would be thrown out as soon as we noticed this problem. So the user would never see the instructions. Now force the points to be ordered before adding the instructions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1172288653;
-const char *source_modtime_str = "19:44 23-Feb-2007";
-const char *source_version_str = "20070223";
+const time_t source_modtime = 1172725465;
+const char *source_modtime_str = "21:04 28-Feb-2007";
+const char *source_version_str = "20070228";
