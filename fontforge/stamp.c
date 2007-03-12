559d47d1f31e76dda79dd02bcef5dfc39ac75980
pfaedit <pfaedit>
2007-03-12 20:15:51 +0000

    If the user specified specific TeX height/depth entries in Glyph Info, those data did not get stored in tfm/ofm files properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1173329722;
-const char *source_modtime_str = "20:55 7-Mar-2007";
-const char *source_version_str = "20070307";
+const time_t source_modtime = 1173730472;
+const char *source_modtime_str = "12:14 12-Mar-2007";
+const char *source_version_str = "20070312";
