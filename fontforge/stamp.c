3025199519f4cb950659f29f0350cc02f13dc35b
pfaedit <pfaedit>
2007-03-14 14:41:18 +0000

    Not following TeX conventions about character depth.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1173739034;
-const char *source_modtime_str = "14:37 12-Mar-2007";
-const char *source_version_str = "20070312";
+const time_t source_modtime = 1173883220;
+const char *source_modtime_str = "06:40 14-Mar-2007";
+const char *source_version_str = "20070314";
