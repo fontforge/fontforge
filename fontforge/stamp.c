d6d44cb79edf785d043d3a190f56156dc8913ded
George Williams <pfaedit@users.sourceforge.net>
2007-03-15 15:16:10 +0000

    Tell users they should add their email address to the License after pressing the [Add SIL Open Font License] button.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1173965369;
-const char *source_modtime_str = "05:29 15-Mar-2007";
+const time_t source_modtime = 1173971642;
+const char *source_modtime_str = "07:14 15-Mar-2007";
 const char *source_version_str = "20070315";
