f2348117944ec952e8df5133ceb3bf0542e69caf
pfaedit <pfaedit>
2007-03-19 14:54:38 +0000

    If one scrolled the fontinfo->lookup window, you could not select things in it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1174276705;
-const char *source_modtime_str = "19:58 18-Mar-2007";
-const char *source_version_str = "20070318";
+const time_t source_modtime = 1174315901;
+const char *source_modtime_str = "06:51 19-Mar-2007";
+const char *source_version_str = "20070319";
