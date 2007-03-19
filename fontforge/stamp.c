00670554ee02d747039b0d3c9e83e5f5b792f8a7
pfaedit <pfaedit>
2007-03-19 17:32:12 +0000

    Add a French translation of the OFL.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1174324235;
-const char *source_modtime_str = "09:10 19-Mar-2007";
+const time_t source_modtime = 1174325462;
+const char *source_modtime_str = "09:31 19-Mar-2007";
 const char *source_version_str = "20070319";
