515ca17f5d075d8f17e9d1a16f4468eeef99fe94
George Williams <pfaedit@users.sourceforge.net>
2007-03-19 19:05:50 +0000

    Grr stupid typo in charinfo.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1174325462;
-const char *source_modtime_str = "09:31 19-Mar-2007";
+const time_t source_modtime = 1174330830;
+const char *source_modtime_str = "11:00 19-Mar-2007";
 const char *source_version_str = "20070319";
