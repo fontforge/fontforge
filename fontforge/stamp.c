bce26bcf9232c7d43ec5440b9e2411d620e9d8c8
pfaedit <pfaedit>
2007-03-19 16:20:59 +0000

    Show ATT didn't work for anchors.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1174315901;
-const char *source_modtime_str = "06:51 19-Mar-2007";
+const time_t source_modtime = 1174320501;
+const char *source_modtime_str = "08:08 19-Mar-2007";
 const char *source_version_str = "20070319";
