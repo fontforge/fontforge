b1e4fc8e2af2e503f2fa5534e9c6896db330f0f0
pfaedit <pfaedit>
2007-03-23 01:24:07 +0000

    Add a pull down list of available script/lang pairs to the script/lang text field in the metrics view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1174588538;
-const char *source_modtime_str = "10:35 22-Mar-2007";
+const time_t source_modtime = 1174612574;
+const char *source_modtime_str = "17:16 22-Mar-2007";
 const char *source_version_str = "20070322";
