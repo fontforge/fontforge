8431846151edb369e701c3ad2da4bd14434f6539
pfaedit <pfaedit>
2007-03-23 22:56:05 +0000

    The "use new indic scripts" flag should be off by default.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1174619026;
-const char *source_modtime_str = "19:03 22-Mar-2007";
-const char *source_version_str = "20070322";
+const time_t source_modtime = 1174690585;
+const char *source_modtime_str = "14:56 23-Mar-2007";
+const char *source_version_str = "20070323";
