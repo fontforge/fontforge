ae5e4fe410de9da2d61938f52c6a703b549186a8
pfaedit <pfaedit>
2007-03-25 22:15:13 +0000

    Couple of problems in parsing kern pairs from glyph info.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1174855348;
-const char *source_modtime_str = "12:42 25-Mar-2007";
+const time_t source_modtime = 1174860822;
+const char *source_modtime_str = "14:13 25-Mar-2007";
 const char *source_version_str = "20070325";
