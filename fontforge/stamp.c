77abcd5002c0b0f4c459ad6083c023d4ad2fab27
pfaedit <pfaedit>
2007-03-25 20:59:35 +0000

    Fix some bugs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1174799597;
-const char *source_modtime_str = "21:13 24-Mar-2007";
-const char *source_version_str = "20070324";
+const time_t source_modtime = 1174855348;
+const char *source_modtime_str = "12:42 25-Mar-2007";
+const char *source_version_str = "20070325";
