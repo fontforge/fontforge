7509dd7ba061d43ebbfaa91b2da9544662c24cc2
pfaedit <pfaedit>
2007-04-16 05:21:08 +0000

    Werner points out that there is no way now to remove individual Pos/Sub data from a glyph (we can remove an entire lookup-subtable, but that's too much). (Old scripting protocol).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1176606233;
-const char *source_modtime_str = "20:03 14-Apr-2007";
-const char *source_version_str = "20070414";
+const time_t source_modtime = 1176700516;
+const char *source_modtime_str = "22:15 15-Apr-2007";
+const char *source_version_str = "20070415";
