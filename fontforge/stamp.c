32bd651dd7e7effba45e1471ba280b8e19d4bc9b
George Williams <pfaedit@users.sourceforge.net>
2007-04-19 23:39:13 +0000

    Add python scripting support for lookups. Redo the way the size feature was handled. Redo the way tt instructions were handled.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1176929212;
-const char *source_modtime_str = "13:46 18-Apr-2007";
-const char *source_version_str = "20070418";
+const time_t source_modtime = 1177025277;
+const char *source_modtime_str = "16:27 19-Apr-2007";
+const char *source_version_str = "20070419";
