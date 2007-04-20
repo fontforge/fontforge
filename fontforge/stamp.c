0fca655366abb7b867729397d435516c976f63f1
pfaedit <pfaedit>
2007-04-20 14:57:32 +0000

    Couple of places where I actually say "FontForge 1.0". Let's bump that to 2.0 now that I've done that for the sfd format.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1177080216;
-const char *source_modtime_str = "07:43 20-Apr-2007";
+const time_t source_modtime = 1177080868;
+const char *source_modtime_str = "07:54 20-Apr-2007";
 const char *source_version_str = "20070420";
