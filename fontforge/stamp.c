c0584f425861f6c87d101d23bf5932a0a7f4bdf9
pfaedit <pfaedit>
2007-04-21 00:25:30 +0000

    Add a transform command to glyph in python.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1177111309;
-const char *source_modtime_str = "16:21 20-Apr-2007";
+const time_t source_modtime = 1177114592;
+const char *source_modtime_str = "17:16 20-Apr-2007";
 const char *source_version_str = "20070420";
