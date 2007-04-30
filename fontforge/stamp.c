990e7ccfcda7867ca9ce555639b369ef3d9a97b6
pfaedit <pfaedit>
2007-04-30 03:20:07 +0000

    Select by lookup subtable had several bugs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1177885401;
-const char *source_modtime_str = "15:23 29-Apr-2007";
+const time_t source_modtime = 1177903180;
+const char *source_modtime_str = "20:19 29-Apr-2007";
 const char *source_version_str = "20070429";
