34d22faac1a481b27e9e3a0c55860d78423ddf41
pfaedit <pfaedit>
2007-04-30 20:28:39 +0000

    Add a "lock" icon beside the width line if the glyph contains a reference with the "use my metrics" bit set.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1177963213;
-const char *source_modtime_str = "13:00 30-Apr-2007";
+const time_t source_modtime = 1177964727;
+const char *source_modtime_str = "13:25 30-Apr-2007";
 const char *source_version_str = "20070430";
