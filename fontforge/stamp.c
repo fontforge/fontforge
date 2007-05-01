0bb31b9dc21c9100cd0ef640849f871809588dba
George Williams <pfaedit@users.sourceforge.net>
2007-05-01 20:11:16 +0000

    Stupid typo.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1177973433;
-const char *source_modtime_str = "15:50 30-Apr-2007";
-const char *source_version_str = "20070430";
+const time_t source_modtime = 1178050211;
+const char *source_modtime_str = "13:10 1-May-2007";
+const char *source_version_str = "20070501";
