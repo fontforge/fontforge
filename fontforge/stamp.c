fa1217571068fcf42f47640adb0efbb4eb66fee9
pfaedit <pfaedit>
2007-05-05 17:28:28 +0000

    Alexej Kryukov points out that the "changed since last hinted" flag gets set inappropriately.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178376260;
-const char *source_modtime_str = "07:44 5-May-2007";
+const time_t source_modtime = 1178386336;
+const char *source_modtime_str = "10:32 5-May-2007";
 const char *source_version_str = "20070505";
