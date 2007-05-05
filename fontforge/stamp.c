52fe596f710c34cc04c069fc7e7563ac89ac7e12
pfaedit <pfaedit>
2007-05-05 18:14:43 +0000

    Metrics View would crash if more than 20 glyphs entered.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178386422;
-const char *source_modtime_str = "10:33 5-May-2007";
+const time_t source_modtime = 1178388852;
+const char *source_modtime_str = "11:14 5-May-2007";
 const char *source_version_str = "20070505";
