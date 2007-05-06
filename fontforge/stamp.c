813f343c0a20ea3fad4e1ca4b33a45e8b08de006
George Williams <pfaedit@users.sourceforge.net>
2007-05-06 14:34:56 +0000

    Inkscape can start X on the mac before it tries to connect. Hmm. I can do that too. Inkscape manages to start X without an xterm. I don't know how to do that.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178398740;
-const char *source_modtime_str = "13:59 5-May-2007";
-const char *source_version_str = "20070505";
+const time_t source_modtime = 1178462199;
+const char *source_modtime_str = "07:36 6-May-2007";
+const char *source_version_str = "20070506";
