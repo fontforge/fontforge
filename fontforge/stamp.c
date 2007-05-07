bdfe52d471a0ff58ee468f4ca554ffce9982e8aa
pfaedit <pfaedit>
2007-05-07 20:28:17 +0000

    Further tweaks to the code to start x on the mac.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178465027;
-const char *source_modtime_str = "08:23 6-May-2007";
-const char *source_version_str = "20070506";
+const time_t source_modtime = 1178535573;
+const char *source_modtime_str = "03:59 7-May-2007";
+const char *source_version_str = "20070507";
