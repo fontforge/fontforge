5b806fa5d76772760f0eff1e88730f88d767e37d
George Williams <pfaedit@users.sourceforge.net>
2007-05-08 23:09:20 +0000

    Typo in realloc of lookups meant that nothing happened (when it should have increased array size by 1).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178577259;
-const char *source_modtime_str = "15:34 7-May-2007";
-const char *source_version_str = "20070507";
+const time_t source_modtime = 1178665532;
+const char *source_modtime_str = "16:05 8-May-2007";
+const char *source_version_str = "20070508";
