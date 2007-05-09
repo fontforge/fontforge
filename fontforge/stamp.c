1e4b7855558cfd072e7f75c658960da27e955b45
pfaedit <pfaedit>
2007-05-09 16:49:47 +0000

    Vertical scrolling in the metrics view tried to scroll the list of features too.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178725665;
-const char *source_modtime_str = "08:47 9-May-2007";
+const time_t source_modtime = 1178729305;
+const char *source_modtime_str = "09:48 9-May-2007";
 const char *source_version_str = "20070509";
