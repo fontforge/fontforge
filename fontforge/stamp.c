d6139b12e5324b6af39a0e15ba2662669e806b87
pfaedit <pfaedit>
2007-05-09 16:44:09 +0000

    Tab, and arrow keys didn't work in the matrixedit widget after I added hidden columns. Make Return do the expected thing (move to start of next line and create it if possible).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178671477;
-const char *source_modtime_str = "17:44 8-May-2007";
-const char *source_version_str = "20070508";
+const time_t source_modtime = 1178725665;
+const char *source_modtime_str = "08:47 9-May-2007";
+const char *source_version_str = "20070509";
