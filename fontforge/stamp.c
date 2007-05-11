12c74feec4db322f57fb601676b8d9687e318799
pfaedit <pfaedit>
2007-05-11 21:14:24 +0000

    Sync up.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178917487;
-const char *source_modtime_str = "14:04 11-May-2007";
+const time_t source_modtime = 1178917880;
+const char *source_modtime_str = "14:11 11-May-2007";
 const char *source_version_str = "20070511";
