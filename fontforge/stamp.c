d7e9f81220d5c7e8bdfe0f3eb671b4ef15605805
pfaedit <pfaedit>
2007-05-11 20:55:56 +0000

    After the user does an [Add Lookup] we should select the new lookup, to make it easier for him/her to do an [Add Subtable].

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1178774283;
-const char *source_modtime_str = "22:18 9-May-2007";
-const char *source_version_str = "20070509";
+const time_t source_modtime = 1178916756;
+const char *source_modtime_str = "13:52 11-May-2007";
+const char *source_version_str = "20070511";
