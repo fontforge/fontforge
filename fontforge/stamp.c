71cb6633a05706d7318c45de9a299ca7f5ab28d6
George Williams <pfaedit@users.sourceforge.net>
2007-05-28 22:19:09 +0000

    Make yet another attempt to correct for rounding errors when loading a postscript font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1180389038;
-const char *source_modtime_str = "14:50 28-May-2007";
+const time_t source_modtime = 1180390650;
+const char *source_modtime_str = "15:17 28-May-2007";
 const char *source_version_str = "20070528";
