63f056181171554d6629a8f907131ff91e1c5e6f
George Williams <pfaedit@users.sourceforge.net>
2007-05-28 21:15:48 +0000

    Display substitutions was broken.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1180384629;
-const char *source_modtime_str = "13:37 28-May-2007";
+const time_t source_modtime = 1180386663;
+const char *source_modtime_str = "14:11 28-May-2007";
 const char *source_version_str = "20070528";
