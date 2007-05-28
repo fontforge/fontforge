d856aeda1f6c424de29d7b30a601a674cb51aa46
George Williams <pfaedit@users.sourceforge.net>
2007-05-28 03:13:25 +0000

    Disambiguate some strings for the polish translation

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1180321708;
-const char *source_modtime_str = "20:08 27-May-2007";
+const time_t source_modtime = 1180322169;
+const char *source_modtime_str = "20:16 27-May-2007";
 const char *source_version_str = "20070527";
