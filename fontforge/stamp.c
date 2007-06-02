5b9a8d116921e264b2957702b84800522337c14d
George Williams <pfaedit@users.sourceforge.net>
2007-06-02 22:38:37 +0000

    Python scripting command font.createChar didn't. Partial patch from MaxRabkin.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1180709948;
-const char *source_modtime_str = "07:59 1-Jun-2007";
-const char *source_version_str = "20070601";
+const time_t source_modtime = 1180823729;
+const char *source_modtime_str = "15:35 2-Jun-2007";
+const char *source_version_str = "20070602";
