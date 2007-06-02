187251fc2703e7398f9c2f062bb4d1ddeab2cb86
pfaedit <pfaedit>
2007-06-02 22:58:33 +0000

    Loading kerning info from a mac 'FOND' resource was broken by the lookup change.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1180823729;
-const char *source_modtime_str = "15:35 2-Jun-2007";
+const time_t source_modtime = 1180825004;
+const char *source_modtime_str = "15:56 2-Jun-2007";
 const char *source_version_str = "20070602";
