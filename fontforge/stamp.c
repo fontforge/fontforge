edaceb0bf36fde24bb50d54463d7e3bcc1157854
pfaedit <pfaedit>
2007-06-03 21:58:11 +0000

    layer += contour  didn't work in python. I was examining a bit through the wrong data type.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1180902682;
-const char *source_modtime_str = "13:31 3-Jun-2007";
+const time_t source_modtime = 1180907703;
+const char *source_modtime_str = "14:55 3-Jun-2007";
 const char *source_version_str = "20070603";
