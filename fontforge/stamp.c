e256d45750f41fe4e1a12f34b2a309b3897656db
George Williams <pfaedit@users.sourceforge.net>
2007-06-04 00:29:30 +0000

    If a glyph contained two copies of another glyph, and one of those copies was off by enough that ff didn't recognize it, the depending on how the contours were ordered, ff might fail to recognize the first glyph too, even though it did match.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1180909091;
-const char *source_modtime_str = "15:18 3-Jun-2007";
+const time_t source_modtime = 1180916798;
+const char *source_modtime_str = "17:26 3-Jun-2007";
 const char *source_version_str = "20070603";
