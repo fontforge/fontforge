6104ccc7eedabe1239abe7605e9d194a79f89a73
George Williams <pfaedit@users.sourceforge.net>
2007-06-06 19:28:58 +0000

    Bug in the selection iterator.
    Also Max Rabkin points out the docs say the iterator returns selected glyphs rather than encoding slots. Fix the docs, then add a member (.byGlyphs) which will make the iterator return glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1181154570;
-const char *source_modtime_str = "11:29 6-Jun-2007";
+const time_t source_modtime = 1181157133;
+const char *source_modtime_str = "12:12 6-Jun-2007";
 const char *source_version_str = "20070606";
