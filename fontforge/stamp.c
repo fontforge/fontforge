92467096262cfee8d18da66bd7f34aa2af435aee
pfaedit <pfaedit>
2007-06-07 23:48:30 +0000

    Remove the code to produce the old, broken, 'size' feature.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1181253063;
-const char *source_modtime_str = "14:51 7-Jun-2007";
+const time_t source_modtime = 1181260092;
+const char *source_modtime_str = "16:48 7-Jun-2007";
 const char *source_version_str = "20070607";
