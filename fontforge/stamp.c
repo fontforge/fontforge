2c1103b083554c2439f296c0f68ef226464866cf
George Williams <pfaedit@users.sourceforge.net>
2007-06-11 20:50:58 +0000

    Oops change for Werner re: bounding box didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1181522607;
-const char *source_modtime_str = "17:43 10-Jun-2007";
-const char *source_version_str = "20070610";
+const time_t source_modtime = 1181594938;
+const char *source_modtime_str = "13:48 11-Jun-2007";
+const char *source_version_str = "20070611";
