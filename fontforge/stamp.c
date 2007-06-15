4fac12899f937fc751d721ae2204ab7bc5379515
pfaedit <pfaedit>
2007-06-15 14:31:03 +0000

    The baseline was not properly located when displaying it in the fontview.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1181915067;
-const char *source_modtime_str = "06:44 15-Jun-2007";
+const time_t source_modtime = 1181917584;
+const char *source_modtime_str = "07:26 15-Jun-2007";
 const char *source_version_str = "20070615";
