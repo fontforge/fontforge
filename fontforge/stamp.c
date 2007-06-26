5bbea3554219af84520f17893d4325854e068f0d
George Williams <pfaedit@users.sourceforge.net>
2007-06-26 21:54:56 +0000

    Graham Asher points out that the meanings of underline position in the 'post' table and the FontInfo dictionary are different. One refers to the top of the underline rectangle and one to the center of it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1182208097;
-const char *source_modtime_str = "16:08 18-Jun-2007";
-const char *source_version_str = "20070618";
+const time_t source_modtime = 1182894488;
+const char *source_modtime_str = "14:48 26-Jun-2007";
+const char *source_version_str = "20070626";
