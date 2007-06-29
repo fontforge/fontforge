f93bcf2a84df6d28e0eccf824cca10025cd6dae3
George Williams <pfaedit@users.sourceforge.net>
2007-06-29 02:19:17 +0000

    mf2pt1 now uses "glyph_dimensions" rather than "bbox"

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183083172;
-const char *source_modtime_str = "19:12 28-Jun-2007";
+const time_t source_modtime = 1183083275;
+const char *source_modtime_str = "19:14 28-Jun-2007";
 const char *source_version_str = "20070628";
