4088e228d2a1656b1c4815da23e8e6e394275de3
George Williams <pfaedit@users.sourceforge.net>
2007-06-29 02:01:29 +0000

    Goto could crash when used on small encodings.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183076859;
-const char *source_modtime_str = "17:27 28-Jun-2007";
+const time_t source_modtime = 1183082545;
+const char *source_modtime_str = "19:02 28-Jun-2007";
 const char *source_version_str = "20070628";
