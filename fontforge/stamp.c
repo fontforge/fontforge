ed951fca62f8c4fa7ca20cfb1e4aad90eb9e84a6
pfaedit <pfaedit>
2007-06-29 14:31:41 +0000

    Oops. Goto dlg bug had two parts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183083275;
-const char *source_modtime_str = "19:14 28-Jun-2007";
-const char *source_version_str = "20070628";
+const time_t source_modtime = 1183127581;
+const char *source_modtime_str = "07:33 29-Jun-2007";
+const char *source_version_str = "20070629";
