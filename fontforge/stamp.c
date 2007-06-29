944321d344f25d13c1ae2b5772905d4229559d0b
George Williams <pfaedit@users.sourceforge.net>
2007-06-29 01:32:10 +0000

    -lang wasn't permitted before -c.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183042049;
-const char *source_modtime_str = "07:47 28-Jun-2007";
+const time_t source_modtime = 1183076859;
+const char *source_modtime_str = "17:27 28-Jun-2007";
 const char *source_version_str = "20070628";
