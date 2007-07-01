fde0e11c510cb560d7a5691ab57064fa40d2fab2
pfaedit <pfaedit>
2007-07-01 23:27:35 +0000

    When building a contextual lookup, don't list that lookup as something that it could invoke (ie. list all lookups in this table (GPOS/GSUB) except for ourselves). Don't want to encourage users to create infinite lookup loops.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183323195;
-const char *source_modtime_str = "13:53 1-Jul-2007";
+const time_t source_modtime = 1183332296;
+const char *source_modtime_str = "16:24 1-Jul-2007";
 const char *source_version_str = "20070701";
