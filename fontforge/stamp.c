558e389ea65314df1261b349e56f1135227043c2
pfaedit <pfaedit>
2007-07-01 20:57:33 +0000

    Point matching didn't work when there were references to references and multiple references within a glyph.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183318173;
-const char *source_modtime_str = "12:29 1-Jul-2007";
+const time_t source_modtime = 1183323195;
+const char *source_modtime_str = "13:53 1-Jul-2007";
 const char *source_version_str = "20070701";
