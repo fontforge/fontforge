adcf18e27a8210d2e2caf2f3bd9314a1348dbc4f
pfaedit <pfaedit>
2007-07-02 19:23:26 +0000

    Problems parsing 'mort' tables could cause a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183332296;
-const char *source_modtime_str = "16:24 1-Jul-2007";
-const char *source_version_str = "20070701";
+const time_t source_modtime = 1183403777;
+const char *source_modtime_str = "12:16 2-Jul-2007";
+const char *source_version_str = "20070702";
