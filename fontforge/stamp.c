499e23fe846d2a82fdd16c316af86a7b5f6a2e5b
George Williams <pfaedit@users.sourceforge.net>
2007-07-05 23:37:48 +0000

    Add ability to display italic side bearings in the char view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183676723;
-const char *source_modtime_str = "16:05 5-Jul-2007";
+const time_t source_modtime = 1183678535;
+const char *source_modtime_str = "16:35 5-Jul-2007";
 const char *source_version_str = "20070705";
