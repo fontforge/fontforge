6815874e03685d3e997d34e64ce421000459b0f4
George Williams <pfaedit@users.sourceforge.net>
2007-07-05 22:36:46 +0000

    If a font did not have any horizontal metrics then ff would not set the em-size.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183674557;
-const char *source_modtime_str = "15:29 5-Jul-2007";
+const time_t source_modtime = 1183674959;
+const char *source_modtime_str = "15:35 5-Jul-2007";
 const char *source_version_str = "20070705";
