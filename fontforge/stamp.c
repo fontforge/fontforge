7385cb5bd9fab770b04a4069332bd2043ee92419
George Williams <pfaedit@users.sourceforge.net>
2007-07-05 20:51:05 +0000

    The import lookups button in fontinfo forgot about the subtables (sort of).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183642555;
-const char *source_modtime_str = "06:35 5-Jul-2007";
+const time_t source_modtime = 1183663224;
+const char *source_modtime_str = "12:20 5-Jul-2007";
 const char *source_version_str = "20070705";
