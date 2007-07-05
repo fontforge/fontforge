e201c8ad42048d81852add32afdbc6cdc1fbd72a
George Williams <pfaedit@users.sourceforge.net>
2007-07-05 22:28:01 +0000

    Wasn't parsing apple's 'lcar' table properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183672491;
-const char *source_modtime_str = "14:54 5-Jul-2007";
+const time_t source_modtime = 1183674334;
+const char *source_modtime_str = "15:25 5-Jul-2007";
 const char *source_version_str = "20070705";
