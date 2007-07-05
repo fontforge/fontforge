2a1ef2833b2c2a64e11624289a8e056707f27692
pfaedit <pfaedit>
2007-07-05 21:58:18 +0000

    if a font contained a 'post' table but didn't name all names (or something like that), then the attempt to name the glyph based on the encoding was broken after the encoding change.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183671411;
-const char *source_modtime_str = "14:36 5-Jul-2007";
+const time_t source_modtime = 1183672491;
+const char *source_modtime_str = "14:54 5-Jul-2007";
 const char *source_version_str = "20070705";
