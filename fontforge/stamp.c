43b7d370299dc39c19d10447f2bb6e745ef8cd99
George Williams <pfaedit@users.sourceforge.net>
2007-07-05 21:38:43 +0000

    FF did not recognize that a bdf file was greymaped. Broken by the bdf properties work a year ago or so.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183668365;
-const char *source_modtime_str = "13:46 5-Jul-2007";
+const time_t source_modtime = 1183671411;
+const char *source_modtime_str = "14:36 5-Jul-2007";
 const char *source_version_str = "20070705";
