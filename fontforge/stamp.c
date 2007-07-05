e3285adbcadbba3603ef6d575865c16c19c07e6e
pfaedit <pfaedit>
2007-07-05 13:29:13 +0000

    In the metrics view, anchored attachments only worked if the base glyph were itself unmoved (that is the mark was placed relative to the unmoved location, not the actual location).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183509259;
-const char *source_modtime_str = "17:34 3-Jul-2007";
-const char *source_version_str = "20070703";
+const time_t source_modtime = 1183641930;
+const char *source_modtime_str = "06:25 5-Jul-2007";
+const char *source_version_str = "20070705";
