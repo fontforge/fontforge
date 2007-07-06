ae2ca49047348552f610633afaef6bfb524c1205
George Williams <pfaedit@users.sourceforge.net>
2007-07-06 22:50:58 +0000

    A recent patch had problems.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183681018;
-const char *source_modtime_str = "17:16 5-Jul-2007";
-const char *source_version_str = "20070705";
+const time_t source_modtime = 1183753174;
+const char *source_modtime_str = "13:19 6-Jul-2007";
+const char *source_version_str = "20070706";
