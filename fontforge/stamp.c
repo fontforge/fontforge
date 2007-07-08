52874c22e693e1434ea7a430d15dc7d5339a9b56
George Williams <pfaedit@users.sourceforge.net>
2007-07-08 01:32:05 +0000

    If nothing changed in the metrics input field and the output contained a ligature (or a mult subs I suspect). FF would complain.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183753174;
-const char *source_modtime_str = "13:19 6-Jul-2007";
-const char *source_version_str = "20070706";
+const time_t source_modtime = 1183858178;
+const char *source_modtime_str = "18:29 7-Jul-2007";
+const char *source_version_str = "20070707";
