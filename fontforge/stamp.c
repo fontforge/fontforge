a8b8ff16a8f849cfd3eb965b0203de292748177d
pfaedit <pfaedit>
2007-07-08 23:52:13 +0000

    FF crashed when trying to View->Show ATT on a font that appears to me to contain an invalid 'kern' sub-table. I have removed the proximate cause of the crash. I have provided a warning that the kern table appears invalid. And I have cleaned up my internals after detecting the bad sub-table (I had a lookup with a feature but no script and this caused problems).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183858429;
-const char *source_modtime_str = "18:33 7-Jul-2007";
-const char *source_version_str = "20070707";
+const time_t source_modtime = 1183938549;
+const char *source_modtime_str = "16:49 8-Jul-2007";
+const char *source_version_str = "20070708";
