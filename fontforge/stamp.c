893601c909abdd1657c39316f18ec9729abaaad8
pfaedit <pfaedit>
2007-07-09 01:29:35 +0000

    In TrueType composite glyphs with the USE_MY_METRICS bit set the lock icon wasn't scrolled properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183942495;
-const char *source_modtime_str = "17:54 8-Jul-2007";
+const time_t source_modtime = 1183944352;
+const char *source_modtime_str = "18:25 8-Jul-2007";
 const char *source_version_str = "20070708";
