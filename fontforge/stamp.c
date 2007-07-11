79ecc7ab3df85c6514c2cdcaa7e3d838fc2e8996
George Williams <pfaedit@users.sourceforge.net>
2007-07-11 21:31:20 +0000

    When creating a mac resource file we only set the type/creator fields and failed to initialize the finderFlags.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1184187163;
-const char *source_modtime_str = "13:52 11-Jul-2007";
+const time_t source_modtime = 1184189354;
+const char *source_modtime_str = "14:29 11-Jul-2007";
 const char *source_version_str = "20070711";
