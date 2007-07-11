9e37339f28ed3f8271605e2071ea282acb308f48
pfaedit <pfaedit>
2007-07-11 20:52:44 +0000

    Crash bug in generating a cursive connection anchor sub-table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1183944352;
-const char *source_modtime_str = "18:25 8-Jul-2007";
-const char *source_version_str = "20070708";
+const time_t source_modtime = 1184187163;
+const char *source_modtime_str = "13:52 11-Jul-2007";
+const char *source_version_str = "20070711";
