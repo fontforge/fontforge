c2508c539bee3af05defa06257700b22f56e252e
George Williams <pfaedit@users.sourceforge.net>
2007-07-14 03:32:06 +0000

    Add a mechanism so the user can ask fontforge to check for the existance of optional libraries.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1184288299;
-const char *source_modtime_str = "17:58 12-Jul-2007";
-const char *source_version_str = "20070712";
+const time_t source_modtime = 1184383141;
+const char *source_modtime_str = "20:19 13-Jul-2007";
+const char *source_version_str = "20070713";
