42e967d6bf76bbc0e24cb223255c2dfe7bb5c7f3
George Williams <pfaedit@users.sourceforge.net>
2007-07-15 23:51:11 +0000

    Misnamed some private dict entries when loading from otf.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1184542933;
-const char *source_modtime_str = "16:42 15-Jul-2007";
+const time_t source_modtime = 1184543450;
+const char *source_modtime_str = "16:50 15-Jul-2007";
 const char *source_version_str = "20070715";
