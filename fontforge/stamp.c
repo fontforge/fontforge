9f64800d10dc6bf9736957cf7701649e23ed92cb
pfaedit <pfaedit>
2007-07-18 16:17:07 +0000

    When processing class-based contextual lookups fontforge could not handle class 0 (the class containing "all glyphs not in another class") add code to do that.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1184773743;
-const char *source_modtime_str = "08:49 18-Jul-2007";
+const time_t source_modtime = 1184775311;
+const char *source_modtime_str = "09:15 18-Jul-2007";
 const char *source_version_str = "20070718";
