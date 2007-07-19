c65de3e3036b43a24913fd069f8f0f2204b48a56
George Williams <pfaedit@users.sourceforge.net>
2007-07-19 15:12:38 +0000

    Still fixing bugs left from extracting ligature anchors from normal anchors. This time in scripting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1184856409;
-const char *source_modtime_str = "07:46 19-Jul-2007";
+const time_t source_modtime = 1184857851;
+const char *source_modtime_str = "08:10 19-Jul-2007";
 const char *source_version_str = "20070719";
