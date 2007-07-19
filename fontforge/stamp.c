e0736e7c46d150f831c91c367275860afd51aab4
George Williams <pfaedit@users.sourceforge.net>
2007-07-19 15:08:32 +0000

    Barry SCHWARTZ complains that font info says "fontnames must" but that the cited adobe tech note only says "should". So change "must" to "should".

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1184775311;
-const char *source_modtime_str = "09:15 18-Jul-2007";
-const char *source_version_str = "20070718";
+const time_t source_modtime = 1184856409;
+const char *source_modtime_str = "07:46 19-Jul-2007";
+const char *source_version_str = "20070719";
