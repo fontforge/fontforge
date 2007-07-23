a2154425cda37ba0c6ffa7990a6b9bdd443e7fd7
pfaedit <pfaedit>
2007-07-23 04:09:39 +0000

    --usage did not document the --last argument.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1185162049;
-const char *source_modtime_str = "20:40 22-Jul-2007";
+const time_t source_modtime = 1185163866;
+const char *source_modtime_str = "21:11 22-Jul-2007";
 const char *source_version_str = "20070722";
