a24d86132be939276d5b19f75834be121aaac500
pfaedit <pfaedit>
2007-07-24 21:42:34 +0000

    MS proposes two new feature tags with it's new 'MATH' table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1185203226;
-const char *source_modtime_str = "08:07 23-Jul-2007";
-const char *source_version_str = "20070723";
+const time_t source_modtime = 1185312727;
+const char *source_modtime_str = "14:32 24-Jul-2007";
+const char *source_version_str = "20070724";
