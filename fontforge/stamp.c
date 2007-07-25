c9faaae93ed3baf85f452e6196466e6223ccc8bf
George Williams <pfaedit@users.sourceforge.net>
2007-07-25 17:40:41 +0000

    Drat. When printing full page glyphs to a pdf file, the glyph was shifted up by almost the full page and we got a second (and incorrect) title.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1185312727;
-const char *source_modtime_str = "14:32 24-Jul-2007";
-const char *source_version_str = "20070724";
+const time_t source_modtime = 1185385015;
+const char *source_modtime_str = "10:36 25-Jul-2007";
+const char *source_version_str = "20070725";
