e3ae8e4ac4213c5a07e6a15f40571d028f40b854
pfaedit <pfaedit>
2007-07-26 19:04:08 +0000

    More bugs related to python kerning classes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1185472260;
-const char *source_modtime_str = "10:51 26-Jul-2007";
+const time_t source_modtime = 1185476594;
+const char *source_modtime_str = "12:03 26-Jul-2007";
 const char *source_version_str = "20070726";
