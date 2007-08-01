216a708ee0cfef2783bd7c96517cc803ba336de2
George Williams <pfaedit@users.sourceforge.net>
2007-08-01 21:13:16 +0000

    Another bug.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186002207;
-const char *source_modtime_str = "14:03 1-Aug-2007";
+const time_t source_modtime = 1186002845;
+const char *source_modtime_str = "14:14 1-Aug-2007";
 const char *source_version_str = "20070801";
