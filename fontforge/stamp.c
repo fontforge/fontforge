ca426599ed2047cc9269cb18f1acb5e39c900465
George Williams <pfaedit@users.sourceforge.net>
2007-08-01 21:03:46 +0000

    Outputting extension subtables appears to have been broken. The code which was supposed to detect two glyphs with the same set of kerning pairs and use the same data for them didn't work (forcing us to get an extension table which also didn't work).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1185936722;
-const char *source_modtime_str = "19:52 31-Jul-2007";
-const char *source_version_str = "20070731";
+const time_t source_modtime = 1186001749;
+const char *source_modtime_str = "13:55 1-Aug-2007";
+const char *source_version_str = "20070801";
