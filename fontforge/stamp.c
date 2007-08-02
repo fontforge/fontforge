d4b78fe2f32356b70320c8e24ce7aaecc6c77f60
George Williams <pfaedit@users.sourceforge.net>
2007-08-02 13:47:39 +0000

    If we were given a cff (otf) font, with a bluevalues array which ended in 0 (as [-20 0]) then we would omit the final zero ([-20]).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186029777;
-const char *source_modtime_str = "21:42 1-Aug-2007";
-const char *source_version_str = "20070801";
+const time_t source_modtime = 1186062321;
+const char *source_modtime_str = "06:45 2-Aug-2007";
+const char *source_version_str = "20070802";
