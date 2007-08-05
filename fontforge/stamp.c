8eea7bfa077535087fe697817b2e035c70d9c699
George Williams <pfaedit@users.sourceforge.net>
2007-08-05 23:28:45 +0000

    Patch from Barry SCHWARTZ. python fontforge.font.addPosSub was expecting its args inside a redundant tuple.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186354916;
-const char *source_modtime_str = "16:01 5-Aug-2007";
+const time_t source_modtime = 1186356450;
+const char *source_modtime_str = "16:27 5-Aug-2007";
 const char *source_version_str = "20070805";
