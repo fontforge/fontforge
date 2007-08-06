70e553ef1f908b120bce350a3d6797a978107540
George Williams <pfaedit@users.sourceforge.net>
2007-08-06 22:42:39 +0000

    ff scripting command CopyReference() was broken.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186439067;
-const char *source_modtime_str = "15:24 6-Aug-2007";
+const time_t source_modtime = 1186440041;
+const char *source_modtime_str = "15:40 6-Aug-2007";
 const char *source_version_str = "20070806";
