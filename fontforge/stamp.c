abf395e7006976005119d4185b0ba509942bd4f8
pfaedit <pfaedit>
2007-08-06 21:36:00 +0000

    Warn the user (rather than just silently failing) when we have a freetype version mismatch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186432089;
-const char *source_modtime_str = "13:28 6-Aug-2007";
+const time_t source_modtime = 1186436216;
+const char *source_modtime_str = "14:36 6-Aug-2007";
 const char *source_version_str = "20070806";
