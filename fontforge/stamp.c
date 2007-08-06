b9ab670f27851538aa799eb389450d850f3071f5
George Williams <pfaedit@users.sourceforge.net>
2007-08-06 19:59:17 +0000

    The math dlg failed to parse italic correction properly in glyph construction.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186410593;
-const char *source_modtime_str = "07:29 6-Aug-2007";
+const time_t source_modtime = 1186430246;
+const char *source_modtime_str = "12:57 6-Aug-2007";
 const char *source_version_str = "20070806";
