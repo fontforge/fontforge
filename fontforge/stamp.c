caa8d005ec3fae53c82ca28c983aab5957735ab9
George Williams <pfaedit@users.sourceforge.net>
2007-08-06 13:55:37 +0000

    Patch by Barry SCHWARTZ to feature file output on 64bit machines.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186374744;
-const char *source_modtime_str = "21:32 5-Aug-2007";
-const char *source_version_str = "20070805";
+const time_t source_modtime = 1186408544;
+const char *source_modtime_str = "06:55 6-Aug-2007";
+const char *source_version_str = "20070806";
