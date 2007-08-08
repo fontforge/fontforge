99753c4a1fa39901d98485c8c0976433865c9c47
George Williams <pfaedit@users.sourceforge.net>
2007-08-08 20:31:28 +0000

    kerning display is off in a pairwise lookup window if a y position adjustment were requested.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186601007;
-const char *source_modtime_str = "12:23 8-Aug-2007";
+const time_t source_modtime = 1186604948;
+const char *source_modtime_str = "13:29 8-Aug-2007";
 const char *source_version_str = "20070808";
