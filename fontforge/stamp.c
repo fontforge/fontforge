30a7077e326b4f2d51fba3730f0812759e93bfd2
George Williams <pfaedit@users.sourceforge.net>
2007-08-08 19:25:50 +0000

    Make some effort to retain context when changing encodings.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186599193;
-const char *source_modtime_str = "11:53 8-Aug-2007";
+const time_t source_modtime = 1186601007;
+const char *source_modtime_str = "12:23 8-Aug-2007";
 const char *source_version_str = "20070808";
