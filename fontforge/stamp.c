8bed05dd627017aa316d187d9a61765e34c59cdb
pfaedit <pfaedit>
2007-08-10 20:51:13 +0000

    The Execute Script dlg would sometimes complain about invalid scrollbar size when it got closed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186778156;
-const char *source_modtime_str = "13:35 10-Aug-2007";
+const time_t source_modtime = 1186778939;
+const char *source_modtime_str = "13:48 10-Aug-2007";
 const char *source_version_str = "20070810";
