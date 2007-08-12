697c0a4c8dd6c3a2ea91bd577c604e63f63f7423
George Williams <pfaedit@users.sourceforge.net>
2007-08-12 20:55:22 +0000

    Counter hints for LCG glyphs came out wrong if autohint hand not been applied first.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186789783;
-const char *source_modtime_str = "16:49 10-Aug-2007";
-const char *source_version_str = "20070810";
+const time_t source_modtime = 1186952085;
+const char *source_modtime_str = "13:54 12-Aug-2007";
+const char *source_version_str = "20070812";
