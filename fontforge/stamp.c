381407ae0ed206cc4f7e957d92f19b9b0c1512fc
pfaedit <pfaedit>
2007-08-12 20:57:33 +0000

    Probably in CJK too.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1186952085;
-const char *source_modtime_str = "13:54 12-Aug-2007";
+const time_t source_modtime = 1186952330;
+const char *source_modtime_str = "13:58 12-Aug-2007";
 const char *source_version_str = "20070812";
