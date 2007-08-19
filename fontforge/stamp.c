6bbea560046d5440e85b91e42debec8526626797
pfaedit <pfaedit>
2007-08-19 01:42:09 +0000

    Back out the patch which brought up a thread to run a validator in the background while the user dawdled over the font save dlg. My allocation routines are not thread safe -- and I have an inexplicable crash to prove it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1187479216;
-const char *source_modtime_str = "16:20 18-Aug-2007";
+const time_t source_modtime = 1187486992;
+const char *source_modtime_str = "18:29 18-Aug-2007";
 const char *source_version_str = "20070818";
