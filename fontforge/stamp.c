8363a1a8841892a054bac87e3ca4786ad0483768
George Williams <pfaedit@users.sourceforge.net>
2007-08-22 19:55:05 +0000

    FF would randomly crash after removing a lot of glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1187809185;
-const char *source_modtime_str = "11:59 22-Aug-2007";
+const time_t source_modtime = 1187812556;
+const char *source_modtime_str = "12:55 22-Aug-2007";
 const char *source_version_str = "20070822";
