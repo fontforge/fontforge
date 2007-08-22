88215f886d417377e8c20d93216dd649d2e9e097
pfaedit <pfaedit>
2007-08-22 18:51:59 +0000

    All blank lines in the display dlg had the same line spacing as that of the first character displayed in the dlg.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1187552690;
-const char *source_modtime_str = "12:44 19-Aug-2007";
-const char *source_version_str = "20070819";
+const time_t source_modtime = 1187808634;
+const char *source_modtime_str = "11:50 22-Aug-2007";
+const char *source_version_str = "20070822";
