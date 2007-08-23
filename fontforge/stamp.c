238d61c78f03d99eebc13fbe907b3f22604ab409
George Williams <pfaedit@users.sourceforge.net>
2007-08-23 22:09:37 +0000

    When dragging truetype points around, adjacent implicit points would remain where they were (and not be implicit any longer).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1187814823;
-const char *source_modtime_str = "13:33 22-Aug-2007";
-const char *source_version_str = "20070822";
+const time_t source_modtime = 1187906790;
+const char *source_modtime_str = "15:06 23-Aug-2007";
+const char *source_version_str = "20070823";
