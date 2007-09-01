b812ec28679827ec3d33880b4ece1bf78ebc0ef0
pfaedit <pfaedit>
2007-09-01 02:20:29 +0000

    Add a search & replace command to python scripting. Search for a contour/layer and replace with another. Add a find command which will iterate over all glyphs which contain the given contour.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1188612344;
-const char *source_modtime_str = "19:05 31-Aug-2007";
+const time_t source_modtime = 1188611789;
+const char *source_modtime_str = "18:56 31-Aug-2007";
 const char *source_version_str = "20070831";
