ec29e2b70a0e423fb80e1456d1485ba9488b3d4c
pfaedit <pfaedit>
2007-09-04 15:29:18 +0000

    Round to Int didn't round anchor points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1188843258;
-const char *source_modtime_str = "11:14 3-Sep-2007";
-const char *source_version_str = "20070903";
+const time_t source_modtime = 1188919709;
+const char *source_modtime_str = "08:28 4-Sep-2007";
+const char *source_version_str = "20070904";
