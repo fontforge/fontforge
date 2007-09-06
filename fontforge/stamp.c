5545d860c6df7f9be71c16477709658975cfb935
George Williams <pfaedit@users.sourceforge.net>
2007-09-06 22:59:00 +0000

    Need to Catagorize points after Tile Path.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189119251;
-const char *source_modtime_str = "15:54 6-Sep-2007";
+const time_t source_modtime = 1189119644;
+const char *source_modtime_str = "16:00 6-Sep-2007";
 const char *source_version_str = "20070906";
