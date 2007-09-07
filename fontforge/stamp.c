df35dc3c5dd579a25a4ec7a692ff5e0a113ca155
George Williams <pfaedit@users.sourceforge.net>
2007-09-07 19:53:24 +0000

    Add the ability to Populate a lookup from the selection. Do a better job of providing default values for single substitutions & positions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189119644;
-const char *source_modtime_str = "16:00 6-Sep-2007";
-const char *source_version_str = "20070906";
+const time_t source_modtime = 1189194618;
+const char *source_modtime_str = "12:50 7-Sep-2007";
+const char *source_version_str = "20070907";
