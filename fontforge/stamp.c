929b53f71c2646ab37a418681afaf309d6fcc76a
pfaedit <pfaedit>
2007-09-08 19:43:40 +0000

    Fix another crash when editing in the Print/Display dialog, related to blank lines.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189210806;
-const char *source_modtime_str = "17:20 7-Sep-2007";
-const char *source_version_str = "20070907";
+const time_t source_modtime = 1189280416;
+const char *source_modtime_str = "12:40 8-Sep-2007";
+const char *source_version_str = "20070908";
