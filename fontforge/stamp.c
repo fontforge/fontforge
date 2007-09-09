f87eea1a010f144c9d9b5583673bcdaccb60569e
George Williams <pfaedit@users.sourceforge.net>
2007-09-09 14:24:19 +0000

    ApplySubstitutions still had problems. Caused crashes in a font with instructions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189298258;
-const char *source_modtime_str = "17:37 8-Sep-2007";
-const char *source_version_str = "20070908";
+const time_t source_modtime = 1189347841;
+const char *source_modtime_str = "07:24 9-Sep-2007";
+const char *source_version_str = "20070909";
