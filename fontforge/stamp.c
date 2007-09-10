4384cf3ce0419ad4739fbf8da7f6ff1dea5511b3
George Williams <pfaedit@users.sourceforge.net>
2007-09-10 14:08:56 +0000

    I introduced a crash in the PSTKD lookup subtable dialog.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189376971;
-const char *source_modtime_str = "15:29 9-Sep-2007";
-const char *source_version_str = "20070909";
+const time_t source_modtime = 1189433294;
+const char *source_modtime_str = "07:08 10-Sep-2007";
+const char *source_version_str = "20070910";
