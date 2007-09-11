7867dbe44c174ddda236903566215295aa2c9a5d
pfaedit <pfaedit>
2007-09-11 02:03:21 +0000

    Give the "Mass Rename" dlg a mode where it behaves the way Zedler thinks it should.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189472959;
-const char *source_modtime_str = "18:09 10-Sep-2007";
+const time_t source_modtime = 1189476110;
+const char *source_modtime_str = "19:01 10-Sep-2007";
 const char *source_version_str = "20070910";
