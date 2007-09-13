31f359303a6713892aa19d9983d58324a6c45ae2
pfaedit <pfaedit>
2007-09-13 21:25:40 +0000

    Provide a tiny measure of security by doing a trivial encryption of the OFLib password before storing in the preference file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189715667;
-const char *source_modtime_str = "13:34 13-Sep-2007";
+const time_t source_modtime = 1189718720;
+const char *source_modtime_str = "14:25 13-Sep-2007";
 const char *source_version_str = "20070913";
