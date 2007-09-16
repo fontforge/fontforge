2c757bbb554d85035e37baebc08ab9f32e931f79
pfaedit <pfaedit>
2007-09-16 15:27:46 +0000

    Add some warnings when autoinstructing fonts. Let them know that they'll get better results if they have a postscript private dictionary and PS hints.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189904252;
-const char *source_modtime_str = "17:57 15-Sep-2007";
-const char *source_version_str = "20070915";
+const time_t source_modtime = 1189955779;
+const char *source_modtime_str = "08:16 16-Sep-2007";
+const char *source_version_str = "20070916";
