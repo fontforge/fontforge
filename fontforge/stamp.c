feaaa1bafe61de78d735f2d76180d5f8583920a9
George Williams <pfaedit@users.sourceforge.net>
2007-09-17 17:44:11 +0000

    Grr. If compile on a system with libpng but without libjpeg then a typo meant it would not compile.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1189955779;
-const char *source_modtime_str = "08:16 16-Sep-2007";
-const char *source_version_str = "20070916";
+const time_t source_modtime = 1190047676;
+const char *source_modtime_str = "09:47 17-Sep-2007";
+const char *source_version_str = "20070917";
