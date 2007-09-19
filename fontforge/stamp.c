bc03f542e6ab3eb6a98b10e51a9d438744f5c3ad
George Williams <pfaedit@users.sourceforge.net>
2007-09-19 16:09:47 +0000

    More patches from Michal Nowakowski dealing with message strings:   1) Some weren't marked for translation   2) Some were marked but weren't being translated   3) Some were inconsistant.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1190129991;
-const char *source_modtime_str = "08:39 18-Sep-2007";
-const char *source_version_str = "20070918";
+const time_t source_modtime = 1190217928;
+const char *source_modtime_str = "09:05 19-Sep-2007";
+const char *source_version_str = "20070919";
