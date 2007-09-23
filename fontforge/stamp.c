5d7e3c19e2b3616d0c19a2898f5e63f2e9f67b07
pfaedit <pfaedit>
2007-09-23 16:00:17 +0000

    FF would crash when building accented glyphs from unicode data that included a space as the base glyph.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1190323812;
-const char *source_modtime_str = "14:30 20-Sep-2007";
-const char *source_version_str = "20070920";
+const time_t source_modtime = 1190563168;
+const char *source_modtime_str = "08:59 23-Sep-2007";
+const char *source_version_str = "20070923";
