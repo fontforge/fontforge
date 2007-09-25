a56f1e278671a44b7d169e8262d2da8af680c9ea
George Williams <pfaedit@users.sourceforge.net>
2007-09-25 22:34:04 +0000

    Guess at the meaning for GDEF glyph class component glyph:    Something not encoded    Something which is refered to by at least one other glyph (as a reference)    Something which is not generated by GSUB

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1190692207;
-const char *source_modtime_str = "20:50 24-Sep-2007";
-const char *source_version_str = "20070924";
+const time_t source_modtime = 1190753152;
+const char *source_modtime_str = "13:45 25-Sep-2007";
+const char *source_version_str = "20070925";
