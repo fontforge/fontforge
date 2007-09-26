847be529ba6a8f3a78f152f680c896557315ec54
pfaedit <pfaedit>
2007-09-26 00:32:20 +0000

    Oops, also need to search names in math variant tables on glyph rename.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1190759545;
-const char *source_modtime_str = "15:32 25-Sep-2007";
+const time_t source_modtime = 1190766473;
+const char *source_modtime_str = "17:27 25-Sep-2007";
 const char *source_version_str = "20070925";
