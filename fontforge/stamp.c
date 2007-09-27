6c3ac443ec0325219e0caa2379c1c1447541c23d
George Williams <pfaedit@users.sourceforge.net>
2007-09-27 15:34:43 +0000

    If there are no slots in the current encoding, then clicking on the encoding menu caused a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1190905065;
-const char *source_modtime_str = "07:57 27-Sep-2007";
+const time_t source_modtime = 1190907101;
+const char *source_modtime_str = "08:31 27-Sep-2007";
 const char *source_version_str = "20070927";
