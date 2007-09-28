75ccdfe68eb7b02bdb67e5cb54254c52669b9d42
George Williams <pfaedit@users.sourceforge.net>
2007-09-28 21:07:15 +0000

    Fabrice Popineau & Taco Hoekwater point out that in the featurefile output routines I was using a single backslash inside a printf string. The intent was to output a single backslash. But I actually need to quote the backslash with another to do that.

--- fontforge/featurefile.c
+++ fontforge/featurefile.c
@@ -44,9 +44,9 @@
 
 static void dump_glyphname(FILE *out, SplineChar *sc) {
     if ( sc->parent->cidmaster!=NULL )
-	fprintf( out, "\%d", sc->orig_pos /* That's the CID */ );
+	fprintf( out, "\\%d", sc->orig_pos /* That's the CID */ );
     else
-	fprintf( out, "\%s", sc->name );
+	fprintf( out, "\\%s", sc->name );
 }
 
 static void dump_glyphbyname(FILE *out, SplineFont *sf, char *name) {
@@ -55,9 +55,9 @@ static void dump_glyphbyname(FILE *out, SplineFont *sf, char *name) {
     if ( sc==NULL )
 	LogError( "No glyph named %s.", name );
     if ( sc->parent->cidmaster==NULL || sc==NULL )
-	fprintf( out, "\%s", name );
+	fprintf( out, "\\%s", name );
     else
-	fprintf( out, "\%s", sc->name );
+	fprintf( out, "\\%s", sc->name );
 }
 
 static void dump_glyphnamelist(FILE *out, SplineFont *sf, char *names) {
