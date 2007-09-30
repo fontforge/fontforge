d69936496d50f674d4a6fb4c612290aa3e2d92c6
pfaedit <pfaedit>
2007-09-30 19:24:36 +0000

    add xdg-open to the list of "known" browsers.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1191179594;
-const char *source_modtime_str = "12:13 30-Sep-2007";
+const time_t source_modtime = 1191180394;
+const char *source_modtime_str = "12:26 30-Sep-2007";
 const char *source_version_str = "20070930";
