b51f85d8cb05ae89be771732d839d0c384f853d1
pfaedit <pfaedit>
2007-10-02 02:04:51 +0000

    If a tt spline had a control point on top of an on-curve point we would lose track of it when generating the font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1191251824;
-const char *source_modtime_str = "08:17 1-Oct-2007";
+const time_t source_modtime = 1191289255;
+const char *source_modtime_str = "18:40 1-Oct-2007";
 const char *source_version_str = "20071001";
