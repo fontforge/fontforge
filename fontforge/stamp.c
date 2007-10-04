c44a03cfe994494929e163f0799f8f51551b984d
pfaedit <pfaedit>
2007-10-04 02:55:51 +0000

    When scaling BlueValues, or other arrays, a trailing space after the last number and before the ']' was considered an error. If found then ff would not scale that entry.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1191463999;
-const char *source_modtime_str = "19:13 3-Oct-2007";
+const time_t source_modtime = 1191466375;
+const char *source_modtime_str = "19:52 3-Oct-2007";
 const char *source_version_str = "20071003";
