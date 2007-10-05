b2638d42d67b16bb4aabdd5643a4b1fd0bed2e7a
pfaedit <pfaedit>
2007-10-05 14:14:11 +0000

    The routine to create an apple state machine from a set of OpenType ('init','medi','fina','isol') forms would often crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1191466375;
-const char *source_modtime_str = "19:52 3-Oct-2007";
-const char *source_version_str = "20071003";
+const time_t source_modtime = 1191593635;
+const char *source_modtime_str = "07:13 5-Oct-2007";
+const char *source_version_str = "20071005";
