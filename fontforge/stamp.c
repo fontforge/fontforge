5f7ddd4db9ccc4324cb4c9a348db264fa398b4a7
George Williams <pfaedit@users.sourceforge.net>
2007-10-05 20:02:32 +0000

    in some rare cases we got an uninitialized value in spline->line display conversion.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1191594830;
-const char *source_modtime_str = "07:33 5-Oct-2007";
+const time_t source_modtime = 1191614308;
+const char *source_modtime_str = "12:58 5-Oct-2007";
 const char *source_version_str = "20071005";
