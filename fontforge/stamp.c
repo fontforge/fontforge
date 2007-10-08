3c902774fe3e47f1faf0440e18ec0a568fb0b578
pfaedit <pfaedit>
2007-10-08 15:14:28 +0000

    Pressing the [Refresh] button could crash ff if a "FontData" depended on another (same font).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1191855506;
-const char *source_modtime_str = "07:58 8-Oct-2007";
+const time_t source_modtime = 1191856501;
+const char *source_modtime_str = "08:15 8-Oct-2007";
 const char *source_version_str = "20071008";
