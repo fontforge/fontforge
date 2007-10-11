1c5e1b95fa4b0002f275eb8bd955d22cf73e58c8
George Williams <pfaedit@users.sourceforge.net>
2007-10-11 19:46:35 +0000

    Several bugs in my mark attachment routines. There was an assumption that only one mark could be attached to a base glyph per sub-table. This is wrong for mark to base, in that there can be several anchor classes per subtable. It is even worse for mark to ligature where there can be many marks per anchor class (one per ligature position).
    Having done this make sure we don't use the same anchor point more than once.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192062971;
-const char *source_modtime_str = "17:36 10-Oct-2007";
-const char *source_version_str = "20071010";
+const time_t source_modtime = 1192131637;
+const char *source_modtime_str = "12:40 11-Oct-2007";
+const char *source_version_str = "20071011";
