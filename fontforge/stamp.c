256e4961b6c1e44e933b6d33f4143b7012d001b5
George Williams <pfaedit@users.sourceforge.net>
2007-10-12 03:19:35 +0000

    I'm told there was a typo in my Bulgarian text for the Print/Display dlg, and that this should fix it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192132289;
-const char *source_modtime_str = "12:51 11-Oct-2007";
+const time_t source_modtime = 1192159219;
+const char *source_modtime_str = "20:20 11-Oct-2007";
 const char *source_version_str = "20071011";
