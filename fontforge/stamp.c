1fcee7f186d6113a70cf3ebe369bfbd32cec25f4
pfaedit <pfaedit>
2007-10-14 20:32:41 +0000

    Use ~/.FontForge instead of ~/.PfaEdit

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192159219;
-const char *source_modtime_str = "20:20 11-Oct-2007";
-const char *source_version_str = "20071011";
+const time_t source_modtime = 1192393898;
+const char *source_modtime_str = "13:31 14-Oct-2007";
+const char *source_version_str = "20071014";
