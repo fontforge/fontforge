01e21cbf76b62673b04056d048045c060423db53
George Williams <pfaedit@users.sourceforge.net>
2007-10-16 18:23:34 +0000

    When copying glyphs from one font to another ff would sometimes use a temporary value of -2 for the orig_pos. This meant that reference fixups didn't work when looking at those glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192558899;
-const char *source_modtime_str = "11:21 16-Oct-2007";
+const time_t source_modtime = 1192559049;
+const char *source_modtime_str = "11:24 16-Oct-2007";
 const char *source_version_str = "20071016";
