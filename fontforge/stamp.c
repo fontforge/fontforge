30bb779000629fdb444723b894cf035060435584
George Williams <pfaedit@users.sourceforge.net>
2007-10-16 19:52:10 +0000

    If you pressed the [*] Default check box in the Font Info->OS/2->Panose area, the values didn't go back to the defaults.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192559049;
-const char *source_modtime_str = "11:24 16-Oct-2007";
+const time_t source_modtime = 1192563198;
+const char *source_modtime_str = "12:33 16-Oct-2007";
 const char *source_version_str = "20071016";
