70f01f8fa93601eee73b185ae612b1dc45937a20
pfaedit <pfaedit>
2007-10-18 23:55:48 +0000

    When I fixed mark to ligature positioning I broke the other three types of anchor positioning (to base, to mark & cursive).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192563198;
-const char *source_modtime_str = "12:33 16-Oct-2007";
-const char *source_version_str = "20071016";
+const time_t source_modtime = 1192751549;
+const char *source_modtime_str = "16:52 18-Oct-2007";
+const char *source_version_str = "20071018";
