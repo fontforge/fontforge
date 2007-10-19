f13ec401b182a975b3c3c1e3c42edfd52a0281da
pfaedit <pfaedit>
2007-10-19 02:30:54 +0000

    Get Info on points would crash after the hvcurve point got added.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192753686;
-const char *source_modtime_str = "17:28 18-Oct-2007";
+const time_t source_modtime = 1192761020;
+const char *source_modtime_str = "19:30 18-Oct-2007";
 const char *source_version_str = "20071018";
