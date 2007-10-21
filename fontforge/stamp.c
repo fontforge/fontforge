98cc919ec1604e3fda4331bc4acbe0d5db931c31
George Williams <pfaedit@users.sourceforge.net>
2007-10-21 20:10:33 +0000

    Most of the items in the popup menu of the lookup pane didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192995862;
-const char *source_modtime_str = "12:44 21-Oct-2007";
+const time_t source_modtime = 1192997300;
+const char *source_modtime_str = "13:08 21-Oct-2007";
 const char *source_version_str = "20071021";
