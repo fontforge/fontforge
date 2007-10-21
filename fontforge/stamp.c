1338d0407a46fd5dd918d8bb42d08547b01dc44d
George Williams <pfaedit@users.sourceforge.net>
2007-10-21 20:43:36 +0000

    Provide reasonable defaults for the wws* ttf names.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192997642;
-const char *source_modtime_str = "13:14 21-Oct-2007";
+const time_t source_modtime = 1192999342;
+const char *source_modtime_str = "13:42 21-Oct-2007";
 const char *source_version_str = "20071021";
