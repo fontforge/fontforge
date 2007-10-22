d297781196c415453776f92c115102d2bec011b3
pfaedit <pfaedit>
2007-10-22 00:31:44 +0000

    Swith the timestamps to GMT. Prior to this all timestamps have been PS/DT, but now that other people are making source changes GMT seems more sensible.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1192999576;
-const char *source_modtime_str = "13:46 21-Oct-2007";
-const char *source_version_str = "20071021";
+const time_t source_modtime = 1193013155;
+const char *source_modtime_str = "00:32 22-Oct-2007";
+const char *source_version_str = "20071022";
