9ab0103e6e1ab433b652986449c5e28a3da40775
akryukov <akryukov>
2007-10-22 07:28:24 +0000

    Make implied points to be ignored when determining a diagonal stem validity.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193013155;
-const char *source_modtime_str = "00:32 22-Oct-2007";
+const time_t source_modtime = 1193038123;
+const char *source_modtime_str = "07:28 GMT 22-Oct-2007";
 const char *source_version_str = "20071022";
