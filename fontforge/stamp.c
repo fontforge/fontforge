fa47b54b9d09ef9ba8e4fd72addcd12b4232739d
michal-n <michal-n>
2007-10-22 15:00:37 +0000

    - Patch from Alexej for his diagonal hinting routines, fixes an ugly problem   reported by Max Rabin.
    - search_edge() tries to pick refpt closer to the edge, this cures some
      stems misalignments.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193043438;
-const char *source_modtime_str = "08:57 GMT 22-Oct-2007";
+const time_t source_modtime = 1193065173;
+const char *source_modtime_str = "14:59 GMT 22-Oct-2007";
 const char *source_version_str = "20071022";
