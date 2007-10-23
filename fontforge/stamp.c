4965cbfe53e92a9afdec9603ba81f652350e3568
pfaedit <pfaedit>
2007-10-23 21:58:53 +0000

    FF marked almost all fonts with the Oblique bit in the OS/2 table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193065173;
-const char *source_modtime_str = "14:59 GMT 22-Oct-2007";
-const char *source_version_str = "20071022";
+const time_t source_modtime = 1193172663;
+const char *source_modtime_str = "20:51 GMT 23-Oct-2007";
+const char *source_version_str = "20071023";
