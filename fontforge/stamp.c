3f4a4f2fb94a240261f2d9f28ba3a48a60367319
George Williams <pfaedit@users.sourceforge.net>
2007-10-24 14:56:31 +0000

    Adam Twardoch suggests that codepoints in the PUA have their script determined by their name (rather than just returning DFLT). Since Adobe used to put things like "a.sc" in this area, that seems a good idea.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193172663;
-const char *source_modtime_str = "20:51 GMT 23-Oct-2007";
-const char *source_version_str = "20071023";
+const time_t source_modtime = 1193237722;
+const char *source_modtime_str = "14:55 GMT 24-Oct-2007";
+const char *source_version_str = "20071024";
