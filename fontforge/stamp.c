717a8e4e90474cb350303a98de14926ec6961702
George Williams <pfaedit@users.sourceforge.net>
2007-10-25 20:11:01 +0000

    Add a "problem" to check for glyphs whose bounding boxes are: too tall, too short, too wide, too much to the left. Used to be I had many problems checked by default. Best to turn them all off.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193330557;
-const char *source_modtime_str = "16:42 GMT 25-Oct-2007";
+const time_t source_modtime = 1193343021;
+const char *source_modtime_str = "20:10 GMT 25-Oct-2007";
 const char *source_version_str = "20071025";
