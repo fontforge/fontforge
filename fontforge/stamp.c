7673817320a199d5b0329bbd40d5ee40f41a1b8f
George Williams <pfaedit@users.sourceforge.net>
2007-10-26 02:14:36 +0000

    Oops. I forgot to get the tool indicators (at the bottom of the tool palette) to use the right images in spiro mode.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193360947;
-const char *source_modtime_str = "01:09 GMT 26-Oct-2007";
+const time_t source_modtime = 1193364850;
+const char *source_modtime_str = "02:14 GMT 26-Oct-2007";
 const char *source_version_str = "20071026";
