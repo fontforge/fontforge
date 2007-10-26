dfd03d7fe161da01043e402a37eeb93536468a87
pfaedit <pfaedit>
2007-10-26 01:12:10 +0000

    People would complain that the python interface would give them "Invalid second order spline" but no one bothered to give me a test case. I hope I've tracked it down now.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193349972;
-const char *source_modtime_str = "22:06 GMT 25-Oct-2007";
-const char *source_version_str = "20071025";
+const time_t source_modtime = 1193361023;
+const char *source_modtime_str = "01:10 GMT 26-Oct-2007";
+const char *source_version_str = "20071026";
