cb820ab392c3ff6307c972757da621581976414e
pfaedit <pfaedit>
2007-10-28 22:58:00 +0000

    Move to having libspiro live in its own package   http://libspiro.sf.net/

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193602680;
-const char *source_modtime_str = "20:18 GMT 28-Oct-2007";
+const time_t source_modtime = 1193609109;
+const char *source_modtime_str = "22:05 GMT 28-Oct-2007";
 const char *source_version_str = "20071028";
