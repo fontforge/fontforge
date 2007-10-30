7bdc942d306af69326ee2ddeece7c164331e6bcf
George Williams <pfaedit@users.sourceforge.net>
2007-10-30 17:58:48 +0000

    Oops. If libspiro didn't exist, spiro code didn't compile.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193705258;
-const char *source_modtime_str = "00:47 GMT 30-Oct-2007";
+const time_t source_modtime = 1193767249;
+const char *source_modtime_str = "18:00 GMT 30-Oct-2007";
 const char *source_version_str = "20071030";
