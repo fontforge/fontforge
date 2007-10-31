7122f2967470be42e8a2b609a1ddd9831bc56b33
pfaedit <pfaedit>
2007-10-31 05:03:41 +0000

    Merge did nothing in spiro mode.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193782164;
-const char *source_modtime_str = "22:09 GMT 30-Oct-2007";
-const char *source_version_str = "20071030";
+const time_t source_modtime = 1193805601;
+const char *source_modtime_str = "04:40 GMT 31-Oct-2007";
+const char *source_version_str = "20071031";
