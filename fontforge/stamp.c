629978be9217bde8d673de5af091a15a771f1059
pfaedit <pfaedit>
2007-10-31 05:29:54 +0000

    Couple of bugs with adding intermediate points into an already existing spiro contour.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193805601;
-const char *source_modtime_str = "04:40 GMT 31-Oct-2007";
+const time_t source_modtime = 1193808519;
+const char *source_modtime_str = "05:28 GMT 31-Oct-2007";
 const char *source_version_str = "20071031";
