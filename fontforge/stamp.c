79143cf13fae35328f507f3b000a276e127dc14a
pfaedit <pfaedit>
2007-11-02 02:52:09 +0000

    Shift-Click on a spiro point to select it had a bad habit of moving it and any previous point selected.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193930357;
-const char *source_modtime_str = "15:19 GMT 1-Nov-2007";
-const char *source_version_str = "20071101";
+const time_t source_modtime = 1193971636;
+const char *source_modtime_str = "02:47 GMT 2-Nov-2007";
+const char *source_version_str = "20071102";
