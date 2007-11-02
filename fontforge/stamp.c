8ed83660e5ca52388b6114e79e59d4279aa4512f
George Williams <pfaedit@users.sourceforge.net>
2007-11-02 03:15:16 +0000

    If a font has no "fullname" field (and fonts aren't required to have one) ff would crash when generating the font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1193971636;
-const char *source_modtime_str = "02:47 GMT 2-Nov-2007";
+const time_t source_modtime = 1193973288;
+const char *source_modtime_str = "03:14 GMT 2-Nov-2007";
 const char *source_version_str = "20071102";
