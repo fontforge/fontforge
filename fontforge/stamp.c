70a354272938b6f7e7fdfd22e667e6aa1edee39d
George Williams <pfaedit@users.sourceforge.net>
2007-11-04 18:40:33 +0000

    Oops. glyph/glyphPen types got the wrong names in the module. (interchanged)

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194197829;
-const char *source_modtime_str = "17:37 GMT 4-Nov-2007";
+const time_t source_modtime = 1194201613;
+const char *source_modtime_str = "18:40 GMT 4-Nov-2007";
 const char *source_version_str = "20071104";
