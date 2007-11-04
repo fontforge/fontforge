42a47051ae1f6b4bf6674aac4d5bb4a2e238bcba
George Williams <pfaedit@users.sourceforge.net>
2007-11-04 19:38:47 +0000

    If freetype gets a single point open contour in postscript mode, it simply skips the contour (does not call FT_MoveTo on it). This screwed up my attempt to match the results of freetype with the actual splines and caused a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194202803;
-const char *source_modtime_str = "19:00 GMT 4-Nov-2007";
+const time_t source_modtime = 1194205075;
+const char *source_modtime_str = "19:37 GMT 4-Nov-2007";
 const char *source_version_str = "20071104";
