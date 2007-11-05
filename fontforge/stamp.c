a85b0530eac6bc102396cc07c07dc8c09885d055
pfaedit <pfaedit>
2007-11-05 02:41:52 +0000

    Simplify sometimes ignored points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194213669;
-const char *source_modtime_str = "22:01 GMT 4-Nov-2007";
-const char *source_version_str = "20071104";
+const time_t source_modtime = 1194229966;
+const char *source_modtime_str = "02:32 GMT 5-Nov-2007";
+const char *source_version_str = "20071105";
