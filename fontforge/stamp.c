1ad35e37f5b054d213ae479ee1b5cd98bcd5f6ec
pfaedit <pfaedit>
2007-11-05 03:40:16 +0000

    People want a Snap To Int preference item.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194232526;
-const char *source_modtime_str = "03:15 GMT 5-Nov-2007";
+const time_t source_modtime = 1194233778;
+const char *source_modtime_str = "03:36 GMT 5-Nov-2007";
 const char *source_version_str = "20071105";
