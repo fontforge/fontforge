93296b34979c57467d89e846905d6afede9a4256
George Williams <pfaedit@users.sourceforge.net>
2007-11-06 20:03:31 +0000

    Still not always getting control points on integers.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194299017;
-const char *source_modtime_str = "21:43 GMT 5-Nov-2007";
-const char *source_version_str = "20071105";
+const time_t source_modtime = 1194379410;
+const char *source_modtime_str = "20:03 GMT 6-Nov-2007";
+const char *source_version_str = "20071106";
