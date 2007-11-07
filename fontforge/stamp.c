9cbec3d9772d376b2c035866514478549bb02837
George Williams <pfaedit@users.sourceforge.net>
2007-11-07 17:49:37 +0000

    Rumors abound on the web tha Apple has deprecated "/rsrc" as a way of getting at the resource fork. I find nothing official from Apple about this, but the /var/log/system.log file on my machine confirms it. Rumors also say that the new approach is to open "/..namedfork/rsrc" instead. That seems to work, and doesn't cause nasty warnings in my system log. There are warnings on the web that this mechanism only works on HFS+ file systems. So I first try namedfork, and then later, if that fails, try the older rsrc.
    Whee!

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194453430;
-const char *source_modtime_str = "16:37 GMT 7-Nov-2007";
+const time_t source_modtime = 1194457704;
+const char *source_modtime_str = "17:48 GMT 7-Nov-2007";
 const char *source_version_str = "20071107";
