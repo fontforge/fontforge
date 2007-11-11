d5e185822b70af079b8661b43f2ab0952b3ca056
pfaedit <pfaedit>
2007-11-11 20:31:17 +0000

    contour.lineTo and friends didn't work (didn't increment the contour's point count).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194751871;
-const char *source_modtime_str = "03:31 GMT 11-Nov-2007";
+const time_t source_modtime = 1194812894;
+const char *source_modtime_str = "20:28 GMT 11-Nov-2007";
 const char *source_version_str = "20071111";
