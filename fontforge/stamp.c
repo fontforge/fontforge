f0057c99e875880f0b839391a61c34b00ad6401a
George Williams <pfaedit@users.sourceforge.net>
2007-11-13 22:33:38 +0000

    Mmm. This is probably a better idea: Only duplicate the glyph if we've got a postscript style (by name) encoding.
    Now we still might end up with two glyphs with the same name...

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194992339;
-const char *source_modtime_str = "22:18 GMT 13-Nov-2007";
+const time_t source_modtime = 1194993049;
+const char *source_modtime_str = "22:30 GMT 13-Nov-2007";
 const char *source_version_str = "20071113";
