ff317b45dcfd47e81a97a1f60e47952729779653
pfaedit <pfaedit>
2007-11-13 20:26:16 +0000

    If a user edited a glyph in spiro mode, and then did something in the fontview ehich caused the spiros to be removed (Lots of things: Overlap, Expand Stroke, Embolden, ...), and then did a Round to Int (and perhaps other things) from the font view, then FontForge would crash.
    It expected to find spiros in a glyph marked as being edited in spiro mode, and was surprised when none were available.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194882551;
-const char *source_modtime_str = "15:49 GMT 12-Nov-2007";
-const char *source_version_str = "20071112";
+const time_t source_modtime = 1194985390;
+const char *source_modtime_str = "20:23 GMT 13-Nov-2007";
+const char *source_version_str = "20071113";
