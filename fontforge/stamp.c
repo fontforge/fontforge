f30a674aa979acb443b350495a28895c1d74fc71
George Williams <pfaedit@users.sourceforge.net>
2007-11-13 21:07:02 +0000

    Some people have though winDescent should be a negative value for things below the baseline. So make the sign of the various descent fields explicit in the corresponding popup messages.   Typo Descent: Negative   Win Descent: Positive   Hhea Descent: Negative

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1194985390;
-const char *source_modtime_str = "20:23 GMT 13-Nov-2007";
+const time_t source_modtime = 1194986707;
+const char *source_modtime_str = "20:45 GMT 13-Nov-2007";
 const char *source_version_str = "20071113";
