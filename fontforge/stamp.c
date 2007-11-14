ec853dd67e65f1e7d110407b347c56bf462c292b
pfaedit <pfaedit>
2007-11-14 20:44:37 +0000

    We used not to mark a glyph as changed if the user changed the hints in an order2 font (thinking they were irrelevant in truetype). But the autoinstructor uses them so they are relevant.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1195064864;
-const char *source_modtime_str = "18:27 GMT 14-Nov-2007";
+const time_t source_modtime = 1195073060;
+const char *source_modtime_str = "20:44 GMT 14-Nov-2007";
 const char *source_version_str = "20071114";
