248f9732808502ad2d915036e44e9c00f1a00c03
pfaedit <pfaedit>
2007-11-15 01:51:01 +0000

    Add some serbian text.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1195073060;
-const char *source_modtime_str = "20:44 GMT 14-Nov-2007";
-const char *source_version_str = "20071114";
+const time_t source_modtime = 1195091152;
+const char *source_modtime_str = "01:45 GMT 15-Nov-2007";
+const char *source_version_str = "20071115";
