2ac18b06618c7c3315afed81156b2810d3e163d4
George Williams <pfaedit@users.sourceforge.net>
2007-11-15 22:27:39 +0000

    Add a Select->Open Contours command.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1195163677;
-const char *source_modtime_str = "21:54 GMT 15-Nov-2007";
+const time_t source_modtime = 1195165017;
+const char *source_modtime_str = "22:16 GMT 15-Nov-2007";
 const char *source_version_str = "20071115";
