11ed209966b09e0d2e5af600b507343e27fd1470
pfaedit <pfaedit>
2007-11-16 23:12:10 +0000

    When importing an eps file, ff did not turn off the "Bitmap-only" flag. This means that new fonts composed entirely of eps files would be treated as bitmap fonts (sometimes).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1195252341;
-const char *source_modtime_str = "22:32 GMT 16-Nov-2007";
+const time_t source_modtime = 1195254425;
+const char *source_modtime_str = "23:07 GMT 16-Nov-2007";
 const char *source_version_str = "20071116";
