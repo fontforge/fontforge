9257574a3c5f93cf477fc7b2dd1fae478c3e2635
Alexey Kryukov <amkryukov@gmail.com>
2007-11-18 20:03:30 +0000

    * Lines are more actively used: stems now have 'leftline' and   'rightline' properties, and for diagonal stems the stem unit   vector is always set equal to the unit vector of the line   corresponding to the left edge of the given stem; * I no longer attempt to calculate "active" zones for bounding   box hints (they usually don't correspond to any actual glyph   features and thus look ugly). So now only edges of such hints   (without any active zones) are displayed in the charview; * diagonal stem detection improved (some checks were too strict   and actually oriented to HV stems); * the 'blue' property is now used not only for ghost hints, but   also for regular horizontal stems. This may be useful if I ever   implement an autoinstructor based directly on the stem detector; * glyph data initialization has been separated from stem detection   and put into a separate routine (a first step for unification   of algorithms used to calculate stem active zones).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1195412003;
-const char *source_modtime_str = "18:53 GMT 18-Nov-2007";
+const time_t source_modtime = 1195416670;
+const char *source_modtime_str = "20:11 GMT 18-Nov-2007";
 const char *source_version_str = "20071118";
