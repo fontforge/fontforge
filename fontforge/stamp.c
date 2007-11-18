4a6e151fc59dfbe47d019b285cd0fb0a0ad6b3f5
George Williams <pfaedit@users.sourceforge.net>
2007-11-18 18:08:33 +0000

    I think setting a compact encoding could overflow an allocated string array.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1195358043;
-const char *source_modtime_str = "03:54 GMT 18-Nov-2007";
+const time_t source_modtime = 1195406122;
+const char *source_modtime_str = "17:15 GMT 18-Nov-2007";
 const char *source_version_str = "20071118";
