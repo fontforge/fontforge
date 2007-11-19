6fce2ca88c95b0e92ff8c57d82dcf7308b94edce
pfaedit <pfaedit>
2007-11-19 03:39:59 +0000

    Both Adobe Acrobat and Apple's Preview exhibit what I consider to be a bug. According to the PDF reference manual, if an Encoding dictionary is not specified for a font object then the built in encoding will be used. But both these programs reencode to AdobeStandard and do not use the font's built in encoding. GhostScript agrees with my interpretation and does not reencode.
    However, they ain't gwana change, so I must. Always dump out an Encoding dictionary even when it shouldn't be needed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1195416670;
-const char *source_modtime_str = "20:11 GMT 18-Nov-2007";
-const char *source_version_str = "20071118";
+const time_t source_modtime = 1195442988;
+const char *source_modtime_str = "03:29 GMT 19-Nov-2007";
+const char *source_version_str = "20071119";
