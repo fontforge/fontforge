ca89cc278969e9249980f7124d6d9cf89dd7209f
George Williams <pfaedit@users.sourceforge.net>
2007-12-07 16:07:52 +0000

    Barry points out that there is a disconnect between the docs and the program about the "spiros" member of a Contour python object. So correct the docs to read "spiros", and change the program so it will accept "spiro" as well as "spiros".
    Add python support for the contour_name.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1196989413;
-const char *source_modtime_str = "01:03 GMT 7-Dec-2007";
+const time_t source_modtime = 1197043476;
+const char *source_modtime_str = "16:04 GMT 7-Dec-2007";
 const char *source_version_str = "20071207";
