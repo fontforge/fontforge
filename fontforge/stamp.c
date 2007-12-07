e3c712ba44a97ad62725f4bc9ccf607d99599114
George Williams <pfaedit@users.sourceforge.net>
2007-12-07 01:06:32 +0000

    Add support for reading Apple's old sfnt wrapped CID format (basically the same as the sfnt wrapped 'typ1' format.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1196963255;
-const char *source_modtime_str = "17:47 GMT 6-Dec-2007";
-const char *source_version_str = "20071206";
+const time_t source_modtime = 1196989413;
+const char *source_modtime_str = "01:03 GMT 7-Dec-2007";
+const char *source_version_str = "20071207";
