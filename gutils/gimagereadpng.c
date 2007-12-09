46f170ba7d759508b9ad886bfa319fb554c5dbb0
pfaedit <pfaedit>
2007-12-09 20:44:15 +0000

    Move some files from gunicode to gutils (files which used to be in gdraw).
    Couple of library files didn't compile with the config.h based system, so fix that.

--- gutils/gimagereadpng.c
+++ gutils/gimagereadpng.c
@@ -24,6 +24,10 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 
 #ifdef _NO_LIBPNG
 static void *a_file_must_define_something=(void *) &a_file_must_define_something;
