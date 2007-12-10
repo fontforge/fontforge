d37595ee7795e220e564a6de478b9da4db08a7dd
George Williams <pfaedit@users.sourceforge.net>
2007-12-10 20:20:13 +0000

    Add the ability to insert a list of words into the metrics view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1197230135;
-const char *source_modtime_str = "19:55 GMT 9-Dec-2007";
-const char *source_version_str = "20071209";
+const time_t source_modtime = 1197318038;
+const char *source_modtime_str = "20:20 GMT 10-Dec-2007";
+const char *source_version_str = "20071210";
