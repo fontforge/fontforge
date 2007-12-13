bfc4237db16df4b4442386291002f75f920eca69
Alexey Kryukov <amkryukov@gmail.com>
2007-12-13 22:09:20 +0000

    If lines are attached to both sides of a diagonal stem, then prefer the longer line's unit vector for this stem.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1197582063;
-const char *source_modtime_str = "21:41 GMT 13-Dec-2007";
+const time_t source_modtime = 1197584294;
+const char *source_modtime_str = "22:18 GMT 13-Dec-2007";
 const char *source_version_str = "20071213";
