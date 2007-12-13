022889a336d761e10006afc93d3d9fc0d2351d77
michal-n <michal-n>
2007-12-13 21:45:20 +0000

    - Fixed snap_to_blues(): it used to mess global accuracy setting up. - Fixed finish_stem(): it did not mark edges of previous hints as done. - CHanged HStemGeninst(): it now tries to interpolate HStems within blues when possible.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1197491743;
-const char *source_modtime_str = "20:35 GMT 12-Dec-2007";
-const char *source_version_str = "20071212";
+const time_t source_modtime = 1197582063;
+const char *source_modtime_str = "21:41 GMT 13-Dec-2007";
+const char *source_version_str = "20071213";
