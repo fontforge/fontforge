158fcc00552d8cc656a4ba09a2bc8cd0c4021340
George Williams <pfaedit@users.sourceforge.net>
2007-12-14 16:13:50 +0000

    When switching to spiro mode on an empty glyph (or perhaps the first glyph in a new font), FF would complain: Internal Error: Character out of bounds in bitmap font 0>=0

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1197644801;
-const char *source_modtime_str = "15:06 GMT 14-Dec-2007";
+const time_t source_modtime = 1197648788;
+const char *source_modtime_str = "16:13 GMT 14-Dec-2007";
 const char *source_version_str = "20071214";
