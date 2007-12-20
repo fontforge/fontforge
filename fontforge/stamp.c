2243973a9ab798541ac2001dcca610537a57bfe4
michal-n <michal-n>
2007-12-20 20:25:34 +0000

    - In HStemGeninst(), new positioning method gives bizarre results   for overlapping stems. Added fallback to old method.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1198035986;
-const char *source_modtime_str = "03:46 GMT 19-Dec-2007";
-const char *source_version_str = "20071219";
+const time_t source_modtime = 1198182117;
+const char *source_modtime_str = "20:21 GMT 20-Dec-2007";
+const char *source_version_str = "20071220";
