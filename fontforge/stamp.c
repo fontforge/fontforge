be2b6836629f196c8378549fe2034c5c2f8d51a1
michal-n <michal-n>
2007-12-21 19:16:39 +0000

    - Alexey suggested improvement for HStemGeninst(). - Added optional experimental code for interpolating strong points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1198212910;
-const char *source_modtime_str = "04:55 GMT 21-Dec-2007";
+const time_t source_modtime = 1198264480;
+const char *source_modtime_str = "19:14 GMT 21-Dec-2007";
 const char *source_version_str = "20071221";
