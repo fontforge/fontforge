7385aefca11e102384d294e52cf7d0625056bccc
pfaedit <pfaedit>
2007-12-27 05:10:31 +0000

    Minor tweaks to fontview.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1198541175;
-const char *source_modtime_str = "00:06 GMT 25-Dec-2007";
-const char *source_version_str = "20071225";
+const time_t source_modtime = 1198732182;
+const char *source_modtime_str = "05:09 GMT 27-Dec-2007";
+const char *source_version_str = "20071227";
