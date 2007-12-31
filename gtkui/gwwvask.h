4a1d100257a811f17fe5ba03807673b7dc8e418f
George Williams <pfaedit@users.sourceforge.net>
2007-12-31 00:22:50 +0000

    Hey, it basically works. It'll display a fontview.
    Nothing else though.

--- gtkui/gwwvask.h
+++ gtkui/gwwvask.h
@@ -4,7 +4,8 @@
 extern char *strndup(const char *str, int n);
 
 extern void gwwv_post_notice(const char *title, const char *msg, ... );
-extern void gwwv_error(const char *title, const char *msg, ... );
+extern void gwwv_post_error(const char *title, const char *msg, ... );
+extern void gwwv_ierror(const char *msg, ... );
 extern int gwwv_ask(const char *title,const char **butnames, int def,int cancel,
 	const char *msg, ... );
 extern char *gwwv_ask_string(const char *title,const char *def,
@@ -15,6 +16,9 @@ extern int gwwv_choose_with_buttons(const char *title,
 extern int gwwv_choose(const char *title,
 	const char **choices, int cnt, int def,
 	const char *msg, ... );
+extern int gwwv_choose_multiple(char *title,
+	const char **choices, char *sel, int cnt, char **buts,
+	const char *msg, ... );
 extern int gwwv_wild_match(char *pattern, const char *name,int ignorecase);
 
 struct gwwv_filter {
@@ -23,8 +27,9 @@ struct gwwv_filter {
     GtkFileFilterFunc filtfunc;
 };
 
-extern char *gwwv_open_filename_mult(const char *title,const char *def_name,const struct gwwv_filter *filters, int mult);
-extern char *gwwv_save_filename_with_gadget(const char *tit,const char *def,const char *filter,GtkWidget *extra)
+extern char *gwwv_open_filename_mult(const char *title,const char *def_name,const struct gwwv_filter *filter, int mult);
+extern char *gwwv_save_filename_with_gadget(const char *title, const char *def_name,
+	const struct gwwv_filter *filters, GtkWidget *extra );
 extern char *gwwv_open_filename(const char *title, const char *def_name,
 	const char *filter);
 extern char *gwwv_saveas_filename(const char *title, const char *def_name,
---
+++ gtkui/stamp.c
@@ -0,0 +1,5 @@
+#include <time.h>
+
+const time_t source_modtime = 1199059958L;
+const char *source_modtime_str = "00:12 GMT 31-Dec-2007";
+const char *source_version_str = "20071231";
