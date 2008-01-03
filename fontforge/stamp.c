9cda2e74979c4b9e14e718920416a2a816102dbe
pfaedit <pfaedit>
2008-01-03 03:46:16 +0000

    If a font had no private dica and one tried to put something in it from python, ff would crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199328312L;
-const char *source_modtime_str = "02:45 GMT 3-Jan-2008";
+const time_t source_modtime = 1199331815L;
+const char *source_modtime_str = "03:43 GMT 3-Jan-2008";
 const char *source_version_str = "20080103";
