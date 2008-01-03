b144a224c84135a0b3bb812d23ef7b2200598b86
George Williams <pfaedit@users.sourceforge.net>
2008-01-03 04:10:03 +0000

    Sometimes the pointers to libspiro didn't get initialized.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199331815L;
-const char *source_modtime_str = "03:43 GMT 3-Jan-2008";
+const time_t source_modtime = 1199333037L;
+const char *source_modtime_str = "04:03 GMT 3-Jan-2008";
 const char *source_version_str = "20080103";
