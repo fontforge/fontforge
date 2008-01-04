8a764cdf4ac68437a8fe3bbe31a1640c255f26c3
michal-n <michal-n>
2008-01-04 16:36:20 +0000

    Patch from Alexey - cures segfaults in code for DStems.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199420993L;
-const char *source_modtime_str = "04:29 GMT 4-Jan-2008";
+const time_t source_modtime = 1199464472L;
+const char *source_modtime_str = "16:34 GMT 4-Jan-2008";
 const char *source_version_str = "20080104";
