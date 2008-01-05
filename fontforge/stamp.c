ab48323453643413c32c24426cc63e01f2b889b5
George Williams <pfaedit@users.sourceforge.net>
2008-01-05 22:04:23 +0000

    FontForge warns the user when a glyph's name does not match its unicode encoding. But in the past it would do this for glyphs with multiple encodings even if the name matched one of the encodings. Fix that.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199549340L;
-const char *source_modtime_str = "16:09 GMT 5-Jan-2008";
+const time_t source_modtime = 1199570568L;
+const char *source_modtime_str = "22:02 GMT 5-Jan-2008";
 const char *source_version_str = "20080105";
