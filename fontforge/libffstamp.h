2facf960cbdb0314a167e01384234576093b0cf8
George Williams <pfaedit@users.sourceforge.net>
2008-01-05 16:08:53 +0000

    When FF uses doubles internally, it still outputs float precision into the sfd file. Fix that.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1199386934L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"19:02 GMT 3-Jan-2008"
-#define LibFF_VersionDate	20080103	/* Year, month, day */
+#define LibFF_ModTime		1199549334L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"16:08 GMT 5-Jan-2008"
+#define LibFF_VersionDate	20080105	/* Year, month, day */
