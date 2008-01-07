f5a9edaeb187da1fed7042b17212f2980b64e8fb
George Williams <pfaedit@users.sourceforge.net>
2008-01-07 03:39:35 +0000

    Scripting access to the new 'PfEd' subtables.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199675710L;
-const char *source_modtime_str = "03:15 GMT 7-Jan-2008";
+const time_t source_modtime = 1199677179L;
+const char *source_modtime_str = "03:39 GMT 7-Jan-2008";
 const char *source_version_str = "20080107";
