340efa6c973982b3c997f9250f6363ed7136b4e0
Alexey Kryukov <amkryukov@gmail.com>
2008-01-08 15:04:11 +0000

    Prevent lines which are not exactly horizontal/vertical from assigning to HV stems.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199762263L;
-const char *source_modtime_str = "03:17 GMT 8-Jan-2008";
+const time_t source_modtime = 1199804990L;
+const char *source_modtime_str = "15:09 GMT 8-Jan-2008";
 const char *source_version_str = "20080108";
