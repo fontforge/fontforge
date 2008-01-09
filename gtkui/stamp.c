e03d91b50a4252aedf30a7d7e2e58b8ad31935fe
pfaedit <pfaedit>
2008-01-09 01:43:36 +0000

    Oops.

--- gtkui/stamp.c
+++ gtkui/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199487402L;
-const char *source_modtime_str = "22:56 GMT 4-Jan-2008";
-const char *source_version_str = "20080104";
+const time_t source_modtime = 1199843152L;
+const char *source_modtime_str = "01:45 GMT 9-Jan-2008";
+const char *source_version_str = "20080109";
