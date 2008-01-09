5790fdd4abe3a3e992432577634f68dba57cd554
George Williams <pfaedit@users.sourceforge.net>
2008-01-09 15:12:48 +0000

    Oops. AutoInstr doesn't set the changed bit on a glyph.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1199850507L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"03:48 GMT 9-Jan-2008"
+#define LibFF_ModTime		1199891511L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"15:11 GMT 9-Jan-2008"
 #define LibFF_VersionDate	20080109	/* Year, month, day */
