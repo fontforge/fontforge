8def07e74f7911e0aebdce945b78c46ed54b26b9
pfaedit <pfaedit>
2008-01-09 03:47:45 +0000

    Missing routine if --with-regular-link set.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1199833298L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"23:01 GMT 8-Jan-2008"
-#define LibFF_VersionDate	20080108	/* Year, month, day */
+#define LibFF_ModTime		1199850507L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"03:48 GMT 9-Jan-2008"
+#define LibFF_VersionDate	20080109	/* Year, month, day */
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199833302L;
-const char *source_modtime_str = "23:01 GMT 8-Jan-2008";
-const char *source_version_str = "20080108";
+const time_t source_modtime = 1199850514L;
+const char *source_modtime_str = "03:48 GMT 9-Jan-2008";
+const char *source_version_str = "20080109";
