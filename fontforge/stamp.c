9a83d8fcd0cafe51b6eaac2f3ed979e413841425
George Williams <pfaedit@users.sourceforge.net>
2008-01-11 01:15:53 +0000

    Should mark hints changed after changing instructions rather than the full blown charchangedupdate.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199988689L;
-const char *source_modtime_str = "18:11 GMT 10-Jan-2008";
-const char *source_version_str = "20080110";
+const time_t source_modtime = 1200014190L;
+const char *source_modtime_str = "01:16 GMT 11-Jan-2008";
+const char *source_version_str = "20080111";
