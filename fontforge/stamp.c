a468f80802542c13e7c40ff56c385d2566d1ef75
pfaedit <pfaedit>
2008-01-12 04:57:26 +0000

    When complaining about a bad class in a mark (GPOS) sub-table, specify the name of the offending mark if we can.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200014190L;
-const char *source_modtime_str = "01:16 GMT 11-Jan-2008";
-const char *source_version_str = "20080111";
+const time_t source_modtime = 1200113785L;
+const char *source_modtime_str = "04:56 GMT 12-Jan-2008";
+const char *source_version_str = "20080112";
