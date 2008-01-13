cba26909a62ed8450f8b49a3e90faf609ee58198
pfaedit <pfaedit>
2008-01-13 19:09:19 +0000

    Previous patch to enable class 0 of contextual lookups had a bug and would save garbage into the sfd file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200113785L;
-const char *source_modtime_str = "04:56 GMT 12-Jan-2008";
-const char *source_version_str = "20080112";
+const time_t source_modtime = 1200251170L;
+const char *source_modtime_str = "19:06 GMT 13-Jan-2008";
+const char *source_version_str = "20080113";
