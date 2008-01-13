ef082891c93f345f8a4a8010b5fa68a73922f78b
George Williams <pfaedit@users.sourceforge.net>
2008-01-13 22:10:59 +0000

    If a font contained an empty anchor class (having either no base glyphs or no mark glyphs) then when dumping out the font subsequent anchor classes got the wrong data associated with them.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200259648L;
-const char *source_modtime_str = "21:27 GMT 13-Jan-2008";
+const time_t source_modtime = 1200262024L;
+const char *source_modtime_str = "22:07 GMT 13-Jan-2008";
 const char *source_version_str = "20080113";
