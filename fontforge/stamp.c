89528a902b0dda0d72fde07def02a959562dcb08
George Williams <pfaedit@users.sourceforge.net>
2008-01-14 22:58:28 +0000

    The anchor control dialog didn't work for mark to mark anchors when looking at a base mark.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200113785L;
-const char *source_modtime_str = "04:56 GMT 12-Jan-2008";
-const char *source_version_str = "20080112";
+const time_t source_modtime = 1200351454L;
+const char *source_modtime_str = "22:57 GMT 14-Jan-2008";
+const char *source_version_str = "20080114";
