41b2594f6c34b2d1406be6674a659017b818b5be
George Williams <pfaedit@users.sourceforge.net>
2008-01-15 17:09:08 +0000

    Just as it is possible to have things in 'morx' that are not in 'GSUB', it is possible that a 'GPOS' table contains no kerning information but a 'kern' table does. We used not to read 'kern' if we had 'GPOS', be a little subtler and check for the presence of a 'kern' feature inside 'GPOS' before deciding whether to read the 'kern' table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200351454L;
-const char *source_modtime_str = "22:57 GMT 14-Jan-2008";
-const char *source_version_str = "20080114";
+const time_t source_modtime = 1200416949L;
+const char *source_modtime_str = "17:09 GMT 15-Jan-2008";
+const char *source_version_str = "20080115";
