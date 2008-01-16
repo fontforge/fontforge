767db47c61a6abb91503826c3e8b2bb1fed78035
pfaedit <pfaedit>
2008-01-16 18:00:40 +0000

    Deep in the heart of Find Problems check for bad contour direction, was a bug where it looked at the wrong spline.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200416949L;
-const char *source_modtime_str = "17:09 GMT 15-Jan-2008";
-const char *source_version_str = "20080115";
+const time_t source_modtime = 1200506379L;
+const char *source_modtime_str = "17:59 GMT 16-Jan-2008";
+const char *source_version_str = "20080116";
