84477484795b99af2366381a0e02be1d95adfe2c
pfaedit <pfaedit>
2008-01-17 18:19:10 +0000

    the glyph.validate function in python was marked as taking NOARGS in the method table, but, in fact, does take arguments.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200532972L;
-const char *source_modtime_str = "01:22 GMT 17-Jan-2008";
+const time_t source_modtime = 1200593645L;
+const char *source_modtime_str = "18:14 GMT 17-Jan-2008";
 const char *source_version_str = "20080117";
