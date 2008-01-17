0d3883d02d1eefa64043b8862707332ccfbc38eb
George Williams <pfaedit@users.sourceforge.net>
2008-01-17 01:03:11 +0000

    Be more lenient when closing a path ... we can get rounding errors.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200530016L;
-const char *source_modtime_str = "00:33 GMT 17-Jan-2008";
+const time_t source_modtime = 1200531546L;
+const char *source_modtime_str = "00:59 GMT 17-Jan-2008";
 const char *source_version_str = "20080117";
