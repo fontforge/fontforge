16f73479d84d90ce18711847e3a1265d961802d4
akryukov <akryukov>
2008-01-21 13:36:19 +0000

    Make the stem detector to collect some additional data useful for the autoinstructor. Fix a crash in AssignPointsToStems().

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200809927L;
-const char *source_modtime_str = "06:18 GMT 20-Jan-2008";
-const char *source_version_str = "20080120";
+const time_t source_modtime = 1200923069L;
+const char *source_modtime_str = "13:44 GMT 21-Jan-2008";
+const char *source_version_str = "20080121";
