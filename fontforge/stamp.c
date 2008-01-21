e6eb38d63cc9a82611867baba754fa22a20cccc3
George Williams <pfaedit@users.sourceforge.net>
2008-01-21 22:41:39 +0000

    The fix I put in the other day for using uninitialized variables, ended up using a different uninitialized variable and the results were far worse than they had been. CVT editor crashed on creation.
    When the instruction editor went from view to edit mode (why do I make that
    distinction?) it shrank and this bothered Alexej.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200949818L;
-const char *source_modtime_str = "21:10 GMT 21-Jan-2008";
+const time_t source_modtime = 1200955034L;
+const char *source_modtime_str = "22:37 GMT 21-Jan-2008";
 const char *source_version_str = "20080121";
