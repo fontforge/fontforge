178581b14ba0c35b683eab599d9e0ee417cc40cc
George Williams <pfaedit@users.sourceforge.net>
2008-01-22 03:54:55 +0000

    If the user closes the font info dlg when there are mark class dialogs floating around then close them too (there was code which was supposed to do this, but it didn't work).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200972709L;
-const char *source_modtime_str = "03:31 GMT 22-Jan-2008";
+const time_t source_modtime = 1200973771L;
+const char *source_modtime_str = "03:49 GMT 22-Jan-2008";
 const char *source_version_str = "20080122";
