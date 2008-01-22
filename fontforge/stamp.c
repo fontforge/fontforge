598e5285968e2a7181fb22fe835a47b8a1cb2974
George Williams <pfaedit@users.sourceforge.net>
2008-01-22 03:36:14 +0000

    Try to make it clear in the Mark Classes pane of the Font Info that mark classes are not anchor classes. Someone got confused...

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1200955034L;
-const char *source_modtime_str = "22:37 GMT 21-Jan-2008";
-const char *source_version_str = "20080121";
+const time_t source_modtime = 1200972709L;
+const char *source_modtime_str = "03:31 GMT 22-Jan-2008";
+const char *source_version_str = "20080122";
