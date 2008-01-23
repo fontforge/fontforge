1962a6e6af8d161a9467614357df9b8e06d86da3
George Williams <pfaedit@users.sourceforge.net>
2008-01-23 00:39:37 +0000

    In python scripting contour.reverseDirection only worked on closed contours.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1201042758L;
-const char *source_modtime_str = "22:59 GMT 22-Jan-2008";
-const char *source_version_str = "20080122";
+const time_t source_modtime = 1201048553L;
+const char *source_modtime_str = "00:35 GMT 23-Jan-2008";
+const char *source_version_str = "20080123";
