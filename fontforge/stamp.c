a641738922370dc5076d07c2c79800f77d73fc66
George Williams <pfaedit@users.sourceforge.net>
2008-01-30 01:01:08 +0000

    Oops. Rotated replaces ignoring endpoints didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1201626567L;
-const char *source_modtime_str = "17:09 GMT 29-Jan-2008";
-const char *source_version_str = "20080129";
+const time_t source_modtime = 1201654521L;
+const char *source_modtime_str = "00:55 GMT 30-Jan-2008";
+const char *source_version_str = "20080130";
