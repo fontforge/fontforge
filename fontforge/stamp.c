224bdd945c4e9aeaf9d9756976555077f3cde12a
michal-n <michal-n>
2008-01-31 12:57:16 +0000

    Autoinstructor: Patch by Alexey, fixes a buglet in DStem code.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1201775415L;
-const char *source_modtime_str = "10:30 GMT 31-Jan-2008";
+const time_t source_modtime = 1201783849L;
+const char *source_modtime_str = "12:50 GMT 31-Jan-2008";
 const char *source_version_str = "20080131";
