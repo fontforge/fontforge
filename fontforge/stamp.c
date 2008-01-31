31a8c0a1cafe56f1f0af659392ab2c497d762a9b
michal-n <michal-n>
2008-01-31 14:42:22 +0000

    Autoinstructor: turned on strong point interpolation: - Various important outline points are correctly placed between hints' edges. - Potential benefits for DStem positioning. - Drawbacks: 2x slower, code generated is now much bigger.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1201788497L;
-const char *source_modtime_str = "14:08 GMT 31-Jan-2008";
+const time_t source_modtime = 1201790278L;
+const char *source_modtime_str = "14:37 GMT 31-Jan-2008";
 const char *source_version_str = "20080131";
