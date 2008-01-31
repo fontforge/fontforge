45c56c044745d0c1a69ba2a9d0acf044c5015381
pfaedit <pfaedit>
2008-01-31 02:48:30 +0000

    The merge (and simplify) command could produce odd results if the points surrounding the merge had no control points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1201721804L;
-const char *source_modtime_str = "19:36 GMT 30-Jan-2008";
-const char *source_version_str = "20080130";
+const time_t source_modtime = 1201746924L;
+const char *source_modtime_str = "02:35 GMT 31-Jan-2008";
+const char *source_version_str = "20080131";
