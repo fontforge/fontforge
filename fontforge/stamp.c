44a05e3b99cef26635a25929c9859c7be4e42cda
pfaedit <pfaedit>
2008-02-01 23:08:13 +0000

    New script/language/feature tags from OpenType 1.5.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1201820415L;
-const char *source_modtime_str = "23:00 GMT 31-Jan-2008";
-const char *source_version_str = "20080131";
+const time_t source_modtime = 1201906965L;
+const char *source_modtime_str = "23:02 GMT 1-Feb-2008";
+const char *source_version_str = "20080201";
