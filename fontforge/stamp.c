0dd4f2568a60ff8444769c967f0cc79f23218ead
pfaedit <pfaedit>
2008-02-03 20:33:16 +0000

    Someone handed me a font with a SJIS encoding cmap. The cmap mapped a glyph to SJIS 128, which isn't a valid SJIS code point. FF got an insane result when trying to convert to unicode and crashed. Add a check against bad code points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202069044L;
-const char *source_modtime_str = "20:04 GMT 3-Feb-2008";
+const time_t source_modtime = 1202070331L;
+const char *source_modtime_str = "20:25 GMT 3-Feb-2008";
 const char *source_version_str = "20080203";
