f5884029dc349eb9216445152e70f9f9badcd244
Alexey Kryukov <amkryukov@gmail.com>
2008-02-03 14:59:08 +0000

    Oops. It was possible to assign a left line (and thus some of this line's points) to the right side of a stem. This resulted into a NaN in AddLIneSegment().

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202017193L;
-const char *source_modtime_str = "05:39 GMT 3-Feb-2008";
+const time_t source_modtime = 1202050913L;
+const char *source_modtime_str = "15:01 GMT 3-Feb-2008";
 const char *source_version_str = "20080203";
