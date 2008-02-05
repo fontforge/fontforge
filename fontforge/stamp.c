6fa6ed18bb590e1dbf4e8aaf2ac11fff39b402e0
michal-n <michal-n>
2008-02-05 15:43:10 +0000

    Autoinstructor: - Finished support for PS family alignment zones. ! Disabling hinting at nano sizes depended on ppem measured along wrong axis. ! Fixing this implied changes to default 'prep' code.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202133820L;
-const char *source_modtime_str = "14:03 GMT 4-Feb-2008";
-const char *source_version_str = "20080204";
+const time_t source_modtime = 1202224958L;
+const char *source_modtime_str = "15:22 GMT 5-Feb-2008";
+const char *source_version_str = "20080205";
