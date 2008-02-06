3cd64b7a1fa2da62ab5f4bf06b85dfdd24df89f2
George Williams <pfaedit@users.sourceforge.net>
2008-02-06 21:42:43 +0000

    The first reference in a glyph without a splineset (or first image) would be lost.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202261999L;
-const char *source_modtime_str = "01:39 GMT 6-Feb-2008";
+const time_t source_modtime = 1202333995L;
+const char *source_modtime_str = "21:39 GMT 6-Feb-2008";
 const char *source_version_str = "20080206";
