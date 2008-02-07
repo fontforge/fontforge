21f197b5bb0e2eaed4fd89535dcbf98f37160217
pfaedit <pfaedit>
2008-02-07 23:15:46 +0000

    When converting cubics to quadratics, if we had a curved spline where one of the endpoints of the spline had no control point, then we'd get a slope at that point of 0/0. This meant that we tried to put a quadratic control point on top of that endpoint which looked confusing and seemed unnecessary.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202424236L;
-const char *source_modtime_str = "22:43 GMT 7-Feb-2008";
+const time_t source_modtime = 1202426030L;
+const char *source_modtime_str = "23:13 GMT 7-Feb-2008";
 const char *source_version_str = "20080207";
