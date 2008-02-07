e99b85185388d7dca5d3e2f790e6b47b990cbbc8
George Williams <pfaedit@users.sourceforge.net>
2008-02-07 16:51:45 +0000

    Didn't handled NULL tokens properly when reading in a feature file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202398642L;
-const char *source_modtime_str = "15:37 GMT 7-Feb-2008";
+const time_t source_modtime = 1202402891L;
+const char *source_modtime_str = "16:48 GMT 7-Feb-2008";
 const char *source_version_str = "20080207";
