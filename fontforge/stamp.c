d1b76c98f9e91bda4a87427204d8301a919d1a59
George Williams <pfaedit@users.sourceforge.net>
2008-02-07 02:20:24 +0000

    I broke converting a font from quadratic to cubic splines with the layer change.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202349463L;
-const char *source_modtime_str = "01:57 GMT 7-Feb-2008";
+const time_t source_modtime = 1202350572L;
+const char *source_modtime_str = "02:16 GMT 7-Feb-2008";
 const char *source_version_str = "20080207";
