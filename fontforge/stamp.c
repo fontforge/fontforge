0c2a2822b124a3d0a59b6ff419113602a221af62
George Williams <pfaedit@users.sourceforge.net>
2008-02-08 22:35:49 +0000

    I had intended not to give warning messages about names like "nine.oldstyle" (defined in Adobe's obsolete PUA mapping) when loading a tt font, if such a glyph had a different unicode mapping (or none) than the one Adobe used ages ago. But I screwed up and was generating messages anyway.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202449746L;
-const char *source_modtime_str = "05:49 GMT 8-Feb-2008";
+const time_t source_modtime = 1202509892L;
+const char *source_modtime_str = "22:31 GMT 8-Feb-2008";
 const char *source_version_str = "20080208";
