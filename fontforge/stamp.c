6bd47c576edc0ed81df342213f6de7cad41a10c7
George Williams <pfaedit@users.sourceforge.net>
2008-02-12 04:29:49 +0000

    Drat. FF really wants to set the latin1 codepage bit, even if the user explicitly provides the codepage settings. The wine people don't like that.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202772903L;
-const char *source_modtime_str = "23:35 GMT 11-Feb-2008";
-const char *source_version_str = "20080211";
+const time_t source_modtime = 1202790198L;
+const char *source_modtime_str = "04:23 GMT 12-Feb-2008";
+const char *source_version_str = "20080212";
