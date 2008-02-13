cfa541057c920ed15e36b60781503dd9f75fef72
michal-n <michal-n>
2008-02-13 13:44:45 +0000

    Autoinstructor: - Alexey points out that perpendicular extremae were not interpolated between edges. - Fixed rare crash due to uninitialized pointers in SetFreedomVector(). - SMD after last diagonal stems is probably no longer needed. - instructpoints() can now use SLOOP.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202866412L;
-const char *source_modtime_str = "01:33 GMT 13-Feb-2008";
+const time_t source_modtime = 1202910309L;
+const char *source_modtime_str = "13:45 GMT 13-Feb-2008";
 const char *source_version_str = "20080213";
