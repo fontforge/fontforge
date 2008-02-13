a7b7907a8de58da6d471aabe359f54360b39fb3f
pfaedit <pfaedit>
2008-02-13 17:53:50 +0000

    Label some of the horizontal metrics lines

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202924962L;
-const char *source_modtime_str = "17:49 GMT 13-Feb-2008";
+const time_t source_modtime = 1202925085L;
+const char *source_modtime_str = "17:51 GMT 13-Feb-2008";
 const char *source_version_str = "20080213";
