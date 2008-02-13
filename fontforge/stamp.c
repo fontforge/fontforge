9ae14623660b57537dd1aa6a8d4be53659c48e82
George Williams <pfaedit@users.sourceforge.net>
2008-02-13 01:36:36 +0000

    Oops, fixup to new calling conventions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1202865540L;
-const char *source_modtime_str = "01:19 GMT 13-Feb-2008";
+const time_t source_modtime = 1202866412L;
+const char *source_modtime_str = "01:33 GMT 13-Feb-2008";
 const char *source_version_str = "20080213";
