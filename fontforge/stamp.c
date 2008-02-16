22aba24ac5be459ae192a56e06ea19e26f680007
michal-n <michal-n>
2008-02-16 14:11:55 +0000

    Autoinstructor: IsInflectionPoint() hung on single-point contours.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203130161L;
-const char *source_modtime_str = "02:49 GMT 16-Feb-2008";
+const time_t source_modtime = 1203170677L;
+const char *source_modtime_str = "14:04 GMT 16-Feb-2008";
 const char *source_version_str = "20080216";
