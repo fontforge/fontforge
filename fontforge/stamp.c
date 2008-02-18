2ff9ad97f38e1c05508830e60dc0916fab901909
George Williams <pfaedit@users.sourceforge.net>
2008-02-18 03:04:58 +0000

    Another layer patch. The layer the fontview displays and controls should now be under user control (View->Layers->*) and not always be the Foreground layer.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203193673L;
-const char *source_modtime_str = "20:27 GMT 16-Feb-2008";
-const char *source_version_str = "20080216";
+const time_t source_modtime = 1203301845L;
+const char *source_modtime_str = "02:30 GMT 18-Feb-2008";
+const char *source_version_str = "20080218";
