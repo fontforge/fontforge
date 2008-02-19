117ddec7c35d903da6ae41edc0219dac4cb81b0a
George Williams <pfaedit@users.sourceforge.net>
2008-02-19 20:58:25 +0000

    When selecting a control point in the outline view, we would always select the next point if both next and prev were in the same place (ie, on the base point itself). But when the point is at the end of an open contour, the one which should be selected is the one which controls a spline (which need not be the next one).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203378677L;
-const char *source_modtime_str = "23:51 GMT 18-Feb-2008";
-const char *source_version_str = "20080218";
+const time_t source_modtime = 1203454204L;
+const char *source_modtime_str = "20:50 GMT 19-Feb-2008";
+const char *source_version_str = "20080219";
