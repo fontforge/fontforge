2718a1cad7d20783c8d2fe51781d95439b6ada51
pfaedit <pfaedit>
2008-02-20 20:00:57 +0000

    All the dlgs based on the charview (mathkern, search, tilepath) started displaying the background layer rather than the foreground.
    Math strings weren't getting initialized in math kern.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203535870L;
-const char *source_modtime_str = "19:31 GMT 20-Feb-2008";
+const time_t source_modtime = 1203537464L;
+const char *source_modtime_str = "19:57 GMT 20-Feb-2008";
 const char *source_version_str = "20080220";
