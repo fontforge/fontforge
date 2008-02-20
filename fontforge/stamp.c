4fc7fdd3b8dd4488f4236c8200a21991c424166a
pfaedit <pfaedit>
2008-02-20 21:07:06 +0000

    Barry points out an #ifdef which wasn't complex enough...

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203541152L;
-const char *source_modtime_str = "20:59 GMT 20-Feb-2008";
+const time_t source_modtime = 1203541585L;
+const char *source_modtime_str = "21:06 GMT 20-Feb-2008";
 const char *source_version_str = "20080220";
