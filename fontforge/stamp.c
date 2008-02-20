c504765c8bb564fdf9dd476bec3f5185e82a863b
George Williams <pfaedit@users.sourceforge.net>
2008-02-20 15:53:44 +0000

    A font-wide transform would transform the background layer twice.
    Also the transform menu item seems to have lost its shortcut. Put it back.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203454204L;
-const char *source_modtime_str = "20:50 GMT 19-Feb-2008";
-const char *source_version_str = "20080219";
+const time_t source_modtime = 1203522064L;
+const char *source_modtime_str = "15:41 GMT 20-Feb-2008";
+const char *source_version_str = "20080220";
