13bac266a341afd273b9b48a82fcfd52568d09b5
George Williams <pfaedit@users.sourceforge.net>
2008-02-20 21:09:28 +0000

    FF didn't compile when the truetype debugger wasn't configured.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203541585L;
-const char *source_modtime_str = "21:06 GMT 20-Feb-2008";
+const time_t source_modtime = 1203541728L;
+const char *source_modtime_str = "21:08 GMT 20-Feb-2008";
 const char *source_version_str = "20080220";
