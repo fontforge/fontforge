71b06d557bc5420d43705b5a5655b969b8a95828
michal-n <michal-n>
2008-02-21 19:17:31 +0000

    Autoinstructor:   Alexey wants strong point interpolation to be moved after instructing diagonals.   He plans to adjust diagonals in relation to other hints otherwise.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203541728L;
-const char *source_modtime_str = "21:08 GMT 20-Feb-2008";
-const char *source_version_str = "20080220";
+const time_t source_modtime = 1203621081L;
+const char *source_modtime_str = "19:11 GMT 21-Feb-2008";
+const char *source_version_str = "20080221";
