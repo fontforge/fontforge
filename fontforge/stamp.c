40a3b048110094d9f5f82340088faff902d629ce
pfaedit <pfaedit>
2008-02-25 05:26:12 +0000

    In Type3 fonts, the layers which aren't current should be drawn as dashed black lines not as green lines. the green lines aren't visible against background images (to my eyes anyway).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203898937L;
-const char *source_modtime_str = "00:22 GMT 25-Feb-2008";
+const time_t source_modtime = 1203915137L;
+const char *source_modtime_str = "04:52 GMT 25-Feb-2008";
 const char *source_version_str = "20080225";
