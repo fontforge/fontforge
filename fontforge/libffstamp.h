7d099486fd517da0a56624912fa43ea65101b199
pfaedit <pfaedit>
2008-02-25 00:46:38 +0000

    Add a command Points->Make Arc, which is similar to Points->Make Line except that it connects the two points with an elliptical arc rather than a straight line. The ellipse is choosen to run through the two points (obviously) and to be tangent to the slope of those points. This isn't enough information to determine an ellipse uniquely (three points and two tangents being required) so in general there will be an infinite number of solutions (and it is possible to provide contradictory input so that no ellipse can be found). FontForge may not choose the ellipse you had your heart set on. FF will first attempt to find an ellipse so that one of the points lies on an axis of the ellipse (this will find a circle if there is a circle which satisfies the constraints above). If that fails it will look more generally for ellipses.
    (If you hold down the <Alt> key when you select the menu item, FF will leave the ellipse it calculated in the background layer. That was a debugging technique but I thought it was kind of interesting and left it in.)

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1203301840L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"02:30 GMT 18-Feb-2008"
-#define LibFF_VersionDate	20080218	/* Year, month, day */
+#define LibFF_ModTime		1203898935L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"00:22 GMT 25-Feb-2008"
+#define LibFF_VersionDate	20080225	/* Year, month, day */
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203856222L;
-const char *source_modtime_str = "12:30 GMT 24-Feb-2008";
-const char *source_version_str = "20080224";
+const time_t source_modtime = 1203898937L;
+const char *source_modtime_str = "00:22 GMT 25-Feb-2008";
+const char *source_version_str = "20080225";
