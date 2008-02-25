61b3fad047fbacda818f84b4c5f9cc77b9ad90cb
George Williams <pfaedit@users.sourceforge.net>
2008-02-25 16:27:47 +0000

    Print dlg always drew the background layer.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203915137L;
-const char *source_modtime_str = "04:52 GMT 25-Feb-2008";
+const time_t source_modtime = 1203956677L;
+const char *source_modtime_str = "16:24 GMT 25-Feb-2008";
 const char *source_version_str = "20080225";
