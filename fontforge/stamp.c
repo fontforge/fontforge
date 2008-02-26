bcfe0270a241684c9b8ad3a86e5af9d288db0bb1
pfaedit <pfaedit>
2008-02-26 03:31:47 +0000

    When running simplify on a quadratic contour and the contour doubles back on itself, FF would do an optimization which only worked for cubics.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203991059L;
-const char *source_modtime_str = "01:57 GMT 26-Feb-2008";
+const time_t source_modtime = 1203996207L;
+const char *source_modtime_str = "03:23 GMT 26-Feb-2008";
 const char *source_version_str = "20080226";
