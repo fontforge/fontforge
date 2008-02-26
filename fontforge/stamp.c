e760a89b308f91d0713810038bbc7757b11a535c
pfaedit <pfaedit>
2008-02-26 22:54:17 +0000

    Put in a way for the user to leap easily from Font Info->Unicode Ranges to Font Info->OS/2->Charsets->Unicode Ranges.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204050104L;
-const char *source_modtime_str = "18:21 GMT 26-Feb-2008";
+const time_t source_modtime = 1204064529L;
+const char *source_modtime_str = "22:22 GMT 26-Feb-2008";
 const char *source_version_str = "20080226";
