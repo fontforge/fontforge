6f0d0d8619ef09bc3531ea738f440ae47f818eec
pfaedit <pfaedit>
2008-02-26 00:35:49 +0000

    Some menu commands were disabled if not in foreground:   Show Grid Fit->children
    Also interpolated points weren't shown as such.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1203985188L;
-const char *source_modtime_str = "00:19 GMT 26-Feb-2008";
+const time_t source_modtime = 1203985838L;
+const char *source_modtime_str = "00:30 GMT 26-Feb-2008";
 const char *source_version_str = "20080226";
