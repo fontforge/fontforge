9fdcc42f7bb2c79bb4005aeba55a92ec1ddeef08
pfaedit <pfaedit>
2008-02-29 22:36:25 +0000

    Make sure the first character in marlett is 0xf020.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204318948L;
-const char *source_modtime_str = "21:02 GMT 29-Feb-2008";
+const time_t source_modtime = 1204324025L;
+const char *source_modtime_str = "22:27 GMT 29-Feb-2008";
 const char *source_version_str = "20080229";
