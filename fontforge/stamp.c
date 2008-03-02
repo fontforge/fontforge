f5b8570f03d844642f822a3f857a31d4b0dfd9ac
michal-n <michal-n>
2008-03-02 19:48:53 +0000

    Fixed a truetype stack overflow.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204475319L;
-const char *source_modtime_str = "16:28 GMT 2-Mar-2008";
+const time_t source_modtime = 1204486902L;
+const char *source_modtime_str = "19:41 GMT 2-Mar-2008";
 const char *source_version_str = "20080302";
