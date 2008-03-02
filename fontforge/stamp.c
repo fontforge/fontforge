8382a6990a8d8f23944fb0d3669ddedde0eababd
michal-n <michal-n>
2008-03-02 14:25:33 +0000

    Refined previous patch. Fixed a memory leak.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204465932L;
-const char *source_modtime_str = "13:52 GMT 2-Mar-2008";
+const time_t source_modtime = 1204467834L;
+const char *source_modtime_str = "14:23 GMT 2-Mar-2008";
 const char *source_version_str = "20080302";
