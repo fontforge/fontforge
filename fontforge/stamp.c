29660ff1a6ed970ca06390c60e67921f27776649
michal-n <michal-n>
2008-03-02 16:30:32 +0000

    Previous patch 'fixed' too much.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204474830L;
-const char *source_modtime_str = "16:20 GMT 2-Mar-2008";
+const time_t source_modtime = 1204475319L;
+const char *source_modtime_str = "16:28 GMT 2-Mar-2008";
 const char *source_version_str = "20080302";
