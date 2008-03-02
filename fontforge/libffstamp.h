d508f8c1c6aa9d0329aaf09ae5f642998acc03fa
pfaedit <pfaedit>
2008-03-02 21:12:53 +0000

    Sync up.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1204421935L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"01:38 GMT 2-Mar-2008"
+#define LibFF_ModTime		1204490688L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"20:44 GMT 2-Mar-2008"
 #define LibFF_VersionDate	20080302	/* Year, month, day */
