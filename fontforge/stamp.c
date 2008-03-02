dde10877e4edd8aa4e2aea4b39571e36079e5de4
michal-n <michal-n>
2008-03-02 14:45:33 +0000

    An obvious opportunity to make things a bit more compact.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204467834L;
-const char *source_modtime_str = "14:23 GMT 2-Mar-2008";
+const time_t source_modtime = 1204468988L;
+const char *source_modtime_str = "14:43 GMT 2-Mar-2008";
 const char *source_version_str = "20080302";
