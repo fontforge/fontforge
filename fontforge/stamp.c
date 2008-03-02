eda88eeab5f8e804b50fc41c9e236259e7eb7523
George Williams <pfaedit@users.sourceforge.net>
2008-03-02 21:45:06 +0000

    Uninitialized value in svg output.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204489782L;
-const char *source_modtime_str = "20:29 GMT 2-Mar-2008";
+const time_t source_modtime = 1204494252L;
+const char *source_modtime_str = "21:44 GMT 2-Mar-2008";
 const char *source_version_str = "20080302";
