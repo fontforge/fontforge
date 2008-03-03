16aa450ff6e501bb78cd8250d64723fdd8697923
michal-n <michal-n>
2008-03-03 07:53:03 +0000

    Autoinstructor: Alexey told me memory problems with his code all had subtler cause than I suspected. So fix them properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204494252L;
-const char *source_modtime_str = "21:44 GMT 2-Mar-2008";
-const char *source_version_str = "20080302";
+const time_t source_modtime = 1204530599L;
+const char *source_modtime_str = "07:49 GMT 3-Mar-2008";
+const char *source_version_str = "20080303";
