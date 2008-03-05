c3c70167b60fe4363f44daa7bfddcc92c4e2ca69
George Williams <pfaedit@users.sourceforge.net>
2008-03-05 16:22:22 +0000

    Deleting something in the guidelines layer probably caused a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204674317L;
-const char *source_modtime_str = "23:45 GMT 4-Mar-2008";
-const char *source_version_str = "20080304";
+const time_t source_modtime = 1204733930L;
+const char *source_modtime_str = "16:18 GMT 5-Mar-2008";
+const char *source_version_str = "20080305";
