4a1deb02aa85994f76e60ba7d669c720de6bfeae
George Williams <pfaedit@users.sourceforge.net>
2008-03-08 04:03:46 +0000

    The code which decided on whether to apply a chaining lookup approached backtrack data in the wrong order (so only lookups with either 0 or 1 backtrack entries worked. Which was all I used to test).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1204944332L;
-const char *source_modtime_str = "02:45 GMT 8-Mar-2008";
+const time_t source_modtime = 1204948669L;
+const char *source_modtime_str = "03:57 GMT 8-Mar-2008";
 const char *source_version_str = "20080308";
