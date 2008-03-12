abca98f044ce0cba082f972f021b8c27d96578fc
pfaedit <pfaedit>
2008-03-12 23:36:44 +0000

    Encoding->Add Encoding Slots didn't work for CID-keyed fonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1205259020L;
-const char *source_modtime_str = "18:10 GMT 11-Mar-2008";
-const char *source_version_str = "20080311";
+const time_t source_modtime = 1205363820L;
+const char *source_modtime_str = "23:17 GMT 12-Mar-2008";
+const char *source_version_str = "20080312";
