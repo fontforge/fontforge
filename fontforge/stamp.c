2f45b08c3c4adc13ab31cbe37a7924730403175c
George Williams <pfaedit@users.sourceforge.net>
2008-03-12 23:38:16 +0000

    Generating a CID-keyed otf font was broken by the layers patch. (Only the background layer was output).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1205363820L;
-const char *source_modtime_str = "23:17 GMT 12-Mar-2008";
+const time_t source_modtime = 1205364807L;
+const char *source_modtime_str = "23:33 GMT 12-Mar-2008";
 const char *source_version_str = "20080312";
