a7a2f8a48b005952dd66938241a9a9acc3e418df
George Williams <pfaedit@users.sourceforge.net>
2008-03-13 01:09:53 +0000

    How silly of me. When the PDFReference manual (page 96) says the /Resources dictionary for a type3 font go in the content stream dictionary, it really means the font dictionary.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1204944326L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"02:45 GMT 8-Mar-2008"
-#define LibFF_VersionDate	20080308	/* Year, month, day */
+#define LibFF_ModTime		1205370116L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"01:01 GMT 13-Mar-2008"
+#define LibFF_VersionDate	20080313	/* Year, month, day */
