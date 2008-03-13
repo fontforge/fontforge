f0cc0d891cf9369282947a0c409c9727ec5c412d
pfaedit <pfaedit>
2008-03-13 02:21:23 +0000

    Add a more obvious way to set the clip path in a type3 font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1205371871L;
-const char *source_modtime_str = "01:31 GMT 13-Mar-2008";
+const time_t source_modtime = 1205374612L;
+const char *source_modtime_str = "02:16 GMT 13-Mar-2008";
 const char *source_version_str = "20080313";
