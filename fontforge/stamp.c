4e7fb4e3ac7ca782f4b55caa1626cc6c01c94449
George Williams <pfaedit@users.sourceforge.net>
2008-03-18 16:58:32 +0000

    Oops. Rasterizer did not handle clipping paths which were larger than thing being clipped.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1205806024L;
-const char *source_modtime_str = "02:07 GMT 18-Mar-2008";
+const time_t source_modtime = 1205859288L;
+const char *source_modtime_str = "16:54 GMT 18-Mar-2008";
 const char *source_version_str = "20080318";
