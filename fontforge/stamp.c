d92ba82c11a1e8260df8a005681f220bbfd6774b
pfaedit <pfaedit>
2008-03-20 03:04:24 +0000

    Minimal support of baseline alignment in the print dialog (not in the metrics view).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1205975971L;
-const char *source_modtime_str = "01:19 GMT 20-Mar-2008";
+const time_t source_modtime = 1205981915L;
+const char *source_modtime_str = "02:58 GMT 20-Mar-2008";
 const char *source_version_str = "20080320";
