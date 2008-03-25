908ba1f519e8d2d1fdf9f4b14e029f060e0f9a96
pfaedit <pfaedit>
2008-03-25 12:11:05 +0000

    Baseline patch broke reading ligature carets from sfd files.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1206217761L;
-const char *source_modtime_str = "20:29 GMT 22-Mar-2008";
-const char *source_version_str = "20080322";
+const time_t source_modtime = 1206404450L;
+const char *source_modtime_str = "00:20 GMT 25-Mar-2008";
+const char *source_version_str = "20080325";
