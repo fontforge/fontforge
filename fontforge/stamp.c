02eb420be2d9f863c932c1d9e9a935d49cdea021
pfaedit <pfaedit>
2008-03-27 20:10:39 +0000

    Werner suggests a non case concious sort for glyph names in show att.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1206575646L;
-const char *source_modtime_str = "23:54 GMT 26-Mar-2008";
-const char *source_version_str = "20080326";
+const time_t source_modtime = 1206648553L;
+const char *source_modtime_str = "20:09 GMT 27-Mar-2008";
+const char *source_version_str = "20080327";
