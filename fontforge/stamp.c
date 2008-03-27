0c6831a9afe0da9a3267edc1d43a4685f0812f75
pfaedit <pfaedit>
2008-03-27 00:04:42 +0000

    Order glyph lists in Show ATT alphabetically by glyph name (it used to be (in most cases) by GID).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1206464186L;
-const char *source_modtime_str = "16:56 GMT 25-Mar-2008";
-const char *source_version_str = "20080325";
+const time_t source_modtime = 1206575646L;
+const char *source_modtime_str = "23:54 GMT 26-Mar-2008";
+const char *source_version_str = "20080326";
