543f690e38b9351cf110321f91a4a36d3351cdbf
George Williams <pfaedit@users.sourceforge.net>
2008-03-28 18:43:33 +0000

    Add python scripting access to baseline information.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1206675382L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"03:36 GMT 28-Mar-2008"
+#define LibFF_ModTime		1206729046L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"18:30 GMT 28-Mar-2008"
 #define LibFF_VersionDate	20080328	/* Year, month, day */
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1206675633L;
-const char *source_modtime_str = "03:40 GMT 28-Mar-2008";
+const time_t source_modtime = 1206729050L;
+const char *source_modtime_str = "18:30 GMT 28-Mar-2008";
 const char *source_version_str = "20080328";
