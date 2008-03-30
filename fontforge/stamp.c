df6ceeb2ba1cb9e51868e3b3601dfff838364e05
pfaedit <pfaedit>
2008-03-30 16:36:12 +0000

    Instead of just dying with a file corrupted error, add a warning that the version number is wrong.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1206892682L;
-const char *source_modtime_str = "15:58 GMT 30-Mar-2008";
+const time_t source_modtime = 1206894848L;
+const char *source_modtime_str = "16:34 GMT 30-Mar-2008";
 const char *source_version_str = "20080330";
