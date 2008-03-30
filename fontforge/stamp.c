de75313001b819fad4097ec2486209d8bd71865f
George Williams <pfaedit@users.sourceforge.net>
2008-03-30 16:32:46 +0000

    Bump the version number on the sfd file to 3.0. Should have done this with the layers change last month, but better late than never (I think).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1206830178L;
-const char *source_modtime_str = "22:36 GMT 29-Mar-2008";
-const char *source_version_str = "20080329";
+const time_t source_modtime = 1206892682L;
+const char *source_modtime_str = "15:58 GMT 30-Mar-2008";
+const char *source_version_str = "20080330";
