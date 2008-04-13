e8a7e15a926a809d80b479e1419efdf8fdc7e7a1
George Williams <pfaedit@users.sourceforge.net>
2008-04-13 17:33:33 +0000

    Clean up the display dlg a bit.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1208102813L;
-const char *source_modtime_str = "16:06 GMT 13-Apr-2008";
+const time_t source_modtime = 1208107861L;
+const char *source_modtime_str = "17:31 GMT 13-Apr-2008";
 const char *source_version_str = "20080413";
