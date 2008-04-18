c9b44055ecc667c8af07850cfc46c9288919caa3
George Williams <pfaedit@users.sourceforge.net>
2008-04-18 04:38:22 +0000

    fontimage didn't draw the first line.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1208298938L;
-const char *source_modtime_str = "22:35 GMT 15-Apr-2008";
-const char *source_version_str = "20080415";
+const time_t source_modtime = 1208493408L;
+const char *source_modtime_str = "04:36 GMT 18-Apr-2008";
+const char *source_version_str = "20080418";
