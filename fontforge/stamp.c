b37be1e685aeeae284d7ecdb79d9ce3066fde9ed
pfaedit <pfaedit>
2008-04-28 21:12:55 +0000

    Hmm. Freetype seems to dislike rasterizing some complex outlines at large pixelsizes in black and white, but seems fine with doing them in greyscale.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1209153421L;
-const char *source_modtime_str = "19:57 GMT 25-Apr-2008";
-const char *source_version_str = "20080425";
+const time_t source_modtime = 1209413822L;
+const char *source_modtime_str = "20:17 GMT 28-Apr-2008";
+const char *source_version_str = "20080428";
