7248d0b334de3a416953e474a1fa6c97636129b0
George Williams <pfaedit@users.sourceforge.net>
2008-04-29 00:38:15 +0000

    Validate didn't work on CID keyed fonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1209427541L;
-const char *source_modtime_str = "00:05 GMT 29-Apr-2008";
+const time_t source_modtime = 1209428967L;
+const char *source_modtime_str = "00:29 GMT 29-Apr-2008";
 const char *source_version_str = "20080429";
