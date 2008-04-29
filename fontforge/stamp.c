2be4021254ebaef0d91d179c09ddeeb89a831340
pfaedit <pfaedit>
2008-04-29 00:11:23 +0000

    Pierre Hanser wants an X Resource (fontforge.CharView.Rulers.FontSize) to specify the font size of text displayed in the info line and the rulers. I have applied his patch, but have made no effort to insure that text will fit in the space allowed for it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1209422742L;
-const char *source_modtime_str = "22:45 GMT 28-Apr-2008";
-const char *source_version_str = "20080428";
+const time_t source_modtime = 1209427541L;
+const char *source_modtime_str = "00:05 GMT 29-Apr-2008";
+const char *source_version_str = "20080429";
