8828d8e347b4f0fd42710330653d1d4028d5e3ee
pfaedit <pfaedit>
2008-05-06 21:04:09 +0000

    If the "changedsincelasthinted" bit got set on a glyph by the unlink/remove overlap flag on a glyph during font saving, then the glyph did not get restored to its original state.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1210016560L;
-const char *source_modtime_str = "19:42 GMT 5-May-2008";
-const char *source_version_str = "20080505";
+const time_t source_modtime = 1210107561L;
+const char *source_modtime_str = "20:59 GMT 6-May-2008";
+const char *source_version_str = "20080506";
