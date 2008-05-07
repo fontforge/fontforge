e15e3355e2301f71cbaee7d4122e7215f1d4cccf
pfaedit <pfaedit>
2008-05-07 21:45:05 +0000

    If an sfnt contains embedded bitmaps, and if those bitmaps contain a glyph whose advance width is different from that expected by scaling the horizontal advance of the vector data, then windows will only get the correct metrics if the "Instructions modify width" bit is set in the 'head'.flags field.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1210107561L;
-const char *source_modtime_str = "20:59 GMT 6-May-2008";
-const char *source_version_str = "20080506";
+const time_t source_modtime = 1210196549L;
+const char *source_modtime_str = "21:42 GMT 7-May-2008";
+const char *source_version_str = "20080507";
