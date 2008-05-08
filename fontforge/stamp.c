b397f15f65b6cffe2fb89c43bb86f22247ace940
pfaedit <pfaedit>
2008-05-08 03:27:55 +0000

    Try a little harder to get some of the obscure code pages right in the OS/2 table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1210200124L;
-const char *source_modtime_str = "22:42 GMT 7-May-2008";
-const char *source_version_str = "20080507";
+const time_t source_modtime = 1210216447L;
+const char *source_modtime_str = "03:14 GMT 8-May-2008";
+const char *source_version_str = "20080508";
