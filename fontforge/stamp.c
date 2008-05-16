78c107891a268c7f546bbc8c8154a5798b41fbdb
George Williams <pfaedit@users.sourceforge.net>
2008-05-16 22:03:24 +0000

    A. Panov provides a patch. Prefs was losing bookmarks.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1210974890L;
-const char *source_modtime_str = "21:54 GMT 16-May-2008";
+const time_t source_modtime = 1210975290L;
+const char *source_modtime_str = "22:01 GMT 16-May-2008";
 const char *source_version_str = "20080516";
