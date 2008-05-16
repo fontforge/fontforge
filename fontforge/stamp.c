02059e0cba5ea2e5ce330645463bbf205fa3aa11
George Williams <pfaedit@users.sourceforge.net>
2008-05-16 22:01:46 +0000

    the new release of volt reveals some new opentype tags.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1210867967L;
-const char *source_modtime_str = "16:12 GMT 15-May-2008";
-const char *source_version_str = "20080515";
+const time_t source_modtime = 1210974890L;
+const char *source_modtime_str = "21:54 GMT 16-May-2008";
+const char *source_version_str = "20080516";
