141ec427decc6a118b96ae55ecf5b6c86328e618
pfaedit <pfaedit>
2008-05-18 15:05:40 +0000

    Oops.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1211122255L;
-const char *source_modtime_str = "14:50 GMT 18-May-2008";
+const time_t source_modtime = 1211123015L;
+const char *source_modtime_str = "15:03 GMT 18-May-2008";
 const char *source_version_str = "20080518";
