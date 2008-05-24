4178049827bef2b64f0a73e224b851006db18746
George Williams <pfaedit@users.sourceforge.net>
2008-05-24 12:45:56 +0000

    Further italic fixes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1211598038L;
-const char *source_modtime_str = "03:00 GMT 24-May-2008";
+const time_t source_modtime = 1211630674L;
+const char *source_modtime_str = "12:04 GMT 24-May-2008";
 const char *source_version_str = "20080524";
