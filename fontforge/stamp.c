0f1181b6c5ae510095447812ac2a1f986fb80b87
George Williams <pfaedit@users.sourceforge.net>
2008-05-25 00:47:40 +0000

    Typo when loading the underline position field from a 'post' table. I was subtracting half the underline width. I should have been adding it instead. (Why did adobe use a different definition of the same concept in two places? GRRR).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1211630674L;
-const char *source_modtime_str = "12:04 GMT 24-May-2008";
-const char *source_version_str = "20080524";
+const time_t source_modtime = 1211675730L;
+const char *source_modtime_str = "00:35 GMT 25-May-2008";
+const char *source_version_str = "20080525";
