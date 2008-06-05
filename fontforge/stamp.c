9ac008bc106a3de580b25aa9438f6f528ecd18c1
George Williams <pfaedit@users.sourceforge.net>
2008-06-05 20:55:07 +0000

    Various bugs in argument handling in python fontforge.askChoices() command.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1212697000L;
-const char *source_modtime_str = "20:16 GMT 5-Jun-2008";
+const time_t source_modtime = 1212699037L;
+const char *source_modtime_str = "20:50 GMT 5-Jun-2008";
 const char *source_version_str = "20080605";
