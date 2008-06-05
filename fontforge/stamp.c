e13a8c2ba379176af3c12af436c7dd84d057fa2a
pfaedit <pfaedit>
2008-06-05 00:04:28 +0000

    FF didn't like decimal points in the metrics view l/r bearing fields -- even though it could put them there itself.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1212623204L;
-const char *source_modtime_str = "23:46 GMT 4-Jun-2008";
-const char *source_version_str = "20080604";
+const time_t source_modtime = 1212624008L;
+const char *source_modtime_str = "00:00 GMT 5-Jun-2008";
+const char *source_version_str = "20080605";
