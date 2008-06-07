edc9016b24f1d9203b95ba90398e81c1cae1bf0e
pfaedit <pfaedit>
2008-06-07 19:01:11 +0000

    Depending on where ff was started it might not set the message locale directory properly. Which meant (for me) that it fell back to an obsolete set of translations hidden in a random directory.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1212810170L;
-const char *source_modtime_str = "03:42 GMT 7-Jun-2008";
+const time_t source_modtime = 1212864727L;
+const char *source_modtime_str = "18:52 GMT 7-Jun-2008";
 const char *source_version_str = "20080607";
