cc8d978c1a66fab4d5d3c8eba28258c2444df9e8
pfaedit <pfaedit>
2008-06-08 00:10:51 +0000

    Tweak

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1212725525L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"04:12 GMT 6-Jun-2008"
-#define LibFF_VersionDate	20080606	/* Year, month, day */
+#define LibFF_ModTime		1212878030L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"22:33 GMT 7-Jun-2008"
+#define LibFF_VersionDate	20080607	/* Year, month, day */
