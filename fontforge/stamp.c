5a69fb7186e999a3d88194dcf9e083a33f55e8fc
pfaedit <pfaedit>
2008-06-10 01:45:04 +0000

    Werner points out that my instruction glosses are out of date:   1) Was following Apple's docs for SCANTYPE, not MS. MS has a number of new 	values.   2) Was using an old convention for accessing PPEM which broke after 	freetype 2.3.5   3) Added the FreeType return for GETINFO.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213060269L;
-const char *source_modtime_str = "01:11 GMT 10-Jun-2008";
+const time_t source_modtime = 1213062206L;
+const char *source_modtime_str = "01:43 GMT 10-Jun-2008";
 const char *source_version_str = "20080610";
