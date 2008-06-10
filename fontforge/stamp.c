9cc53cb30dcaa5f79b23fcb59bf8f4abc56b9975
George Williams <pfaedit@users.sourceforge.net>
2008-06-10 22:40:09 +0000

    If fontforge got an end of file in the middle of reading a (corrupt) cff font, then ff would sit there reading endlessly and ignoring the error.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213133797L;
-const char *source_modtime_str = "21:36 GMT 10-Jun-2008";
+const time_t source_modtime = 1213136579L;
+const char *source_modtime_str = "22:22 GMT 10-Jun-2008";
 const char *source_version_str = "20080610";
