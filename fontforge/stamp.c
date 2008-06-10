bf58ec5e48d4a3ecd32a347a11d99c017549ae73
George Williams <pfaedit@users.sourceforge.net>
2008-06-10 02:47:25 +0000

    Lower case letters with ascenders/descenders had those ascenders/descenders badly scaled by Sub/Super.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213062206L;
-const char *source_modtime_str = "01:43 GMT 10-Jun-2008";
+const time_t source_modtime = 1213065854L;
+const char *source_modtime_str = "02:44 GMT 10-Jun-2008";
 const char *source_version_str = "20080610";
