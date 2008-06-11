e3442db994cae5fe377ff2bbb96417f12007b85c
George Williams <pfaedit@users.sourceforge.net>
2008-06-11 18:36:26 +0000

    Merge fonts broken again. This time references didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213198473L;
-const char *source_modtime_str = "15:34 GMT 11-Jun-2008";
+const time_t source_modtime = 1213209179L;
+const char *source_modtime_str = "18:32 GMT 11-Jun-2008";
 const char *source_version_str = "20080611";
