27ee5f40dec7852715d627da4b90c862bf54526f
pfaedit <pfaedit>
2008-06-16 02:48:38 +0000

    Still problems with building accented.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213565134L;
-const char *source_modtime_str = "21:25 GMT 15-Jun-2008";
-const char *source_version_str = "20080615";
+const time_t source_modtime = 1213584470L;
+const char *source_modtime_str = "02:47 GMT 16-Jun-2008";
+const char *source_version_str = "20080616";
