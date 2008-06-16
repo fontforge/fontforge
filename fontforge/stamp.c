92c87d35c2a4cf2cb94519426de46de98e195a06
pfaedit <pfaedit>
2008-06-16 03:47:49 +0000

    Some new menu icons.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213584470L;
-const char *source_modtime_str = "02:47 GMT 16-Jun-2008";
+const time_t source_modtime = 1213587347L;
+const char *source_modtime_str = "03:35 GMT 16-Jun-2008";
 const char *source_version_str = "20080616";
