a707f51559f7e392c0a03bf8d0a7ee7f8ee69941
pfaedit <pfaedit>
2008-06-17 17:17:50 +0000

    When ff changed the name of a glyph, it fixed up all the tabs in charviews to the new name -- but in neglected to refresh them. So they still showed on the screen with the old name (until something forced a refresh).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213654735L;
-const char *source_modtime_str = "22:18 GMT 16-Jun-2008";
-const char *source_version_str = "20080616";
+const time_t source_modtime = 1213722826L;
+const char *source_modtime_str = "17:13 GMT 17-Jun-2008";
+const char *source_version_str = "20080617";
