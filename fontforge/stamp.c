4db9ace6319386244f92bd0ec286e04756621555
George Williams <pfaedit@users.sourceforge.net>
2008-06-17 21:40:16 +0000

    I forgot pixmaps for the Points menu of the charview.
    Add a preference which allows people to change the pixmap dir -- and do what we can to reload images from the new dir. Some old windows may not be updated, but new ones should reflect the new stuff.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213722826L;
-const char *source_modtime_str = "17:13 GMT 17-Jun-2008";
+const time_t source_modtime = 1213738271L;
+const char *source_modtime_str = "21:31 GMT 17-Jun-2008";
 const char *source_version_str = "20080617";
