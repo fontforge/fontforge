9d7cb0ce214460c36e8cad715a6a5137e70ea44c
pfaedit <pfaedit>
2008-06-18 22:09:50 +0000

    The Change X-Height dialog would crash if you clicked in the "current xheight" field.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1213824638L;
-const char *source_modtime_str = "21:30 GMT 18-Jun-2008";
+const time_t source_modtime = 1213826934L;
+const char *source_modtime_str = "22:08 GMT 18-Jun-2008";
 const char *source_version_str = "20080618";
