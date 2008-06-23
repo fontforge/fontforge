7318369f18c16d259d5be16c314be0fc5e11bbbb
George Williams <pfaedit@users.sourceforge.net>
2008-06-23 20:17:11 +0000

    Freetype now (or in the next release, 2.3.7) supports dropout modes, which means fontforge must do a bit of work when generating rasters for the debugger.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1214250306L;
-const char *source_modtime_str = "19:45 GMT 23-Jun-2008";
+const time_t source_modtime = 1214252220L;
+const char *source_modtime_str = "20:17 GMT 23-Jun-2008";
 const char *source_version_str = "20080623";
