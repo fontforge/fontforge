5b39354e5425ddf140420aa46c04f0d00c3e623b
pfaedit <pfaedit>
2008-06-24 00:09:52 +0000

    Barry points out that "print" is a python keyword. So font.print() doesn't work. Call it printSample() instead.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1214252220L;
-const char *source_modtime_str = "20:17 GMT 23-Jun-2008";
-const char *source_version_str = "20080623";
+const time_t source_modtime = 1214265702L;
+const char *source_modtime_str = "00:01 GMT 24-Jun-2008";
+const char *source_version_str = "20080624";
