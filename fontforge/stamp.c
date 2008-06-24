3312b1999cfbd455e97f88abed3bc8792c97ab4f
pfaedit <pfaedit>
2008-06-24 01:11:13 +0000

    The psMat.skew command skewed vertically, rather than horizontally and by the wrong amoung.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1214266129L;
-const char *source_modtime_str = "00:08 GMT 24-Jun-2008";
+const time_t source_modtime = 1214269724L;
+const char *source_modtime_str = "01:08 GMT 24-Jun-2008";
 const char *source_version_str = "20080624";
