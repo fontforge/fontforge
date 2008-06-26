ffed0914c19f2f8fa8e527b380907b7184d063ed
George Williams <pfaedit@users.sourceforge.net>
2008-06-26 20:08:30 +0000

    in python layer.boundingBox returned the wrong values.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1214447703L;
-const char *source_modtime_str = "02:35 GMT 26-Jun-2008";
+const time_t source_modtime = 1214510706L;
+const char *source_modtime_str = "20:05 GMT 26-Jun-2008";
 const char *source_version_str = "20080626";
