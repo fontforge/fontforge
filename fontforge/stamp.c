d9841bec954867802185db83a9864ab39c3c11ad
George Williams <pfaedit@users.sourceforge.net>
2008-06-30 19:12:08 +0000

    Still bugs in revert glyph. If a font had more than the default two layers, then for all glyphs but the first, any non-cubic layers would be read in as cubic.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1214784050L;
-const char *source_modtime_str = "00:00 GMT 30-Jun-2008";
+const time_t source_modtime = 1214852923L;
+const char *source_modtime_str = "19:08 GMT 30-Jun-2008";
 const char *source_version_str = "20080630";
