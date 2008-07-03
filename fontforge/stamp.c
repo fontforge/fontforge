2d5d7281677fda9ea3439e47c8897fb7b052c7bc
pfaedit <pfaedit>
2008-07-03 21:25:20 +0000

    When importing lookups into a font, append the fontname to the lookup name only if there is already a lookup in the new font with the original name.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1214934556L;
-const char *source_modtime_str = "17:49 GMT 1-Jul-2008";
-const char *source_version_str = "20080701";
+const time_t source_modtime = 1215120212L;
+const char *source_modtime_str = "21:23 GMT 3-Jul-2008";
+const char *source_version_str = "20080703";
