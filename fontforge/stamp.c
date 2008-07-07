de5cc7a4986878e00487e19c88a43c575230d2ad
George Williams <pfaedit@users.sourceforge.net>
2008-07-07 02:32:03 +0000

    I find it really hard to believe that this is an error, but I have a second hand report that MicroSoft claims that it is. To me making it erroneous is pointless and annoying, but I don't write the spec.
    Add to Find Problems and Validate a check to see that if a base glyph (or mark) contains an anchor point in one anchor class in a lookup subtable, then it must have anchor points for ALL anchor classes in that subtable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1215379278L;
-const char *source_modtime_str = "21:21 GMT 6-Jul-2008";
-const char *source_version_str = "20080706";
+const time_t source_modtime = 1215397502L;
+const char *source_modtime_str = "02:25 GMT 7-Jul-2008";
+const char *source_version_str = "20080707";
