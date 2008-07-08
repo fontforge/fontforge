86a117f78d0d691451f84ccebca8487f43698c48
pfaedit <pfaedit>
2008-07-08 19:22:34 +0000

    Fix a translation issue.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1215465793L;
-const char *source_modtime_str = "21:23 GMT 7-Jul-2008";
-const char *source_version_str = "20080707";
+const time_t source_modtime = 1215544925L;
+const char *source_modtime_str = "19:22 GMT 8-Jul-2008";
+const char *source_version_str = "20080708";
