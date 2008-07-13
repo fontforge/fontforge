aad286f8490a036a461c9d60a9bfbef480e94548
George Williams <pfaedit@users.sourceforge.net>
2008-07-13 16:36:57 +0000

    Allow the user to customize the colors in the metrics view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1215810371L;
-const char *source_modtime_str = "21:06 GMT 11-Jul-2008";
-const char *source_version_str = "20080711";
+const time_t source_modtime = 1215966628L;
+const char *source_modtime_str = "16:30 GMT 13-Jul-2008";
+const char *source_version_str = "20080713";
