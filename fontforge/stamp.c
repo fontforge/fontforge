d57bb21fd6c00b549f8407d95c2403741df03df7
pfaedit <pfaedit>
2008-07-16 21:05:15 +0000

    The change glyph dlg (and friends, small caps, sub/super script) will now remember settings from the last use (settings are not saved to prefs though).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216240488L;
-const char *source_modtime_str = "20:34 GMT 16-Jul-2008";
+const time_t source_modtime = 1216241972L;
+const char *source_modtime_str = "20:59 GMT 16-Jul-2008";
 const char *source_version_str = "20080716";
