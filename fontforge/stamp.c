0f79aba011e12ce0155aac185f774b27befd062a
pfaedit <pfaedit>
2008-07-16 20:12:08 +0000

    In a CID-keyed font, when opening an empty CID slot, ff could trample memory.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216211391L;
-const char *source_modtime_str = "12:29 GMT 16-Jul-2008";
+const time_t source_modtime = 1216238957L;
+const char *source_modtime_str = "20:09 GMT 16-Jul-2008";
 const char *source_version_str = "20080716";
