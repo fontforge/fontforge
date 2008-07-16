61f2a07fe5fb31dbdc2d779c84315698d390269a
pfaedit <pfaedit>
2008-07-16 00:27:46 +0000

    Oops. Refs didn't get updated quite right in some cases.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216161654L;
-const char *source_modtime_str = "22:40 GMT 15-Jul-2008";
-const char *source_version_str = "20080715";
+const time_t source_modtime = 1216167957L;
+const char *source_modtime_str = "00:25 GMT 16-Jul-2008";
+const char *source_version_str = "20080716";
