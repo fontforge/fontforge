6f9219527c3a8663147d1ba8a7cca5472975f871
George Williams <pfaedit@users.sourceforge.net>
2008-07-17 01:07:40 +0000

    Add some "How I Did It" information to the OFLib upload.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216248589L;
-const char *source_modtime_str = "22:49 GMT 16-Jul-2008";
-const char *source_version_str = "20080716";
+const time_t source_modtime = 1216256663L;
+const char *source_modtime_str = "01:04 GMT 17-Jul-2008";
+const char *source_version_str = "20080717";
