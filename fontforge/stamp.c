2770b1caedd6f5d1a9f2f75e35041ea61c460898
George Williams <pfaedit@users.sourceforge.net>
2008-07-17 19:35:33 +0000

    Add a [reset] button to the Change GLyph (Small caps, etc) dlg to reset all fields back to their defaults.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216301078L;
-const char *source_modtime_str = "13:24 GMT 17-Jul-2008";
+const time_t source_modtime = 1216323323L;
+const char *source_modtime_str = "19:35 GMT 17-Jul-2008";
 const char *source_version_str = "20080717";
