45e66cfc1946eb4d3846aaf7747034070fed2af0
pfaedit <pfaedit>
2008-07-17 20:53:09 +0000

    On systems using different font metrics, the debug pane looks bad with the buttons partly covered by the instruction pane.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216325284L;
-const char *source_modtime_str = "20:08 GMT 17-Jul-2008";
+const time_t source_modtime = 1216327905L;
+const char *source_modtime_str = "20:51 GMT 17-Jul-2008";
 const char *source_version_str = "20080717";
