dd5e612ded5d197e83a95f3711e6e484521e0af3
George Williams <pfaedit@users.sourceforge.net>
2008-07-18 20:58:06 +0000

    Fixes for long double mode.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216351831L;
-const char *source_modtime_str = "03:30 GMT 18-Jul-2008";
+const time_t source_modtime = 1216410193L;
+const char *source_modtime_str = "19:43 GMT 18-Jul-2008";
 const char *source_version_str = "20080718";
