f8e66598cdce7a60b86ee76e67a65bc2939c866b
pfaedit <pfaedit>
2008-07-18 22:06:46 +0000

    Fix some bugs in feature file generation of contextual chaining lookups which refer to marks. Note these are not supported in the spec. This is a FontForge only extension.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216410193L;
-const char *source_modtime_str = "19:43 GMT 18-Jul-2008";
+const time_t source_modtime = 1216418679L;
+const char *source_modtime_str = "22:04 GMT 18-Jul-2008";
 const char *source_version_str = "20080718";
