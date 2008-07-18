768691b1301e6033aa243e116ee0a80f55347b2e
George Williams <pfaedit@users.sourceforge.net>
2008-07-18 02:27:04 +0000

    If a script contain no default language features (but did contain some languages) then ff produced a garbage script table for GPOS/GSUB.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216345959L;
-const char *source_modtime_str = "01:52 GMT 18-Jul-2008";
+const time_t source_modtime = 1216347843L;
+const char *source_modtime_str = "02:24 GMT 18-Jul-2008";
 const char *source_version_str = "20080718";
