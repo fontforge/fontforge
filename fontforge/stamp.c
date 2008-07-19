989f533de64ea006a749b6a80bd99dc1d6b2adf7
Alexey Kryukov <amkryukov@gmail.com>
2008-07-19 12:09:47 +0000

    There was a bug in flipping DStem hints.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216418679L;
-const char *source_modtime_str = "22:04 GMT 18-Jul-2008";
-const char *source_version_str = "20080718";
+const time_t source_modtime = 1216468994L;
+const char *source_modtime_str = "12:03 GMT 19-Jul-2008";
+const char *source_version_str = "20080719";
