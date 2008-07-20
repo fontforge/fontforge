47a38fde0df132b54bcb2601ae9c41a38191e08d
George Williams <pfaedit@users.sourceforge.net>
2008-07-20 22:53:16 +0000

    Copy/Paste of references only worked well in foreground layer.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216592729L;
-const char *source_modtime_str = "22:25 GMT 20-Jul-2008";
+const time_t source_modtime = 1216594290L;
+const char *source_modtime_str = "22:51 GMT 20-Jul-2008";
 const char *source_version_str = "20080720";
