99a4af74d4db1826278133cb666d1c8912281573
pfaedit <pfaedit>
2008-07-21 19:02:50 +0000

    When deleting the last quadratic layer, delete the tt instructions also.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216666647L;
-const char *source_modtime_str = "18:57 GMT 21-Jul-2008";
+const time_t source_modtime = 1216666904L;
+const char *source_modtime_str = "19:01 GMT 21-Jul-2008";
 const char *source_version_str = "20080721";
