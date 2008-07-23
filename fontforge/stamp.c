74dff5cbbbdb51665cccf4cb1252d5103d347396
pfaedit <pfaedit>
2008-07-23 23:23:28 +0000

    Importing a kerning class from one font to another (and perhaps merging fonts) could cause memory problems if the number of second classes were bigger than the number of first classes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216854232L;
-const char *source_modtime_str = "23:03 GMT 23-Jul-2008";
+const time_t source_modtime = 1216855356L;
+const char *source_modtime_str = "23:22 GMT 23-Jul-2008";
 const char *source_version_str = "20080723";
