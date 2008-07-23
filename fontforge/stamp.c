6c99b7fbf7faff096fe35e60b97272a6b7ea8814
pfaedit <pfaedit>
2008-07-23 21:10:39 +0000

    Find Problems would occasionally pop up a blank problem dlg.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216766336L;
-const char *source_modtime_str = "22:38 GMT 22-Jul-2008";
-const char *source_version_str = "20080722";
+const time_t source_modtime = 1216847318L;
+const char *source_modtime_str = "21:08 GMT 23-Jul-2008";
+const char *source_version_str = "20080723";
