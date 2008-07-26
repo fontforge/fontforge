b19263113d275dbd8a3c163d109958595d802bac
George Williams <pfaedit@users.sourceforge.net>
2008-07-26 04:19:11 +0000

    Changes to create a stub Mac application. This little guy sits there eating apple events and passing them on to fontforge. The result is that it looks as though fontforge is responding to these events. So one can double click on the application icon and fontforge pops up. One can double click on an sfd file and ff pops up editing that file. One can drag some sfd files to the FF icon and they open. Etc.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1216957328L;
-const char *source_modtime_str = "03:42 GMT 25-Jul-2008";
-const char *source_version_str = "20080725";
+const time_t source_modtime = 1217041667L;
+const char *source_modtime_str = "03:07 GMT 26-Jul-2008";
+const char *source_version_str = "20080726";
