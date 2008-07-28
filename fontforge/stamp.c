c2fe7090c3c38e8811b9bfc992ae8ef4c541b281
George Williams <pfaedit@users.sourceforge.net>
2008-07-28 04:13:16 +0000

    Barry wants a way to control the use_my_metrics bit of a reference from python. Give him a hack   glyph.useRefsMetrics(name[,set])

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217216739L;
-const char *source_modtime_str = "03:45 GMT 28-Jul-2008";
+const time_t source_modtime = 1217218152L;
+const char *source_modtime_str = "04:09 GMT 28-Jul-2008";
 const char *source_version_str = "20080728";
