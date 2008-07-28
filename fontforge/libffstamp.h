8ad73cd1a916c2a58aefed2515b307884c5a9bdc
pfaedit <pfaedit>
2008-07-28 02:53:38 +0000

    Werner points out that if a type2 charstring contained a sequence <num> endchar then ff would set the width of the charater even if the the width had already been set. Width can only be set on first stack clearing operation, any extra params on subsequent calls get ignored.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1217117639L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"00:13 GMT 27-Jul-2008"
-#define LibFF_VersionDate	20080727	/* Year, month, day */
+#define LibFF_ModTime		1217213474L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"02:51 GMT 28-Jul-2008"
+#define LibFF_VersionDate	20080728	/* Year, month, day */
