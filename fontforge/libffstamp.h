b1e9948403614abe7eea7b610f7539a53af1d426
pfaedit <pfaedit>
2008-07-28 03:03:27 +0000

    Oops. Try this once more.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1217213474L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"02:51 GMT 28-Jul-2008"
+#define LibFF_ModTime		1217213902L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"02:58 GMT 28-Jul-2008"
 #define LibFF_VersionDate	20080728	/* Year, month, day */
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217213475L;
-const char *source_modtime_str = "02:51 GMT 28-Jul-2008";
+const time_t source_modtime = 1217213903L;
+const char *source_modtime_str = "02:58 GMT 28-Jul-2008";
 const char *source_version_str = "20080728";
