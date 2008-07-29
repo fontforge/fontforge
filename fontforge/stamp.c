2a9ac279976a65141018073bd9b315512c725ad3
pfaedit <pfaedit>
2008-07-29 02:12:34 +0000

    If the user has opened a backup sfd file (one that ends in .sfd~) and does a save on it, then pop up a save as dlg instead of blindly saving it. Saving it back to the backup is probably wrong, yet saving it onto the non-backup version without prompting the user is questionable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217293325L;
-const char *source_modtime_str = "01:02 GMT 29-Jul-2008";
+const time_t source_modtime = 1217297360L;
+const char *source_modtime_str = "02:09 GMT 29-Jul-2008";
 const char *source_version_str = "20080729";
