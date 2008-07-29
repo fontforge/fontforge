c89525c9a4eb373e5d6e5a1ffb4c56bd25e66822
pfaedit <pfaedit>
2008-07-29 01:06:09 +0000

    glyph.appendAccents didn't mark the glyph as changed nor did it force an update.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217218152L;
-const char *source_modtime_str = "04:09 GMT 28-Jul-2008";
-const char *source_version_str = "20080728";
+const time_t source_modtime = 1217293325L;
+const char *source_modtime_str = "01:02 GMT 29-Jul-2008";
+const char *source_version_str = "20080729";
