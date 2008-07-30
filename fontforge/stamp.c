81a3d5a54c3a8e1dda000c243d6b9288eb1bfc6d
George Williams <pfaedit@users.sourceforge.net>
2008-07-30 21:32:41 +0000

    Valgrind finds a bug in drawing non-BMP characters in the metrics view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217449413L;
-const char *source_modtime_str = "20:23 GMT 30-Jul-2008";
+const time_t source_modtime = 1217453249L;
+const char *source_modtime_str = "21:27 GMT 30-Jul-2008";
 const char *source_version_str = "20080730";
