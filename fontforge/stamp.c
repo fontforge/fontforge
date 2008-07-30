8e9c03f788efa9754156e3fbf7b00d184cc32843
pfaedit <pfaedit>
2008-07-30 22:21:41 +0000

    Adobe have admitted that the obvious reading of their spec is incorrect (they can't quite bring themselves to say the spec is wrong). So revert to the way we were parsing type2 charstrings.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217455174L;
-const char *source_modtime_str = "21:59 GMT 30-Jul-2008";
+const time_t source_modtime = 1217456387L;
+const char *source_modtime_str = "22:19 GMT 30-Jul-2008";
 const char *source_version_str = "20080730";
