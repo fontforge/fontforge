a615edde0c2ef48c5544d8e3bc62cd51e31297b9
pfaedit <pfaedit>
2008-08-01 02:37:31 +0000

    Change sfd.c so that it will treat the sequence "backslash newline" as no characters on input. And so that it thinks it can parse version 4 or the sfd format.
    We don't generate version 4 yet, and we won't for a while. Version 4 will include the ability to insert backslash newline into long lines. This is so we can conform to the internet requirements of fewer than 990 bytes on a line. And the delay is so that people don't scream at me for making an incompatible change to the format -- if I give the parser time to perculate through the user community, with luck no one will even notice -- especially as long lines are rare.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217553708L;
-const char *source_modtime_str = "01:21 GMT 1-Aug-2008";
+const time_t source_modtime = 1217556522L;
+const char *source_modtime_str = "02:08 GMT 1-Aug-2008";
 const char *source_version_str = "20080801";
