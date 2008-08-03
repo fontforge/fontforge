cc1baedbade94c22d1a8e515ff9f6dc4b9ae8d45
George Williams <pfaedit@users.sourceforge.net>
2008-08-03 01:34:49 +0000

    Moving ligature carets always marked for an undo in the foreground layer. Register the movement in the layer in which it happens.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217626843L;
-const char *source_modtime_str = "21:40 GMT 1-Aug-2008";
-const char *source_version_str = "20080801";
+const time_t source_modtime = 1217727186L;
+const char *source_modtime_str = "01:33 GMT 3-Aug-2008";
+const char *source_version_str = "20080803";
