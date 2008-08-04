2ce18988a296fc5d8aecd7b67941b74cba527aa5
Alexey Kryukov <amkryukov@gmail.com>
2008-08-04 14:56:05 +0000

    Forgot to remove a debug operator.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217861491L;
-const char *source_modtime_str = "14:51 GMT 4-Aug-2008";
+const time_t source_modtime = 1217861773L;
+const char *source_modtime_str = "14:56 GMT 4-Aug-2008";
 const char *source_version_str = "20080804";
