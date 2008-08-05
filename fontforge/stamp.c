641f1de490b22ae801297bb83620d36506a4d30b
pfaedit <pfaedit>
2008-08-05 21:08:06 +0000

    Another layers problem. Only in the foreground layer could anchor points be moved.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217906936L;
-const char *source_modtime_str = "03:28 GMT 5-Aug-2008";
+const time_t source_modtime = 1217970068L;
+const char *source_modtime_str = "21:01 GMT 5-Aug-2008";
 const char *source_version_str = "20080805";
