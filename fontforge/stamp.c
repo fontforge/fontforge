e49a8e85aadb8fcd81062e49d5e6ae9043409773
George Williams <pfaedit@users.sourceforge.net>
2008-08-06 21:45:12 +0000

    Allow the user to restrict mousing in the metrics view so that it will only change kerning. (and make that the default).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1217970593L;
-const char *source_modtime_str = "21:09 GMT 5-Aug-2008";
-const char *source_version_str = "20080805";
+const time_t source_modtime = 1218058905L;
+const char *source_modtime_str = "21:41 GMT 6-Aug-2008";
+const char *source_version_str = "20080806";
