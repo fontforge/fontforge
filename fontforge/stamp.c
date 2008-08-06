19852ab7391dcfd1d6b42d9ea10af98755b4cfaa
George Williams <pfaedit@users.sourceforge.net>
2008-08-06 22:00:16 +0000

    Some more cases where python doesn't note that a glyph has changed (Patch by Barry).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1218058905L;
-const char *source_modtime_str = "21:41 GMT 6-Aug-2008";
+const time_t source_modtime = 1218059905L;
+const char *source_modtime_str = "21:58 GMT 6-Aug-2008";
 const char *source_version_str = "20080806";
