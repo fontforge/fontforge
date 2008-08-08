9f82d34e6178238470a6e40a859a8bea5e1a60b6
George Williams <pfaedit@users.sourceforge.net>
2008-08-08 22:40:44 +0000

    When I put in the backslash newline patch I did so secure in the knowledge that a backslash could not occur at the end of a line. Well, it can. In ASCII85 encoded text (images, ttf tables, bitmaps). However ASCII85 output already has short lines so there is no need to worry about it there. So turn of backslash detection in those places.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1218169076L;
-const char *source_modtime_str = "04:17 GMT 8-Aug-2008";
+const time_t source_modtime = 1218234079L;
+const char *source_modtime_str = "22:21 GMT 8-Aug-2008";
 const char *source_version_str = "20080808";
