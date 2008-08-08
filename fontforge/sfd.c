2b0ae94d4b7f9b111b0c74fca4b97525877af73f
pfaedit <pfaedit>
2008-08-08 22:40:44 +0000

    When I put in the backslash newline patch I did so secure in the knowledge that a backslash could not occur at the end of a line. Well, it can. In ASCII85 encoded text (images, ttf tables, bitmaps). However ASCII85 output already has short lines so there is no need to worry about it there. So turn of backslash detection in those places.

--- fontforge/sfd.c
+++ fontforge/sfd.c
@@ -2769,20 +2769,22 @@ static int getreal(FILE *sfd, real *val) {
 return( pt!=tokbuf && *nend=='\0'?1:ch==EOF?-1: 0 );
 }
 
+/* Don't use nlgetc here. We carefully control newlines when dumping in 85 */
+/*  but backslashes can occur at end of line. */
 static int Dec85(struct enc85 *dec) {
     int ch1, ch2, ch3, ch4, ch5;
     unsigned int val;
 
     if ( dec->pos<0 ) {
-	while ( isspace(ch1=nlgetc(dec->sfd)));
+	while ( isspace(ch1=getc(dec->sfd)));
 	if ( ch1=='z' ) {
 	    dec->sofar[0] = dec->sofar[1] = dec->sofar[2] = dec->sofar[3] = 0;
 	    dec->pos = 3;
 	} else {
-	    while ( isspace(ch2=nlgetc(dec->sfd)));
-	    while ( isspace(ch3=nlgetc(dec->sfd)));
-	    while ( isspace(ch4=nlgetc(dec->sfd)));
-	    while ( isspace(ch5=nlgetc(dec->sfd)));
+	    while ( isspace(ch2=getc(dec->sfd)));
+	    while ( isspace(ch3=getc(dec->sfd)));
+	    while ( isspace(ch4=getc(dec->sfd)));
+	    while ( isspace(ch5=getc(dec->sfd)));
 	    val = ((((ch1-'!')*85+ ch2-'!')*85 + ch3-'!')*85 + ch4-'!')*85 + ch5-'!';
 	    dec->sofar[3] = val>>24;
 	    dec->sofar[2] = val>>16;
