3b426f35a791c169c5956dc147ae8e4ea5232040
George Williams <pfaedit@users.sourceforge.net>
2008-08-11 00:11:18 +0000

    Typo in the code to display side bearings in the charview.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1218159538L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"01:38 GMT 8-Aug-2008"
-#define LibFF_VersionDate	20080808	/* Year, month, day */
+#define LibFF_ModTime		1218334856L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"02:20 GMT 10-Aug-2008"
+#define LibFF_VersionDate	20080810	/* Year, month, day */
