6619885a45714d78a7f0795ca8a8df43dbab2e2c
George Williams <pfaedit@users.sourceforge.net>
2008-08-23 01:32:47 +0000

    Change the python font.importLookup to be importLookups which will accept a tuple of lookup names so that many lookkups can be imported at once (and in order) and an optional third argument which will allow them to be placed properly.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1219101606L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"23:20 GMT 18-Aug-2008"
-#define LibFF_VersionDate	20080818	/* Year, month, day */
+#define LibFF_ModTime		1219454694L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"01:24 GMT 23-Aug-2008"
+#define LibFF_VersionDate	20080823	/* Year, month, day */
