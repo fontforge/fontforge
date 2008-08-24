2472c19b2ade0109195f862ca719a2d1445d8989
George Williams <pfaedit@users.sourceforge.net>
2008-08-24 22:07:10 +0000

    Barry points out a typo introduced in the background layers patch which would cause crashes in fonts with many layers.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1219583099L;
-const char *source_modtime_str = "13:04 GMT 24-Aug-2008";
+const time_t source_modtime = 1219615137L;
+const char *source_modtime_str = "21:58 GMT 24-Aug-2008";
 const char *source_version_str = "20080824";
