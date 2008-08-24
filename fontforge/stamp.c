edb3385b8066d21acc2042b65749bc26312e3f16
pfaedit <pfaedit>
2008-08-24 23:15:46 +0000

    The truetype debugger really wanted to debug the foreground layer and not any other.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1219618375L;
-const char *source_modtime_str = "22:52 GMT 24-Aug-2008";
+const time_t source_modtime = 1219619485L;
+const char *source_modtime_str = "23:11 GMT 24-Aug-2008";
 const char *source_version_str = "20080824";
