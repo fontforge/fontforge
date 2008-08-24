07ab03daef6372a3363164562efaffb76325a52f
George Williams <pfaedit@users.sourceforge.net>
2008-08-24 00:30:10 +0000

    Patch by Barry. He doesn't explain what it's for.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1219529105L;
-const char *source_modtime_str = "22:05 GMT 23-Aug-2008";
-const char *source_version_str = "20080823";
+const time_t source_modtime = 1219537212L;
+const char *source_modtime_str = "00:20 GMT 24-Aug-2008";
+const char *source_version_str = "20080824";
