074ab7cc6ccc2b828b307076585f7568797fc82d
pfaedit <pfaedit>
2008-08-30 18:47:46 +0000

    The recent patch to insure that [Imported] lookups from one font to another came across in the right order didn't work if there were nested lookups involved. The order of nested lookups is irrelevant -- unless they are also used in a non-nested context.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1220060517L;
-const char *source_modtime_str = "01:41 GMT 30-Aug-2008";
+const time_t source_modtime = 1220121744L;
+const char *source_modtime_str = "18:42 GMT 30-Aug-2008";
 const char *source_version_str = "20080830";
