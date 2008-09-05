1be3d06ace03dfe819a898f465e4a3c4a06fbf91
George Williams <pfaedit@users.sourceforge.net>
2008-09-05 19:19:22 +0000

    Python contour conversions would often introduce a control point on a linear order2 spline. The control point was at an end-point so it simply vanished when converted back into fontforge format, but it was disconcerting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1220627911L;
-const char *source_modtime_str = "15:18 GMT 5-Sep-2008";
+const time_t source_modtime = 1220642124L;
+const char *source_modtime_str = "19:15 GMT 5-Sep-2008";
 const char *source_version_str = "20080905";
