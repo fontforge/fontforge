17c04ac684a8c2eaa7d0caf05b15aad76ba960b4
George Williams <pfaedit@users.sourceforge.net>
2008-09-05 22:49:39 +0000

    Python quadratic patch was wrong. Hope this is better.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1220651661L;
-const char *source_modtime_str = "21:54 GMT 5-Sep-2008";
+const time_t source_modtime = 1220654672L;
+const char *source_modtime_str = "22:44 GMT 5-Sep-2008";
 const char *source_version_str = "20080905";
