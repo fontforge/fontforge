2f21e34f2e1d08ad94e3abfe75f4a16db0ad3f67
George Williams <pfaedit@users.sourceforge.net>
2008-09-06 00:27:29 +0000

    Extend the small caps dlg to create petite caps as well.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1220657017L;
-const char *source_modtime_str = "23:23 GMT 5-Sep-2008";
-const char *source_version_str = "20080905";
+const time_t source_modtime = 1220660802L;
+const char *source_modtime_str = "00:26 GMT 6-Sep-2008";
+const char *source_version_str = "20080906";
