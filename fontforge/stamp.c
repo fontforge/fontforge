c8e68d6f8e84f4cd5c4bd1a023bf1222d47d1195
pfaedit <pfaedit>
2008-09-07 17:03:29 +0000

    Internal conversion from fontforge splinesets to ff python contours would often leave interpolated points showing in the contour.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1220662816L;
-const char *source_modtime_str = "01:00 GMT 6-Sep-2008";
-const char *source_version_str = "20080906";
+const time_t source_modtime = 1220806964L;
+const char *source_modtime_str = "17:02 GMT 7-Sep-2008";
+const char *source_version_str = "20080907";
