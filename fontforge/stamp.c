d7fbaf10cdeb411d302792c41002d8eeb70bd320
akryukov <akryukov>
2008-09-09 16:02:30 +0000

    We used to move left and right point coordinates to the baseline for italic stems, so that they may well fall outside of the glyph bounding box. So prevent the situation where this could lead to a nan in spline creation. Also don't change the slope of italic stems when resizing a glyph.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1220920200L;
-const char *source_modtime_str = "00:30 GMT 9-Sep-2008";
+const time_t source_modtime = 1220975871L;
+const char *source_modtime_str = "15:57 GMT 9-Sep-2008";
 const char *source_version_str = "20080909";
