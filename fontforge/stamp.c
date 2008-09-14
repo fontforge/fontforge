9108bf483342ef357ff6485a8370504f1438c10f
George Williams <pfaedit@users.sourceforge.net>
2008-09-14 04:19:27 +0000

    Change the menu handler so it can draw mac menu icons (cloverleaf for command, up arrow for shift, ^ for control and weird squiggly for option). Remove the hack that if the modifier mask for the command key was set then we'd also set the control mask (this meant that the command key would work as control, but it meant it could only work as control). Let the command key work as itself, and put in special code so that if X11 is going to give us the command key (instead of using it in the X11 menubar) then we load a special set of keymap bindings (which, presumably, will use command). If we don't find a special set of keymaps, then fall back on normal processing.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1221156319L;
-const char *source_modtime_str = "18:05 GMT 11-Sep-2008";
-const char *source_version_str = "20080911";
+const time_t source_modtime = 1221365554L;
+const char *source_modtime_str = "04:12 GMT 14-Sep-2008";
+const char *source_version_str = "20080914";
