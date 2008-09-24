6a391e308f31e824adf730f757aa3ef44c192cea
pfaedit <pfaedit>
2008-09-24 22:37:06 +0000

    Completion fields did not have an input context specified for the popup list of completions.
    When the user typed the key after which the string was unambiguous, ff would fill in the rest of the string. It should have filled in the rest of the string, and then selected the new stuff. (If I had "a" and typed a "." I might get "a.sc" and when I typed the "s" I'd get "a.scs" rather than "a.s[c]".

--- gdraw/gtextfield.c
+++ gdraw/gtextfield.c
@@ -2946,6 +2946,7 @@ static void GCompletionCreatePopup(GCompletionField *gc) {
     pos.x = pt.x; pos.y = pt.y;
 
     gc->choice_popup = GDrawCreateTopWindow(disp,&pos,popup_eh,gc,&pattrs);
+    GDrawSetGIC(gc->choice_popup,gc->gl.gt.gic,gc->gl.gt.g.inner.x,gc->gl.gt.g.inner.y+gc->gl.gt.as);
     GDrawSetVisible(gc->choice_popup,true);
     /* Don't grab this one. User should be free to ignore it */
 }
@@ -2972,21 +2973,13 @@ static void GTextFieldComplete(GTextField *gt,int from_tab) {
 	else
 	    GTextFieldChanged(gt,-1);
 	free(ret);
-    } else if ( ret[1]==NULL ) {
-	GTextFieldSetTitle(&gt->g,ret[0]);
-	GTextFieldChanged(gt,-1);
-	free(ret[1]);
-	free(ret);
     } else {
-	gc->choices = ret;
-	gc->selected = -1;
 	orig_len = u_strlen(gt->text);
 	len = u_strlen(ret[0]);
 	for ( i=1; ret[i]!=NULL; ++i ) {
 	    for ( pt1=ret[0], pt2=ret[i]; *pt1==*pt2 && pt1-ret[0]<len ; ++pt1, ++pt2 );
 	    len = pt1-ret[0];
 	}
-	if ( from_tab ) GDrawBeep(NULL);
 	if ( orig_len!=len ) {
 	    ch = ret[0][len]; ret[0][len] = '\0';
 	    GTextFieldSetTitle(&gt->g,ret[0]);
@@ -2996,71 +2989,79 @@ static void GTextFieldComplete(GTextField *gt,int from_tab) {
 	    GTextFieldChanged(gt,-1);
 	} else if ( !from_tab )
 	    GTextFieldChanged(gt,-1);
-	qsort(ret,i,sizeof(unichar_t *),ucmp);
-	gc->ctot = i;
-	if ( i>=MAXLINES ) {
-	    /* Try to shrink the list by just showing initial stubs of the */
-	    /*  names with multiple entries with a common next character */
-	    /* So if we have matched against "a" and we have "abc", "abd" "acc" */
-	    /*  the show "ab..." and "acc" */
-	    unichar_t **ret2=NULL, last_ch = -1;
-	    int cnt, doit, type2=false;
-	    for ( doit=0; doit<2; ++doit ) {
-		for ( i=cnt=0; ret[i]!=NULL; ++i ) {
-		    if ( last_ch!=ret[i][len] ) {
-			if ( doit && type2 ) {
-			    int c2 = cnt/MAXBRACKETS, c3 = cnt%MAXBRACKETS;
-			    if ( ret[i][len]=='\0' )
-		continue;
-			    if ( c3==0 ) {
-				ret2[c2] = gcalloc((len+MAXBRACKETS+2+4+1),sizeof(unichar_t));
-			        memcpy(ret2[c2],ret[i],len*sizeof(unichar_t));
-			        ret2[c2][len] = '[';
+	if ( ret[1]!=NULL ) {
+	    gc->choices = ret;
+	    gc->selected = -1;
+	    if ( from_tab ) GDrawBeep(NULL);
+	    qsort(ret,i,sizeof(unichar_t *),ucmp);
+	    gc->ctot = i;
+	    if ( i>=MAXLINES ) {
+		/* Try to shrink the list by just showing initial stubs of the */
+		/*  names with multiple entries with a common next character */
+		/* So if we have matched against "a" and we have "abc", "abd" "acc" */
+		/*  the show "ab..." and "acc" */
+		unichar_t **ret2=NULL, last_ch = -1;
+		int cnt, doit, type2=false;
+		for ( doit=0; doit<2; ++doit ) {
+		    for ( i=cnt=0; ret[i]!=NULL; ++i ) {
+			if ( last_ch!=ret[i][len] ) {
+			    if ( doit && type2 ) {
+				int c2 = cnt/MAXBRACKETS, c3 = cnt%MAXBRACKETS;
+				if ( ret[i][len]=='\0' )
+		    continue;
+				if ( c3==0 ) {
+				    ret2[c2] = gcalloc((len+MAXBRACKETS+2+4+1),sizeof(unichar_t));
+				    memcpy(ret2[c2],ret[i],len*sizeof(unichar_t));
+				    ret2[c2][len] = '[';
+				}
+				ret2[c2][len+1+c3] = ret[i][len];
+				uc_strcpy(ret2[c2]+len+2+c3,"] ...");
+			    } else if ( doit ) {
+				ret2[cnt] = galloc((u_strlen(ret[i])+5)*sizeof(unichar_t));
+				u_strcpy(ret2[cnt],ret[i]);
 			    }
-			    ret2[c2][len+1+c3] = ret[i][len];
-			    uc_strcpy(ret2[c2]+len+2+c3,"] ...");
-			} else if ( doit ) {
-			    ret2[cnt] = galloc((u_strlen(ret[i])+5)*sizeof(unichar_t));
-			    u_strcpy(ret2[cnt],ret[i]);
+			    ++cnt;
+			    last_ch = ret[i][len];
+			} else if ( doit && !type2 ) {
+			    int j;
+			    for ( j=len+1; ret[i][j]!='\0' && ret[i][j] == ret2[cnt-1][j]; ++j );
+			    uc_strcpy(ret2[cnt-1]+j," ...");
 			}
-			++cnt;
-			last_ch = ret[i][len];
-		    } else if ( doit && !type2 ) {
-			int j;
-			for ( j=len+1; ret[i][j]!='\0' && ret[i][j] == ret2[cnt-1][j]; ++j );
-			uc_strcpy(ret2[cnt-1]+j," ...");
+		    }
+		    if ( cnt>=MAXLINES*MAXBRACKETS )
+		break;
+		    if ( cnt>=MAXLINES && !doit ) {
+			type2 = (cnt+MAXBRACKETS-1)/MAXBRACKETS;
+			ret2 = galloc((type2+1)*sizeof(unichar_t *));
+		    } else if ( !doit )
+			ret2 = galloc((cnt+1)*sizeof(unichar_t *));
+		    else {
+			if ( type2 )
+			    cnt = type2;
+			ret2[cnt] = NULL;
 		    }
 		}
-		if ( cnt>=MAXLINES*MAXBRACKETS )
-	    break;
-		if ( cnt>=MAXLINES && !doit ) {
-		    type2 = (cnt+MAXBRACKETS-1)/MAXBRACKETS;
-		    ret2 = galloc((type2+1)*sizeof(unichar_t *));
-		} else if ( !doit )
-		    ret2 = galloc((cnt+1)*sizeof(unichar_t *));
-		else {
-		    if ( type2 )
-			cnt = type2;
-		    ret2[cnt] = NULL;
+		if ( ret2!=NULL ) {
+		    for ( i=0; ret[i]!=NULL; ++i )
+			free(ret[i]);
+		    free(ret);
+		    ret = gc->choices = ret2;
+		    i = gc->ctot = cnt;
 		}
 	    }
-	    if ( ret2!=NULL ) {
+	    if ( gc->ctot>=MAXLINES ) {
+		/* Too many choices. Don't popup a list of them */
+		gc->choices = NULL;
 		for ( i=0; ret[i]!=NULL; ++i )
 		    free(ret[i]);
 		free(ret);
-		ret = gc->choices = ret2;
-		i = gc->ctot = cnt;
+	    } else {
+		gc->ctot = i;
+		GCompletionCreatePopup(gc);
 	    }
-	}
-	if ( gc->ctot>=MAXLINES ) {
-	    /* Too many choices. Don't popup a list of them */
-	    gc->choices = NULL;
-	    for ( i=0; ret[i]!=NULL; ++i )
-		free(ret[i]);
-	    free(ret);
 	} else {
-	    gc->ctot = i;
-	    GCompletionCreatePopup(gc);
+	    free(ret[1]);
+	    free(ret);
 	}
     }
 }
