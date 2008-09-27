ecfcc6fd046dd32ffabee63e3b2d535837a56b0b
pfaedit <pfaedit>
2008-09-27 02:24:53 +0000

    Patch to custumize fonts for dlgs had a typo for edit cvt dlg.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1222162460L;
-const char *source_modtime_str = "09:34 GMT 23-Sep-2008";
-const char *source_version_str = "20080923";
+const time_t source_modtime = 1222482337L;
+const char *source_modtime_str = "02:25 GMT 27-Sep-2008";
+const char *source_version_str = "20080927";
