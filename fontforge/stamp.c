4b2865fcd2d46477d245384ac388d0e7f252bb38
George Williams <pfaedit@users.sourceforge.net>
2008-10-02 14:02:13 +0000

    When processing svg arc commands (in a path) we could get rounding errors that lead to NANs. Clip the range so that doesn't happen.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1222719005L;
-const char *source_modtime_str = "20:10 GMT 29-Sep-2008";
-const char *source_version_str = "20080929";
+const time_t source_modtime = 1222804755L;
+const char *source_modtime_str = "19:59 GMT 30-Sep-2008";
+const char *source_version_str = "20080930";
