ed5638fd9c10af74bcea0507b386bc17e6cc0deb
George Williams <pfaedit@users.sourceforge.net>
2008-10-06 21:35:27 +0000

    It used to be that when we changed glyphs in a charview we would turn off gridfit. Now it will simply refigure gridfit info for the new glyph.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1222965786L;
-const char *source_modtime_str = "16:43 GMT 2-Oct-2008";
-const char *source_version_str = "20081002";
+const time_t source_modtime = 1223326268L;
+const char *source_modtime_str = "20:51 GMT 6-Oct-2008";
+const char *source_version_str = "20081006";
