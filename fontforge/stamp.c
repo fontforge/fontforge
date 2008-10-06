db77198538170e74229ee89ddd577438dece4ca5
George Williams <pfaedit@users.sourceforge.net>
2008-10-06 22:25:18 +0000

    Make it so that moving a point around will not (exactly) turn off grid fit.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1223328980L;
-const char *source_modtime_str = "21:36 GMT 6-Oct-2008";
+const time_t source_modtime = 1223331578L;
+const char *source_modtime_str = "22:19 GMT 6-Oct-2008";
 const char *source_version_str = "20081006";
