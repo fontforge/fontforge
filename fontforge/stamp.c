3ecfa5c8ff68ba7a35ebe005f5af43a7f58ba430
pfaedit <pfaedit>
2008-10-10 04:08:09 +0000

    Be a little more careful about TeX pk and gf files in the openfont dlg. Alexej points out that .*gf also matches .xgf.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1223343705L;
-const char *source_modtime_str = "01:41 GMT 7-Oct-2008";
-const char *source_version_str = "20081007";
+const time_t source_modtime = 1223609424L;
+const char *source_modtime_str = "03:30 GMT 10-Oct-2008";
+const char *source_version_str = "20081010";
