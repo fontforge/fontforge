2fd482f54e35975040131a8b29eec9b4ccfbf201
George Williams <pfaedit@users.sourceforge.net>
2008-10-16 23:45:05 +0000

    Back in Feb, Werner gave me clear instructions on how to handle fonts where bit one of 'head'.flags was unset. I did something I thought was equivalent, but which wasn't. The result broke some glyphs in the "Vista fonts".

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1224181398L;
-const char *source_modtime_str = "18:23 GMT 16-Oct-2008";
+const time_t source_modtime = 1224199474L;
+const char *source_modtime_str = "23:24 GMT 16-Oct-2008";
 const char *source_version_str = "20081016";
