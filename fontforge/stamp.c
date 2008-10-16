08aeebe25ef76b5c5d5e1ca41b819210371a6f31
michal-n <michal-n>
2008-10-16 18:24:40 +0000

    Autoinstructor: some fixes to strong point optimizer.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1224129412L;
-const char *source_modtime_str = "03:56 GMT 16-Oct-2008";
+const time_t source_modtime = 1224181398L;
+const char *source_modtime_str = "18:23 GMT 16-Oct-2008";
 const char *source_version_str = "20081016";
