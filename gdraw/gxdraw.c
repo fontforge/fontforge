bcfdfdcaa51476cce3248938a367825fdf5dc415
pfaedit <pfaedit>
2008-10-21 22:06:37 +0000

    I did scrolling by having cairo copy the window around, just the way I used to have X do a CopyArea. That works great -- unless there are windows on top of the area being scrolled (child windows, random overlapping top level windows), then nothing has been drawn in those areas, and nothing will be copied so the old stuff remains. X used to give us special expose events when that happened, but cairo doesn't understand the window structure and can't. So I can't copy areas I have to just redraw the entire thing.

--- gdraw/gxdraw.c
+++ gdraw/gxdraw.c
@@ -2399,9 +2399,15 @@ static void GXDrawScroll(GWindow _w, GRect *rect, int32 hor, int32 vert) {
 #else
     _GXDraw_SetClipFunc(gdisp,gw->ggc);
 #ifndef _NO_LIBCAIRO
-    if ( gw->usecairo )
-	_GXCDraw_CopyArea(gw,gw,rect,rect->x+hor,rect->y+vert);
-    else
+    if ( gw->usecairo ) {
+	/* Cairo can happily scroll the window -- except it doesn't know about*/
+	/*  child windows, and so we don't get the requisit events to redraw */
+	/*  areas covered by children. Rats. */
+	GXDrawSendExpose(gw,rect->x,rect->y,rect->x+rect->width,rect->y+rect->height);
+	GXDrawPopClip(_w,&old);
+return;
+	/* _GXCDraw_CopyArea(gw,gw,rect,rect->x+hor,rect->y+vert); */
+    } else
 #endif
 	XCopyArea(gdisp->display,gw->w,gw->w,gdisp->gcstate[gw->ggc->bitmap_col].gc,
 		rect->x,rect->y,	rect->width,rect->height,
