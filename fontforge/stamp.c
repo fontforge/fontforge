076b2829b4d50145e8bda01e815686a3ef9ef5f5
pfaedit <pfaedit>
2008-10-22 19:18:35 +0000

    Reapply the patch from the other day.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1224687919L;
-const char *source_modtime_str = "15:05 GMT 22-Oct-2008";
+const time_t source_modtime = 1224702975L;
+const char *source_modtime_str = "19:16 GMT 22-Oct-2008";
 const char *source_version_str = "20081022";
