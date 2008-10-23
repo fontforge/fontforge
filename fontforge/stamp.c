c677878ff343f67163e20c9cffddcda2e374f287
George Williams <pfaedit@users.sourceforge.net>
2008-10-23 13:58:31 +0000

    the "Define Almost" menu item should not be checkable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1224723603L;
-const char *source_modtime_str = "01:00 GMT 23-Oct-2008";
+const time_t source_modtime = 1224770152L;
+const char *source_modtime_str = "13:55 GMT 23-Oct-2008";
 const char *source_version_str = "20081023";
