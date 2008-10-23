12615828cb443c51bf0dc4cd894dcd35cc5489b4
pfaedit <pfaedit>
2008-10-23 01:01:52 +0000

    Snap Outlines to Pixel Grid should be disabled but checked if cairo isn't active.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1224711423L;
-const char *source_modtime_str = "21:37 GMT 22-Oct-2008";
-const char *source_version_str = "20081022";
+const time_t source_modtime = 1224723603L;
+const char *source_modtime_str = "01:00 GMT 23-Oct-2008";
+const char *source_version_str = "20081023";
