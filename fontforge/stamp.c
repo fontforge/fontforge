44c595c6d14cad3c95622fa6c2ae971edbb06e5b
Alexey Kryukov <amkryukov@gmail.com>
2008-10-28 09:26:52 +0000

    Points could be snapped to a wrong edge of a non-ghost stem controlling a blue zone.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1225133849L;
-const char *source_modtime_str = "18:57 GMT 27-Oct-2008";
-const char *source_version_str = "20081027";
+const time_t source_modtime = 1225185869L;
+const char *source_modtime_str = "09:24 GMT 28-Oct-2008";
+const char *source_version_str = "20081028";
