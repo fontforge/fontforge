6b69e3d95d90e6935c67e371ab8212c7a7bc4a0a
pfaedit <pfaedit>
2008-10-30 03:33:28 +0000

    Alexey wanted a way to set the foreground color of selected glyphs in the fontview.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1225223238L;
-const char *source_modtime_str = "19:47 GMT 28-Oct-2008";
-const char *source_version_str = "20081028";
+const time_t source_modtime = 1225337525L;
+const char *source_modtime_str = "03:32 GMT 30-Oct-2008";
+const char *source_version_str = "20081030";
