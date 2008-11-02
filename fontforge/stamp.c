b4a6fe0ad98db547e2df8b8081caf1bed0e08832
pfaedit <pfaedit>
2008-11-02 00:59:17 +0000

    Add pango and its suite of sublibraries to the --library-status command line argument.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1225585923L;
-const char *source_modtime_str = "00:32 GMT 2-Nov-2008";
+const time_t source_modtime = 1225587594L;
+const char *source_modtime_str = "00:59 GMT 2-Nov-2008";
 const char *source_version_str = "20081102";
