28b9bf1b638467046bb2bee010ea080bb56bc624
George Williams <pfaedit@users.sourceforge.net>
2008-11-02 00:09:50 +0000

    Fix a crash bug in the print dialog.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1225510591L;
-const char *source_modtime_str = "03:36 GMT 1-Nov-2008";
-const char *source_version_str = "20081101";
+const time_t source_modtime = 1225584538L;
+const char *source_modtime_str = "00:08 GMT 2-Nov-2008";
+const char *source_version_str = "20081102";
