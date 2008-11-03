8b155f691c677d45ab6a4cf839bb9690bfc84a97
pfaedit <pfaedit>
2008-11-03 05:17:43 +0000

    Peter Baker says: In both "Show Grid Fit" and the TT debugger it used to be possible to turn off the display of outlines in the foreground layer simply by unchecking "fore". Now it can no longer be done, and as a result it's become more difficult to check at a glance the overall effect of TT instructions. It would be nice to have this capability back.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1225685604L;
-const char *source_modtime_str = "04:13 GMT 3-Nov-2008";
+const time_t source_modtime = 1225689302L;
+const char *source_modtime_str = "05:15 GMT 3-Nov-2008";
 const char *source_version_str = "20081103";
