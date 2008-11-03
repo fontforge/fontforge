847f56d6a146df5e121a2dde75de321092237468
George Williams <pfaedit@users.sourceforge.net>
2008-11-03 02:05:35 +0000

    Nicolas Spalinger points out that a license search in the oflib dialog (of fontforge) finds exactly the opposite of what was asked for (searching for PD finds OFL and vice versa).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1225674537L;
-const char *source_modtime_str = "01:08 GMT 3-Nov-2008";
+const time_t source_modtime = 1225677959L;
+const char *source_modtime_str = "02:05 GMT 3-Nov-2008";
 const char *source_version_str = "20081103";
