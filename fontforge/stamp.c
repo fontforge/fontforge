0b4b15e39b136d22184b0d5edd3e527ffd238759
pfaedit <pfaedit>
2008-11-03 05:24:26 +0000

    It used to be that if a glyph had instructions, and the background was order2, then editing the background (or any order2 background layer) would clear the instructions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1225689302L;
-const char *source_modtime_str = "05:15 GMT 3-Nov-2008";
+const time_t source_modtime = 1225689867L;
+const char *source_modtime_str = "05:24 GMT 3-Nov-2008";
 const char *source_version_str = "20081103";
