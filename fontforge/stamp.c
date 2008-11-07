356f474acc68b11e0d6ac9a7bf35c626aa97d3c9
pfaedit <pfaedit>
2008-11-07 22:10:02 +0000

    Patch by Kanou. SetPanose in legacy scripting did not set the panose_set bit so all changes were lost.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226092031L;
-const char *source_modtime_str = "21:07 GMT 7-Nov-2008";
+const time_t source_modtime = 1226095626L;
+const char *source_modtime_str = "22:07 GMT 7-Nov-2008";
 const char *source_version_str = "20081107";
