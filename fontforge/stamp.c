15cbcc904fd9d24ee236784c1f4d2476d00d68eb
George Williams <pfaedit@users.sourceforge.net>
2008-11-07 04:04:41 +0000

    Give instructions in Find Problems on how to figure out which two glyphs have the same name, or the same unicode code point.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226028941L;
-const char *source_modtime_str = "03:35 GMT 7-Nov-2008";
+const time_t source_modtime = 1226030086L;
+const char *source_modtime_str = "03:54 GMT 7-Nov-2008";
 const char *source_version_str = "20081107";
