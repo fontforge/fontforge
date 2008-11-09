0606012a870e4e413f33acd45bce9ac8153bab0e
George Williams <pfaedit@users.sourceforge.net>
2008-11-09 17:24:56 +0000

    Oops. The name of a reference in the active layer of the charview was drawn with the wrong font if an anchor point were present.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226109027L;
-const char *source_modtime_str = "01:50 GMT 8-Nov-2008";
-const char *source_version_str = "20081108";
+const time_t source_modtime = 1226251398L;
+const char *source_modtime_str = "17:23 GMT 9-Nov-2008";
+const char *source_version_str = "20081109";
