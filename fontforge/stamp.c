d38bd28d93e7d42fdc38bc9910e04dac70fb32d7
George Williams <pfaedit@users.sourceforge.net>
2008-11-10 21:28:11 +0000

    Michal suggests merging two messages which should be identical. He says they aren't and I'm sure he's right. Merging them is easier than figuring out how they differ.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226352141L;
-const char *source_modtime_str = "21:22 GMT 10-Nov-2008";
+const time_t source_modtime = 1226352463L;
+const char *source_modtime_str = "21:27 GMT 10-Nov-2008";
 const char *source_version_str = "20081110";
