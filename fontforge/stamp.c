8060cc09f2bf1d9cfe0babc9433bec727c600825
pfaedit <pfaedit>
2008-11-12 05:34:55 +0000

    make the lock icon (used to show the glyph has a use-my-metrics reference) a triffle smaller.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226467419L;
-const char *source_modtime_str = "05:23 GMT 12-Nov-2008";
+const time_t source_modtime = 1226467984L;
+const char *source_modtime_str = "05:33 GMT 12-Nov-2008";
 const char *source_version_str = "20081112";
