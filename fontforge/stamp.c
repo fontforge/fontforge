9ddbd67e1ef6851954bcf093af7a8ddc46079e45
George Williams <pfaedit@users.sourceforge.net>
2008-11-12 05:26:50 +0000

    Add a new problem to find glyphs with multiple references with use my metrics set. Also put something in to the ttf parser to check for this as we load a font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226443356L;
-const char *source_modtime_str = "22:42 GMT 11-Nov-2008";
-const char *source_version_str = "20081111";
+const time_t source_modtime = 1226467419L;
+const char *source_modtime_str = "05:23 GMT 12-Nov-2008";
+const char *source_version_str = "20081112";
