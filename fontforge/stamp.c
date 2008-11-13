df1f176b109a5f89ae643ac9e00009fa1bb31605
pfaedit <pfaedit>
2008-11-13 21:15:01 +0000

    FontForge has been generating bad bounding box information for BDF fonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226543685L;
-const char *source_modtime_str = "02:34 GMT 13-Nov-2008";
+const time_t source_modtime = 1226610666L;
+const char *source_modtime_str = "21:11 GMT 13-Nov-2008";
 const char *source_version_str = "20081113";
