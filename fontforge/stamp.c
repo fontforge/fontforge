0fcd3fdcd4c102e22b6d4b3d1dda6a60b5965d7e
George Williams <pfaedit@users.sourceforge.net>
2008-11-14 01:27:52 +0000

    Boxify the TeX More Params dialog of fontinfo.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226613096L;
-const char *source_modtime_str = "21:51 GMT 13-Nov-2008";
-const char *source_version_str = "20081113";
+const time_t source_modtime = 1226625836L;
+const char *source_modtime_str = "01:23 GMT 14-Nov-2008";
+const char *source_version_str = "20081114";
