4bcb1c56c31f82ba829279e32cfd8bded41e12a3
George Williams <pfaedit@users.sourceforge.net>
2008-11-14 14:55:10 +0000

    Boxify the Print Setup dialog.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226673091L;
-const char *source_modtime_str = "14:31 GMT 14-Nov-2008";
+const time_t source_modtime = 1226674474L;
+const char *source_modtime_str = "14:54 GMT 14-Nov-2008";
 const char *source_version_str = "20081114";
