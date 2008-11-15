c10eda34e4015a349359021eb41ceb04c9515720
pfaedit <pfaedit>
2008-11-15 04:24:59 +0000

    Oh, let the user know that we can import jpegs.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1226717569L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"02:52 GMT 15-Nov-2008"
+#define LibFF_ModTime		1226720169L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"03:36 GMT 15-Nov-2008"
 #define LibFF_VersionDate	20081115	/* Year, month, day */
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1226717573L;
-const char *source_modtime_str = "02:52 GMT 15-Nov-2008";
+const time_t source_modtime = 1226723126L;
+const char *source_modtime_str = "04:25 GMT 15-Nov-2008";
 const char *source_version_str = "20081115";
