9e9870d9991a7da1aeb56ede134586d43142a26f
George Williams <pfaedit@users.sourceforge.net>
2008-11-15 03:14:20 +0000

    Show grid fit missed a round to int, making grit fit outlines subtly wrong.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1226377041L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"04:17 GMT 11-Nov-2008"
-#define LibFF_VersionDate	20081111	/* Year, month, day */
+#define LibFF_ModTime		1226717569L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"02:52 GMT 15-Nov-2008"
+#define LibFF_VersionDate	20081115	/* Year, month, day */
