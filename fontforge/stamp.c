1631166a99be17dbd1b90141e3a17c3592fbc569
pfaedit <pfaedit>
2008-11-23 01:35:58 +0000

    Scrolling the GPOS/GSUB lookups panes for font info cause lots of nasty flickering.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1227109543L;
-const char *source_modtime_str = "15:45 GMT 19-Nov-2008";
-const char *source_version_str = "20081119";
+const time_t source_modtime = 1227404035L;
+const char *source_modtime_str = "01:33 GMT 23-Nov-2008";
+const char *source_version_str = "20081123";
