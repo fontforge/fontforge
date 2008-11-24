86aa6326e1580863f54d3af8c9df4803bc1a6b4c
George Williams <pfaedit@users.sourceforge.net>
2008-11-24 03:01:40 +0000

    Find Problems needs to check self-intersection too, I guess. Validation already does.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1227494765L;
-const char *source_modtime_str = "02:46 GMT 24-Nov-2008";
+const time_t source_modtime = 1227495635L;
+const char *source_modtime_str = "03:00 GMT 24-Nov-2008";
 const char *source_version_str = "20081124";
