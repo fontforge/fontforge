b9a7a0509c07a5b82d03baa2f4b7f560bdc21c5a
George Williams <pfaedit@users.sourceforge.net>
2008-11-24 01:30:09 +0000

    We could get into infinite recursion when CVChar and DVChar each passed non-latin characters to the other in hopes the other could handle it. Neither could. They kept calling deeper and deeper.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1227489355L;
-const char *source_modtime_str = "01:15 GMT 24-Nov-2008";
+const time_t source_modtime = 1227489986L;
+const char *source_modtime_str = "01:26 GMT 24-Nov-2008";
 const char *source_version_str = "20081124";
