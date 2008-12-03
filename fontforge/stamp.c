14e17d456d881ec402d63d90155be6d67aecb952
pfaedit <pfaedit>
2008-12-03 00:36:43 +0000

    Change the definition of FontView.FontSize. Used to be in pixels. Make it be in points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1228202804L;
-const char *source_modtime_str = "07:26 GMT 2-Dec-2008";
-const char *source_version_str = "20081202";
+const time_t source_modtime = 1228264182L;
+const char *source_modtime_str = "00:29 GMT 3-Dec-2008";
+const char *source_version_str = "20081203";
