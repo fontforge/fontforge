67d2e63e89a9de2591dacf8b4f15885774dc90a7
pfaedit <pfaedit>
2008-12-04 04:23:24 +0000

    When doing a "Scale Emsize", glyphs containing references with UseMyMetrics set were not set to the correct width if the refered glyph occured later in the encoding.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1228317154L;
-const char *source_modtime_str = "15:12 GMT 3-Dec-2008";
-const char *source_version_str = "20081203";
+const time_t source_modtime = 1228364374L;
+const char *source_modtime_str = "04:19 GMT 4-Dec-2008";
+const char *source_version_str = "20081204";
