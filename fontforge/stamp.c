6d6f2bfcd75f221fb743f61c9938d977e843d36a
pfaedit <pfaedit>
2008-12-09 00:14:11 +0000

    Boxify the goto dialog.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1228779108L;
-const char *source_modtime_str = "23:31 GMT 8-Dec-2008";
-const char *source_version_str = "20081208";
+const time_t source_modtime = 1228781727L;
+const char *source_modtime_str = "00:15 GMT 9-Dec-2008";
+const char *source_version_str = "20081209";
