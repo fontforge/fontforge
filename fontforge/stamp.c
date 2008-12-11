61328862d3ddcd982e6caaad77c7726d096fe2d7
akryukov <akryukov>
2008-12-11 22:34:02 +0000

    When comparing slopes, compare angles themselves, rather than sines/cosines. This makes the code a bit clearer and allows to make the allowed deviations customizable via the Prefs dialog.
    Fix one more problems with points being snapped to a wrong stem side (it
    was still possible in case of off-curve points).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1228956500L;
-const char *source_modtime_str = "00:48 GMT 11-Dec-2008";
+const time_t source_modtime = 1229034277L;
+const char *source_modtime_str = "22:24 GMT 11-Dec-2008";
 const char *source_version_str = "20081211";
