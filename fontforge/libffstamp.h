bf31c9ca12af43c8648c2b5ab59ec05b57cc58ce
George Williams <pfaedit@users.sourceforge.net>
2008-12-15 21:35:23 +0000

    Prefs with no ui should also be locale independant.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1229368768L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"19:19 GMT 15-Dec-2008"
+#define LibFF_ModTime		1229376675L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"21:31 GMT 15-Dec-2008"
 #define LibFF_VersionDate	20081215	/* Year, month, day */
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1229376228L;
-const char *source_modtime_str = "21:23 GMT 15-Dec-2008";
+const time_t source_modtime = 1229376677L;
+const char *source_modtime_str = "21:31 GMT 15-Dec-2008";
 const char *source_version_str = "20081215";
