34fd74cf2462618cf7317c92edf14043dd2af8f6
George Williams <pfaedit@users.sourceforge.net>
2008-12-15 14:14:39 +0000

    Patch by Taco to fix a memory problem reading bad baseline data.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1229349803L;
-const char *source_modtime_str = "14:03 GMT 15-Dec-2008";
+const time_t source_modtime = 1229350443L;
+const char *source_modtime_str = "14:14 GMT 15-Dec-2008";
 const char *source_version_str = "20081215";
