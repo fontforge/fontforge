804e813681f3cf386a208896caf654f782659c22
George Williams <pfaedit@users.sourceforge.net>
2008-12-20 01:20:39 +0000

    I think a buffer was too small for the sprintf that wrote to it -- at least sometimes.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1229544932L;
-const char *source_modtime_str = "20:15 GMT 17-Dec-2008";
-const char *source_version_str = "20081217";
+const time_t source_modtime = 1229735992L;
+const char *source_modtime_str = "01:19 GMT 20-Dec-2008";
+const char *source_version_str = "20081220";
