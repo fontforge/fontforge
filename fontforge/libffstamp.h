b6ff3e589968004c2d648c0f447922f8d40330e5
George Williams <pfaedit@users.sourceforge.net>
2009-01-02 23:50:28 +0000

    Mark to ligature anchor points not output properly to feature files.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1229989181L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"23:39 GMT 22-Dec-2008"
-#define LibFF_VersionDate	20081222	/* Year, month, day */
+#define LibFF_ModTime		1230135070L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"16:11 GMT 24-Dec-2008"
+#define LibFF_VersionDate	20081224	/* Year, month, day */
