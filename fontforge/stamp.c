556724010d4b53d011b6281e55f29eab7cd08e2b
pfaedit <pfaedit>
2009-01-03 00:22:06 +0000

    Points of extrema weren't being marked correctly in the glyph view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1230939887L;
-const char *source_modtime_str = "23:44 GMT 2-Jan-2009";
-const char *source_version_str = "20090102";
+const time_t source_modtime = 1230941957L;
+const char *source_modtime_str = "00:19 GMT 3-Jan-2009";
+const char *source_version_str = "20090103";
