a380d605efb9b0a5a1cc086eb9793bd651995dd9
George Williams <pfaedit@users.sourceforge.net>
2009-01-21 21:31:21 +0000

    When creating a metrics view with many glyphs, one of which needed to be autohinted, then ff might crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1232552923L;
-const char *source_modtime_str = "15:48 GMT 21-Jan-2009";
+const time_t source_modtime = 1232573343L;
+const char *source_modtime_str = "21:29 GMT 21-Jan-2009";
 const char *source_version_str = "20090121";
