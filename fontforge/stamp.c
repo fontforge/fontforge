754f4f37d99dcacd99a6d0b6b52778aa53ffba7b
George Williams <pfaedit@users.sourceforge.net>
2009-01-21 15:49:39 +0000

    Width was not set when building some small-caps glyphs out of references.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1232310282L;
-const char *source_modtime_str = "20:24 GMT 18-Jan-2009";
-const char *source_version_str = "20090118";
+const time_t source_modtime = 1232552923L;
+const char *source_modtime_str = "15:48 GMT 21-Jan-2009";
+const char *source_version_str = "20090121";
