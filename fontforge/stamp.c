c5b887050780d4bcbf7a81a017cafbb89dd193e7
pfaedit <pfaedit>
2009-01-27 22:36:09 +0000

    If there were no undoes in a glyph then Expand Stroke would crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1232748513L;
-const char *source_modtime_str = "22:08 GMT 23-Jan-2009";
-const char *source_version_str = "20090123";
+const time_t source_modtime = 1233095756L;
+const char *source_modtime_str = "22:35 GMT 27-Jan-2009";
+const char *source_version_str = "20090127";
