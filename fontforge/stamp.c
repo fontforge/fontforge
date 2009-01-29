a2938cd58ada0daf21df87efd27cdf67df506c21
pfaedit <pfaedit>
2009-01-29 15:16:33 +0000

    FF needed libxml to compile.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1233095756L;
-const char *source_modtime_str = "22:35 GMT 27-Jan-2009";
-const char *source_version_str = "20090127";
+const time_t source_modtime = 1233240276L;
+const char *source_modtime_str = "14:44 GMT 29-Jan-2009";
+const char *source_version_str = "20090129";
