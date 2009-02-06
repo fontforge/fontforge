e3bedba7aa5e6c6f6e6b91c9788679687ace0f16
George Williams <pfaedit@users.sourceforge.net>
2009-02-06 23:21:49 +0000

    Oops. One of the checks I put in for Werner on maxAdvanceWidth had a bug which caused a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1233961884L;
-const char *source_modtime_str = "23:11 GMT 6-Feb-2009";
+const time_t source_modtime = 1233962087L;
+const char *source_modtime_str = "23:14 GMT 6-Feb-2009";
 const char *source_version_str = "20090206";
