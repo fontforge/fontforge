266edde036542ff08e4049413cedff2770f47789
Alexey Kryukov <amkryukov@gmail.com>
2009-03-14 19:26:58 +0000

    Forgot to remove a debug statement.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1237058405L;
-const char *source_modtime_str = "19:20 GMT 14-Mar-2009";
+const time_t source_modtime = 1237058699L;
+const char *source_modtime_str = "19:24 GMT 14-Mar-2009";
 const char *source_version_str = "20090314";
