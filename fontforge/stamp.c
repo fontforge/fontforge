3d61768a45b48f37c067219f64ab99c2f6e4d6d1
pfaedit <pfaedit>
2009-03-26 03:51:05 +0000

    Oops. I broke contextual ligatures in feature file parsing.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1238035422L;
-const char *source_modtime_str = "02:43 GMT 26-Mar-2009";
+const time_t source_modtime = 1238039402L;
+const char *source_modtime_str = "03:50 GMT 26-Mar-2009";
 const char *source_version_str = "20090326";
