126d44ee8879cda632cab1e236923b3a51894f3a
George Williams <pfaedit@users.sourceforge.net>
2009-03-26 02:32:25 +0000

    Add support for reading/writing version 1.8 of Adobe's feature file format (while, I hope, retaining the ability to read version 1.6). Fix a number of bugs in parsing feature files.
    FontForge now supports the 'BASE' table (in feature files).
    
    The new format is considerably better than the old and can now be used to
    express almost all of OpenType.
    
    Changes:
      * ability to name anchors and value records and use them later.
      * contourpoints used to be expressed as "<contourpoint 2>" and are
          now expressed as "contourpoint 2" (no brokets). Adobe does not
          mention this in the changelog.
          [Incompatible change]
      * It is now possible to specify a GDEF mark attachment class in the
          lookupflags statement.
      * The syntax for mark to base/ligature/mark lookups is completely
          different.
          [Incompatible change]
      * The syntax for contextual lookups has been extended to allow them
          to reference lookups by name.
          [FontForge has always done this. When I suggested to Adobe that
           they follow suit they said they would not. They have now followed
           suit but have used an incompatible syntax to accomplish the same
           ends. So this is an
           Incompatible change
           for FontForge]
       * The syntax for contextual lookups has been extended to allow for
           the new mark lookups.
       * It is now possible to specify a reverse chaining substitution
           lookup.
       * It is now possible to specify descriptive names for the ss00-ss99
           features.
           FontForge doesn't support this but will parse and ignore it
    
    There are some ambiguities in the spec, however
     * In section 4.d we are told that MarkAttachmentType/UseMarkFilteringSet
      both take <glyph class name>s, yet in 9.b we are told they take
      <mark class name>s
     * In 6.e a NULL anchor is defined by <anchorNULL>, shouldn't this be
      <anchor NULL>? In that same example the NULL anchor is not followed by
      a mark keyword and class, is this correct syntax?

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1237545502L;
-const char *source_modtime_str = "10:38 GMT 20-Mar-2009";
-const char *source_version_str = "20090320";
+const time_t source_modtime = 1238033292L;
+const char *source_modtime_str = "02:08 GMT 26-Mar-2009";
+const char *source_version_str = "20090326";
