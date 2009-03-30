9e71686633a583b0c06f7912c3d41a402dd8c6cb
akryukov <akryukov>
2009-03-30 18:48:35 +0000

    Create faked HV stems for corner points at the ends of diagonal stems when preparing data for the autoinstructor.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1238357651L;
-const char *source_modtime_str = "20:14 GMT 29-Mar-2009";
-const char *source_version_str = "20090329";
+const time_t source_modtime = 1238438372L;
+const char *source_modtime_str = "18:39 GMT 30-Mar-2009";
+const char *source_version_str = "20090330";
