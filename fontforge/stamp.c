a9dd79475d15611361690219881e33e9bec8260b
George Williams <pfaedit@users.sourceforge.net>
2009-04-06 22:33:31 +0000

    Metrics view had problems with cid fonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1238697232L;
-const char *source_modtime_str = "18:33 GMT 2-Apr-2009";
+const time_t source_modtime = 1238695846L;
+const char *source_modtime_str = "18:10 GMT 2-Apr-2009";
 const char *source_version_str = "20090402";
