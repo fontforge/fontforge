43c6a0c53fa88a6557a0693c564a5ccb13951589
George Williams <pfaedit@users.sourceforge.net>
2009-04-07 20:54:40 +0000

    Add a set of new python functions for manipulating CID keyed fonts:   * cidConvertTo(registry,ordering,supplement)   * cidConvertByCmap(filename)   * cidFlatten()   * cidFlattenByCmap(filename)   * cidInsertBlankSubFont()   * cidRemoveSubFont()

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1239117399L;
-const char *source_modtime_str = "15:16 GMT 7-Apr-2009";
+const time_t source_modtime = 1239137400L;
+const char *source_modtime_str = "20:50 GMT 7-Apr-2009";
 const char *source_version_str = "20090407";
