91f4747c1eae0239383ab55f25c9ce4f9defe88c
George Williams <pfaedit@users.sourceforge.net>
2009-04-08 19:11:02 +0000

    Last release Werner asked me to put in checks for glyphs that exceded various limits in the font (glyphs outside the bounding box, glyphs with advance widths bigger than the max, etc.) The problem is that erroneous fonts often have thousands of these errors, so only report once -- unless they turn on fontlint.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1238163310L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"14:15 GMT 27-Mar-2009"
-#define LibFF_VersionDate	20090327	/* Year, month, day */
+#define LibFF_ModTime		1239217644L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"19:07 GMT 8-Apr-2009"
+#define LibFF_VersionDate	20090408	/* Year, month, day */
