3071b814aff5fe338a465545c23058d9f294ba72
George Williams <pfaedit@users.sourceforge.net>
2009-04-10 22:15:22 +0000

    Exporting bitmap formats chose the wrong format.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1239217652L;
-const char *source_modtime_str = "19:07 GMT 8-Apr-2009";
-const char *source_version_str = "20090408";
+const time_t source_modtime = 1239401721L;
+const char *source_modtime_str = "22:15 GMT 10-Apr-2009";
+const char *source_version_str = "20090410";
