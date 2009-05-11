522b900131734547aaf9f254823aeb14a2542770
pfaedit <pfaedit>
2009-05-11 15:17:48 +0000

    Patch by Andrey Panov to add GetAnchorPoints to legacy scripting.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1242054711L;
-const char *source_modtime_str = "15:11 GMT 11-May-2009";
+const time_t source_modtime = 1242054957L;
+const char *source_modtime_str = "15:15 GMT 11-May-2009";
 const char *source_version_str = "20090511";
