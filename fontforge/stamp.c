6089f0908b87ac55a9f2dd5bc65cc4f2bd9040e8
pfaedit <pfaedit>
2009-05-11 15:14:31 +0000

    Widhaya Trisarnwadhana points out that the mnemonic _N is bound to two items in the View menu of outline glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1241981210L;
-const char *source_modtime_str = "18:46 GMT 10-May-2009";
-const char *source_version_str = "20090510";
+const time_t source_modtime = 1242054711L;
+const char *source_modtime_str = "15:11 GMT 11-May-2009";
+const char *source_version_str = "20090511";
