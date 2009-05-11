3c98b52fac9dad6690786d20ce1165db683a99ef
George Williams <pfaedit@users.sourceforge.net>
2009-05-11 22:53:54 +0000

    Show Att didn't display cursive anchor positioning anchor classes properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1242071085L;
-const char *source_modtime_str = "19:44 GMT 11-May-2009";
+const time_t source_modtime = 1242082343L;
+const char *source_modtime_str = "22:52 GMT 11-May-2009";
 const char *source_version_str = "20090511";
