ea7509bcb52e34f591395ab8e93963c35c933681
George Williams <pfaedit@users.sourceforge.net>
2009-05-14 01:39:51 +0000

    If the user didn't have freetype, then the anti-aliased metrics view was faint.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1242174566L;
-const char *source_modtime_str = "00:29 GMT 13-May-2009";
-const char *source_version_str = "20090513";
+const time_t source_modtime = 1242265100L;
+const char *source_modtime_str = "01:38 GMT 14-May-2009";
+const char *source_version_str = "20090514";
