5d1c7c46d48274aae6ce3b7c0be7fd76f9135966
pfaedit <pfaedit>
2009-05-18 04:24:38 +0000

    Patch by Alexey to unlink bitmap code.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1242306794L;
-const char *source_modtime_str = "13:13 GMT 14-May-2009";
-const char *source_version_str = "20090514";
+const time_t source_modtime = 1242620673L;
+const char *source_modtime_str = "04:24 GMT 18-May-2009";
+const char *source_version_str = "20090518";
