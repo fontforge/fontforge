5a45c05e482d380468a2f7d589dc88c25f9997ee
pfaedit <pfaedit>
2009-05-25 00:30:03 +0000

    Jakub Steiner thinks it would be a good idea if the tool bindings of mouse clicks in the glyph editor were sticky across invocations.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1243206896L;
-const char *source_modtime_str = "23:14 GMT 24-May-2009";
-const char *source_version_str = "20090524";
+const time_t source_modtime = 1243211059L;
+const char *source_modtime_str = "00:24 GMT 25-May-2009";
+const char *source_version_str = "20090525";
