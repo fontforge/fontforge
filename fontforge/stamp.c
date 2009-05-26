2fff5d67de1e80f2c806f2bb5be8b3e11af978d2
George Williams <pfaedit@users.sourceforge.net>
2009-05-26 00:25:14 +0000

    Typo.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1243223545L;
-const char *source_modtime_str = "03:52 GMT 25-May-2009";
-const char *source_version_str = "20090525";
+const time_t source_modtime = 1243297418L;
+const char *source_modtime_str = "00:23 GMT 26-May-2009";
+const char *source_version_str = "20090526";
