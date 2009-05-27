433f451e2a572bcf6bf5053e63dbb1cf7c4c3d69
pfaedit <pfaedit>
2009-05-27 15:54:14 +0000

    Was outputting dates in <date> but should be in <string>

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1243304645L;
-const char *source_modtime_str = "02:24 GMT 26-May-2009";
-const char *source_version_str = "20090526";
+const time_t source_modtime = 1243439705L;
+const char *source_modtime_str = "15:55 GMT 27-May-2009";
+const char *source_version_str = "20090527";
