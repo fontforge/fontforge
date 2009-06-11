cadf83ac046cb7a72f0128e1ff5d3607a3d46d0d
George Williams <pfaedit@users.sourceforge.net>
2009-06-11 01:48:54 +0000

    When reading strings from the name table, certain platform/specific settings would cause ff to:   1) read the wrong string   2) screw up memory.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1243808203L;
-const char *source_modtime_str = "22:16 GMT 31-May-2009";
-const char *source_version_str = "20090531";
+const time_t source_modtime = 1244684546L;
+const char *source_modtime_str = "01:42 GMT 11-Jun-2009";
+const char *source_version_str = "20090611";
