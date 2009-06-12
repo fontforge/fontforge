0924461dea9f8b0dda350484467211b70a6de73c
pfaedit <pfaedit>
2009-06-12 20:49:37 +0000

    Patch by Michal Nowakowski
    when saving an sfd after adding a new
    "kerning by classes" subtable and cancelling its edit dialog.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1244685017L;
-const char *source_modtime_str = "01:50 GMT 11-Jun-2009";
-const char *source_version_str = "20090611";
+const time_t source_modtime = 1244839213L;
+const char *source_modtime_str = "20:40 GMT 12-Jun-2009";
+const char *source_version_str = "20090612";
