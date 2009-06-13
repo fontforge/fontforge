eeeb04539e4e8e2f09604cdc3895ee889553fcea
michal-n <michal-n>
2009-06-13 14:22:52 +0000

    Fixed frequent losses of kerning pairs in quadratic fonts.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1244902272L;
-const char *source_modtime_str = "14:11 GMT 13-Jun-2009";
+const time_t source_modtime = 1244902816L;
+const char *source_modtime_str = "14:20 GMT 13-Jun-2009";
 const char *source_version_str = "20090613";
