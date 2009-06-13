fbe325e2460263f2333e11e2a94bd76816221542
michal-n <michal-n>
2009-06-13 14:08:30 +0000

    Fixed a bug reported by Louis Simard:  'Build composite glyph' was enabled in wrong situations and crashed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1244839213L;
-const char *source_modtime_str = "20:40 GMT 12-Jun-2009";
-const char *source_version_str = "20090612";
+const time_t source_modtime = 1244901847L;
+const char *source_modtime_str = "14:04 GMT 13-Jun-2009";
+const char *source_version_str = "20090613";
