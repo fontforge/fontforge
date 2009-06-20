d90ab717653166d8cf96f4a655fd83dd7dc8c505
pfaedit <pfaedit>
2009-06-20 00:52:55 +0000

    When faced with non-integral coordinates ff would sometimes introduce rounding errors so the start and end of a contour were slightly different in type1 output. I think this is now fixed...

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1243460773L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"21:46 GMT 27-May-2009"
-#define LibFF_VersionDate	20090527	/* Year, month, day */
+#define LibFF_ModTime		1245455421L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"23:50 GMT 19-Jun-2009"
+#define LibFF_VersionDate	20090619	/* Year, month, day */
