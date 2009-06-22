4ebfe7ee60e392f6a0f43d7615302fabc68de918
George Williams <pfaedit@users.sourceforge.net>
2009-06-22 21:42:51 +0000

    Two issues: Apple has changed the name of the preference file for X11. Annoying. Pointless.
    Apple (or xorg) has changed the default setting of DISPLAY.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1245455427L;
-const char *source_modtime_str = "23:50 GMT 19-Jun-2009";
-const char *source_version_str = "20090619";
+const time_t source_modtime = 1245706957L;
+const char *source_modtime_str = "21:42 GMT 22-Jun-2009";
+const char *source_version_str = "20090622";
