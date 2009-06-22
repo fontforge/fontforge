f924f9db9d884f1561c1aed8b55c1ad4215ead8c
pfaedit <pfaedit>
2009-06-22 22:49:16 +0000

    Sync.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1245455421L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"23:50 GMT 19-Jun-2009"
-#define LibFF_VersionDate	20090619	/* Year, month, day */
+#define LibFF_ModTime		1245710154L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"22:35 GMT 22-Jun-2009"
+#define LibFF_VersionDate	20090622	/* Year, month, day */
