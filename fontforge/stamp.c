25e415031e3ad422bab9010ec34455afef85fa66
George Williams <pfaedit@users.sourceforge.net>
2009-06-29 22:20:42 +0000

    Set Width (Set Right Bearing, etc.) from the fontview would change the width of a glyph even if it were composite with a "use my metrics" reference.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1246310581L;
-const char *source_modtime_str = "21:23 GMT 29-Jun-2009";
+const time_t source_modtime = 1246314054L;
+const char *source_modtime_str = "22:20 GMT 29-Jun-2009";
 const char *source_version_str = "20090629";
