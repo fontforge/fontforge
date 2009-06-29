c32798b1bcf2cb7b06d3cd15b2b3ee7c37aca334
George Williams <pfaedit@users.sourceforge.net>
2009-06-29 19:10:09 +0000

    FF can save spiro info into an sfnt. Unfortunately FF didn't read it back in properly.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1246294992L;
-const char *source_modtime_str = "17:03 GMT 29-Jun-2009";
+const time_t source_modtime = 1246302474L;
+const char *source_modtime_str = "19:07 GMT 29-Jun-2009";
 const char *source_version_str = "20090629";
