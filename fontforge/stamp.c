251d4a5cb7437287cfdab3abc9d6af44c6c2c0f8
George Williams <pfaedit@users.sourceforge.net>
2009-07-04 19:04:03 +0000

    FF was not translating the spiro point menu in charview.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1246550579L;
-const char *source_modtime_str = "16:02 GMT 2-Jul-2009";
-const char *source_version_str = "20090702";
+const time_t source_modtime = 1246734150L;
+const char *source_modtime_str = "19:02 GMT 4-Jul-2009";
+const char *source_version_str = "20090704";
