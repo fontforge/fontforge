4f8781faf9d3fa2b01a64886525a193a5bee7825
pfaedit <pfaedit>
2009-07-04 19:44:25 +0000

    When creating an ellipse or rectangle, ff would round the points to be integers -- but it would not refigure the splines after doing that (so the spline polynomials do not actually reach their endpoints causing peculiar errors).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1246734432L;
-const char *source_modtime_str = "19:07 GMT 4-Jul-2009";
+const time_t source_modtime = 1246736662L;
+const char *source_modtime_str = "19:44 GMT 4-Jul-2009";
 const char *source_version_str = "20090704";
