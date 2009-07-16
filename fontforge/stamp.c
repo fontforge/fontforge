10795f8b0e761da3e2aa1c9a7a8d271b31257fdf
George Williams <pfaedit@users.sourceforge.net>
2009-07-16 16:49:17 +0000

    Patch by Herbert Duerr. With traditional 'kern' tables, windows has problems with kerning glyphs which do not have BMP encodings. So produce a warning in this case.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1247762403L;
-const char *source_modtime_str = "16:40 GMT 16-Jul-2009";
+const time_t source_modtime = 1247762928L;
+const char *source_modtime_str = "16:48 GMT 16-Jul-2009";
 const char *source_version_str = "20090716";
