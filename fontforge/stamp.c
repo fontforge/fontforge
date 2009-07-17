4082eb99aef49171a43fa55cb621850029c95c41
pfaedit <pfaedit>
2009-07-17 13:42:17 +0000

    When generating a temporary font for rasterization purposes, don't output GPOS GSUB, or any AAT tables.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1247801587L;
-const char *source_modtime_str = "03:33 GMT 17-Jul-2009";
+const time_t source_modtime = 1247837890L;
+const char *source_modtime_str = "13:38 GMT 17-Jul-2009";
 const char *source_version_str = "20090717";
