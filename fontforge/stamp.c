8722c897428989792c35890cd435edfc8d5f5f31
George Williams <pfaedit@users.sourceforge.net>
2009-07-17 14:44:45 +0000

    Just in case...

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1247837890L;
-const char *source_modtime_str = "13:38 GMT 17-Jul-2009";
+const time_t source_modtime = 1247841563L;
+const char *source_modtime_str = "14:39 GMT 17-Jul-2009";
 const char *source_version_str = "20090717";
