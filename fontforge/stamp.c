ecbac9fec1bc18635370cad3564816db3f704eb6
George Williams <pfaedit@users.sourceforge.net>
2009-07-29 17:24:46 +0000

    When generating a type3 font from a script, ff would convert to type0 if it had a two byte encoding.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1247884404L;
-const char *source_modtime_str = "02:33 GMT 18-Jul-2009";
-const char *source_version_str = "20090718";
+const time_t source_modtime = 1248888314L;
+const char *source_modtime_str = "17:25 GMT 29-Jul-2009";
+const char *source_version_str = "20090729";
