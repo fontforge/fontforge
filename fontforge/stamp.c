914defc124ebf0d6ff4c5fe61b26b7c119f6ae6f
George Williams <pfaedit@users.sourceforge.net>
2009-08-03 22:56:43 +0000

    Interpolate fonts would screw up internals if the first font had a layer count which wasn't 2. This would almost certainly cause a crash later.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1249333832L;
-const char *source_modtime_str = "21:10 GMT 3-Aug-2009";
+const time_t source_modtime = 1249338671L;
+const char *source_modtime_str = "22:31 GMT 3-Aug-2009";
 const char *source_version_str = "20090803";
