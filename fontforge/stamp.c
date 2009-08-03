d750a2d7eb302fb3d2a7f0e43e4ff6b1d049ceff
George Williams <pfaedit@users.sourceforge.net>
2009-08-03 21:21:47 +0000

    Werner suggests that .02 is a better default value for delta checking than .01.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1248888314L;
-const char *source_modtime_str = "17:25 GMT 29-Jul-2009";
-const char *source_version_str = "20090729";
+const time_t source_modtime = 1249325938L;
+const char *source_modtime_str = "18:58 GMT 3-Aug-2009";
+const char *source_version_str = "20090803";
