97e1d553df2c83cae44a53c2b86f419163eaaa0a
George Williams <pfaedit@users.sourceforge.net>
2009-08-03 21:40:00 +0000

    Hunh. Python creates an iterator for my cvt type which doesn't work. So create a real one.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1249329989L;
-const char *source_modtime_str = "20:06 GMT 3-Aug-2009";
+const time_t source_modtime = 1249333832L;
+const char *source_modtime_str = "21:10 GMT 3-Aug-2009";
 const char *source_version_str = "20090803";
