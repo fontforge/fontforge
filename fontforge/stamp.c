2c9a18cfbc2881fdb2d4e65a5f158f16b227d023
George Williams <pfaedit@users.sourceforge.net>
2009-08-03 21:25:10 +0000

    Python cvt.find function crashed if there were no cvt table.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1249325938L;
-const char *source_modtime_str = "18:58 GMT 3-Aug-2009";
+const time_t source_modtime = 1249327011L;
+const char *source_modtime_str = "19:16 GMT 3-Aug-2009";
 const char *source_version_str = "20090803";
