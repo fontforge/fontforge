e8f5432e8f6232f002dd3494fb5ae2c66dc2b7e2
pfaedit <pfaedit>
2009-08-04 02:00:03 +0000

    Add a new python method glyph.preserveLayerAsUndo() to add an undo to the undo list which will restore the layer to its current state.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1249347586L;
-const char *source_modtime_str = "00:59 GMT 4-Aug-2009";
+const time_t source_modtime = 1249350570L;
+const char *source_modtime_str = "01:49 GMT 4-Aug-2009";
 const char *source_version_str = "20090804";
