63fadf1cded06d901b2d95044b41b6c57691c586
pfaedit <pfaedit>
2009-08-06 01:43:00 +0000

    When generating truetype from cubic splines, each glyph needs a bounding box, and ff would calculate the bounding box before converting to quadratic. In almost all cases this won't make a difference, but every now and then the bounding box is a little too small (due to approximations in the conversion).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1249513185L;
-const char *source_modtime_str = "22:59 GMT 5-Aug-2009";
-const char *source_version_str = "20090805";
+const time_t source_modtime = 1249522698L;
+const char *source_modtime_str = "01:38 GMT 6-Aug-2009";
+const char *source_version_str = "20090806";
