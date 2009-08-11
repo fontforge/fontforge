713efcd43a0094f3bb49bf0c3486af99a316751e
pfaedit <pfaedit>
2009-08-11 00:37:36 +0000

    Oops. Yesterday (or the day before) I put in a free I thought was missing, but valgrind points out it was somewhere else. So take it out again.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1249843750L;
-const char *source_modtime_str = "18:49 GMT 9-Aug-2009";
-const char *source_version_str = "20090809";
+const time_t source_modtime = 1249950983L;
+const char *source_modtime_str = "00:36 GMT 11-Aug-2009";
+const char *source_version_str = "20090811";
