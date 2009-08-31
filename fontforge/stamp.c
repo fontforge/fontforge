3cc0b5db20a9711218555106e33b8bcea6b3ea1e
pfaedit <pfaedit>
2009-08-31 20:01:50 +0000

    Typo in code for converting an OpenType contextual sub to an apple state machine.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1250474349L;
-const char *source_modtime_str = "01:59 GMT 17-Aug-2009";
-const char *source_version_str = "20090817";
+const time_t source_modtime = 1251748835L;
+const char *source_modtime_str = "20:00 GMT 31-Aug-2009";
+const char *source_version_str = "20090831";
