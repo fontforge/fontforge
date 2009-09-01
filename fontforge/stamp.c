c0ea9aced05fb3afbebc9483357e3bb121394aee
George Williams <pfaedit@users.sourceforge.net>
2009-09-01 03:15:33 +0000

    I was supposed to translate espace_espace_i to space_space_i and espace.small to space.small but that didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1251772045L;
-const char *source_modtime_str = "02:27 GMT 1-Sep-2009";
+const time_t source_modtime = 1251774468L;
+const char *source_modtime_str = "03:07 GMT 1-Sep-2009";
 const char *source_version_str = "20090901";
