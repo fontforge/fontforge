f4e92cde362b59897fe5c7958f8fc6f9a784f9ad
michal-n <michal-n>
2009-09-02 19:43:28 +0000

    Autoinstructor: Alexey pointed out that inaccurate strong points interpolation caused assymetry of implied perpendicular extremas.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1251774468L;
-const char *source_modtime_str = "03:07 GMT 1-Sep-2009";
-const char *source_version_str = "20090901";
+const time_t source_modtime = 1251920346L;
+const char *source_modtime_str = "19:39 GMT 2-Sep-2009";
+const char *source_version_str = "20090902";
