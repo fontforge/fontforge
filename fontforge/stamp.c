a4aeb11d4867e5fead30f280ea069116d5af16db
George Williams <pfaedit@users.sourceforge.net>
2009-09-04 19:06:00 +0000

    Patch by Alexandre Prokoudine to make an error message translatable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252083311L;
-const char *source_modtime_str = "16:55 GMT 4-Sep-2009";
+const time_t source_modtime = 1252091146L;
+const char *source_modtime_str = "19:05 GMT 4-Sep-2009";
 const char *source_version_str = "20090904";
