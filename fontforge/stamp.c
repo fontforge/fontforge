0a71214607e6035297eab5374dccdd3b709a378e
pfaedit <pfaedit>
2009-09-04 16:51:45 +0000

    Back in the dark ages adobe provide a set of encodings for the PUA. For instance A.small was encoded into PUA. Nowadays it is thought better to leave A.small unencoded. But if a modern font contained an A.small glyph FontForge would think it should be encoded into the PUA. So I added a preference item to turn off adobe's obsolete names.
    Sadly the preference item wasn't very smart and turned off names like uE001 as well. Fix that.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252055702L;
-const char *source_modtime_str = "09:15 GMT 4-Sep-2009";
+const time_t source_modtime = 1252082639L;
+const char *source_modtime_str = "16:43 GMT 4-Sep-2009";
 const char *source_version_str = "20090904";
