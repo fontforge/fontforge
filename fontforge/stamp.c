8421933818d0ff5c2535a0fc72055156e060474d
pfaedit <pfaedit>
2009-09-04 16:57:10 +0000

    Patch by Stephen Hartke to make contour splicing (in fontforge's python module) work the way splicing normally does in python.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252082639L;
-const char *source_modtime_str = "16:43 GMT 4-Sep-2009";
+const time_t source_modtime = 1252083311L;
+const char *source_modtime_str = "16:55 GMT 4-Sep-2009";
 const char *source_version_str = "20090904";
