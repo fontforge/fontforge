224b8344a4c9df8bcd1d4a8aad86213f504034bc
michal-n <michal-n>
2009-09-04 09:17:48 +0000

    Autoinstructor: made much faster by caching SplinePointListIsClockwise() results.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1251940522L;
-const char *source_modtime_str = "01:15 GMT 3-Sep-2009";
-const char *source_version_str = "20090903";
+const time_t source_modtime = 1252055702L;
+const char *source_modtime_str = "09:15 GMT 4-Sep-2009";
+const char *source_version_str = "20090904";
