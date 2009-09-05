f1e3983c129ebe271f8d63f332b82b6b9a0b7b09
George Williams <pfaedit@users.sourceforge.net>
2009-09-05 19:35:44 +0000

    File->X Resource Editor...->FontView->Font Size was supposed to allow the user to set the size of the glyph labels in the fontview. It didn't. Fixed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252157387L;
-const char *source_modtime_str = "13:29 GMT 5-Sep-2009";
+const time_t source_modtime = 1252178873L;
+const char *source_modtime_str = "19:27 GMT 5-Sep-2009";
 const char *source_version_str = "20090905";
