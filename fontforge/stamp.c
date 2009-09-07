77e70cdc515903d6f8a850629dbe9eb7d3e121d7
michal-n <michal-n>
2009-09-07 17:01:45 +0000

    Autoinstructor: pushes EF2Dot14 values in a more compact manner.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252191497L;
-const char *source_modtime_str = "22:58 GMT 5-Sep-2009";
-const char *source_version_str = "20090905";
+const time_t source_modtime = 1252342508L;
+const char *source_modtime_str = "16:55 GMT 7-Sep-2009";
+const char *source_version_str = "20090907";
