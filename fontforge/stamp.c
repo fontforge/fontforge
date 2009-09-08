d9b1f5e2736f1b10f81ce138e41d72dd3c570bf4
pfaedit <pfaedit>
2009-09-08 02:41:24 +0000

    When opening a ttf file the guideline psuedo-layer was cubic not quadratic.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252376894L;
-const char *source_modtime_str = "02:28 GMT 8-Sep-2009";
+const time_t source_modtime = 1252377292L;
+const char *source_modtime_str = "02:34 GMT 8-Sep-2009";
 const char *source_version_str = "20090908";
