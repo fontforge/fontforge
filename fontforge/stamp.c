19d9d7c09ad05b6af9f5b6d074bc57f1ff11b29d
George Williams <pfaedit@users.sourceforge.net>
2009-09-09 22:39:11 +0000

    See if I can speed up execution of "<glyphname> in <font>"

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252532671L;
-const char *source_modtime_str = "21:44 GMT 9-Sep-2009";
+const time_t source_modtime = 1252535803L;
+const char *source_modtime_str = "22:36 GMT 9-Sep-2009";
 const char *source_version_str = "20090909";
