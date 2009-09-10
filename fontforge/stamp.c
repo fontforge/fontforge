44f7da2de91bc531b7adc2245fb196e5ca703cb9
pfaedit <pfaedit>
2009-09-10 20:04:36 +0000

    Patch by Michal to render more strings translateable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252535803L;
-const char *source_modtime_str = "22:36 GMT 9-Sep-2009";
-const char *source_version_str = "20090909";
+const time_t source_modtime = 1252613097L;
+const char *source_modtime_str = "20:04 GMT 10-Sep-2009";
+const char *source_version_str = "20090910";
