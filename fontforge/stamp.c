5af87e8935903c33a7645f89d705bfcaeda91f2d
George Williams <pfaedit@users.sourceforge.net>
2009-09-11 19:48:15 +0000

    Drag & dropping a glyph into the metrics view didn't mark the end of string properly so cruft left over could be displayed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252691810L;
-const char *source_modtime_str = "17:56 GMT 11-Sep-2009";
+const time_t source_modtime = 1252698380L;
+const char *source_modtime_str = "19:46 GMT 11-Sep-2009";
 const char *source_version_str = "20090911";
