8a70f5c7e26e267e9eb4a78bf0a0f9c9ae13d9ca
pfaedit <pfaedit>
2009-09-12 23:37:53 +0000

    The pulldown list in the apple version of the MM dlg did not get set to the correct default value.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252797463L;
-const char *source_modtime_str = "23:17 GMT 12-Sep-2009";
+const time_t source_modtime = 1252798650L;
+const char *source_modtime_str = "23:37 GMT 12-Sep-2009";
 const char *source_version_str = "20090912";
