2fabef7b53eafcb5fd945b3e847eb634b8d7e3bd
pfaedit <pfaedit>
2009-09-13 03:23:29 +0000

    Fix a couple of problems with the Set Point Size dlg of the metrics view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252800245L;
-const char *source_modtime_str = "00:04 GMT 13-Sep-2009";
+const time_t source_modtime = 1252812182L;
+const char *source_modtime_str = "03:23 GMT 13-Sep-2009";
 const char *source_version_str = "20090913";
