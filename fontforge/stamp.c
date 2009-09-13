3ffb22f6496f40dbaaefbe2c223367d9ac95fe9f
George Williams <pfaedit@users.sourceforge.net>
2009-09-13 03:34:15 +0000

    Alexey suggests that Control + Mouse wheel should Zoom the metrics window just as it does the glyph view.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1252812182L;
-const char *source_modtime_str = "03:23 GMT 13-Sep-2009";
+const time_t source_modtime = 1252812821L;
+const char *source_modtime_str = "03:33 GMT 13-Sep-2009";
 const char *source_version_str = "20090913";
