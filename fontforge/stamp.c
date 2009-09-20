6ee32c5e5838bfc0917b8eccbe15553277b2c61e
George Williams <pfaedit@users.sourceforge.net>
2009-09-20 14:06:41 +0000

    Patch by Alexey K so that ff will look for (and parse) unicode cmaps when loading fonts from pdf files. Loaded fonts will then have an encoding rather than just being a list of glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1253133867L;
-const char *source_modtime_str = "20:44 GMT 16-Sep-2009";
-const char *source_version_str = "20090916";
+const time_t source_modtime = 1253455629L;
+const char *source_modtime_str = "14:07 GMT 20-Sep-2009";
+const char *source_version_str = "20090920";
