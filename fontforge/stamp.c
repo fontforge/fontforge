fc057e9a7f2c4cf878e27a9a911f7486006f04cc
George Williams <pfaedit@users.sourceforge.net>
2009-09-21 03:13:19 +0000

    PDF has changed since I last downloaded the spec, and Barry wanted support for Cross-reference streams (which required also supporting object streams).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1253466430L;
-const char *source_modtime_str = "17:07 GMT 20-Sep-2009";
-const char *source_version_str = "20090920";
+const time_t source_modtime = 1253502513L;
+const char *source_modtime_str = "03:08 GMT 21-Sep-2009";
+const char *source_version_str = "20090921";
