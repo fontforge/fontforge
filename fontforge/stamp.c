5f27bc9e2aa16a1899f44d5511db300f9db43f76
pfaedit <pfaedit>
2009-10-27 00:02:23 +0000

    Kerning class dlg didn't expand the kerning matrix when the window was resized.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1256339619L;
-const char *source_modtime_str = "23:13 GMT 23-Oct-2009";
-const char *source_version_str = "20091023";
+const time_t source_modtime = 1256601662L;
+const char *source_modtime_str = "00:01 GMT 27-Oct-2009";
+const char *source_version_str = "20091027";
