4da3edabd985e18442568751830a759599218cd2
pfaedit <pfaedit>
2009-10-28 20:25:40 +0000

    Fix a dangling pointer when we get a delete window event for the kern format dlg.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1256682775L;
-const char *source_modtime_str = "22:32 GMT 27-Oct-2009";
-const char *source_version_str = "20091027";
+const time_t source_modtime = 1256759393L;
+const char *source_modtime_str = "19:49 GMT 28-Oct-2009";
+const char *source_version_str = "20091028";
