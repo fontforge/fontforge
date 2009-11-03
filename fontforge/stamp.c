4dc43118f2865408d5b6bbbd6d6a2c27c5f34e38
pfaedit <pfaedit>
2009-11-03 22:44:26 +0000

    More improvements (I hope) to the kerning by classes dialog, suggested by Panov.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1256941642L;
-const char *source_modtime_str = "22:27 GMT 30-Oct-2009";
-const char *source_version_str = "20091030";
+const time_t source_modtime = 1257287971L;
+const char *source_modtime_str = "22:39 GMT 3-Nov-2009";
+const char *source_version_str = "20091103";
