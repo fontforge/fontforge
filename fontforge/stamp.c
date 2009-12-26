c0930614e236143b05d275cec2ba0255767909c1
pfaedit <pfaedit>
2009-12-26 21:02:52 +0000

    Patch from Alexey Kryukov to make legacy scripting commands GetAnchorPoints and AddAnchorPoint to be consistant.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1261861177L;
-const char *source_modtime_str = "20:59 GMT 26-Dec-2009";
+const time_t source_modtime = 1261861304L;
+const char *source_modtime_str = "21:01 GMT 26-Dec-2009";
 const char *source_version_str = "20091226";
