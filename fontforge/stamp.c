f10fda52460a78654fe22137da53dd11266d38ef
George Williams <pfaedit@users.sourceforge.net>
2009-12-26 21:29:40 +0000

    Patch by Tilman Blumenbach. Legacy scripting GetPrefs crashed when asked about "AutotraceArgs".

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1261862315L;
-const char *source_modtime_str = "21:18 GMT 26-Dec-2009";
+const time_t source_modtime = 1261862973L;
+const char *source_modtime_str = "21:29 GMT 26-Dec-2009";
 const char *source_version_str = "20091226";
