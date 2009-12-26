ab17f3edcd5d0b5ceb0209c08eb8503597df92ed
George Williams <pfaedit@users.sourceforge.net>
2009-12-26 22:26:40 +0000

    A bad 'kern' table could send fontforge into an infinite loop.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1261865389L;
-const char *source_modtime_str = "22:09 GMT 26-Dec-2009";
+const time_t source_modtime = 1261866344L;
+const char *source_modtime_str = "22:25 GMT 26-Dec-2009";
 const char *source_version_str = "20091226";
