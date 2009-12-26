4b7e443b73f3fd53e1f6d9aec653596b7a35cfbb
George Williams <pfaedit@users.sourceforge.net>
2009-12-26 20:50:41 +0000

    Patch by Michal Nowakowski to fix an autohinting problem with hint masks.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1261860337L;
-const char *source_modtime_str = "20:45 GMT 26-Dec-2009";
+const time_t source_modtime = 1261860675L;
+const char *source_modtime_str = "20:51 GMT 26-Dec-2009";
 const char *source_version_str = "20091226";
