ba795d5dee923f239771754f3eec6dda18cab295
pfaedit <pfaedit>
2009-12-31 04:46:02 +0000

    Unicode 5.2

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1261868649L;
-const char *source_modtime_str = "23:04 GMT 26-Dec-2009";
-const char *source_version_str = "20091226";
+const time_t source_modtime = 1262228026L;
+const char *source_modtime_str = "02:53 GMT 31-Dec-2009";
+const char *source_version_str = "20091231";
