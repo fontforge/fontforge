23c2bbd577490253febea374995deb7949567926
pfaedit <pfaedit>
2009-12-31 21:07:29 +0000

    Oops. Samaritan moved from its proposed location. Remove the old.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1262228026L;
-const char *source_modtime_str = "02:53 GMT 31-Dec-2009";
+const time_t source_modtime = 1262292550L;
+const char *source_modtime_str = "20:49 GMT 31-Dec-2009";
 const char *source_version_str = "20091231";
