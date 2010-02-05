75afe462b12204451b6551cc4a378c3e9acc61ca
pfaedit <pfaedit>
2010-02-05 14:59:22 +0000

    At Kevin Fenzi's request warn about use of seac instructions in type2 charstrings. Similarly warn about other type1 but not type2 operators (we warned about some. I think I'm now warning about all).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1265300633L;
-const char *source_modtime_str = "16:23 GMT 4-Feb-2010";
-const char *source_version_str = "20100204";
+const time_t source_modtime = 1265381842L;
+const char *source_modtime_str = "14:57 GMT 5-Feb-2010";
+const char *source_version_str = "20100205";
