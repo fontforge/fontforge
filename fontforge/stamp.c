b45c7809e26e0e6d6789c1c623dac2c01dd04ef9
pfaedit <pfaedit>
2010-02-07 19:20:59 +0000

    On 64 bit machines, in a contextual chaining display, nested lookups were not shown by name.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1265565908L;
-const char *source_modtime_str = "18:05 GMT 7-Feb-2010";
+const time_t source_modtime = 1265570262L;
+const char *source_modtime_str = "19:17 GMT 7-Feb-2010";
 const char *source_version_str = "20100207";
