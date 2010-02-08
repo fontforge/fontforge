27a55dba4bb2515405032db7cde096fc3c60c9ce
George Williams <pfaedit@users.sourceforge.net>
2010-02-08 04:46:23 +0000

    When doing a Rename to Namelist operation prior to generating a font, FF neglected to clear the glyph-name hash table. This meant that any glyph lookups by name on renamed glyphs would fail. Meaning we would inexplicably lose substitutions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1265570262L;
-const char *source_modtime_str = "19:17 GMT 7-Feb-2010";
-const char *source_version_str = "20100207";
+const time_t source_modtime = 1265603670L;
+const char *source_modtime_str = "04:34 GMT 8-Feb-2010";
+const char *source_version_str = "20100208";
