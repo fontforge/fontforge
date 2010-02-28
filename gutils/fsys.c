ef38d4660f650aa452ec5c9795eda691ff5e0138
pfaedit <pfaedit>
2010-02-28 03:04:18 +0000

    Sigh. And one other place.

--- gutils/fsys.c
+++ gutils/fsys.c
@@ -395,8 +395,8 @@ unichar_t *u_GFileGetAbsoluteName(unichar_t *name, unichar_t *result, int rsiz) 
 	    for ( pt = spt; *pt!='\0' && *pt!='/'; ++pt );
 	    if ( pt==spt )	/* Found // in a path spec, reduce to / (we've*/
 		u_strcpy(spt,pt); /*  skipped past the :// of the machine name) */
-	    else if ( pt==spt+1 && spt[0]=='.' )	/* Noop */
-		u_strcpy(spt,pt);
+	    else if ( pt==spt+1 && spt[0]=='.' && *pt=='/' ) {	/* Noop */
+		u_strcpy(spt,spt+2);
 	    else if ( pt==spt+2 && spt[0]=='.' && spt[1]=='.' ) {
 		for ( bpt=spt-2 ; bpt>rpt && *bpt!='/'; --bpt );
 		if ( bpt>=rpt && *bpt=='/' ) {
