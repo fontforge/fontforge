259d7188e67d42e5eee283bd90bd5b6e6eeb9b03
George Williams <pfaedit@users.sourceforge.net>
2010-04-09 21:26:53 +0000

    Some time ago MS put in a patch to their OS so that they would refuse to load a font with a name table >5K. They have now decided that was an error http://support.microsoft.com/kb/978909/ And have removed the limitation.
    So remove the warning ff used to generate about name tables bigger than 5K.

--- fontforge/fontinfo.c
+++ fontforge/fontinfo.c
@@ -3725,12 +3725,15 @@ static void StoreTTFNames(struct gfi_data *d) {
 	if ( strings[3*i+2].u.md_str!=NULL )
 	    len += 2*utf8_strlen(strings[3*i+2].u.md_str);
     }
+#if 0
+    /* Windows has decided that this was an error */
     if ( len>=5*1024 )
 	ff_post_notice(_("Name table too big for windows"),
 		    _("Windows has decided that fonts with 'name' tables\n"
 		    "bigger than 5K are insecure and will refuse to load\n"
 		    "them. Don't ask me why they believe this.\n"
 		    "This font will have a 'name' table bigger than that."));
+#endif
     
     TTF_PSDupsDefault(sf);
 }
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1270847900L;
-const char *source_modtime_str = "21:18 GMT 9-Apr-2010";
+const time_t source_modtime = 1270848169L;
+const char *source_modtime_str = "21:22 GMT 9-Apr-2010";
 const char *source_version_str = "20100409";
