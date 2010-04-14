3b2c686fcd65c8ca83d8826fbb1c8061584387ee
George Williams <pfaedit@users.sourceforge.net>
2010-04-14 23:46:39 +0000

    Three bugs in reading feature files: 1) FF dereference garbage (and probably, but not necessarily) crash when parsing a glyph class with no glyphs in it inside an anchor specification. 2) When freeing data describing a list of anchors FF would crash if one of the anchors were defined to be NULL 3) A multiple substitution entry with just a single element in it looks exactly like a single substitution.  FF would complain about multiple substitution lookups containing both single and multiple entries.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271282984L;
-const char *source_modtime_str = "22:09 GMT 14-Apr-2010";
+const time_t source_modtime = 1271288468L;
+const char *source_modtime_str = "23:41 GMT 14-Apr-2010";
 const char *source_version_str = "20100414";
