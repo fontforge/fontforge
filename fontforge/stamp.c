86a92d6fd5a58ca0dea65750fae6a2cc1e5a85da
George Williams <pfaedit@users.sourceforge.net>
2010-04-14 22:12:47 +0000

    Hot keys for the grid fit Bigger/Smaller pointsize menu commands got lost when I merged in the mingw changes. (Not saying the fault lies with the mingw people, that's just when they vanished. I might have removed them myself).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271281997L;
-const char *source_modtime_str = "21:53 GMT 14-Apr-2010";
+const time_t source_modtime = 1271282984L;
+const char *source_modtime_str = "22:09 GMT 14-Apr-2010";
 const char *source_version_str = "20100414";
