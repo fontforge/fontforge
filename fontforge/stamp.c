24ddf90ffba8e1995a5cd54eea3c3cee81da3a99
George Williams <pfaedit@users.sourceforge.net>
2010-04-14 21:03:07 +0000

    FF crashes if asked to produce a small-caps variant of a lower case letter for which there is no corresponding upper case.
    Patch from Rogerio Brito via debian.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271017295L;
-const char *source_modtime_str = "20:21 GMT 11-Apr-2010";
-const char *source_version_str = "20100411";
+const time_t source_modtime = 1271278793L;
+const char *source_modtime_str = "20:59 GMT 14-Apr-2010";
+const char *source_version_str = "20100414";
