8108baaab6c336329f2f2751c78a25227a7c1fef
pfaedit <pfaedit>
2010-04-15 16:51:50 +0000

    The default __iter__ for a selection type (in python) didn't work. It was supposed to return all the selected encoding slots (that is, numbers) instead it returned the empty set (when I tested it, actually it's return would be random depending on an uninitialized variable).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271348251L;
-const char *source_modtime_str = "16:17 GMT 15-Apr-2010";
+const time_t source_modtime = 1271350050L;
+const char *source_modtime_str = "16:47 GMT 15-Apr-2010";
 const char *source_version_str = "20100415";
