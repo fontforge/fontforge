b7ed2026ffd94c7f88e0cfac8fbbaf6d21473ff0
pfaedit <pfaedit>
2010-04-15 16:17:20 +0000

    Barry thinks an error message should be improved.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271328157L;
-const char *source_modtime_str = "10:42 GMT 15-Apr-2010";
+const time_t source_modtime = 1271348251L;
+const char *source_modtime_str = "16:17 GMT 15-Apr-2010";
 const char *source_version_str = "20100415";
