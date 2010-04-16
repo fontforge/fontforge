24bc1e481c8b1a18eb6bc491ceed2c0d228c31f6
pfaedit <pfaedit>
2010-04-16 02:28:42 +0000

    Typo in code for parsing extenders in tfm file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271383661L;
-const char *source_modtime_str = "02:07 GMT 16-Apr-2010";
+const time_t source_modtime = 1271384873L;
+const char *source_modtime_str = "02:27 GMT 16-Apr-2010";
 const char *source_version_str = "20100416";
