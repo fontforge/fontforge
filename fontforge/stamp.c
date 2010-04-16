533002d8a874f110927bb416796b74a646bbc8ee
George Williams <pfaedit@users.sourceforge.net>
2010-04-16 02:18:04 +0000

    The python operators which tested if two contours were equivalent would crash when called. Add a "dup()" member to Points, Layers and Contours to produce a deep copy of the object.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271363716L;
-const char *source_modtime_str = "20:35 GMT 15-Apr-2010";
-const char *source_version_str = "20100415";
+const time_t source_modtime = 1271383661L;
+const char *source_modtime_str = "02:07 GMT 16-Apr-2010";
+const char *source_version_str = "20100416";
