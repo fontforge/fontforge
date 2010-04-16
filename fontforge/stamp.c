7a0106895c77e82ad80dd2e45ed614f5ad96eacf
pfaedit <pfaedit>
2010-04-16 21:55:14 +0000

    FF failed to put a trailing NUL on some strings for glyph classes resulting in strange displays (and complaints from pango about bad utf8 strings).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271454063L;
-const char *source_modtime_str = "21:41 GMT 16-Apr-2010";
+const time_t source_modtime = 1271454819L;
+const char *source_modtime_str = "21:53 GMT 16-Apr-2010";
 const char *source_version_str = "20100416";
