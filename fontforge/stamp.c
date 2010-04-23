0bf666e4aa34f9cad2f34cc7b3578179163bac19
pfaedit <pfaedit>
2010-04-23 02:40:24 +0000

    Speed up approximating a spline given a bunch of on-curve points and slopes at the endpoints. I have been trying for years to figure out how to apply least-squares to this problem. I kept having 4 equations and 2 unknowns. An over specified system with no solutions. I realized last night that I was looking at the problem wrong (of course). Instead of seperating it into x and y components and minimizing errors in each, I should have been minimizing the sum of the x and y errors. Simple. Worked first try. Makes stroking about 10-15 times faster.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1271988817L;
-const char *source_modtime_str = "02:13 GMT 23-Apr-2010";
+const time_t source_modtime = 1271989867L;
+const char *source_modtime_str = "02:31 GMT 23-Apr-2010";
 const char *source_version_str = "20100423";
