0b69e3046b2295748a6e1c86a2c5c1c4f02f469d
pfaedit <pfaedit>
2010-04-28 19:27:20 +0000

    Khaled would like access to the math constant table from python.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1272414413L;
-const char *source_modtime_str = "00:26 GMT 28-Apr-2010";
+const time_t source_modtime = 1272482016L;
+const char *source_modtime_str = "19:13 GMT 28-Apr-2010";
 const char *source_version_str = "20100428";
