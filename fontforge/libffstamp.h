528e28c1785369e1c896ac431e4a5cd7190ef05f
George Williams <pfaedit@users.sourceforge.net>
2010-04-29 00:26:43 +0000

    Khaled also wants the per-glyph math tabled data, like glyph variants, construction and math kerning.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1272482013L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"19:13 GMT 28-Apr-2010"
-#define LibFF_VersionDate	20100428	/* Year, month, day */
+#define LibFF_ModTime		1272500371L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"00:19 GMT 29-Apr-2010"
+#define LibFF_VersionDate	20100429	/* Year, month, day */
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1272482016L;
-const char *source_modtime_str = "19:13 GMT 28-Apr-2010";
-const char *source_version_str = "20100428";
+const time_t source_modtime = 1272500374L;
+const char *source_modtime_str = "00:19 GMT 29-Apr-2010";
+const char *source_version_str = "20100429";
