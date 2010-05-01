708fb8c2688c5d90e8a0890b9b000d5e30961aef
michal-n <michal-n>
2010-05-01 08:07:11 +0000

    Patch by Khaled Hosny: fix invalid unicode message string.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1272512609L;
-const char *source_modtime_str = "03:43 GMT 29-Apr-2010";
-const char *source_version_str = "20100429";
+const time_t source_modtime = 1272701125L;
+const char *source_modtime_str = "08:05 GMT 1-May-2010";
+const char *source_version_str = "20100501";
