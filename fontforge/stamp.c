e55a585b523119ec704a3de6f459f7b30b6086b7
pfaedit <pfaedit>
2010-05-10 23:16:04 +0000

    Further improvements to stroking (subtle and technical, not likely to be noticeable). 1) Fixups to least squares when it gives bad results (slopes diametrically opposed to what they should be). 2) Was calculating t values incorrectly (slightly) when building TPoints to appr oximate. 3) On circular joins could get very few intermediate points on circular joins, try to fix this by adding more points.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1272798321L;
-const char *source_modtime_str = "11:05 GMT 2-May-2010";
-const char *source_version_str = "20100502";
+const time_t source_modtime = 1273532378L;
+const char *source_modtime_str = "22:59 GMT 10-May-2010";
+const char *source_version_str = "20100510";
