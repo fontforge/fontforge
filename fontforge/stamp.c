6b5909d6a9783d605eb0430e5306a8080877d7e1
pfaedit <pfaedit>
2010-05-11 02:15:54 +0000

    Add a miterlimit to stroking. So turn miters into bevels if they get tighter than about 18 degrees.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1273532378L;
-const char *source_modtime_str = "22:59 GMT 10-May-2010";
-const char *source_version_str = "20100510";
+const time_t source_modtime = 1273544059L;
+const char *source_modtime_str = "02:14 GMT 11-May-2010";
+const char *source_version_str = "20100511";
