ded86bebd59682e104deb7f18d77d9907e7dfc37
pfaedit <pfaedit>
2010-05-12 20:41:24 +0000

    Had a problem with mitered joins. My algorithm didn't work because the miter itself can produce a large spike which covers (hides) all sorts of things. Solution: keep a list of miter polygons, and see what they cover as well as what gets covered by the conventional circular pen. Square line caps have a similar problem (though much less severe) and can be fixed by a similar solution. Outstanding bug with butt caps: Say we have a contour which twists around so that its expanded edge comes back to within radius of the end point -- but in the direction that the cap is a butt (so no coverage). Then we are going to think that the end-point will cover part of the contour's edge (since a full circular pen would), while in truth the end-point does not extend in that direction. Similar problem (but less severe) for bevel joins.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1273544059L;
-const char *source_modtime_str = "02:14 GMT 11-May-2010";
-const char *source_version_str = "20100511";
+const time_t source_modtime = 1273696029L;
+const char *source_modtime_str = "20:27 GMT 12-May-2010";
+const char *source_version_str = "20100512";
