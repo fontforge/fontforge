198599131f2c3cf46e8ba056bca19a004f652a44
michal-n <michal-n>
2010-06-16 07:38:58 +0000

    Patch from Khaled Hosny: instead of dumping all OtfFeatName's into a single line, dump each on a line of its own, this make VCS diffs more readable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1276259917L;
-const char *source_modtime_str = "12:38 GMT 11-Jun-2010";
-const char *source_version_str = "20100611";
+const time_t source_modtime = 1276673723L;
+const char *source_modtime_str = "07:35 GMT 16-Jun-2010";
+const char *source_version_str = "20100616";
