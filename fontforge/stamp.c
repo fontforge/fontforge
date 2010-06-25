cbdeca16c978be641b055971cc9ad5e8ae1526de
michal-n <michal-n>
2010-06-25 06:31:07 +0000

    New Russian translation from Alexandre.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1276679336L;
-const char *source_modtime_str = "09:08 GMT 16-Jun-2010";
-const char *source_version_str = "20100616";
+const time_t source_modtime = 1277447393L;
+const char *source_modtime_str = "06:29 GMT 25-Jun-2010";
+const char *source_version_str = "20100625";
