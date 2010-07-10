9a7839730ad66c49ead772343db0177ca120cd0f
akryukov <akryukov>
2010-07-10 00:35:48 +0000

    A fix for the previous patch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1278721860L;
-const char *source_modtime_str = "00:31 GMT 10-Jul-2010";
+const time_t source_modtime = 1278722101L;
+const char *source_modtime_str = "00:35 GMT 10-Jul-2010";
 const char *source_version_str = "20100710";
