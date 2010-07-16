6c809eda860321f6bf3ae7a22d9b0ecb73d963cb
George Williams <pfaedit@users.sourceforge.net>
2010-07-16 20:56:23 +0000

    fixes for remove overlap.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1279210598L;
-const char *source_modtime_str = "16:16 GMT 15-Jul-2010";
-const char *source_version_str = "20100715";
+const time_t source_modtime = 1279308643L;
+const char *source_modtime_str = "19:30 GMT 16-Jul-2010";
+const char *source_version_str = "20100716";
