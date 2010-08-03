6e4d4feeafd02122af649e2a20eb03cfb96b850d
akryukov <akryukov>
2010-08-03 09:00:19 +0000

    Make the head_optimized_for_cleartype flag accessible via Python interface.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1280820677L;
-const char *source_modtime_str = "07:31 GMT 3-Aug-2010";
+const time_t source_modtime = 1280825765L;
+const char *source_modtime_str = "08:56 GMT 3-Aug-2010";
 const char *source_version_str = "20100803";
