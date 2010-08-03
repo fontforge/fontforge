8c1d0583318e6778e615e1725a14787a4d331fe3
akryukov <akryukov>
2010-08-03 07:34:20 +0000

    Forgot an old difference between my metricsview.c and the CVS version. Now revert it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1280818548L;
-const char *source_modtime_str = "06:55 GMT 3-Aug-2010";
+const time_t source_modtime = 1280820677L;
+const char *source_modtime_str = "07:31 GMT 3-Aug-2010";
 const char *source_version_str = "20100803";
