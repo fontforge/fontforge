3167ed707385e9de0df64f4980b498be024b0b1f
michal-n <michal-n>
2010-08-15 09:03:05 +0000

    Patch by Khaled Hosny to allow setting default font file base name from python.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1281191923L;
-const char *source_modtime_str = "14:38 GMT 7-Aug-2010";
-const char *source_version_str = "20100807";
+const time_t source_modtime = 1281862734L;
+const char *source_modtime_str = "08:58 GMT 15-Aug-2010";
+const char *source_version_str = "20100815";
