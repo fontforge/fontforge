b402ce606e505830f27d3b9c88fd8c8e0708d5e5
michal-n <michal-n>
2010-09-05 09:20:36 +0000

    Khaled Hosny found an infinite loop while processing certain glyphs.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1281862734L;
-const char *source_modtime_str = "08:58 GMT 15-Aug-2010";
-const char *source_version_str = "20100815";
+const time_t source_modtime = 1283678402L;
+const char *source_modtime_str = "09:20 GMT 5-Sep-2010";
+const char *source_version_str = "20100905";
