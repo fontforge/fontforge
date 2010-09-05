d8f5a5eb19d9149c05fcc9f4f9b326243c791938
michal-n <michal-n>
2010-09-05 09:25:19 +0000

    Khaled Hosny found that deleting the last anchor in a glyph causes a crash.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1283678402L;
-const char *source_modtime_str = "09:20 GMT 5-Sep-2010";
+const time_t source_modtime = 1283678644L;
+const char *source_modtime_str = "09:24 GMT 5-Sep-2010";
 const char *source_version_str = "20100905";
