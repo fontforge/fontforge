b9cf26aeda787cdd9bbfc5462bf99ec659a0842f
michal-n <michal-n>
2010-09-12 08:27:10 +0000

    Patch by Paul Flo Williams to fix a crash when attempting to edit glyphs in Unicode Plane 16.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1284279615L;
-const char *source_modtime_str = "08:20 GMT 12-Sep-2010";
+const time_t source_modtime = 1284279997L;
+const char *source_modtime_str = "08:26 GMT 12-Sep-2010";
 const char *source_version_str = "20100912";
