30d603ae07dd4967ff89850af4fa2cac192200fb
michal-n <michal-n>
2010-09-14 07:04:05 +0000

    Patch by Khaled Hosny to prevent crash when adding certain lookup types.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1284280151L;
-const char *source_modtime_str = "08:29 GMT 12-Sep-2010";
-const char *source_version_str = "20100912";
+const time_t source_modtime = 1284447797L;
+const char *source_modtime_str = "07:03 GMT 14-Sep-2010";
+const char *source_version_str = "20100914";
