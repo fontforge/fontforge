2ac46fe71122cf23f5cbaaa549a4aa4d9d74ed5f
michal-n <michal-n>
2010-09-21 11:15:16 +0000

    Fixed crashes when deleting anchor points from anchor control window. Spotted by Khaled Hosny.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1285054427L;
-const char *source_modtime_str = "07:33 GMT 21-Sep-2010";
+const time_t source_modtime = 1285067614L;
+const char *source_modtime_str = "11:13 GMT 21-Sep-2010";
 const char *source_version_str = "20100921";
