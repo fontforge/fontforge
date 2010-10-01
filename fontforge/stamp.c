1c6c08c83bf446c62a5905b8aedf020d0195ca35
michal-n <michal-n>
2010-10-01 08:23:10 +0000

    Khaled Hosny noticed that deleting anchors from Anchor Info dialog didn't mark glyphs affected as changed.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1285491965L;
-const char *source_modtime_str = "09:06 GMT 26-Sep-2010";
-const char *source_version_str = "20100926";
+const time_t source_modtime = 1285921281L;
+const char *source_modtime_str = "08:21 GMT 1-Oct-2010";
+const char *source_version_str = "20101001";
