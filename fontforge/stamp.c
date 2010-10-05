314210b9fe28bcd9c5e25d73066917641c469b7d
michal-n <michal-n>
2010-10-05 17:13:32 +0000

    Patch by Khaled Hosny, who noticed that OpenType glyph positioning is applied from left to right even for right to left scripts. As he writes: 'This patch fixes the kerning pairs dialog to render kerning as close to what OpenType engines does, also makes sure FF activates by default and adjusts the correct fields. There are other places in the code that need to be checked too, but I think the code affected by this patch is self contained.'

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1285921281L;
-const char *source_modtime_str = "08:21 GMT 1-Oct-2010";
-const char *source_version_str = "20101001";
+const time_t source_modtime = 1286296600L;
+const char *source_modtime_str = "16:36 GMT 5-Oct-2010";
+const char *source_version_str = "20101005";
