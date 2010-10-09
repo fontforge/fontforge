9ff989f955c7880f45c83ac4bede8532ab9adeb8
michal-n <michal-n>
2010-10-09 17:25:38 +0000

    Patch by Keith Stribley, to fix crashing lookup metadata that don't have scripts specified - he points that AAT doesn't need them.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1286644630L;
-const char *source_modtime_str = "17:17 GMT 9-Oct-2010";
+const time_t source_modtime = 1286644932L;
+const char *source_modtime_str = "17:22 GMT 9-Oct-2010";
 const char *source_version_str = "20101009";
