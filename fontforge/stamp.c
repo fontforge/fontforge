dc11feb729ca17202b97c9e6a4d6713ebaaadf6e
michal-n <michal-n>
2010-10-09 17:56:03 +0000

    Patch by Keith Stribley, to fix export of Mac features, though it may not cover every case.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1286646747L;
-const char *source_modtime_str = "17:52 GMT 9-Oct-2010";
+const time_t source_modtime = 1286646857L;
+const char *source_modtime_str = "17:54 GMT 9-Oct-2010";
 const char *source_version_str = "20101009";
