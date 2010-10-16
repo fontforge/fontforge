634228f81083988394032487b48f1fe73ed6a38e
michal-n <michal-n>
2010-10-16 09:28:18 +0000

    Patch by Khaled Hosny, to update Unicode support to Unicode 6.0.0

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1286646857L;
-const char *source_modtime_str = "17:54 GMT 9-Oct-2010";
-const char *source_version_str = "20101009";
+const time_t source_modtime = 1287221177L;
+const char *source_modtime_str = "09:26 GMT 16-Oct-2010";
+const char *source_version_str = "20101016";
