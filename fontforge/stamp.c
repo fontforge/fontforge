a239618903c3ba95222d6314dff67dd9905e89da
michal-n <michal-n>
2010-11-21 19:24:56 +0000

    Fontforge forgot to categorize points after importing a svg font.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1289761466L;
-const char *source_modtime_str = "19:04 GMT 14-Nov-2010";
-const char *source_version_str = "20101114";
+const time_t source_modtime = 1290367441L;
+const char *source_modtime_str = "19:24 GMT 21-Nov-2010";
+const char *source_version_str = "20101121";
