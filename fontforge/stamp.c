09551996224a79d45c0573ec0b592d6029c52a8f
michal-n <michal-n>
2010-11-28 09:55:29 +0000

    Patch by Philipp Stephani, to fix wrong interpretation of most top accent data for newer versions of Cambria Math. Thanks to Khaled Hosny, who sent me the patch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1290937703L;
-const char *source_modtime_str = "09:48 GMT 28-Nov-2010";
+const time_t source_modtime = 1290937949L;
+const char *source_modtime_str = "09:52 GMT 28-Nov-2010";
 const char *source_version_str = "20101128";
