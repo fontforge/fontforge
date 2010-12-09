f78ea9f887bb6953d10746230389367124c7b5ac
michal-n <michal-n>
2010-12-09 09:50:19 +0000

    Patch by Barry Schwartz to fix error in handling of glyph name ranges in feature files, reported by Matthew Skala:

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1291546502L;
-const char *source_modtime_str = "10:55 GMT 5-Dec-2010";
-const char *source_version_str = "20101205";
+const time_t source_modtime = 1291888081L;
+const char *source_modtime_str = "09:48 GMT 9-Dec-2010";
+const char *source_version_str = "20101209";
