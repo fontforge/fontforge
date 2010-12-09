0cdbcb1002a6e3e0781e9bb277c1452b5ac32bdd
michal-n <michal-n>
2010-12-09 10:16:14 +0000

    There wasn't a shortcut to scroll vertically in charview, in case the mouse had only vertical scroll. Now it's Ctrl+VScroll.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1291888081L;
-const char *source_modtime_str = "09:48 GMT 9-Dec-2010";
+const time_t source_modtime = 1291889622L;
+const char *source_modtime_str = "10:13 GMT 9-Dec-2010";
 const char *source_version_str = "20101209";
