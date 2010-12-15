3dcb5f9f4abe83df26d4b26a5bf79af926985a28
pfaedit <pfaedit>
2010-12-15 21:35:47 +0000

    Remove one source of rounding errors in solving cubics. Cleanup function calls to match new calling conventions.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1291920759L;
-const char *source_modtime_str = "18:52 GMT 9-Dec-2010";
-const char *source_version_str = "20101209";
+const time_t source_modtime = 1292448895L;
+const char *source_modtime_str = "21:34 GMT 15-Dec-2010";
+const char *source_version_str = "20101215";
