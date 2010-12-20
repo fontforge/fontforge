b13755096df78c7e0106255c766616c05baec998
pfaedit <pfaedit>
2010-12-20 22:35:50 +0000

    Michael found an infinite loop bug in rmoverlap, but as I look at the code, the fix will fail in some cases, so be a try a little harder if needed...

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1292877406L;
-const char *source_modtime_str = "20:36 GMT 20-Dec-2010";
+const time_t source_modtime = 1292883587L;
+const char *source_modtime_str = "22:19 GMT 20-Dec-2010";
 const char *source_version_str = "20101220";
