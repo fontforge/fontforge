a8edcf189b2f513d8511d4022b07c04118bb54f6
michal-n <michal-n>
2010-12-31 15:47:13 +0000

    Khaled Hosny fixed some errors in fontforge.desktop file.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1293239925L;
-const char *source_modtime_str = "01:18 GMT 25-Dec-2010";
-const char *source_version_str = "20101225";
+const time_t source_modtime = 1293810383L;
+const char *source_modtime_str = "15:46 GMT 31-Dec-2010";
+const char *source_version_str = "20101231";
