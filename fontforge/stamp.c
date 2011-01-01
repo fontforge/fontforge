ee817a5466a52a8ec070dcaac4685e5177dd5001
George Williams <pfaedit@users.sourceforge.net>
2011-01-01 01:18:26 +0000

    Bit more work on freehand tool.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1293819231L;
-const char *source_modtime_str = "18:13 GMT 31-Dec-2010";
-const char *source_version_str = "20101231";
+const time_t source_modtime = 1293844575L;
+const char *source_modtime_str = "01:16 GMT 1-Jan-2011";
+const char *source_version_str = "20110101";
