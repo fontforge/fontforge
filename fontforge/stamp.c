63f6d706fcdb3b2844aa84dab376c5768eb5a0bd
pfaedit <pfaedit>
2011-01-09 17:31:23 +0000

    More work on remove overlap (or find intersections actually). If an intersection is within 16 rounding errors of the end of the spline pretend it is at the end. Multiple intersections that are too close mean we get problems with rounding errors when looking for winding numbers.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1294171345L;
-const char *source_modtime_str = "20:02 GMT 4-Jan-2011";
-const char *source_version_str = "20110104";
+const time_t source_modtime = 1294594004L;
+const char *source_modtime_str = "17:26 GMT 9-Jan-2011";
+const char *source_version_str = "20110109";
