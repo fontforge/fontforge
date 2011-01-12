d1e824d3677f79dd961a258d55ab278567e681fc
George Williams <pfaedit@users.sourceforge.net>
2011-01-12 23:28:40 +0000

    Daniel Johnson sent me a print sample of Mayan in the K'iche' dialect used in Guatemala from the prolog to the Popol Wuj.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1294871408L;
-const char *source_modtime_str = "22:30 GMT 12-Jan-2011";
+const time_t source_modtime = 1294874725L;
+const char *source_modtime_str = "23:25 GMT 12-Jan-2011";
 const char *source_version_str = "20110112";
