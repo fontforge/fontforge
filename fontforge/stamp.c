45433ad0f3bbaa4cf68c6565d5df44f5143f5e9b
pfaedit <pfaedit>
2011-01-13 03:13:05 +0000

    Legacy scripting did not support importing pdf files.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1294887231L;
-const char *source_modtime_str = "02:53 GMT 13-Jan-2011";
+const time_t source_modtime = 1294888189L;
+const char *source_modtime_str = "03:09 GMT 13-Jan-2011";
 const char *source_version_str = "20110113";
