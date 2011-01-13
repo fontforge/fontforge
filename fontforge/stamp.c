8d004c4eb149cd2fbf3a95932595c786c64471df
pfaedit <pfaedit>
2011-01-13 21:16:51 +0000

    More problems with zero length splines in expand stroke. Biggest problem was that the routine which was suppposed to remove them didn't (at least in some cases).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1294948665L;
-const char *source_modtime_str = "19:57 GMT 13-Jan-2011";
+const time_t source_modtime = 1294953013L;
+const char *source_modtime_str = "21:10 GMT 13-Jan-2011";
 const char *source_version_str = "20110113";
