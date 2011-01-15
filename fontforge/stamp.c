e208efe1279a9cd18d5d13f1e20dec24a41de99e
michal-n <michal-n>
2011-01-15 11:56:19 +0000

    Updated Polish translation.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295091663L;
-const char *source_modtime_str = "11:41 GMT 15-Jan-2011";
+const time_t source_modtime = 1295092550L;
+const char *source_modtime_str = "11:55 GMT 15-Jan-2011";
 const char *source_version_str = "20110115";
