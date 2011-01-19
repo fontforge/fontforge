cfc9321fcb4553fb1848d4ae76f8316c35337974
George Williams <pfaedit@users.sourceforge.net>
2011-01-19 21:08:50 +0000

    More work on remove overlap when all data are doubles.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295469489L;
-const char *source_modtime_str = "20:38 GMT 19-Jan-2011";
+const time_t source_modtime = 1295471152L;
+const char *source_modtime_str = "21:05 GMT 19-Jan-2011";
 const char *source_version_str = "20110119";
