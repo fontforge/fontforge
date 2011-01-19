133e26293c8f805134399cdc15790f6dd17b109e
pfaedit <pfaedit>
2011-01-19 20:39:31 +0000

    A slight change which makes no difference (that I've seen) when using floats, but which improve things in some cases when using doubles.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295389669L;
-const char *source_modtime_str = "22:27 GMT 18-Jan-2011";
-const char *source_version_str = "20110118";
+const time_t source_modtime = 1295469489L;
+const char *source_modtime_str = "20:38 GMT 19-Jan-2011";
+const char *source_version_str = "20110119";
