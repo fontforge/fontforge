d13022b0ec8b53de6a86191b40920453774ced22
George Williams <pfaedit@users.sourceforge.net>
2011-01-20 16:40:47 +0000

    More fixes to remove overlap when using doubles. This one is a typo that's been there a long time.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295500824L;
-const char *source_modtime_str = "05:20 GMT 20-Jan-2011";
+const time_t source_modtime = 1295541372L;
+const char *source_modtime_str = "16:36 GMT 20-Jan-2011";
 const char *source_version_str = "20110120";
