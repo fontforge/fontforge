e3e491b3e8650d8427ed3a633ab37df22a528814
Alexey Kryukov <amkryukov@gmail.com>
2011-01-21 08:11:10 +0000

    Use FT_LOAD_TARGET_MONO instead of FT_LOAD_MONOCHROME for spline rasterization with freetype. This guarantees more accurate rendering of hinted fonts in monochrome mode.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295597026L;
-const char *source_modtime_str = "08:03 GMT 21-Jan-2011";
+const time_t source_modtime = 1295597342L;
+const char *source_modtime_str = "08:09 GMT 21-Jan-2011";
 const char *source_version_str = "20110121";
