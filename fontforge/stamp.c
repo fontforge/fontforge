f78cd989bb1c7bed09379e2868970980fee96d03
akryukov <akryukov>
2011-01-21 10:02:13 +0000

    Pasting bitmaps from fontview didn't work.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295599427L;
-const char *source_modtime_str = "08:43 GMT 21-Jan-2011";
+const time_t source_modtime = 1295604041L;
+const char *source_modtime_str = "10:00 GMT 21-Jan-2011";
 const char *source_version_str = "20110121";
