21428483f9f05134154c5aaf99cbdb8a680a4fb8
pfaedit <pfaedit>
2011-01-22 04:17:28 +0000

    Allow a user to enter values for the PS private dictionary in his/her own locale but convert them to the "C"==PostScript locale when storing them. That is a French user could enter [3,5 20] for BlueValues, but it needs to be converted to [3.5 20] when put in the sfd file (or PS font file).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295666302L;
-const char *source_modtime_str = "03:18 GMT 22-Jan-2011";
+const time_t source_modtime = 1295669663L;
+const char *source_modtime_str = "04:14 GMT 22-Jan-2011";
 const char *source_version_str = "20110122";
