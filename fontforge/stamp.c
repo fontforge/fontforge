21adfc22a566c9f6f39a070196dec4e24d20625d
George Williams <pfaedit@users.sourceforge.net>
2011-01-22 03:19:44 +0000

    tweak for greater precision.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295627361L;
-const char *source_modtime_str = "16:29 GMT 21-Jan-2011";
-const char *source_version_str = "20110121";
+const time_t source_modtime = 1295666302L;
+const char *source_modtime_str = "03:18 GMT 22-Jan-2011";
+const char *source_version_str = "20110122";
