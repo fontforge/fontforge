6c226e1fefae5e36f86f2319b968ee691142411b
pfaedit <pfaedit>
2011-01-22 21:21:44 +0000

    Change the Histogram dialog to use boxes (cosmetic change, shouldn't matter to English speakers, dlg won't have fields overrunning other fields in French).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295669663L;
-const char *source_modtime_str = "04:14 GMT 22-Jan-2011";
+const time_t source_modtime = 1295731125L;
+const char *source_modtime_str = "21:18 GMT 22-Jan-2011";
 const char *source_version_str = "20110122";
