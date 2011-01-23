d0f1a4e6d79dd63d06b925d93494a89778c0ab47
Alexey Kryukov <amkryukov@gmail.com>
2011-01-23 11:08:48 +0000

    Editing PS Private was broken after the recent patch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295751714L;
-const char *source_modtime_str = "03:01 GMT 23-Jan-2011";
+const time_t source_modtime = 1295780809L;
+const char *source_modtime_str = "11:06 GMT 23-Jan-2011";
 const char *source_version_str = "20110123";
