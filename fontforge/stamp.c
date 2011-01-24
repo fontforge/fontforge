2cb04a47100ef43ffd043ef447e38dc9f4284064
akryukov <akryukov>
2011-01-24 08:46:59 +0000

    A small correction to the previous patch.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1295858466L;
-const char *source_modtime_str = "08:41 GMT 24-Jan-2011";
+const time_t source_modtime = 1295858757L;
+const char *source_modtime_str = "08:45 GMT 24-Jan-2011";
 const char *source_version_str = "20110124";
