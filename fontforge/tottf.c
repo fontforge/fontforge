b1f91e7c1eb794ff3f5c4b297471b59f330adc7d
George Williams <pfaedit@users.sourceforge.net>
2011-02-10 21:30:38 +0000

    When ff calculated the size of a kerning subtable it was off by two bytes per glyph in the coverage table. This meant that it would sometimes make a table that was slightly too big to be supported and not realize it. I've fixed the bug, and put in checks so that if there's yet another problem like this, at list it will be reported to the user.

--- fontforge/tottf.c
+++ fontforge/tottf.c
@@ -506,6 +506,8 @@ return( (ch1<<24)|(ch2<<16)|(ch3<<8)|ch4 );
 }
 
 void putshort(FILE *file,int sval) {
+    if ( sval<-32768 || sval>65535 )
+	IError(_("Attempt to output %d into a 16-bit field. It will be truncated and the file may not be useful."), sval );
     putc((sval>>8)&0xff,file);
     putc(sval&0xff,file);
 }
