8bcc8b6d959224ea7cd5e1b5c915a437d001d9a2
Alexey Kryukov <amkryukov@gmail.com>
2011-02-11 11:40:53 +0000

    Still problems with vertical scrolling in matrix edits (the bug was introduced by my previous patch and occurs due to a wrong selection of the expose area: when doing vertical scrolling, the whole table, rather than just the current line, should be redrawn).
    Also FF used to set the list mark width (and related values) at the very end
    of the matrix edit control creation process, and so function indicators
    were disregarded when calculating the initial column widths.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1297387007L;
-const char *source_modtime_str = "01:16 GMT 11-Feb-2011";
+const time_t source_modtime = 1297423976L;
+const char *source_modtime_str = "11:32 GMT 11-Feb-2011";
 const char *source_version_str = "20110211";
