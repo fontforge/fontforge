97a941e2fc634a7412884e836d196563d4bc05d5
George Williams <pfaedit@users.sourceforge.net>
2011-02-11 01:23:20 +0000

    In the patch of 20 Dec 2010, a significant line was deleted by mistake. This broke Add Extrema. Reinstate it.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1297295727L;
-const char *source_modtime_str = "23:55 GMT 9-Feb-2011";
-const char *source_version_str = "20110209";
+const time_t source_modtime = 1297387007L;
+const char *source_modtime_str = "01:16 GMT 11-Feb-2011";
+const char *source_version_str = "20110211";
