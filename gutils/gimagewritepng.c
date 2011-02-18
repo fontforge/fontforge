81dd0e7e914a38bcb831c7b77a45413d6f1d231b
George Williams <pfaedit@users.sourceforge.net>
2011-02-18 09:49:47 -0800

    Barry points out a spot I missed in the libpng change the other day.

--- gutils/gimagewritepng.c
+++ gutils/gimagewritepng.c
@@ -324,7 +324,12 @@ return(false);
 return(false);
    }
 
-   if (setjmp(*_png_set_longjmp_fn(png_ptr, longjmp, sizeof (jmp_buf)))) {
+#if (PNG_LIBPNG_VER < 10500)
+    if (setjmp(png_ptr->jmpbuf))
+#else
+   if (setjmp(*_png_set_longjmp_fn(png_ptr, longjmp, sizeof (jmp_buf))))
+#endif
+   {
       png_destroy_write_struct(&png_ptr,  (png_infopp)NULL);
 return(false);
    }
