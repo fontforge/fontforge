bcb680ed200d39d60fd2ef393c102b086f5b870f
George Williams <pfaedit@users.sourceforge.net>
2011-02-21 12:38:23 -0800

    Provide time stamps for the release.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
-#define LibFF_ModTime		1298070200L	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	"23:03 GMT 18-Feb-2011"
-#define LibFF_VersionDate	20110218	/* Year, month, day */
+#define LibFF_ModTime		1298320590L	/* Seconds since 1970 (standard unix time) */
+#define LibFF_ModTime_Str	"20:36 GMT 21-Feb-2011"
+#define LibFF_VersionDate	20110221	/* Year, month, day */
--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1298070206L;
-const char *source_modtime_str = "23:03 GMT 18-Feb-2011";
-const char *source_version_str = "20110218";
+const time_t source_modtime = 1298320590L;
+const char *source_modtime_str = "20:36 GMT 21-Feb-2011";
+const char *source_version_str = "20110221";
