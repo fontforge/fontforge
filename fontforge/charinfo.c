48405152be6dac5b37d00b1ff30d2eb2e4585246
michal-n <michal-n@users.sourceforge.net>
2011-12-17 15:00:30 +0100

    Patch by Khaled Hosny to fix the following problem:
    > When closing Glyph Info window, if the glyph name or encoding is already
    > used a confirmation dialogue will be opened, clicking Cancel will crash
    > FontForge. Looks like there is a typo in _CI_OK() returning true when it
    > should return false.

--- fontforge/charinfo.c
+++ fontforge/charinfo.c
@@ -1518,7 +1518,7 @@ return( false );
 	free( accentdevtab );
 	free( italicdevtab );
 	free(hicdt); free(vicdt);
-return( true );
+return( false );
     }
     ci->cachedsc->unlink_rm_ovrlp_save_undo = GGadgetIsChecked(GWidgetGetControl(ci->gw,CID_UnlinkRmOverlap));
     ci->cachedsc->glyph_class = GGadgetGetFirstListSelectedItem(GWidgetGetControl(ci->gw,CID_GClass));
