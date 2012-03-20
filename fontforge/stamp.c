5a60139d96a5f20ed42aaf1ffb4d00189e1ed466
michal-n <michal-n@users.sourceforge.net>
2012-03-20 20:41:38 +0100

    Patch from Khaled Hosny, to disable automatic setting "Is offset" field on in the font metrics. This confused users.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1332272297L;
-const char *source_modtime_str = "19:38 GMT 20-Mar-2012";
+const time_t source_modtime = 1332272476L;
+const char *source_modtime_str = "19:41 GMT 20-Mar-2012";
 const char *source_version_str = "20120320";
