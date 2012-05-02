10a7e232030ae3189f4b34de3537f953b57022f9
michal-n <michal-n@users.sourceforge.net>
2012-05-02 19:43:10 +0200

    Patch by Daniel Kahn Gillmor to finalize support for libpng 1.5.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1335980324L;
-const char *source_modtime_str = "17:38 GMT 2-May-2012";
+const time_t source_modtime = 1335980583L;
+const char *source_modtime_str = "17:43 GMT 2-May-2012";
 const char *source_version_str = "20120502";
