61e4d4cffac58351581099f6e3ccbe817a7690ba
michal-n <michal-n@users.sourceforge.net>
2012-05-02 14:17:15 +0200

    Fixed a typo spotted by Steve White (Devangari -> Devanagari).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1332273234L;
-const char *source_modtime_str = "19:53 GMT 20-Mar-2012";
-const char *source_version_str = "20120320";
+const time_t source_modtime = 1335960994L;
+const char *source_modtime_str = "12:16 GMT 2-May-2012";
+const char *source_version_str = "20120502";
