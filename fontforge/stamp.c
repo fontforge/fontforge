d4ec96901983a8d2941fab8b17e41a4ad49f226b
michal-n <michal-n@users.sourceforge.net>
2012-05-02 14:24:54 +0200

    Encoding ID should be set to 10 for fonts containing unicode characters outside of BMP. Patch by Steve White.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1335960994L;
-const char *source_modtime_str = "12:16 GMT 2-May-2012";
+const time_t source_modtime = 1335961308L;
+const char *source_modtime_str = "12:21 GMT 2-May-2012";
 const char *source_version_str = "20120502";
