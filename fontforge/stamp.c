e15ccfbd848869de8fd4c3c7b40c2b51116df4c6
michal-n <michal-n@users.sourceforge.net>
2012-05-03 19:42:20 +0200

    Path by Steve White to enable support for ASMO 708 codepage.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1335982481L;
-const char *source_modtime_str = "18:14 GMT 2-May-2012";
-const char *source_version_str = "20120502";
+const time_t source_modtime = 1336066931L;
+const char *source_modtime_str = "17:42 GMT 3-May-2012";
+const char *source_version_str = "20120503";
