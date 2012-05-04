7237d97575853a80064ecfa28ed2f2dffe52ed1c
michal-n <michal-n@users.sourceforge.net>
2012-05-04 12:46:33 +0200

    FontForge escaped 'mark' feature tag when exporting feature files. According to the specification, escaping is only for glyph names and CID indices. Spotted and patched by Khaled Hosny.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1336127857L;
-const char *source_modtime_str = "10:37 GMT 4-May-2012";
+const time_t source_modtime = 1336128384L;
+const char *source_modtime_str = "10:46 GMT 4-May-2012";
 const char *source_version_str = "20120504";
