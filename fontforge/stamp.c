2ead657589efe579b9a6cc03d575704ffc54e4eb
michal-n <michal-n@users.sourceforge.net>
2012-05-09 20:46:41 +0200

    FontForge didn't build on some systems where /bin/sh is synonymous with dash, yet the usual user shell is bash. Configure and libtool were thus using different shells, that turned out to be incompatible. Luigi Scarso suggested patching the makefiles with explicitly setting the SHELL variable.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1336578682L;
-const char *source_modtime_str = "15:51 GMT 9-May-2012";
+const time_t source_modtime = 1336589185L;
+const char *source_modtime_str = "18:46 GMT 9-May-2012";
 const char *source_version_str = "20120509";
