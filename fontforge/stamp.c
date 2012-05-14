f5d451c45853e2dcf160843bcf16bdde6d282be7
michal-n <michal-n@users.sourceforge.net>
2012-05-14 22:31:22 +0200

    Updated translations: - Chinese traditional, by 趙惟倫(Wei-Lun Chao, sf user: chaoweilun) - Polish, by Michał Nowakowski

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1337023489L;
-const char *source_modtime_str = "19:24 GMT 14-May-2012";
+const time_t source_modtime = 1337027473L;
+const char *source_modtime_str = "20:31 GMT 14-May-2012";
 const char *source_version_str = "20120514";
