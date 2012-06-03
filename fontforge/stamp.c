b92b1c94796ff8642d3c90cfbdd498186aa066ce
michal-n <michal-n@users.sourceforge.net>
2012-06-03 15:48:00 +0200

    Lee Chenhwa (leechenhwa) updated Simplified Chinese translation for FontForge.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1338716134L;
-const char *source_modtime_str = "09:35 GMT 3-Jun-2012";
+const time_t source_modtime = 1338731273L;
+const char *source_modtime_str = "13:47 GMT 3-Jun-2012";
 const char *source_version_str = "20120603";
