b70df9754a304a9ba647b46113a0a3700b49b302
michal-n <michal-n@users.sourceforge.net>
2012-06-03 16:28:20 +0200

    Patch by Khaled Hosny to handle new, unescaped syntax for mark lookup tags in feature files.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1338731273L;
-const char *source_modtime_str = "13:47 GMT 3-Jun-2012";
+const time_t source_modtime = 1338733307L;
+const char *source_modtime_str = "14:21 GMT 3-Jun-2012";
 const char *source_version_str = "20120603";
