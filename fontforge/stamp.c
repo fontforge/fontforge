cca7d79c451cab18bc343935ad58ac66438a0758
michal-n <michal-n@users.sourceforge.net>
2012-06-07 13:22:28 +0200

    Catalan translation of FontForge, following the guidelines of SoftCatalà. Contributed by Rafael Ferran i Peralta (Sourceforge user: rferran).

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1338733307L;
-const char *source_modtime_str = "14:21 GMT 3-Jun-2012";
-const char *source_version_str = "20120603";
+const time_t source_modtime = 1339068021L;
+const char *source_modtime_str = "11:20 GMT 7-Jun-2012";
+const char *source_version_str = "20120607";
