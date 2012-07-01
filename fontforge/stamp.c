5ab445d753e65e73decb0c9e7d9401456ed8da8c
michal-n <michal-n@users.sourceforge.net>
2012-07-01 11:28:56 +0200

    Patch by Akihiro Sagawa to add support for removing range attributes from os2_unicoderanges (or os2_codepages). Without this patch, once these Python properties are set, there is no way to call OS2FigureUnicodeRanges() (or OS2FigureUnicodeRanges()) before generation.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1341134551L;
-const char *source_modtime_str = "09:22 GMT 1-Jul-2012";
+const time_t source_modtime = 1341134931L;
+const char *source_modtime_str = "09:28 GMT 1-Jul-2012";
 const char *source_version_str = "20120701";
