13f96f150a3ec423d25b3c17f0ae0f931c94ed36
michal-n <michal-n@users.sourceforge.net>
2012-07-01 11:22:37 +0200

    Akihiro Sagawa points out a typo thet prevented setting os2_unicoderanges from Python script.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1340908553L;
-const char *source_modtime_str = "18:35 GMT 28-Jun-2012";
-const char *source_version_str = "20120628";
+const time_t source_modtime = 1341134551L;
+const char *source_modtime_str = "09:22 GMT 1-Jul-2012";
+const char *source_version_str = "20120701";
