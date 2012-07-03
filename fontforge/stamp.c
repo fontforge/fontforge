4cefee299cfbdce4f3e3c2b1a94f2f268e2c791b
michal-n <michal-n@users.sourceforge.net>
2012-07-03 19:22:33 +0200

    Very preliminary code to support UI icons with alpha channel.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1341134931L;
-const char *source_modtime_str = "09:28 GMT 1-Jul-2012";
-const char *source_version_str = "20120701";
+const time_t source_modtime = 1341336139L;
+const char *source_modtime_str = "17:22 GMT 3-Jul-2012";
+const char *source_version_str = "20120703";
