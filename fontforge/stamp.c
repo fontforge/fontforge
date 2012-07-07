66e941265a3fdba07508e49bae3a2db703ffb627
michal-n <michal-n@users.sourceforge.net>
2012-07-07 14:59:45 +0200

    Fixed a crash for fonts with kerning entries containing negative values. Thanks for Khaled Hosny for providing a font sample.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1341421874L;
-const char *source_modtime_str = "17:11 GMT 4-Jul-2012";
-const char *source_version_str = "20120704";
+const time_t source_modtime = 1341665978L;
+const char *source_modtime_str = "12:59 GMT 7-Jul-2012";
+const char *source_version_str = "20120707";
