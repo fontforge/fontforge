38c35ba025fe30d3e142dcf0e0acbbecd1a0c124
michal-n <michal-n@users.sourceforge.net>
2012-07-08 11:32:31 +0200

    My first effort at supporting icons with alpha channel was buggy. Getting background rect from X is supported only if it's contained within screen, so clipping was needed. Also, my blending routine worked only for null offsets. That's fixed now, I hope.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1341665978L;
-const char *source_modtime_str = "12:59 GMT 7-Jul-2012";
-const char *source_version_str = "20120707";
+const time_t source_modtime = 1341739943L;
+const char *source_modtime_str = "09:32 GMT 8-Jul-2012";
+const char *source_version_str = "20120708";
