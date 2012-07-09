e775a184e9b3c0cf411831748ae33a45f2e4dc25
michal-n <michal-n@users.sourceforge.net>
2012-07-09 21:56:01 +0200

    Add and use new icons by Severin Meyer.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1341854584L;
-const char *source_modtime_str = "17:23 GMT 9-Jul-2012";
+const time_t source_modtime = 1341863658L;
+const char *source_modtime_str = "19:54 GMT 9-Jul-2012";
 const char *source_version_str = "20120709";
