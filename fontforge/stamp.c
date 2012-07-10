ce3da5b8832b858ee1d50f131af385470c6fe583
michal-n <michal-n@users.sourceforge.net>
2012-07-10 18:39:17 +0200

    Patches from thread "Building fonts with optical size infos" by Khaled Hosny.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1341863658L;
-const char *source_modtime_str = "19:54 GMT 9-Jul-2012";
-const char *source_version_str = "20120709";
+const time_t source_modtime = 1341938340L;
+const char *source_modtime_str = "16:39 GMT 10-Jul-2012";
+const char *source_version_str = "20120710";
