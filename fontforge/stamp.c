e986e22891beb6df8c12d5c3ca2bb4cf91a31163
michal-n <michal-n@users.sourceforge.net>
2012-07-11 21:13:06 +0200

    I found a way to call InitToolIcons() from better place. Twis will eventually allow me to load all stuff of icons.c from external files.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1342023539L;
-const char *source_modtime_str = "16:18 GMT 11-Jul-2012";
+const time_t source_modtime = 1342033971L;
+const char *source_modtime_str = "19:12 GMT 11-Jul-2012";
 const char *source_version_str = "20120711";
