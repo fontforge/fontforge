5164aa29ca98c8dab756c6213c8f459aaba2a4b2
michal-n <michal-n@users.sourceforge.net>
2012-07-14 11:05:13 +0200

    Fixed a (probably) misplaced warning reported by Matthew Skala, displayed on openning an OpenType file with a design size and design range but no stylename.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1342033971L;
-const char *source_modtime_str = "19:12 GMT 11-Jul-2012";
-const char *source_version_str = "20120711";
+const time_t source_modtime = 1342256478L;
+const char *source_modtime_str = "09:01 GMT 14-Jul-2012";
+const char *source_version_str = "20120714";
