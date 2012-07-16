6ad7c66a1f34d5045e16f182d823205b42d6759f
michal-n <michal-n@users.sourceforge.net>
2012-07-16 18:45:52 +0200

    Fixed a crask in warning window of GTK UI stub (we forgot to divide measures by PANGO_SCALE and thus requested an absurdly large window to fit the text).

--- gtkui/stamp.c
+++ gtkui/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1199843152L;
-const char *source_modtime_str = "01:45 GMT 9-Jan-2008";
-const char *source_version_str = "20080109";
+const time_t source_modtime = 1342457142L;
+const char *source_modtime_str = "16:45 GMT 16-Jul-2012";
+const char *source_version_str = "20120716";
