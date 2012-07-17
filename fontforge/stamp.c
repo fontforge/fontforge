7ccd9065a576d1826b34ce32ded5d30769c43af8
michal-n <michal-n@users.sourceforge.net>
2012-07-17 21:10:59 +0200

    Dave Crossland reposrs a crash in my alpha blending code run on Mac. I learned that XServer on Mac is "rootless". So we need to get screen dimensions otherwise.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1342466982L;
-const char *source_modtime_str = "19:29 GMT 16-Jul-2012";
-const char *source_version_str = "20120716";
+const time_t source_modtime = 1342552250L;
+const char *source_modtime_str = "19:10 GMT 17-Jul-2012";
+const char *source_version_str = "20120717";
