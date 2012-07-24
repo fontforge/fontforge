ba0e4823f44c10459019d99fecea9bbd4b3d3128
michal-n <michal-n@users.sourceforge.net>
2012-07-24 18:39:01 +0200

    Thanks to Jose Da Silva for pointing me to this little patch from Debian. It makes FF take x-www-browser, Google Chrome and Konqueror into account when trying to display help pages.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1342556273L;
-const char *source_modtime_str = "20:17 GMT 17-Jul-2012";
-const char *source_version_str = "20120717";
+const time_t source_modtime = 1343147771L;
+const char *source_modtime_str = "16:36 GMT 24-Jul-2012";
+const char *source_version_str = "20120724";
