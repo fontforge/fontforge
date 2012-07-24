8e662937fc47b53de9f0f068fb28d1db4acc86b3
michal-n <michal-n@users.sourceforge.net>
2012-07-24 19:49:57 +0200

    An attempt to update the last place where we still have an old anvill logo. The tricky part is, this has to be a 32x32 B&W bitmap without alpha channel, as we don't support arbitrary window icons as yet. So this will look inferior to Sev's original.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1343147771L;
-const char *source_modtime_str = "16:36 GMT 24-Jul-2012";
+const time_t source_modtime = 1343152187L;
+const char *source_modtime_str = "17:49 GMT 24-Jul-2012";
 const char *source_version_str = "20120724";
