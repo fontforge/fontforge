63a4d0a8ff6f42e556b53902384e851054fc1dd1
michal-n <michal-n@users.sourceforge.net>
2012-07-31 16:59:15 +0200

    Provide time stamps for the release.

--- fontforge/stamp.c
+++ fontforge/stamp.c
@@ -1,5 +1,5 @@
 #include <time.h>
 
-const time_t source_modtime = 1343152187L;
-const char *source_modtime_str = "17:49 GMT 24-Jul-2012";
-const char *source_version_str = "20120724";
+const time_t source_modtime = 1343746639L;
+const char *source_modtime_str = "14:57 GMT 31-Jul-2012";
+const char *source_version_str = "20120731";
