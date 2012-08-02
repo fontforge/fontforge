baa54526cbde28a9490f735b9cce51eb612f309c
michal-n <michal-n@users.sourceforge.net>
2012-08-02 17:10:27 +0200

    A stupid error that botched the release.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,3 @@
 #define LibFF_ModTime		1343746639L	/* Seconds since 1970 (standard unix time) */
 #define LibFF_ModTime_Str	"14:57 GMT 31-Jul-2012"
-#define LibFF_VersionDate	"20120731"
+#define LibFF_VersionDate	20120731
