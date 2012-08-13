0b41c5850184011b99e07cce6581b2c2aec8c2b2
Khaled Hosny <khaledhosny@eglug.org>
2012-08-13 18:42:47 +0200

    Fix build breakage

--- inc/gimage.h
+++ inc/gimage.h
@@ -189,7 +189,7 @@ extern GImage *GImageReadRgb(char *filename);		/* SGI */
 extern GImage *GImageRead(char *filename);
 
 extern void GImageDrawRect(GImage *img,GRect *r,Color col);
-extern void GImageDrawImage(GImage *dest,GImage *src,void *junk,int x, int y);
+extern void GImageDrawImage(GImage *dest,GImage *src,GRect *junk,int x, int y);
 extern void GImageBlendOver(GImage *dest,GImage *src,GRect *from,int x, int y);
 
 extern void gRGB2HSL(struct hslrgb *col);
