d0e2a50d7494e2cb169a005a3a7b883792e4e4a3
Barry Schwartz <github@crudfactory.com>
2012-09-02 21:27:11 -0500

    Remove dlopen stuff from gimagereadtiff.c.

--- gutils/gimagereadtiff.c
+++ gutils/gimagereadtiff.c
@@ -29,87 +29,11 @@
 #endif
 
 #ifdef _NO_LIBTIFF
+
 static int a_file_must_define_something=0;	/* ANSI says so */
-#elif !defined(_STATIC_LIBTIFF) && !defined(NODYNAMIC)	/* I don't know how to deal with dynamic libs on mac OS/X, hence this */
-#include <dynamic.h>
-#include <tiffio.h>
 
-#define int32 _int32
-#define uint32 _uint32
-#define int16 _int16
-#define uint16 _uint16
-#define int8 _int8
-#define uint8 _uint8
-
-#include "gimage.h"
-
-static DL_CONST void *libtiff=NULL;
-static TIFF *(*_TIFFOpen)(char *, char *);
-static void (*_TIFFGetField)(TIFF *, int, uint32 *);
-static int (*_TIFFReadRGBAImage)(TIFF *, uint32, uint32, uint32 *, int);
-static int (*_TIFFClose)(TIFF *);
-
-static int loadtiff() {
-    libtiff = dlopen("libtiff" SO_EXT,RTLD_LAZY);
-    if ( libtiff==NULL ) {
-	fprintf(stderr,"%s\n", dlerror());
-return( 0 );
-    }
-    _TIFFOpen = (TIFF *(*)(char *, char *)) dlsym(libtiff,"TIFFOpen");
-    _TIFFGetField = (void (*)(TIFF *, int, uint32 *))
-		dlsym(libtiff,"TIFFGetField");
-    _TIFFReadRGBAImage = (int (*)(TIFF *, uint32, uint32, uint32 *, int))
-		dlsym(libtiff,"TIFFReadRGBAImage");
-    _TIFFClose = (int (*)(TIFF *)) dlsym(libtiff,"TIFFClose");
-    if ( _TIFFOpen && _TIFFGetField && _TIFFReadRGBAImage && _TIFFClose )
-return( 1 );
-    dlclose(libtiff);
-    fprintf(stderr,"%s\n", dlerror());
-return( 0 );
-}
-
-GImage *GImageReadTiff(char *filename) {
-    TIFF* tif;
-    uint32 w, h, i,j;
-    uint32 *ipt, *fpt;
-    size_t npixels;
-    uint32* raster;
-    GImage *ret=NULL;
-    struct _GImage *base;
-
-    if ( libtiff==NULL )
-	if ( !loadtiff())
-return( NULL );
-
-    tif = _TIFFOpen(filename, "r");
-
-    if (tif==NULL )
-return( ret );
-
-    _TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
-    _TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
-    npixels = w * h;
-    raster = (uint32*) galloc(npixels * sizeof (uint32));
-    if (raster != NULL) {
-	if (_TIFFReadRGBAImage(tif, w, h, raster, 0)) {
-	    ret = GImageCreate(it_true,w,h);
-	    if ( ret!=NULL ) {
-		base = ret->u.image;
-		for ( i=0; i<h; ++i ) {
-		    ipt = (uint32 *) (base->data+i*base->bytes_per_line);
-		    fpt = raster+(h-1-i)*w;
-		    for ( j=0; j<w; ++j )
-			*ipt++ = COLOR_CREATE(
-				TIFFGetR(fpt[j]), TIFFGetG(fpt[j]), TIFFGetB(fpt[j]));
-		}
-	    }
-	} 
-	gfree(raster);
-    }
-    _TIFFClose(tif);
-return( ret );
-}
 #else
+
 #include <tiffio.h>
 
 #define int32 _int32
@@ -160,4 +84,5 @@ return( ret );
     TIFFClose(tif);
 return( ret );
 }
+
 #endif
