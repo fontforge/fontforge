44d41f51d71d565e9b4997716bea848621ba8491
Deron Meranda <deron.meranda@gmail.com>
2012-09-04 04:10:00 -0400

    Add extern keyword to function declarations in flaglist.h

--- fontforge/flaglist.h
+++ fontforge/flaglist.h
@@ -6,7 +6,7 @@ struct flaglist { const char *name; int flag; };
 #define FLAGLIST_EMPTY { NULL, 0 }
 #define FLAG_UNKNOWN ((int32)0x80000000)
 
-int FindFlagByName( struct flaglist *flaglist, const char *name );
-const char *FindNameOfFlag( struct flaglist *flaglist, int flag );
+extern int FindFlagByName( struct flaglist *flaglist, const char *name );
+extern const char *FindNameOfFlag( struct flaglist *flaglist, int flag );
 
 #endif
