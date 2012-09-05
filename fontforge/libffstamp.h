881270d75f862a5dd696a014763dbb9055b4038e
Joe Da Silva <digital@joescat.com>
2012-09-04 21:34:10 -0700

    Comment stamper.c and output files so that developers know where/how to update these files.

--- fontforge/libffstamp.h
+++ fontforge/libffstamp.h
@@ -1,3 +1,6 @@
+/* This file was generated using stamper.c to create the next version release.    */
+/* If you need to update this to the next release version, then run: make stamper */
+
 #define LibFF_ModTime		1343746639L	/* Seconds since 1970 (standard unix time) */
 #define LibFF_ModTime_Str	"14:57 GMT 31-Jul-2012"
-#define LibFF_VersionDate	20120731
+#define LibFF_VersionDate	20120731	/* Year, month, day */
