d35b5e01b9e8a59b7da24f76a3adc47705d4b310
Barry Schwartz <github@crudfactory.com>
2012-09-10 17:11:38 -0500

    Move pixmaps list into fontforge/pixmaps/Makefile.am.

--- fontforge/pixmaps/RUN_ME_TO_CHANGE_THE_PNGS.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Having the files listed explicitly rather than looking for them in
-# the Makefile lets automake keep track of the distribution.
-
-echo "PNG_LIST = "`ls *.png` > png_list.mk
