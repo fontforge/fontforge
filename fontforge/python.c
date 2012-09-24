520ad21c83a909ee5dfc373915e979065bdbfc0d
Matthew Skala <mskala@ansuz.sooke.bc.ca>
2012-09-23 19:37:48 -0500

    add an optional argument to AddExtrema; it will REALLY add them if this is specified and nonzero

--- fontforge/python.c
+++ fontforge/python.c
@@ -16786,7 +16786,7 @@ static PyObject *PyFFFont_AddExtrema(PyFF_Font *self, PyObject *UNUSED(args)) {
     if ( CheckIfFontClosed(self) )
 return (NULL);
     fv = self->fv;
-    FVAddExtrema(fv);
+    FVAddExtrema(fv, false);
 Py_RETURN( self );
 }
 
