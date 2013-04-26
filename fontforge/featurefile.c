fontforge/fontforge

commit c852d958c10cfb1d7fe9f7014fc89774d2b14dc8

Patch by Khaled to feature file code. If a feature contained both multiple and single substitutions, then ff would lose the single subs. Instead change the single subs to multiples.

fontforge/featurefile.c
@@ -5549,6 +5549,7 @@ static void fea_ParseFeatureDef(struct parseState *tok) {
     uint32 feat_tag;
     struct feat_item *item, *size_item = NULL;
     int type, ret;
+    int has_single, has_multiple;
 
     fea_ParseTag(tok);
     if ( tok->type!=tk_name || !tok->could_be_tag ) {
@@ -5678,6 +5679,27 @@ return;
 	++tok->err_count;
     }
     fea_end_statement(tok);
+
+    /* Now a multiple substitution may have a single destination. In which case*/
+    /*  it will look just like a single substitution. So if there are both */
+    /*  multiple and single subs in a feature, translate all the singles into */
+    /*  multiples */
+    /* Another approach would be to make two lookups, but this is easier... */
+    has_single = has_multiple = false;
+    for ( item=tok->sofar ; item!=NULL && item->type!=ft_lookup_start; item=item->next ) {
+	enum otlookup_type cur = fea_LookupTypeFromItem(item);
+	if ( cur==gsub_multiple )
+	    has_multiple = true;
+	else if ( cur==gsub_single )
+	    has_single = true;
+    }
+    if ( has_multiple && has_single ) {
+	for ( item=tok->sofar ; item!=NULL && item->type!=ft_lookup_start; item=item->next ) {
+	    enum otlookup_type cur = fea_LookupTypeFromItem(item);
+	    if ( cur==gsub_single )
+		item->u2.pst->type = pst_multiple;
+	}
+    }
 
     item = chunkalloc(sizeof(struct feat_item));
     item->type = ft_feat_end;

commit 9ba23cc0a246e37630ed3cfc3ff919debe73d88a

Fix a thinko introduced in c852d95
When checking for lookup type in feature block, stop at feature limit
not a non-existing lookup limit; I blindly copy pasted that block of
code. This was causing all features to be of multiple substitution type
if one of them has a multiple substitution.

fontforge/featurefile.c
@@ -5384,7 +5384,7 @@ return;
     /*  multiples */
     /* Another approach would be to make two lookups, but this is easier... */
     has_single = has_multiple = false;
-    for ( item=tok->sofar ; item!=NULL && item->type!=ft_lookup_start; item=item->next ) {
+    for ( item=tok->sofar ; item!=NULL && item->type!=ft_feat_start; item=item->next ) {
 	enum otlookup_type cur = fea_LookupTypeFromItem(item);
 	if ( cur==gsub_multiple )
 	    has_multiple = true;
@@ -5392,7 +5392,7 @@ return;
 	    has_single = true;
     }
     if ( has_multiple && has_single ) {
-	for ( item=tok->sofar ; item!=NULL && item->type!=ft_lookup_start; item=item->next ) {
+	for ( item=tok->sofar ; item!=NULL && item->type!=ft_feat_start; item=item->next ) {
 	    enum otlookup_type cur = fea_LookupTypeFromItem(item);
 	    if ( cur==gsub_single )
 		item->u2.pst->type = pst_multiple;