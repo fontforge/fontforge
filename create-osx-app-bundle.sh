5dc32925727e11ca7528c61f285a1ebb86046edd
Ben Martin <monkeyiq@users.sourceforge.net>
2013-07-13 17:41:44 +1000

    For OSX, have a go at the libedits used by the collab server too

--- create-osx-app-bundle.sh
+++ create-osx-app-bundle.sh
@@ -43,6 +43,14 @@ do
   ldd  $if | grep libedit
   install_name_tool -change /usr/lib/libedit.3.dylib @executable_path/../lib/libedit.3.dylib $if
 done
+cd $bundle_bin/FontForgeInternal/collablib/
+for if in *dylib 
+do 
+  echo $if 
+  ldd  $if | grep libedit
+  install_name_tool -change /usr/lib/libedit.3.dylib @executable_path/../lib/libedit.3.dylib $if
+done
+
 
 cd $bundle_bin
 install_name_tool -change /usr/lib/libedit.3.dylib @executable_path/../lib/libedit.3.dylib fontforge 
