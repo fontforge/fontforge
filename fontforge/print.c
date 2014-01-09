eab849b400bf3624edfd362fe4d98308f9beccb6
Jeremy Tan <jeremy@jx.tan.fake>
2014-01-09 12:47:09 +0800

    Fix compilation when _NO_TZ is defined

--- fontforge/print.c
+++ fontforge/print.c
@@ -1148,7 +1148,7 @@ static void dump_pdfprologue(PI *pi) {
     fprintf( pi->out, "    /CreationDate (D:%04d%02d%02d%02d%02d%02d",
 	    tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec );
 #ifdef _NO_TZSET
-    fprintf( pdf, "Z)\n" );
+    fprintf( pi->out, "Z)\n" );
 #else
     if ( timezone==0 )
 	fprintf( pi->out, "Z)\n" );
