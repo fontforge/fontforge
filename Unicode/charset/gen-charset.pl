e80d9e17d819d208fd17d0c5f8842f30d6d0f2f8
Abel Cheung <abelcheung@gmail.com>
2014-02-01 01:24:22 +0800

    Supports generating gb2312 & JIS charsets

--- Unicode/charset/gen-charset.pl
+++ Unicode/charset/gen-charset.pl
@@ -2,17 +2,68 @@
 
 use strict;
 use warnings;
+use diagnostics;
 use File::Temp qw/tempfile/;
+use Getopt::Std;
+use constant VERTICAL => 1<<31;
 
+my @size = ("char", "short");
+my @struct = ("charmap", "charmap2");
+
+# For adobe CMap, column count starts from 0
 my %charmaps = (
-	'alphabet' => { 'get_map' => \&get_map_alpha, },
-	'zapf'     => { 'get_map' => \&get_map_zapf , },
+	'default' => {
+		'print_b2u'    => \&print_b2u,
+		'print_u2b'    => \&print_u2b,
+		'get_map'      => \&get_map_default,
+	},
+	'zapf'    => {
+		'print_b2u'    => \&print_b2u,
+		'print_u2b'    => \&print_u2b,
+		'get_map'      => \&get_map_zapf,
+	},
+	'big5'    => {
+		'print_b2u'    => \&print_b2u,
+		'print_u2b'    => \&print_u2b,
+		'get_map'      => \&get_map_default,
+		'offset'       => 0xA100,
+	},
+	'jis'     => {},
+	'jis208'  => {
+		'print_b2u'    => \&print_b2u,
+		'print_u2b'    => \&print_u2b,
+		'get_map'      => \&get_map_adobe,
+		'write_val'    => \&write_compact,
+		'bcol'         => 1,
+		'ucol'         => 21,
+	},
+	'jis212'  => {
+		'print_b2u'    => \&print_b2u,
+		'print_u2b'    => \&print_u2b,
+		'get_map'      => \&get_map_adobe,
+		'write_val'    => \&write_compact,
+		'bcol'         => 1,
+		'ucol'         => 6,
+	},
+	'gb2312'  => {
+		'print_b2u'    => \&print_b2u,
+		'print_u2b'    => \&print_u2b,
+		'get_map'      => \&get_map_adobe,
+		'write_val'    => \&write_compact,
+		'bcol'         => 1,
+		'ucol'         => 13,
+	},
 );
 
-my ($maptype, $funcname, $infile, $outfile, $in_fh, $out_fh, @b2u, @u2b);
+my ($maptype, $funcname, $infile, $outfile, $out_fh, @b2u, @u2b, %opts);
+my $is_mb = 0;
 
 sub errmsg {
 	print STDERR (shift)."\n";
+	if (defined $outfile) {
+		close $out_fh;
+		unlink $outfile;
+	}
 	exit (shift);
 }
 
@@ -20,16 +71,24 @@ sub usage {
 	print <<_EOT_;
 Generates character map files for use in FontForge.
 
-Usage: $0 map_type short_name in_file [out_file]
+Usage: $0 [-o out_file] map_type short_name in_file
 
 If <out_file> is not specified, default is printing to standard output.
+When <map_type> is 'jis', different invoking arguments will be used:
+
+$0 [-o out_file] jis in_file1 in_file2
 
 _EOT_
 	exit 1;
 }
 
-sub get_map_alpha {
-	while (<$in_fh>) {
+# 2 columns: (byte value) (unicode code point)
+sub get_map_default {
+	my ($file, $barray, $uarray) = @_;
+	errmsg ("Can't open input file '$file': $!", 3) if (! open( my $fh, '<', $file ));
+
+	my $o = $charmaps{$maptype}->{'offset'} // 0;
+	while (<$fh>) {
 		next if (/^#/ || /^\s*$/);
 		if (! /^0x([[:xdigit:]]+)\s+0x([[:xdigit:]]+)\s/) {
 			print STDERR "Unrecognised line format at line $.:\n";
@@ -38,14 +97,22 @@ sub get_map_alpha {
 		}
 		my ($b, $u) = map {hex} ($1, $2);
 		print STDERR "Unicode code point beyond BMP at line $.\n" if ($u > 0xFFFF);
-		print STDERR "Not a single byte character at line $.\n" if ($b > 0xFF);
-		$b2u[$b] = $u;
-		$u2b[$u >> 8][$u & 0xFF] = $b;
+		if ($is_mb) {
+			$barray->[$b - $o] = $u if ($b >= $o);
+		} else {
+			$barray->[$b] = $u;
+		}
+		$uarray->[$u >> 8][$u & 0xFF] = $b;
 	}
+	close $fh;
+	return ((@{$barray} > 0) && (@{$uarray} > 0));
 }
 
 sub get_map_zapf {
-	while (<$in_fh>) {
+	my ($file, $barray, $uarray) = @_;
+	errmsg ("Can't open input file '$file': $!", 3) if (! open( my $fh, '<', $file ));
+
+	while (<$fh>) {
 		next if (/^#/ || /^\s*$/);
 		if (! /^([[:xdigit:]]+)\s+([[:xdigit:]]+)\s/) {
 			print STDERR "Unrecognised line format at line $.:\n";
@@ -63,9 +130,127 @@ sub get_map_zapf {
 		$u -= 0xD16F if (($u >= 0xF8D7) && ($u <= 0xF8E4));
 
 		# 0x20 identity mapping has priority over U+A0 mapping
-		$b2u[$b] = $u if (!$b2u[$b]);
-		$u2b[$u >> 8][$u & 0xFF] = $b;
+		$barray->[$b] = $u if (!$b2u[$b]);
+		$uarray->[$u >> 8][$u & 0xFF] = $b;
 	}
+	close $fh;
+	return ((@{$barray} > 0) && (@{$uarray} > 0));
+}
+
+# algorithm mainly from KANOU Hiroki
+sub ucs2_score {
+	my ($chr) = @_;
+	return
+		(($chr >= 0xe000) && ($chr <= 0xf8ff)) ? -1 :
+		(($chr >= 0x2e80) && ($chr <= 0x2fff)) ? 1 :
+		($chr & VERTICAL)                      ? 0 :
+		(($chr >= 0xf000) && ($chr <= 0xffff)) ? 3 :
+		(($chr >= 0x3400) && ($chr <= 0x4dff)) ? 4 : 5;
+}
+
+# selects *first* value in array having highest ucs2_score
+sub pick_value {
+	my ($score, $best) = (0, 0);
+	my @vals = split /,/, $_[0];
+	foreach (@vals) {
+		next if /v/;	# all vertical glyphs are discarded
+		my $chr = hex($_);
+		if (ucs2_score($chr) > $score) {
+			$score = ucs2_score($chr);
+			$best = $chr;
+		}
+	}
+	return $best;
+}
+
+# how many values are non-vertical hex digits?
+sub eligible {
+	my @vals = split /,/, shift;
+	my $count = 0;
+	foreach (@vals) {
+		$count++ if ($_ =~ /^[[:xdigit:]]+$/);
+	}
+	return $count;
+}
+
+sub get_map_adobe {
+	my ($file, $barray, $uarray, $flag, $dummy) = @_;
+	my (@cols, $bcol, $ucol);
+
+	$bcol = $charmaps{$maptype}->{'bcol'};
+	$ucol = $charmaps{$maptype}->{'ucol'};
+
+	return if (($bcol <= 0) || ($ucol <= 0));	# 0th column is CID
+	errmsg ("Can't open input file '$file': $!", 3) if (! open( my $fh, '<', $file ));
+
+	# for safety: how many columns are there in the file?
+	while (<$fh>) {
+		next if (!/^\d/);
+		@cols = split;
+		last;
+	}
+	return if (($bcol > @cols) || ($ucol > @cols));
+
+	seek($fh, 0, 0);
+
+	while (<$fh>) {
+		next if (!/^\d/);
+		@cols = split /\s+/;
+		my ($b, $u) = ($cols[$bcol], $cols[$ucol]);
+
+		# Discard when:
+		# byte value or unicode code point doesn't exist
+		# byte value indicates glyph is in vertical form
+		# byte value is not unique
+		next if (($b =~ /\*/) || ($u =~ /\*/));
+		if ($b !~ /[,v]/) {
+			$b = hex($b);
+		} else {
+			next if (eligible($b) != 1);
+			$b = pick_value($b);
+		}
+
+		if ($u !~ /[,v]/) {
+			$u = hex($u);
+		} else {
+			# IMO this arbitrary character selection process is retarded,
+			# but for compatibility... sigh
+			$u = pick_value($u);
+		}
+		next if (!$u);
+
+		$charmaps{$maptype}->{'write_val'}->($b, $u, $barray, $uarray, $flag);
+	}
+	close $fh;
+	return ((@{$barray} > 0) && (@{$uarray} > 0));
+}
+
+# byte values for some CJK charsets are compacted into 94x94 array
+# before writing
+sub write_compact {
+	my ($b, $u, $barray, $uarray, $flag_jis212, $dummy) = @_;
+
+	if ((($b>>8) < 0x21) || (($b & 0xff) < 0x21) ||
+	    (($b>>8) > 0x7e) || (($b & 0xff) > 0x7e)) {
+		printf STDERR "Byte value out of bound: byte = %04x, unicode = %04x\n", $b, $u;
+		return;
+	}
+	if ($u > 0xffff) {
+		printf STDERR "Unicode beyond BMP: byte = %04x, unicode = %04x\n", $b, $u;
+		return;
+	}
+	if ($flag_jis212) {
+		if ($uarray->[$u >> 8][$u & 0xff]) {
+			printf STDERR "Unicode value already defined: unicode = %04x, orig byte = %04x, new = %04x\n", $u, $uarray->[$u>>8][$u&0xff], $b;
+		} else {
+			$uarray->[$u >> 8][$u & 0xff] = ($b | 0x8000);
+		}
+	} else {
+		$uarray->[$u >> 8][$u & 0xff] = $b;
+	}
+	$b -= 0x2121;
+	$b = ($b>>8) * 94 + ($b & 0xff);
+	$barray->[$b] = $u;
 }
 
 sub print_header {
@@ -75,66 +260,80 @@ sub print_header {
 
 #include <chardata.h>
 
-static const unsigned char c_allzeros[256] = { 0 };
-
 _EOT_
+	print $out_fh "static const unsigned " . $size[$is_mb] .
+		" allzeros[256] = { 0 };\n\n";
 }
 
 ### Byte to Unicode part
 sub print_b2u {
-	print $out_fh "const unichar_t unicode_from_" . $funcname . "[] = {\n  ";
-	printf $out_fh "0x%04x%s", $b2u[$_] // 0,
-		($_ == 255)  ? "\n" :
-		(($_+1) % 8) ? ", " : ",\n  " for (0 .. 255);
+	my ($barray, $name) = @_;
+
+	my $last = $is_mb ? $#{$barray} : 255;	# always dump 256 element for alphabet charset
+	my $o = $charmaps{$maptype}->{'offset'} // 0;
+
+	print $out_fh "const unichar_t unicode_from_".$name."[".($last+1)."] = {\n  ";
+	for (0 .. $last) {
+		($_ % 64) or printf $out_fh "/*** 0x%04x ***/\n  ", $_ + $o;
+		printf $out_fh "0x%04x%s", $barray->[$_] // 0,
+			($_ == $last) ? "\n" :
+			(($_+1) % 8 ) ? ", " : ",\n  ";
+	}
 	print $out_fh "};\n\n";
 }
 
-#
 # Theorecticall should split u2b and footer, but doing them
 # together requires less code
 #
 sub print_u2b {
+	my ($uarray, $name, $name2) = @_;
+
+	$name2 = $name if (!$name2);
 	my $first;
-	my $last = $#u2b;
-	my $s = $funcname;
+	my $last = $#{$uarray};
+
 	for (0 .. $last) {
-		if (defined $u2b[$_]) {
+		if (defined $uarray->[$_]) {
 			$first = $_; last;
 		}
 	}
 
 	# each unicode block
 	for my $block ($first .. $last) {
-		next if (!defined $u2b[$block]);
-		my $arrname = sprintf $s."_from_unicode_%x", $block;
-		print $out_fh "static const unsigned char ".$arrname."[256] = {\n  ";
-		printf $out_fh "0x%02x%s", $u2b[$block][$_] // 0,
-			($_ == 255)   ? "\n" :
-			(($_+1) % 16) ? ", " : ",\n  " for (0 .. 255);
+		next if (!defined $uarray->[$block]);
+		my $arrname = sprintf $name2 . "_from_unicode_%x", $block;
+		print $out_fh "static const unsigned " .
+			$size[$is_mb] . " " . $arrname . "[] = {\n  ";
+		for (0 .. 255) {
+			($_ % 64) or printf $out_fh "/*** 0x%02x ***/\n  ", $_;
+			printf $out_fh ($is_mb ? "0x%04x%s" : "0x%02x%s"), $uarray->[$block][$_] // 0,
+				($_ == 255)  ? "\n" :
+				(($_+1) % 8) ? ", " : ",\n  ";
+		}
 		print $out_fh "};\n\n";
 	}
 
 	# map of unicode -> various byte arrays
-	print $out_fh "static const unsigned char * const ".$s."_from_unicode_[] = {\n";
+	print $out_fh "static const unsigned " .
+		$size[$is_mb] . " * const " . $name2 . "_from_unicode_[] = {\n";
 	for ($first .. $last) {
-		if (!defined $u2b[$_]) {
-			print $out_fh "    c_allzeros,\n";
+		if (!defined $uarray->[$_]) {
+			print $out_fh "    allzeros,\n";
 		} else {
-			printf $out_fh "    %s_from_unicode_%x%s\n", $s, $_, ($_ == $last) ? "" : ",";
+			printf $out_fh "    %s_from_unicode_%x%s\n", $name2, $_, ($_ == $last) ? "" : ",";
 		}
 	}
 	print $out_fh "};\n\n";
 
 	# final part
-	print $out_fh "struct charmap ".$s."_from_unicode = { ";
+	print $out_fh "struct " . $struct[$is_mb] . " " . $name2 . "_from_unicode = { ";
 	printf $out_fh "%d, %d, ", $first, $last;
-	print $out_fh "(unsigned char **) ".$s."_from_unicode_, (unichar_t *) unicode_from_".$s." };\n\n";
+	print $out_fh "(unsigned " . $size[$is_mb] . " **) " . $name2 .
+		"_from_unicode_, (unichar_t *) unicode_from_" . $name . " };\n\n";
 }
 
-usage if ((@ARGV < 3) || (@ARGV > 4));
+getopts('o:', \%opts);
 $maptype  = shift;
-$funcname = shift;
-$infile   = shift;
 
 if (! $charmaps{$maptype}) {
 	print STDERR "The map type '$maptype' is not supported. Supported types:\n";
@@ -142,31 +341,50 @@ if (! $charmaps{$maptype}) {
 	exit 2;
 }
 
-errmsg ("Input file '$infile' not found"     , 3) if (! -f $infile);
-errmsg ("Can't open input file '$infile': $!", 3) if (! open($in_fh, '<', $infile));
+# no reliable way to check multibyte w/o reading files, so define manually
+$is_mb = 1 if grep ($maptype, ['big5', 'jis']);
 
-$charmaps{$maptype}->{'get_map'}->();
-
-close $in_fh;
-
-if (@ARGV) {
+if ($opts{'o'}) {
 	# tempfile will be renamed later
-	($out_fh, $outfile) = tempfile( "charsetXXXXXX", UNLINK => 0 );
+	($out_fh, $outfile) = tempfile( "tmpcharsetXXXXXX", UNLINK => 0 );
 } else {
 	$out_fh = *STDOUT;
 }
 
-print_header;
-if (@b2u == 0) {
-	unlink $outfile if $outfile;
-	errmsg ("Failed to parse file", 4);
+if ( $maptype eq "jis" ) {
+	usage if (@ARGV != 2);
+
+	$maptype = "jis208";
+	$infile = shift;
+	if (! $charmaps{$maptype}->{'get_map'}->( $infile, \@b2u, \@u2b ) ) {
+		errmsg ("Failed to parse file '$infile'", 4);
+	}
+	print_header;
+	$charmaps{$maptype}->{'print_b2u'}->( \@b2u, "jis208" );
+
+	$maptype = "jis212";
+	$infile = shift;
+	undef @b2u;
+	if (! $charmaps{$maptype}->{'get_map'}->( $infile, \@b2u, \@u2b, 1 ) ) {
+		errmsg ("Failed to parse file '$infile'", 4);
+	}
+	$charmaps{$maptype}->{'print_b2u'}->( \@b2u, "jis212" );
+	$charmaps{$maptype}->{'print_u2b'}->( \@u2b, "jis212", "jis" );
+
+} else {
+	usage if (@ARGV != 2);
+	($funcname, $infile) = (shift, shift);
+	if (! $charmaps{$maptype}->{'get_map'}->( $infile, \@b2u, \@u2b ) ) {
+		errmsg ("Failed to parse file '$infile'", 4);
+	}
+	print_header;
+	$charmaps{$maptype}->{'print_b2u'}->( \@b2u, $funcname );
+	$charmaps{$maptype}->{'print_u2b'}->( \@u2b, $funcname );
 }
-print_b2u;
-print_u2b;
 
 if (defined $outfile) {
 	close $outfile;
-	rename $outfile, shift;
+	rename $outfile, $opts{'o'};
 }
 
-# vim: sw=4 ts=4
+# vim: sw=4 ts=4 fdn=1 fdm=indent fml=5
