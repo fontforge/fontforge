958a7fad45af85ffd191117d2125957cb33e784f
Reuben Thomas <rrt@sc3d.org>
2014-03-01 12:33:04 +0000

    Use gnulib tzset module to provide tzset

--- fontforge/cvexport.c
+++ fontforge/cvexport.c
@@ -213,9 +213,6 @@ int _ExportPDF(FILE *pdf,SplineChar *sc,int layer) {
     fprintf( pdf, "    /CreationDate (D:%04d%02d%02d%02d%02d%02d",
 	    1900+tm->tm_year, tm->tm_mon+1, tm->tm_mday,
 	    tm->tm_hour, tm->tm_min, tm->tm_sec );
-#ifdef _NO_TZSET
-    fprintf( pdf, "Z)\n" );
-#else
     tzset();
     if ( timezone==0 )
 	fprintf( pdf, "Z)\n" );
@@ -226,7 +223,6 @@ int _ExportPDF(FILE *pdf,SplineChar *sc,int layer) {
 	    fprintf( pdf, "+" );
 	fprintf( pdf, "%02d'%02d')\n", (int)(timezone/3600),(int)(timezone/60-(timezone/3600)*60) );
     }
-#endif
     fprintf( pdf, "    /Title (%s from %s)\n", sc->name, sc->parent->fontname );
     if ( author!=NULL )
 	fprintf( pdf, "    /Author (%s)\n", author );
--- fontforge/print.c
+++ fontforge/print.c
@@ -1128,9 +1128,6 @@ static void dump_pdfprologue(PI *pi) {
     tm = localtime(&now);
     fprintf( pi->out, "    /CreationDate (D:%04d%02d%02d%02d%02d%02d",
 	    tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec );
-#ifdef _NO_TZSET
-    fprintf( pi->out, "Z)\n" );
-#else
     if ( timezone==0 )
 	fprintf( pi->out, "Z)\n" );
     else {
@@ -1140,7 +1137,6 @@ static void dump_pdfprologue(PI *pi) {
 	    fprintf( pi->out, "+" );
 	fprintf( pi->out, "%02d'%02d')\n", (int)(timezone/3600),(int)(timezone/60-(timezone/3600)*60) );
     }
-#endif
     if ( author!=NULL )
 	fprintf( pi->out, "  /Author (%s)\n", author );
     fprintf( pi->out, ">>\n" );
