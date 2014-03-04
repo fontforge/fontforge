c22f4e37191706a2d3cb6ac8ecaf15bc74a63dac
Reuben Thomas <rrt@sc3d.org>
2014-03-04 15:01:13 +0000

    .dir-locals.el: actually make it all work
    Since I use dtrt-indent, I hadn't noticed this wasn't working at all;
    sorry. Actually tested now.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
-((nil . ((show-trailing-whitespace . nil)))
- (c-mode . ((c-style-alist . '("stroustrup"
-                               (indent-tabs-mode . t)
-                               (tab-width . 8))))))
+((nil . ((show-trailing-whitespace . nil)
+         (eval . (c-add-style "fontforge"
+                              '("stroustrup"
+                                (indent-tabs-mode . t)
+                                (tab-width . 8)
+                                (c-offsets-alist
+                                 (case-label . *)))))))
+ (c-mode . ((c-file-style . "fontforge"))))
