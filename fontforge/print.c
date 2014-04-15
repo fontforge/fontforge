edea537664f2136073e4090912913f31692d49c4
Frank Trampe <frank.trampe@gmail.com>
2014-04-14 19:33:08 -0500

    Revert "Use gnulib tzset module to provide tzset"
    This reverts commit 958a7fad45af85ffd191117d2125957cb33e784f.

--- fontforge/print.c
+++ fontforge/print.c
@@ -1148,6 +1148,9 @@ static void dump_pdfprologue(PI *pi) {
     tm = localtime(&now);
     fprintf( pi->out, "    /CreationDate (D:%04d%02d%02d%02d%02d%02d",
 	    tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec );
+#ifdef _NO_TZSET
+    fprintf( pi->out, "Z)\n" );
+#else
     if ( timezone==0 )
 	fprintf( pi->out, "Z)\n" );
     else {
@@ -1157,6 +1160,7 @@ static void dump_pdfprologue(PI *pi) {
 	    fprintf( pi->out, "+" );
 	fprintf( pi->out, "%02d'%02d')\n", (int)(timezone/3600),(int)(timezone/60-(timezone/3600)*60) );
     }
+#endif
     if ( author!=NULL )
 	fprintf( pi->out, "  /Author (%s)\n", author );
     fprintf( pi->out, ">>\n" );
