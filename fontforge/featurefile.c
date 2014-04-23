15593b7007630bc03a60906df6b46b83c763adaf
mozbugbox <mozbugbox@yahoo.com.au>
2014-04-23 11:29:15 +0800

    Initial a variable in fea_ParseGlyphClass()

--- fontforge/featurefile.c
+++ fontforge/featurefile.c
@@ -2906,6 +2906,7 @@ return( NULL );
 	char *pt1, *start1, *pt2, *start2;
 	int v1, v2;
 
+	last_val = -1;
 	for (;;) {
 	    fea_ParseTok(tok);
 	    if ( tok->type==tk_char && tok->tokbuf[0]==']' )
