1432797ae2bec8651cf224a0d3ab3aaef7d3a661
Jeremy Tan <jtanx@users.noreply.github.com>
2014-05-13 21:58:26 +0800

    Split inclusion of unistd.h in gxdraw.c to after gxdrawp.h
    It requires fontforge-config.h, but it also must not be included
    on Windows

--- gdraw/gxdraw.c
+++ gdraw/gxdraw.c
@@ -28,16 +28,17 @@
 #if defined(__MINGW32__)
 #include <winsock2.h>
 #include <windows.h>
-#else
-#include <unistd.h>		/* for timers & select */
 #endif
-
+ 
 #include "gxdrawP.h"
 #include "gxcdrawP.h"
 
 #include <stdlib.h>
 #include <math.h>
 
+#if !defined(__MINGW32__)
+#include <unistd.h>		/* for timers & select */
+#endif
 #include <sys/types.h>		/* for timers & select */
 #include <sys/time.h>		/* for timers & select */
 #include <signal.h>		/* error handler */
