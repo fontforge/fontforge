ecb2caac360221b9743422cb9f7a3c774fe408ba
Frank Trampe <frank.trampe@gmail.com>
2014-07-15 00:12:40 -0500

    Fix U. F. O. opening.
    This reverts commit 60f8622ecf170ee718b03cb8779e4ebe7fbc60b2.

--- gdraw/gfilechooser.c
+++ gdraw/gfilechooser.c
@@ -1326,7 +1326,7 @@ return;
     if ( u_GFileIsAbsolute(tit) ){
 	base = uc_strstr(tit, "://");
 	if(!base) base = (unichar_t*) tit;
-	if(pt > base && pt[1] && (pt[1]!='.' || pt[2]!='\0') && !u_GFileIsDir(tit)){
+	if(pt > base && pt[1] && (pt[1]!='.' || pt[2]!='\0')){
 	    gfc->lastname = u_copy(pt+1);
 	    dir = u_copyn(tit, pt-tit);
 	}
