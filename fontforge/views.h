fontforge/fontforge

commit 82322a4a4898aeb099b2a1f834bd111bf434ea3e

Fix a few warnings. (And, yes, Frank knows that this introduces some memory leaks. Just wait.)

fontforgeexe/charview.c
@@ -12922,9 +12922,9 @@ return;
 	// Note that because we are doing this ourself we have to set
 	// the text_untranslated ourself too.
  	if( mblist_nomm[i].shortcut )
-	    mblist_nomm[i].ti.text_untranslated = mblist_nomm[i].shortcut;
+	    mblist_nomm[i].ti.text_untranslated = copy(mblist_nomm[i].shortcut);
 	else
-	    mblist_nomm[i].ti.text_untranslated = mblist_nomm[i].ti.text;
+	    mblist_nomm[i].ti.text_untranslated = cu_copy(mblist_nomm[i].ti.text);
 
 	mblist_nomm[i].ti.text = (unichar_t *) _((char *) mblist_nomm[i].ti.text);
     }

fontforgeexe/metricsview.c
@@ -115,8 +115,12 @@ static void selectUserChosenWordListGlyphs( MetricsView *mv, void* userdata )
 {
     printf("selectUserChosenWordListGlyphs(top)\n");
     MVSelectSetForAll( mv, 0 );
-    if( userdata > 0 && userdata!=-1 && userdata!=-2 )
+    // The previous check thought that userdata was in integer and wanted to verify that
+    // it was positive and not equal to -1 or to -2. Frank changed it.
+    if( userdata != NULL)
     {
+	if (userdata == (void*)(-1) || userdata == (void*)(-2))
+	  fprintf(stderr, "Possible error; see the code here.\n");
 	GArray* selected = (GArray*)userdata;
 	int i = 0;
 	for (i = 0; i < selected->len; i++)
@@ -1856,7 +1860,7 @@ static void MVTextChanged(MetricsView *mv) {
     // convert the slash escpae codes and the like to the real string we will use
     // for the metrics window
     printf("MVTextChanged(top) p:%p ret:%s\n", ret, u_to_c(ret));
-    GArray* selected = 0;
+    GArray* selected = NULL;
     unichar_t* retnew = WordlistEscapedInputStringToRealString(
 	mv->sf,
 	ret, &selected,

fontforgeexe/windowmenu.c
@@ -26,10 +26,11 @@
  */
 #include <fontforge-config.h>
 
-# include "fontforgeui.h"
-# include <gfile.h>
-# include "splinefont.h"
-#  include "ustring.h"
+#include "fontforgeui.h"
+#include "basics.h"
+#include <gfile.h>
+#include "splinefont.h"
+#include "ustring.h"
 
 static void WindowSelect(GWindow base,struct gmenuitem *mi,GEvent *e) {
     GDrawRaise(mi->ti.userdata);
@@ -266,10 +267,13 @@ void mbDoGetText(GMenuItem *mb) {
     if ( mb==NULL )
 return;
     for ( i=0; mb[i].ti.text!=NULL || mb[i].ti.line || mb[i].ti.image!=NULL; ++i ) {
-	if( mb[i].shortcut )
-	    mb[i].ti.text_untranslated = mb[i].shortcut;
-	else
-	    mb[i].ti.text_untranslated = mb[i].ti.text;
+	if( mb[i].shortcut ) {
+	    unichar_t tmp[2];
+	    tmp[0] = mb[i].shortcut;
+	    tmp[1] = (unichar_t)(0);
+	    mb[i].ti.text_untranslated = cu_copy(tmp);
+	} else
+	    mb[i].ti.text_untranslated = cu_copy(mb[i].ti.text);
 	if ( mb[i].ti.text!=NULL ) {
 	    mb[i].ti.text = (unichar_t *) S_((char *) mb[i].ti.text);
 	    if ( mb[i].sub!=NULL )
@@ -286,9 +290,9 @@ void mb2DoGetText(GMenuItem2 *mb) {
 return;
     for ( i=0; mb[i].ti.text!=NULL || mb[i].ti.line || mb[i].ti.image!=NULL; ++i ) {
 	if( mb[i].shortcut )
-	    mb[i].ti.text_untranslated = mb[i].shortcut;
+	    mb[i].ti.text_untranslated = copy(mb[i].shortcut);
 	else
-	    mb[i].ti.text_untranslated = mb[i].ti.text;
+	    mb[i].ti.text_untranslated = cu_copy(mb[i].ti.text);
 	if ( mb[i].ti.text!=NULL ) {
 	    mb[i].ti.text = (unichar_t *) S_((char *) mb[i].ti.text);
 	    if ( mb[i].sub!=NULL )

fontforge/encoding.c
@@ -26,6 +26,7 @@
  */
 
 #include "fontforgevw.h"
+#include "splinefont.h"
 #include <ustring.h>
 #include <utype.h>
 #include <math.h>
@@ -120,7 +121,7 @@ static Encoding texbase = { "TeX-Base-Encoding", 256, tex_base_encoding, NULL, N
 static Encoding original = { "Original", 0, NULL, NULL, &custom,                     1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, "", 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0 };
 static Encoding unicodebmp = { "UnicodeBmp", 65536, NULL, NULL, &original,           1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, "", 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0 };
 static Encoding unicodefull = { "UnicodeFull", 17*65536, NULL, NULL, &unicodebmp,    1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, "", 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0 };
-static Encoding adobestd = { "AdobeStandard", 256, unicode_from_adobestd, AdobeStandardEncoding, &unicodefull,
+static Encoding adobestd = { "AdobeStandard", 256, unicode_from_adobestd, (char**)AdobeStandardEncoding, &unicodefull,
                                                                                      1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "", 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0 };
 static Encoding symbol = { "Symbol", 256, unicode_from_MacSymbol, NULL, &adobestd,   1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "", 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0 };
 

fontforge/parsettf.c
@@ -25,6 +25,7 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "fontforge.h"
+#include "splinefont.h"
 #include <chardata.h>
 #include <utype.h>
 #include <ustring.h>
@@ -3314,7 +3315,7 @@ return;						/* Use cids instead */
     map = EncMapNew(256,256,&custom);
     if ( dict->encodingoff==0 || dict->encodingoff==1 ) {
 	/* Standard Encodings */
-	char **enc = dict->encodingoff==0 ? AdobeStandardEncoding : AdobeExpertEncoding;
+	char **enc = dict->encodingoff==0 ? (char **)AdobeStandardEncoding : (char **)AdobeExpertEncoding;
 	map->enc = FindOrMakeEncoding( dict->encodingoff==0 ?
 		"AdobeStandard" : "Custom" );
 	if ( map->enc==NULL )

fontforge/sfd.c
@@ -7980,6 +7980,12 @@ bool SFD_GetFontMetaData( FILE *sfd,
     return true;
 }
 
+void SFD_GetFontMetaDataVoid( FILE *sfd,
+			  char *tok,
+			  SplineFont *sf,
+			  void* d ) {
+  SFD_GetFontMetaData(sfd, tok, sf, d);
+}
 
 static SplineFont *SFD_GetFont( FILE *sfd,SplineFont *cidmaster,char *tok,
 				int fromdir, char *dirname, float sfdversion )

fontforge/splinefont.c
@@ -1278,7 +1278,8 @@ return( copy(buffer));
 
 SplineFont *LoadSplineFont(const char *filename,enum openflags openflags) {
     SplineFont *sf;
-    char *pt, *ept, *tobefreed1=NULL, *tobefreed2=NULL;
+    const char *pt;
+    char *ept, *tobefreed1=NULL, *tobefreed2=NULL;
     static char *extens[] = { ".sfd", ".pfa", ".pfb", ".ttf", ".otf", ".ps", ".cid", ".bin", ".dfont", ".PFA", ".PFB", ".TTF", ".OTF", ".PS", ".CID", ".BIN", ".DFONT", NULL };
     int i;
     char * fname = NULL;

fontforge/splinefont.h
@@ -2511,7 +2511,7 @@ extern void SplineCharMerge(SplineChar *sc,SplineSet **head,int type);
 extern void SPLNearlyHvCps(SplineChar *sc,SplineSet *ss,bigreal err);
 extern void SPLNearlyHvLines(SplineChar *sc,SplineSet *ss,bigreal err);
 extern int  SPLNearlyLines(SplineChar *sc,SplineSet *ss,bigreal err);
-extern int SPInterpolate(SplinePoint *sp);
+extern int SPInterpolate(const SplinePoint *sp);
 extern void SplinePointListSimplify(SplineChar *sc,SplinePointList *spl,
 	struct simplifyinfo *smpl);
 extern SplineSet *SplineCharSimplify(SplineChar *sc,SplineSet *head,
@@ -2740,6 +2740,10 @@ extern bool SFD_GetFontMetaData( FILE *sfd,
                                  char *tok,
                                  SplineFont *sf,
                                  SFD_GetFontMetaDataData* d );
+extern void SFD_GetFontMetaDataVoid( FILE *sfd,
+                                 char *tok,
+                                 SplineFont *sf,
+                                 void* d );
 typedef void (*visitSFDFragmentFunc)( FILE *sfd, char *tokbuf, SplineFont *sf, void* udata );
 extern void visitSFDFragment( FILE *sfd, SplineFont *sf, visitSFDFragmentFunc ufunc, void* udata );
 extern char* DumpSplineFontMetadata( SplineFont *sf );

commit 4f19aa6bef1f34ef99f196de419ee464d8b5fa71

Fix leaks introduced by 82322a4a4898aeb099b2a1f834bd111bf434ea3e.

fontforge/views.h
@@ -812,7 +812,9 @@ extern void MenuNew(GWindow gw,struct gmenuitem *mi,GEvent *e);
 extern void WindowMenuBuild(GWindow base,struct gmenuitem *mi,GEvent *);
 extern void MenuRecentBuild(GWindow base,struct gmenuitem *mi,GEvent *);
 extern void MenuScriptsBuild(GWindow base,struct gmenuitem *mi,GEvent *);
+extern void mb2FreeGetText(GMenuItem2 *mb);
 extern void mb2DoGetText(GMenuItem2 *mb);
+extern void mbFreeGetText(GMenuItem *mb);
 extern void mbDoGetText(GMenuItem *mb);
 extern int RecentFilesAny(void);
 extern void _aplistbuild(struct gmenuitem *mi,SplineFont *sf,
@@ -936,6 +938,7 @@ extern void PI_ShowHints(SplineChar *sc, GGadget *list, int set);
 extern GTextInfo *SCHintList(SplineChar *sc,HintMask *);
 extern void CVResize(CharView *cv );
 extern CharView *CharViewCreate(SplineChar *sc,FontView *fv,int enc);
+extern void CharViewFinishNonStatic();
 
 /**
  * Extended version of CharViewCreate() which allows a window to be created but
@@ -1058,6 +1061,7 @@ extern int GotoChar(SplineFont *sf,EncMap *map, int *merge_with_selection);
 
 extern void CVShowPoint(CharView *cv, BasePoint *me);
 
+extern void BitmapViewFinishNonStatic();
 extern BitmapView *BitmapViewCreate(BDFChar *bc, BDFFont *bdf, FontView *fv,int enc);
 extern BitmapView *BitmapViewCreatePick(int enc, FontView *fv);
 extern void BitmapViewFree(BitmapView *bv);
@@ -1073,6 +1077,7 @@ extern void MVSetSCs(MetricsView *mv, SplineChar **scs);
 extern void MVRefreshChar(MetricsView *mv, SplineChar *sc);
 extern void MVRegenChar(MetricsView *mv, SplineChar *sc);
 extern void MVReKern(MetricsView *mv);
+extern void MetricsViewFinishNonStatic();
 extern MetricsView *MetricsViewCreate(FontView *fv,SplineChar *sc,BDFFont *bdf);
 extern void MetricsViewFree(MetricsView *mv);
 extern void MVRefreshAll(MetricsView *mv);
@@ -1293,6 +1298,7 @@ extern void MVColInit(void);
 extern void CVColInit( void );
 
 extern void FontViewRemove(FontView *fv);
+extern void FontViewFinishNonStatic();
 extern void FVChar(FontView *fv,GEvent *event);
 extern void FVDrawInfo(FontView *fv,GWindow pixmap,GEvent *event);
 extern void FVRedrawAllCharViews(FontView *fv);

fontforgeexe/bitmapview.c
@@ -2225,17 +2225,30 @@ static unsigned char bitmap_bits[] = {
    0x30, 0x0c, 0xf0, 0x03, 0xf0, 0x03, 0x30, 0x0c, 0x30, 0x0c, 0x30, 0x0c,
    0x30, 0x0c, 0xfc, 0x03, 0xfc, 0x03, 0x00, 0x00};
 
+static int bitmapview_ready = false;
+
+static void BitmapViewFinish() {
+  if ( !bitmapview_ready ) return;
+  bitmapview_ready = 0;
+  mb2FreeGetText(mblist);
+}
+
+void BitmapViewFinishNonStatic() {
+  BitmapViewFinish();
+}
+
 static void BitmapViewInit(void) {
-    static int done = false;
+    // static int done = false; // superseded by bitmapview_ready.
     int i;
 
-    if ( done )
+    if ( bitmapview_ready )
 return;
-    done = true;
+    bitmapview_ready = true;
 
     mb2DoGetText(mblist);
     for ( i=0; BVFlipNames[i]!=NULL ; ++i )
 	BVFlipNames[i] = S_(BVFlipNames[i]);
+    atexit(&BitmapViewFinishNonStatic);
 }
 
 BitmapView *BitmapViewCreate(BDFChar *bc, BDFFont *bdf, FontView *fv, int enc) {

fontforgeexe/charview.c
@@ -12902,13 +12902,31 @@ return( true );
 return( false );
 }
 
+static int charview_ready = false;
+
+static void CharViewFinish() {
+  // The memory leak is limited and reachable.
+  if ( !charview_ready ) return;
+    charview_ready = false;
+    mb2FreeGetText(mblist);
+    mb2FreeGetText(spiroptlist);
+    int i;
+    for ( i=0; mblist_nomm[i].ti.text!=NULL; ++i ) {
+      free(mblist_nomm[i].ti.text_untranslated); mblist_nomm[i].ti.text_untranslated = NULL;
+    }
+}
+
+void CharViewFinishNonStatic() {
+  CharViewFinish();
+}
+
 static void CharViewInit(void) {
     int i;
-    static int done = false;
+    // static int done = false; // superseded by charview_active.
 
-    if ( done )
+    if ( charview_ready )
 return;
-    done = true;
+    charview_ready = true;
 //    TRACE("CharViewInit(top) mblist[0].text before translation: %s\n", mblist[0].ti.text );
 
     mb2DoGetText(mblist);
@@ -12928,6 +12946,7 @@ return;
 
 	mblist_nomm[i].ti.text = (unichar_t *) _((char *) mblist_nomm[i].ti.text);
     }
+    atexit(&CharViewFinishNonStatic);
 }
 
 static int nested_cv_e_h(GWindow gw, GEvent *event) {

fontforgeexe/fontview.c
@@ -7313,16 +7313,29 @@ static FontView *__FontViewCreate(SplineFont *sf) {
 return( fv );
 }
 
-static void FontViewInit(void) {
-    static int done = false;
+static int fontview_ready = false;
 
-    if ( done )
+static void FontViewFinish() {
+    if (!fontview_ready) return;
+    mb2FreeGetText(mblist);
+    mbFreeGetText(fvpopupmenu);
+}
+
+void FontViewFinishNonStatic() {
+    FontViewFinish();
+}
+
+static void FontViewInit(void) {
+    // static int done = false; // superseded by fontview_ready.
+
+    if ( fontview_ready )
 return;
 
-    done = true;
+    fontview_ready = true;
 
     mb2DoGetText(mblist);
     mbDoGetText(fvpopupmenu);
+    atexit(&FontViewFinishNonStatic);
 }
 
 static struct resed fontview_re[] = {

fontforgeexe/metricsview.c
@@ -5070,14 +5070,24 @@ static unsigned char metricsicon_bits[] = {
    0x24, 0x10, 0x20, 0x00, 0x24, 0x10, 0x20, 0x00, 0x74, 0x10, 0x00, 0x00,
    0x55, 0x55, 0x00, 0x00, 0x04, 0x10, 0x00, 0x00};
 
-static void MetricsViewInit(void ) {
-    static int inited = false;
+static int metricsview_ready = false;
 
-    if ( !inited ) {
+static void MetricsViewFinish() {
+  if (!metricsview_ready) return;
+  mb2FreeGetText(mblist);
+}
+
+void MetricsViewFinishNonStatic() {
+  MetricsViewFinish();
+}
+
+static void MetricsViewInit(void ) {
+    // static int inited = false; // superseded by metricsview_ready.
+    if (metricsview_ready) return;
 	mv_text_init[2].text = (unichar_t *) _((char *) mv_text_init[2].text);
 	mb2DoGetText(mblist);
 	MVColInit();
-    }
+    atexit(&MetricsViewFinishNonStatic);
 }
 
 MetricsView *MetricsViewCreate(FontView *fv,SplineChar *sc,BDFFont *bdf) {

fontforgeexe/startui.c
@@ -1344,6 +1344,13 @@ exit( 0 );
 	_FVMenuOpen(NULL);
     GDrawEventLoop(NULL);
     GDrawDestroyDisplays();
+
+    // These free menu translations, mostly.
+    BitmapViewFinishNonStatic();
+    MetricsViewFinishNonStatic();
+    CharViewFinishNonStatic();
+    FontViewFinishNonStatic();
+
     ClearImageCache(); // This frees the contents of imagecache.
     hotkeysSave();
     LastFonts_Save();

fontforgeexe/windowmenu.c
@@ -260,6 +260,21 @@ void _aplistbuild(struct gmenuitem *top,SplineFont *sf,
     top->sub = sub;
 }
 
+void mbFreeGetText(GMenuItem *mb) {
+    /* free gettext substitutions on this menu and all sub menus */
+    int i;
+
+    if ( mb==NULL )
+return;
+    for ( i=0; mb[i].ti.text!=NULL || mb[i].ti.line || mb[i].ti.image!=NULL; ++i ) {
+        if (mb[i].ti.text_untranslated != NULL) { free(mb[i].ti.text_untranslated); mb[i].ti.text_untranslated = NULL; }
+	if ( mb[i].ti.text!=NULL ) {
+	    if ( mb[i].sub!=NULL )
+		mbFreeGetText(mb[i].sub);
+	}
+    }
+}
+
 void mbDoGetText(GMenuItem *mb) {
     /* perform gettext substitutions on this menu and all sub menus */
     int i;
@@ -278,6 +293,21 @@ return;
 	    mb[i].ti.text = (unichar_t *) S_((char *) mb[i].ti.text);
 	    if ( mb[i].sub!=NULL )
 		mbDoGetText(mb[i].sub);
+	}
+    }
+}
+
+void mb2FreeGetText(GMenuItem2 *mb) {
+    /* free gettext substitutions on this menu and all sub menus */
+    int i;
+
+    if ( mb==NULL )
+return;
+    for ( i=0; mb[i].ti.text!=NULL || mb[i].ti.line || mb[i].ti.image!=NULL; ++i ) {
+	if (mb[i].ti.text_untranslated != NULL) { free(mb[i].ti.text_untranslated); mb[i].ti.text_untranslated = NULL; }
+	if ( mb[i].ti.text!=NULL ) {
+	    if ( mb[i].sub!=NULL )
+		mb2FreeGetText(mb[i].sub);
 	}
     }
 }