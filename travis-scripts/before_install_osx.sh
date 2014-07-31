bceb5426bd70d67c71f8a04e002609116bd8b9d5
Ben Martin <monkeyiq@users.sourceforge.net>
2014-07-31 15:30:23 +1000

    ok, see if this gets the branch of the PR.

--- travis-scripts/before_install_osx.sh
+++ travis-scripts/before_install_osx.sh
@@ -17,5 +17,12 @@ echo "checking system for existing fontforge.rb"
 #find /usr -name "fontforge.rb"
 ls -l /usr/local/Library/Formula/fontforge.rb
 echo "------------------------"
+sed -e -s "s/url 'https://github.com/fontforge/fontforge.git'.*/url 'https://github.com/fontforge/fontforge.git', :branch => 'BRANCHNAME'/g" ./travis-scripts/fontforge.rb
+cat ./travis-scripts/fontforge.rb
+sed -i -e "s|BRANCHNAME|pull/${TRAVIS_PULL_REQUEST}/head|g" ./travis-scripts/fontforge.rb
 sudo cp ./travis-scripts/fontforge.rb /usr/local/Library/Formula/fontforge.rb
+cat ./travis-scripts/fontforge.rb
 echo "updated the brew fontforge Formula..."
+
+
+
