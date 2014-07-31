86387b55f7739f18c1dbb75220a9187c56c402ba
Ben Martin <monkeyiq@users.sourceforge.net>
2014-07-31 21:10:03 +1000

    x

--- travis-scripts/script_osx.sh
+++ travis-scripts/script_osx.sh
@@ -4,6 +4,7 @@ set -ev
 #brew install --verbose fontforge --HEAD --with-x
 
 ./bootstrap
+HOMEBREW_PREFIX=$(brew --prefix);
 PYLIBDIR=$(${HOMEBREW_PREFIX}/bin/python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
 PYPKGCONFIG="$( cd "$PYLIBDIR/../../pkgconfig" && pwd )"
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PYPKGCONFIG"
