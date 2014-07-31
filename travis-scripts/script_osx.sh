eb5889894f7e25ea0fe82f498ab3fddd04bc5c4a
Ben Martin <monkeyiq@users.sourceforge.net>
2014-07-31 21:24:33 +1000

    x

--- travis-scripts/script_osx.sh
+++ travis-scripts/script_osx.sh
@@ -4,10 +4,18 @@ set -ev
 #brew install --verbose fontforge --HEAD --with-x
 
 ./bootstrap
+
+
 HOMEBREW_PREFIX=$(brew --prefix);
 PYLIBDIR=$(${HOMEBREW_PREFIX}/bin/python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
 PYPKGCONFIG="$( cd "$PYLIBDIR/../../pkgconfig" && pwd )"
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PYPKGCONFIG"
+
+
+export ZLIB_CFLAGS="-I/usr/include"
+export ZLIB_LIBS="-L/usr/lib -lz"
+
 ./configure --prefix=/usr/local --with-x \
             --with-pythonbinary=/usr/local/opt/python/bin/python
+
 fontforge -version
