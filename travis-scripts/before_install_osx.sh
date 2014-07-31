30e2151b077cb4ba0aabf3ab9ede7e239aa20526
Ben Martin <monkeyiq@users.sourceforge.net>
2014-07-31 15:42:59 +1000

    grr

--- travis-scripts/before_install_osx.sh
+++ travis-scripts/before_install_osx.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -ev
 
+brew update
+
+echo "checking system for existing fontforge.rb"
+#find /usr -name "fontforge.rb"
+ls -l /usr/local/Library/Formula/fontforge.rb
+echo "------------------------"
+sed -i -e "s/url 'https://github.com/fontforge/fontforge.git'.*/url 'https://github.com/fontforge/fontforge.git', :branch => 'BRANCHNAME'/g" ./travis-scripts/fontforge.rb
+cat ./travis-scripts/fontforge.rb
+sed -i -e "s|BRANCHNAME|pull/${TRAVIS_PULL_REQUEST}/head|g" ./travis-scripts/fontforge.rb
+sudo cp ./travis-scripts/fontforge.rb /usr/local/Library/Formula/fontforge.rb
+cat ./travis-scripts/fontforge.rb
+echo "updated the brew fontforge Formula..."
+
+
 pushd .
 cd /tmp
 wget http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg
@@ -9,20 +23,9 @@ sudo installer -pkg /Volumes/XQuartz-*/XQuartz.pkg -target /
 popd 
 
 echo "doing an OSX before install step."
-brew update
 brew install cairo python libspiro fontconfig
 brew link python
 
-echo "checking system for existing fontforge.rb"
-#find /usr -name "fontforge.rb"
-ls -l /usr/local/Library/Formula/fontforge.rb
-echo "------------------------"
-sed -e -s "s/url 'https://github.com/fontforge/fontforge.git'.*/url 'https://github.com/fontforge/fontforge.git', :branch => 'BRANCHNAME'/g" ./travis-scripts/fontforge.rb
-cat ./travis-scripts/fontforge.rb
-sed -i -e "s|BRANCHNAME|pull/${TRAVIS_PULL_REQUEST}/head|g" ./travis-scripts/fontforge.rb
-sudo cp ./travis-scripts/fontforge.rb /usr/local/Library/Formula/fontforge.rb
-cat ./travis-scripts/fontforge.rb
-echo "updated the brew fontforge Formula..."
 
 
 
