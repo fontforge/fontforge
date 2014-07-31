cb90ccd1d41f100c38a898b5b83106aa9210e03f
Ben Martin <monkeyiq@users.sourceforge.net>
2014-07-31 13:01:26 +1000

    use the force luke

--- travis-scripts/before_install_osx.sh
+++ travis-scripts/before_install_osx.sh
@@ -7,5 +7,5 @@ brew install cairo python libspiro fontconfig
 echo "checking system for existing fontforge.rb"
 find /usr -name "fontforge.rb"
 echo "------------------------"
-cp ./travis-scripts/fontforge.rb /usr/local/Library/Formula/fontforge.rb
+sudo cp ./travis-scripts/fontforge.rb /usr/local/Library/Formula/fontforge.rb
 echo "updated the brew fontforge Formula..."
