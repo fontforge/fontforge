295dd0bf1bf977be30956868ced750e7e2027c98
Ben Martin <monkeyiq@users.sourceforge.net>
2014-07-31 23:29:42 +1000

    not so nice to just remove shallow, but we'll see if it builds

--- travis-scripts/before_install_osx.sh
+++ travis-scripts/before_install_osx.sh
@@ -20,13 +20,13 @@ sudo cp ./travis-scripts/fontforge.rb /usr/local/Library/Formula/fontforge.rb
 cat ./travis-scripts/fontforge.rb
 echo "updated the brew fontforge Formula..."
 
-
-pushd .
-cd /tmp
-wget http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg
-hdiutil attach XQuartz-2.7.6.dmg 
-sudo installer -pkg /Volumes/XQuartz-*/XQuartz.pkg -target /
-popd 
+# cut out X while testing git stuff
+#pushd .
+#cd /tmp
+#wget http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg
+#hdiutil attach XQuartz-2.7.6.dmg 
+#sudo installer -pkg /Volumes/XQuartz-*/XQuartz.pkg -target /
+#popd 
 
 echo "doing an OSX before install step."
 brew install cairo python libspiro fontconfig
--- travis-scripts/script_osx.sh
+++ travis-scripts/script_osx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 
-brew install --verbose fontforge --HEAD --with-x
+rm -f /Users/travis/build/fontforge/fontforge/.git/shallow
+brew install --verbose fontforge --HEAD #--with-x
 fontforge -version
