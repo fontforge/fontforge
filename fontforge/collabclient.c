800c3e415a82237cace0c052e4df137eb3a2fe7f
Ben Martin <monkeyiq@users.sourceforge.net>
2014-08-04 20:04:52 +1000

    issue527: save last address:ip connected to with collab for next time.

--- fontforge/collabclient.c
+++ fontforge/collabclient.c
@@ -28,9 +28,12 @@
 
 int pref_collab_sessionJoinTimeoutMS = 1000;
 int pref_collab_roundTripTimerMS = 2000;
+char* pref_collab_last_server_connected_to = 0;
 
 #include "collabclientpriv.h"
 #include "inc/basics.h"
+
+
 
 void collabclient_CVPreserveStateCalled( CharViewBase *cv )
 {
