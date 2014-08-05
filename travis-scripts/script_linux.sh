63706fbe29a3357033192d13129f59df6fe3698f
Jeremy Tan <jtanx@users.noreply.github.com>
2014-08-05 20:33:07 +0800

    Check the python extension in travis
    Under the travis environment, the python FF extension is for some reason
    installed to /usr/local/lib/python3/dist-packages instead of say
    /usr/local/lib/python3.2/dist-packages. The former is not in the default python
    site-packages path, so it must be manually added.
    
    testing
    
    more test
    
    Fix typo; expedite python3 path checking
    
    Cleanup the script [ci-skip]

--- travis-scripts/script_linux.sh
+++ travis-scripts/script_linux.sh
@@ -3,9 +3,14 @@ set -ev
 
 export LIBRARY_PATH=/usr/local/lib
 export LD_LIBRARY_PATH=/usr/local/lib
+export PYTHON=python3
+#For some reason on travis, the FF hook installs to this instead of the version-specific (e.g python3.2) path
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/$PYTHON/dist-packages
+
 ./bootstrap
-PYTHON=python3 ./configure --with-freetype-source=../freetype-2.5.0.1
+./configure --with-freetype-source=../freetype-2.5.0.1
 make V=1
 sudo make install
 fontforge -version
-make DISTCHECK_CONFIGURE_FLAGS="PYTHON=python3 UPDATE_MIME_DATABASE=/bin/true UPDATE_DESKTOP_DATABASE=/bin/true" distcheck
+$PYTHON -c "import fontforge; print(fontforge.__version__, fontforge.version());"
+make DISTCHECK_CONFIGURE_FLAGS="UPDATE_MIME_DATABASE=/bin/true UPDATE_DESKTOP_DATABASE=/bin/true" distcheck
