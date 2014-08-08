42d0f337cb361d5aee46ff124eaa41ffa5dda11f
Frank Trampe <frank.trampe@gmail.com>
2014-08-07 23:04:29 -0500

    Fetch uthash automatically during all builds.

--- pkg.mk
+++ pkg.mk
@@ -49,11 +49,13 @@ deb-src-control:
 endif
 
 deb-src: deb-src-control
-	cd tests ; make prefetch-fonts ;
+	$(MAKE) uthash/src;
+	cd tests ; $(MAKE) prefetch-fonts ;
 	cd .. ; if [ "$(F_FAKE_PRODUCT_NAME)" != "$(F_PACKAGE_NAME)" ] ; then cp -pRP $(F_FAKE_PRODUCT_NAME) $(F_PACKAGE_NAME) ; fi ; cd $(F_PACKAGE_NAME) ; yes | debuild -S -sa ;
 
 deb: deb-src-control
-	cd tests ; make prefetch-fonts ;
+	$(MAKE) uthash/src;
+	cd tests ; $(MAKE) prefetch-fonts ;
 	cd .. ; if [ "$(F_FAKE_PRODUCT_NAME)" != "$(F_PACKAGE_NAME)" ] ; then cp -pRP $(F_FAKE_PRODUCT_NAME) $(F_PACKAGE_NAME) ; fi ; cd $(F_PACKAGE_NAME) ; yes | debuild ;
 
 rpm-src-control:
