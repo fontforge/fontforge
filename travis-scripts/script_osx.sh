359352385ab6aca2b59d04ac7cba108964786069
Ben Martin <monkeyiq@users.sourceforge.net>
2014-08-19 12:15:02 +1000

    see that log when thing fail

--- travis-scripts/script_osx.sh
+++ travis-scripts/script_osx.sh
@@ -3,8 +3,10 @@ set -ev
 
 LOGFILE=/tmp/travisci-osx-brewlog.txt
 rm -f /Users/travis/build/fontforge/fontforge/.git/shallow
+set +e
 brew install --verbose fontforge --HEAD --with-x | tee $LOGFILE
 scp $LOGFILE ~/FontForge.app.zip bigv:/tmp/
+set -e
 fontforge -version
 
 
