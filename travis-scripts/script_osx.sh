5ea52a2d3d846bb6f383e112b9e7940bf6ba684f
Ben Martin <monkeyiq@users.sourceforge.net>
2014-08-19 15:04:05 +1000

    try with link derefs

--- travis-scripts/script_osx.sh
+++ travis-scripts/script_osx.sh
@@ -9,10 +9,10 @@ echo "brew build starting..."
 brew install --verbose fontforge --HEAD --with-x 
 echo "brew build complete..."
 PR=$TRAVIS_PULL_REQUEST
-#chmod +x ./travis-scripts/create-osx-app-bundle-homebrew.sh
-#cd /tmp/fontforge-source-tree
-#./travis-scripts/create-osx-app-bundle-homebrew.sh >/tmp/bundle-output-${PR}.log 2>&1
-#ls -lh /tmp/bundle-output-${PR}.log
+chmod +x ./travis-scripts/create-osx-app-bundle-homebrew.sh
+cd /tmp/fontforge-source-tree
+./travis-scripts/create-osx-app-bundle-homebrew.sh >/tmp/bundle-output-${PR}.log 2>&1
+ls -lh /tmp/bundle-output-${PR}.log
 echo "grabbing logs to server for inspection..."
 scp /tmp/bundle-output*.log ~/FontForge.app.zip bigv:/tmp/
 set -e
