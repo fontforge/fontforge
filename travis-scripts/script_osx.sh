85da5b01bb17b6f00077d920e7174af685bca243
Ben Martin <monkeyiq@users.sourceforge.net>
2014-10-06 20:48:23 +1000

    x2

--- travis-scripts/script_osx.sh
+++ travis-scripts/script_osx.sh
@@ -16,6 +16,8 @@ chmod +x ./travis-scripts/create-osx-app-bundle-homebrew.sh
 cd /tmp/fontforge-source-tree
 ./travis-scripts/create-osx-app-bundle-homebrew.sh >/tmp/bundle-output-${PR}.log 2>&1
 ls -lh /tmp/bundle-output-${PR}.log
+echo "build zip info..."
+ls -lh ~/FontForge.app.zip
 echo "grabbing logs to server for inspection..."
 cp /tmp/bundle-output-${PR}.log $TO_BIGV_OUTPUTPATH
 cp ~/FontForge.app.zip          $TO_BIGV_OUTPUTPATH
