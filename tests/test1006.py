318e5dfd003e0dc3c90cb9c41b6c5a0bf40dfdb3
Frédéric Wang <fred.wang@free.fr>
2015-05-01 08:49:50 +0200

    Add math constants tests.

--- tests/test1006.py
+++ tests/test1006.py
@@ -16,6 +16,35 @@ if not math.exists():
 if ( math.ScriptPercentScaleDown!=3 or math.SubscriptBaselineDropMin != 6) :
   raise ValueError("Assignment failed")
 
+# Test some default values for math constants
+math.clear()
+if ( math.ScriptPercentScaleDown != 80 or
+     math.ScriptScriptPercentScaleDown != 60 or
+     math.DelimitedSubFormulaMinHeight != int(round(font.em*1.5)) or
+     math.SubSuperscriptGapMin != int(round(4*font.uwidth)) or
+     math.SpaceAfterScript != int(round(font.em/24)) or
+     math.StackGapMin != int(round(3*font.uwidth)) or
+     math.StackDisplayStyleGapMin != int(round(7*font.uwidth)) or
+     math.FractionNumeratorGapMin != int(round(font.uwidth)) or
+     math.FractionNumeratorDisplayStyleGapMin != int(round(3*font.uwidth)) or
+     math.FractionRuleThickness != int(round(font.uwidth)) or
+     math.FractionDenominatorGapMin != int(round(font.uwidth)) or
+     math.OverbarVerticalGap != int(round(3*font.uwidth)) or
+     math.OverbarRuleThickness != int(round(font.uwidth)) or
+     math.OverbarExtraAscender != int(round(font.uwidth)) or
+     math.UnderbarVerticalGap != int(round(3*font.uwidth)) or
+     math.UnderbarRuleThickness != int(round(font.uwidth)) or
+     math.UnderbarExtraDescender != int(round(font.uwidth)) or
+     math.RadicalVerticalGap != int(round(font.uwidth)) or
+     math.RadicalDisplayStyleVerticalGap != int(round(font.uwidth)) or
+     math.RadicalRuleThickness != int(round(font.uwidth)) or
+     math.RadicalRuleThickness != int(round(font.uwidth)) or
+     math.RadicalExtraAscender != int(round(font.uwidth)) or
+     math.RadicalKernBeforeDegree != int(round(5*font.em/18)) or
+     math.RadicalKernAfterDegree != -int(round(10*font.em/18)) or
+     math.RadicalDegreeBottomRaisePercent != 60 ):
+  raise ValueError("Unexpected default value for one math constant")
+
 math.clear()
 if math.exists():
   raise ValueError("Thinks there is a math table in an empty font")
