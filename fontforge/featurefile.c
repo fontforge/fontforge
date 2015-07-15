76d154a26ec5cb0ebcaf9511acdac705a577446b
Khaled Hosny <khaledhosny@eglug.org>
2015-07-14 23:39:15 -0700

    Fix order of backtrack glyphs
    This was fixed for coverage based contextual lookups in
    86d6ced3bc54550a7ecd5c1f07e7d0b76ccc0489, and this follow up is for glyph
    based ones.
    
    Reported in https://github.com/fontforge/fontforge/issues/2206.

--- fontforge/featurefile.c
+++ fontforge/featurefile.c
@@ -409,7 +409,7 @@ int kernclass_for_feature_file(struct splinefont *sf, struct kernclass *kc, int 
 static void dump_kernclass(FILE *out,SplineFont *sf,struct lookup_subtable *sub) {
     int i,j;
     KernClass *kc = sub->kc;
-    
+
     // We only export classes and rules here that have not been emitted in groups.plist and kerning.plist.
     // The feature file can reference classes from groups.plist, but kerning.plist cannot reference groups from the feature file.
 
@@ -4546,7 +4546,12 @@ static FPST *fea_markedglyphs_to_fpst(struct parseState *tok,struct markedglyphs
 	r->lookups[i].seq = i;
 
     if ( all_single ) {
-	g = fea_glyphs_to_names(glyphs,bcnt,&r->u.glyph.back);
+	char *temp = NULL;
+	// backtrack glyphs should be in reverse order, but they are in
+	// natural order in the feature file, so we reverse them
+	g = fea_glyphs_to_names(glyphs,bcnt,&temp);
+	r->u.glyph.back = reverseGlyphNames (temp);
+	free (temp);
 	g = fea_glyphs_to_names(g,ncnt,&r->u.glyph.names);
 	g = fea_glyphs_to_names(g,fcnt,&r->u.glyph.fore);
     } else {
