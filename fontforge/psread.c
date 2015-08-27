82a1230f185b61d05d45b5716cc0523dc701311b
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2015-08-26 21:51:54 -0300

    cleanup nextpstoken function in psread.c

--- fontforge/psread.c
+++ fontforge/psread.c
@@ -429,126 +429,148 @@ static int nextpstoken(IO *wrapper, real *val, char *tokbuf, int tbsize) {
     float mf2pt_advance_width;
 
     pt = tokbuf;
-    end = pt+tbsize-1;
+    end = pt + tbsize - 1;
 
     /* Eat whitespace and comments. Comments last to eol (or formfeed) */
-    while ( 1 ) {
-	while ( isspace(ch = nextch(wrapper)) );
-	if ( ch!='%' )
-    break;
-	while ( (ch=nextch(wrapper))!=EOF && ch!='\r' && ch!='\n' && ch!='\f' )
-	    if ( pt<end )
-		*pt++ = ch;
-	*pt='\0';
-	/* Some comments have meanings (that we care about) */
-	if ( sscanf( tokbuf, " MF2PT1: bbox %*g %*g %g %*g", &mf2pt_advance_width )==1 )
-	    wrapper->advance_width = mf2pt_advance_width;
-	else if ( sscanf( tokbuf, " MF2PT1: glyph_dimensions %*g %*g %g %*g", &mf2pt_advance_width )==1 )
-	    wrapper->advance_width = mf2pt_advance_width;
-	pt = tokbuf;
+    while (true) {
+        while (isspace(ch = nextch(wrapper)));
+
+        if (ch != '%')
+            break;
+
+        while ((ch = nextch(wrapper)) != EOF
+               && ch != '\r'
+               && ch != '\n'
+               && ch != '\f') {
+            if (pt < end)
+                *pt++ = ch;
+        }
+        *pt='\0';
+
+        /* Some comments have meanings (that we care about) */
+        if (sscanf(tokbuf, " MF2PT1: bbox %*g %*g %g %*g", &mf2pt_advance_width) == 1) {
+            wrapper->advance_width = mf2pt_advance_width;
+        } else if (sscanf(tokbuf, " MF2PT1: glyph_dimensions %*g %*g %g %*g", &mf2pt_advance_width) == 1) {
+            wrapper->advance_width = mf2pt_advance_width;
+        }
+        pt = tokbuf;
     }
 
-    if ( ch==EOF )
-return( pt_eof );
+    if (ch == EOF)
+        return pt_eof;
 
     pt = tokbuf;
-    end = pt+tbsize-1;
-    *pt++ = ch; *pt='\0';
+    end = pt + tbsize - 1;
+    *pt++ = ch;
+    *pt = '\0';
 
-    if ( ch=='(' ) {
-	int nest=1, quote=0;
-	while ( (ch=nextch(wrapper))!=EOF ) {
-	    if ( pt<end ) *pt++ = ch;
-	    if ( quote )
-		quote=0;
-	    else if ( ch=='(' )
-		++nest;
-	    else if ( ch==')' ) {
-		if ( --nest==0 )
-	break;
-	    } else if ( ch=='\\' )
-		quote = 1;
-	}
-	*pt='\0';
-return( pt_string );
-    } else if ( ch=='<' ) {
-	ch = nextch(wrapper);
-	if ( pt<end ) *pt++ = ch;
-	if ( ch=='>' )
-	    /* Done */;
-	else if ( ch!='~' ) {
-	    while ( (ch=nextch(wrapper))!=EOF && ch!='>' )
-		if ( pt<end ) *pt++ = ch;
-	} else {
-	    int twiddle=0;
-	    while ( (ch=nextch(wrapper))!=EOF ) {
-		if ( pt<end ) *pt++ = ch;
-		if ( ch=='~' ) twiddle = 1;
-		else if ( twiddle && ch=='>' )
-	    break;
-		else twiddle = 0;
+    if (ch == '(') {
+        int nest=1, quote=0;
+        while ((ch = nextch(wrapper)) != EOF) {
+            if (pt < end)
+                *pt++ = ch;
+
+            if (quote) {
+                quote=0;
+            } else if (ch == '(') {
+                ++nest;
+            } else if (ch == ')') {
+                if (--nest == 0)
+                    break;
+            } else if (ch == '\\'){
+                quote = 1;
+            }
+        }
+        *pt = '\0';
+
+        return pt_string;
+    } else if (ch == '<') {
+        ch = nextch(wrapper);
+        if (pt < end)
+            *pt++ = ch;
+        if (ch == '>'){
+            /* Done */;
+        } else if (ch != '~') {
+            while ((ch = nextch(wrapper)) != EOF && ch != '>') {
+                if ( pt<end )
+                    *pt++ = ch;
+            }
+        } else {
+            int twiddle=0;
+            while ((ch = nextch(wrapper)) != EOF) {
+                if (pt < end)
+                    *pt++ = ch;
+                if (ch == '~')
+                    twiddle = 1;
+                else if (twiddle && ch == '>')
+                    break;
+                else
+                    twiddle = 0;
+            }
 	    }
-	}
-	*pt='\0';
-return( pt_string );
-    } else if ( ch==')' || ch=='>' || ch=='[' || ch==']' || ch=='{' || ch=='}' ) {
-	if ( ch=='{' )
-return( pt_opencurly );
-	else if ( ch=='}' )
-return( pt_closecurly );
-	if ( ch=='[' )
-return( pt_openarray );
-	else if ( ch==']' )
-return( pt_closearray );
+        *pt = '\0';
+        return pt_string;
+    } else if (ch==')' || ch=='>' || ch=='[' || ch==']' || ch=='{' || ch=='}') {
+        if (ch == '{')
+            return(pt_opencurly);
+        else if (ch == '}')
+            return(pt_closecurly);
+        if (ch == '[')
+            return(pt_openarray);
+        else if (ch == ']')
+            return(pt_closearray);
 
-return( pt_unknown );	/* single character token */
-    } else if ( ch=='/' ) {
-	pt = tokbuf;
-	while ( (ch=nextch(wrapper))!=EOF && !isspace(ch) && ch!='%' &&
-		ch!='(' && ch!=')' && ch!='<' && ch!='>' && ch!='[' && ch!=']' &&
-		ch!='{' && ch!='}' && ch!='/' )
-	    if ( pt<tokbuf+tbsize-2 )
-		*pt++ = ch;
-	*pt = '\0';
-	unnextch(ch,wrapper);
-return( pt_namelit );	/* name literal */
+        return(pt_unknown); /* single character token */
+    } else if (ch == '/') {
+        pt = tokbuf;
+        while ((ch = nextch(wrapper)) != EOF && !isspace(ch) && ch != '%' &&
+                ch != '(' && ch != ')' && ch != '<' && ch != '>' && ch != '[' && ch != ']' &&
+                ch != '{' && ch != '}' && ch != '/' ) {
+            if (pt < tokbuf + tbsize - 2)
+                *pt++ = ch;
+        }
+        *pt = '\0';
+        unnextch(ch, wrapper);
+        return pt_namelit; /* name literal */
     } else {
-	while ( (ch=nextch(wrapper))!=EOF && !isspace(ch) && ch!='%' &&
-		ch!='(' && ch!=')' && ch!='<' && ch!='>' && ch!='[' && ch!=']' &&
-		ch!='{' && ch!='}' && ch!='/' ) {
-	    if ( pt<tokbuf+tbsize-2 )
-		*pt++ = ch;
-	}
-	*pt = '\0';
-	unnextch(ch,wrapper);
-	r = strtol(tokbuf,&end,10);
-	pt = end;
-	if ( *pt=='\0' ) {		/* It's a normal integer */
-	    *val = r;
-return( pt_number );
-	} else if ( *pt=='#' ) {
-	    r = strtol(pt+1,&end,r);
-	    if ( *end=='\0' ) {		/* It's a radix integer */
-		*val = r;
-return( pt_number );
-	    }
-	} else {
-	    *val = strtod(tokbuf,&end);
-	    if ( !finite(*val) ) {
-/* GT: NaN is a concept in IEEE floating point which means "Not a Number" */
-/* GT: it is used to represent errors like 0/0 or sqrt(-1). */
-		LogError( _("Bad number, infinity or nan: %s\n"), tokbuf );
-		*val = 0;
-	    }
-	    if ( *end=='\0' )		/* It's a real */
-return( pt_number );
-	}
-	/* It's not a number */
-	for ( i=0; toknames[i]!=NULL; ++i )
-	    if ( strcmp(tokbuf,toknames[i])==0 )
-return( i );
+        while ((ch = nextch(wrapper)) != EOF && !isspace(ch) && ch != '%' &&
+                ch != '(' && ch != ')' && ch != '<' && ch != '>' && ch != '[' && ch != ']' &&
+                ch != '{' && ch != '}' && ch != '/' ) {
+            if (pt < tokbuf + tbsize - 2)
+                *pt++ = ch;
+        }
+        *pt = '\0';
+        unnextch(ch, wrapper);
+        r = strtol(tokbuf, &end, 10);
+        pt = end;
+        if (*pt == '\0') { /* It's a normal integer */
+            *val = r;
+            return pt_number;
+        } else if (*pt == '#') {
+            r = strtol(pt + 1, &end, r);
+            if (*end == '\0') { /* It's a radix integer */
+                *val = r;
+                return pt_number;
+            }
+        } else {
+            *val = strtod(tokbuf, &end);
+            if (!finite(*val)) {
+                /* GT: NaN is a concept in IEEE floating point which means "Not a Number" */
+                /* GT: it is used to represent errors like 0/0 or sqrt(-1). */
+                LogError(_("Bad number, infinity or NaN: %s\n"), tokbuf);
+                *val = 0;
+            }
+            if (*end == '\0') /* It's a real */
+                return pt_number;
+        }
 
-return( pt_unknown );
+        /* It's not a number */
+        for (i=0; toknames[i] != NULL; ++i) {
+            if (strcmp(tokbuf, toknames[i]) == 0)
+                return i;
+        }
+
+        return pt_unknown;
     }
 }
 
