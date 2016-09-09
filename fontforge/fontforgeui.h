2f2ecb45111032d00317993403221490e652dea8
Gioele Barabucci <gioele@svario.it>
2016-09-10 01:22:52 +0200

    Move declarations of non-static functions in cursors.c to own file

--- fontforge/fontforgeui.h
+++ fontforge/fontforgeui.h
@@ -142,7 +142,6 @@ extern Encoding *ParseEncodingNameFromList(GGadget *listfield);
 extern GTextInfo *GetEncodingTypes(void);
 extern void cvtoollist_check(GWindow gw,struct gmenuitem *mi,GEvent *e);
 
-extern void InitCursors(void);
 extern void InitToolIconClut(Color bg);
 extern void InitToolIcons(void); /* needs image cache already working */
 
