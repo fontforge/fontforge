cd43e5a6aa10a1c22365c6dcfa83526961ca9dbb
Gioele Barabucci <gioele@svario.it>
2016-09-14 23:52:47 +0200

    Add include guards to ofl.h

--- fontforge/ofl.h
+++ fontforge/ofl.h
@@ -3,6 +3,9 @@ Copyright: 2007 George Williams
 License: BSD-3-clause
 */
 
+#ifndef FONTFORGE_OFL_H
+#define FONTFORGE_OFL_H
+
 #include "splinefont.h"
 
 extern struct str_lang_data {
@@ -10,3 +13,5 @@ extern struct str_lang_data {
     int lang;
     char **data;
 } ofl_str_lang_data[];
+
+#endif /* FONTFORGE_OFL_H */
