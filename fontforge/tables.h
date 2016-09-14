00d7e33ed1e2f88cf768d3e961591363695b0873
Gioele Barabucci <gioele@svario.it>
2016-09-14 23:58:46 +0200

    Add include guards to tables.h

--- fontforge/tables.h
+++ fontforge/tables.h
@@ -1,4 +1,9 @@
+#ifndef FONTFORGE_TABLES_H
+#define FONTFORGE_TABLES_H
+
 /* Declarations for data tables */
 
 extern const int amspua[], cns14pua[];
 extern const char (*SaveTablesPref[]);
+
+#endif /* FONTFORGE_TABLES_H */
