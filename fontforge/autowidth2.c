29a98322650ec97e63ec742000a06548c3183965
Jeremy Tan <jtanx@outlook.com>
2017-07-09 19:20:28 +1000

    Fix some compiler warnings

--- fontforge/autowidth2.c
+++ fontforge/autowidth2.c
@@ -444,7 +444,7 @@ SplineChar ***GlyphClassesFromNames(SplineFont *sf,char **classnames,
 	    else
 		classes[i] = malloc((clen+1)*sizeof(SplineChar *));
 	}
-	if ( cn != NULL ) free( cn ) ; cn = NULL ;
+	if ( cn != NULL ) { free( cn ) ; cn = NULL ; }
     }
 return( classes );
 }
