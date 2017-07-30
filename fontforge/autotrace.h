b785242b7f955896958317acc8b869e108c77e98
Jeremy Tan <jtanx@outlook.com>
2017-07-30 15:47:07 +1000

    Switch autotrace to use GLib (reducing MINGW specific codepaths) Also: Move ProgramExists->GFileProgramExists (into gutils/fsys.c)
    Refactoring the autotrace code

--- fontforge/autotrace.h
+++ fontforge/autotrace.h
@@ -6,13 +6,12 @@
 
 extern char **AutoTraceArgs(int ask);
 extern char *ProgramExists(const char *prog, char *buffer);
-extern const char *FindAutoTraceName(void);
+extern int FindAutoTrace(char *path, size_t bufsiz);
 extern const char *FindMFName(void);
 extern SplineFont *SFFromMF(char *filename);
 extern void FVAutoTrace(FontViewBase *fv, int ask);
 extern void *GetAutoTraceArgs(void);
 extern void MfArgsInit(void);
-extern void _SCAutoTrace(SplineChar *sc, int layer, char **args);
 extern void SetAutoTraceArgs(void *a);
 
 #endif /* FONTFORGE_AUTOTRACE_H */
