7aea5c0fbe5f4f31b9e3d37ce56fd881e3b96575
Jeremy Tan <jtanx@outlook.com>
2018-06-17 11:43:52 +1000

    woff2: Move implementation into woff.c
    gnulib doesn't really play well with C++; to use it, you must
    and prefix replaced functions with gnulib:: - because cstdio
    unconditionally #undef's the macros that gnulib sets up.
    
    We rely on gnulib for some things to work properly on windows,
    most notably tmpfile tmpfile. (see also: 8efadf08b)
    
    I don't want to prefix gnulib::, so I've moved the actual implementaiton
    into C/woff.c, and just have woff2.cc contain a C interface to the
    woff2 functions that we call.

--- fontforge/woff.h
+++ fontforge/woff.h
@@ -1,6 +1,7 @@
 #ifndef FONTFORGE_WOFF_H
 #define FONTFORGE_WOFF_H
 
+#include "fontforge-config.h"
 #include <stdio.h>
 
 #include "splinefont.h"
@@ -10,6 +11,12 @@ extern int _WriteWOFFFont(FILE *woff, SplineFont *sf, enum fontformat format, in
 extern SplineFont *_SFReadWOFF(FILE *woff, int flags, enum openflags openflags, char *filename, struct fontdict *fd);
 
 #ifdef FONTFORGE_CAN_USE_WOFF2
+#define WOFF2_DEFAULT_MAX_SIZE (30 * 1024 * 1024) /* = woff2::kDefaultMaxSize */
+extern size_t woff2_max_woff2_compressed_size(const uint8_t* data, size_t length);
+extern size_t woff2_compute_woff2_final_size(const uint8_t *data, size_t length);
+extern int woff2_convert_ttf_to_woff2(const uint8_t *data, size_t length, uint8_t *result, size_t *result_length);
+extern int woff2_convert_woff2_to_ttf(const uint8_t *data, size_t length, uint8_t *result, size_t *result_length);
+
 extern int WriteWOFF2Font(char *fontname, SplineFont *sf, enum fontformat format, int32 *bsizes, enum bitmapformat bf, int flags, EncMap *enc, int layer);
 extern int _WriteWOFF2Font(FILE *woff, SplineFont *sf, enum fontformat format, int32 *bsizes, enum bitmapformat bf, int flags, EncMap *enc, int layer);
 extern SplineFont *_SFReadWOFF2(FILE *woff, int flags, enum openflags openflags, char *filename, struct fontdict *fd);
--- fontforge/woff2.cc
+++ fontforge/woff2.cc
@@ -22,172 +22,50 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdint.h>
-#include <string>
-#include <vector>
 #include <woff2/decode.h>
 #include <woff2/encode.h>
 
-/**
- * Fontforge's headers aren't C++ includable, so forward-declare
- * everything ourselves.
- */
 extern "C" {
-typedef struct splinefont SplineFont;
-typedef struct encmap EncMap;
-enum fontformat {};
-enum bitmapformat {};
-enum openflags {};
-
-SplineFont *_SFReadTTF(FILE *ttf, int flags, enum openflags openflags, char *filename, struct fontdict *fd);
-int _WriteTTFFont(FILE *ttf, SplineFont *sf, enum fontformat format, int32_t *bsizes, enum bitmapformat bf, int flags, EncMap *map, int layer);
-
-int WriteWOFF2Font(char *fontname, SplineFont *sf, enum fontformat format, int32_t *bsizes, enum bitmapformat bf, int flags, EncMap *enc, int layer);
-int _WriteWOFF2Font(FILE *fp, SplineFont *sf, enum fontformat format, int32_t *bsizes, enum bitmapformat bf, int flags, EncMap *enc, int layer);
-SplineFont *_SFReadWOFF2(FILE *fp, int flags, enum openflags openflags, char *filename, struct fontdict *fd);
-}
-
-/**
- * Read the contents of fp into buf
- */
-static bool ReadFile(FILE *fp, std::vector<uint8_t> &buf)
-{
-    if (fseek(fp, 0, SEEK_END) < 0) {
-        return false;
-    }
-
-    long length = ftell(fp);
-    if (length <= 0 || fseek(fp, 0, SEEK_SET) < 0) {
-        return false;
-    }
-
-    buf.resize(length);
-    size_t actual = fread(&buf[0], 1, length, fp);
-    if (fgetc(fp) != EOF) {
-        return false;
-    }
-    buf.resize(actual);
-    return true;
-}
-
-/**
- * Write the contents of buf into fp. 
- * On success, the returned file pointer is equal to fp.
- * It will be positioned at the start of the file.
- */
-static FILE *WriteToFile(FILE *fp, const std::string &buf)
-{
-    if (fp && fwrite(&buf[0], 1, buf.size(), fp) == buf.size()) {
-        if (fseek(fp, 0, SEEK_SET) >= 0) {
-            return fp;
-        }
-    }
-    return nullptr;
-}
-
-/**
- * Write the contents of buf into a temporary file.
- * On success, the returned file pointer is at the start of the file.
- * The caller is responsible for closing it.
- */
-static FILE *WriteToFile(const std::string &buf)
-{
-    FILE *fp = tmpfile();
-    if (!WriteToFile(fp, buf)) {
-        fclose(fp);
-        return nullptr;
-    }
-    return fp;
-}
-
-/**
- * Convert fp (woff2) to ttf, stored in a temporary file.
- * The caller is responsible for closing the returned file.
- */
-static FILE *WOFF2ToTTF(FILE *fp)
+size_t woff2_max_woff2_compressed_size(const uint8_t *data, size_t length)
 {
     try {
-        std::vector<uint8_t> raw_input;
-        if (!ReadFile(fp, raw_input)) {
-            return nullptr;
-        }
-
-        std::string ttfBuffer(std::min(woff2::ComputeWOFF2FinalSize(&raw_input[0], raw_input.size()),
-                                       woff2::kDefaultMaxSize),
-                              0);
-        woff2::WOFF2StringOut output(&ttfBuffer);
-
-        if (!woff2::ConvertWOFF2ToTTF(&raw_input[0], raw_input.size(), &output)) {
-            return nullptr;
-        }
-        ttfBuffer.resize(output.Size());
-
-        return WriteToFile(ttfBuffer);
-    }
-    catch (std::exception) {
-        return nullptr;
+        return woff2::MaxWOFF2CompressedSize(data, length);
+    } catch (const std::exception &) {
+        return 0;
     }
 }
 
-/**
- * Convert ttf to woff.
- * Returns true on success.
- */
-static bool TTFToWOFF2(FILE *ttf, FILE *woff)
+size_t woff2_compute_woff2_final_size(const uint8_t *data, size_t length)
 {
     try {
-        std::vector<uint8_t> raw_input;
-        if (!ReadFile(ttf, raw_input)) {
-            return false;
-        }
-
-        std::string woffBuffer(woff2::MaxWOFF2CompressedSize(&raw_input[0], raw_input.size()), 0);
-        size_t woffActualSize = woffBuffer.size();
-        if (!woff2::ConvertTTFToWOFF2(&raw_input[0], raw_input.size(), reinterpret_cast<uint8_t *>(&woffBuffer[0]), &woffActualSize)) {
-            return false;
-        }
-        woffBuffer.resize(woffActualSize);
-
-        return WriteToFile(woff, woffBuffer) != nullptr;
+        return woff2::ComputeWOFF2FinalSize(data, length);
+    } catch (const std::exception &) {
+        return 0;
     }
-    catch (std::exception) {
+}
+
+int woff2_convert_ttf_to_woff2(const uint8_t *data, size_t length, uint8_t *result, size_t *result_length)
+{
+    try {
+        return woff2::ConvertTTFToWOFF2(data, length, result, result_length);
+    } catch (const std::exception &) {
         return false;
     }
 }
 
-int WriteWOFF2Font(char *fontname, SplineFont *sf, enum fontformat format, int32_t *bsizes, enum bitmapformat bf, int flags, EncMap *enc, int layer)
+int woff2_convert_woff2_to_ttf(const uint8_t *data, size_t length, uint8_t *result, size_t *result_length)
 {
-    FILE *woff = fopen(fontname, "wb");
-    int ret = 0;
-    if (woff) {
-        ret = _WriteWOFF2Font(woff, sf, format, bsizes, bf, flags, enc, layer);
-        fclose(woff);
-    }
-    return ret;
-}
-
-int _WriteWOFF2Font(FILE *fp, SplineFont *sf, enum fontformat format, int32_t *bsizes, enum bitmapformat bf, int flags, EncMap *enc, int layer)
-{
-    FILE *tmp = tmpfile();
-    int ret = 0;
-    if (tmp) {
-        if (_WriteTTFFont(tmp, sf, format, bsizes, bf, flags, enc, layer)) {
-            if (TTFToWOFF2(tmp, fp)) {
-                ret = 1;
-            }
+    try {
+        woff2::WOFF2MemoryOut output(result, *result_length);
+        bool ret = woff2::ConvertWOFF2ToTTF(data, length, &output);
+        if (ret) {
+            *result_length = output.Size();
+        } else {
+            *result_length = 0;
         }
-        fclose(tmp);
-    }
-    return ret;
-}
-
-SplineFont *_SFReadWOFF2(FILE *fp, int flags, enum openflags openflags, char *filename, struct fontdict *fd)
-{
-    FILE *ttf = WOFF2ToTTF(fp);
-    if (ttf) {
-        SplineFont *ret = _SFReadTTF(ttf, flags, openflags, filename, fd);
-        fclose(ttf);
         return ret;
+    } catch (const std::exception &) {
+        return false;
     }
-    return NULL;
+}
 }
