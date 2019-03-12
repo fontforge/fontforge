5392389d3384f1d0bee3b0693cb8f15f73bca363
Jeremy Tan <jtanx@outlook.com>
2019-03-12 16:51:43 +0800

    Fix loading of internal python module when used from pyhook
    Fixes #3518

---
+++ travis-scripts/pyhook_smoketest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import fontforge
+import psMat
+import pickle
+
+print(fontforge.__version__, fontforge.version())
+
+fontforge.font()
+
+print(pickle.loads(pickle.dumps(fontforge.point())))
