fontforge/fontforge

commit 27c9bc86acb0b1a1bf37f3b27178112197387bf1

Fix Coverity reports: (#3593)
1083071
1082944
1082946
1082947
1399595
1083276
1082939

fontforge/autosave.c
@@ -28,6 +28,7 @@
 #include "autosave.h"
 
 #include "baseviews.h"
+#include "splineutil.h"
 #include "fontforgevw.h"
 #include "sfd.h"
 /*#include "ustring.h"*/
@@ -77,6 +78,7 @@ return;
 	sprintf( buffer, "%s/auto%06x-%d.asfd", autosavedir, getpid(), ++cnt );
 	if ( access(buffer,F_OK)==-1 ) {
 	    sf->autosavename = copy(buffer);
+            free(autosavedir);
 return;
 	}
     }
@@ -90,13 +92,15 @@ int DoAutoRecoveryExtended(int inquire)
     DIR *dir;
     struct dirent *entry;
     int any = false;
-    SplineFont *sf;
+    SplineFont *sf = NULL;
     int inquire_state=0;
 
     if ( recoverdir==NULL )
 return( false );
-    if ( (dir = opendir(recoverdir))==NULL )
+    if ( (dir = opendir(recoverdir))==NULL ) {
+        free(recoverdir);
 return( false );
+    }
     while ( (entry=readdir(dir))!=NULL ) {
 	if ( strcmp(entry->d_name,".")==0 || strcmp(entry->d_name,"..")==0 )
     continue;
@@ -109,6 +113,8 @@ return( false );
 	    fprintf( stderr, " Done\n" );
 	}
     }
+    SplineFontFree(sf);
+    free(recoverdir);
     closedir(dir);
 return( any );
 }
@@ -127,8 +133,10 @@ void CleanAutoRecovery(void) {
 
     if ( recoverdir==NULL )
 return;
-    if ( (dir = opendir(recoverdir))==NULL )
+    if ( (dir = opendir(recoverdir))==NULL ) {
+        free(recoverdir);
 return;
+    }
     while ( (entry=readdir(dir))!=NULL ) {
 	if ( strcmp(entry->d_name,".")==0 || strcmp(entry->d_name,"..")==0 )
     continue;
@@ -138,6 +146,7 @@ return;
 	    perror(buffer);
 	}
     }
+    free(recoverdir);
     closedir(dir);
 }
 

fontforge/asmfpst.c
@@ -810,13 +810,21 @@ static ASM *ASMFromCoverageFPST(SplineFont *sf,FPST *fpst,int ordered) {
     match_len = j;
 
     for ( i=0; i<match_len; ++i )
-	if ( tables[i]==NULL || tables[i][0]==NULL )
+	if ( tables[i]==NULL || tables[i][0]==NULL ) {
+            for ( i=0; i<match_len; ++i )
+	        free(tables[i]);
+            free(tables);
 return( NULL );
+       }
 
     glyphs = morx_cg_FigureClasses(tables,match_len,
 	    &classes,&class_cnt,&map,&gcnt,fpst,sf,ordered);
-    if ( glyphs==NULL )
+    if ( glyphs==NULL ) {
+        for ( i=0; i<match_len; ++i )
+	    free(tables[i]);
+        free(tables);
 return( NULL );
+    }
 
     for ( i=0; i<match_len; ++i )
 	free(tables[i]);

fontforge/autotrace.c
@@ -836,6 +836,7 @@ return( NULL );
 	ff_post_error(_("Can't run mf"),_("Can't run mf"));
     free(arglist[1]);
     cleantempdir(tempdir);
+    free(tempdir);
 return( sf );
 #endif
 }

fontforge/cvimages.c
@@ -223,11 +223,13 @@ return;
     break;
 	if ( ch!='(' ) {
 	    ff_post_error( _("Not a plate file"), _("This does not seem to be a plate file\nExpected left paren"));
+            free(spiros);
 return;
 	}
 	ch = getc(plate);
 	if ( ch!='v' && ch!='o' && ch!='c' && ch!='[' && ch!=']' && ch!='z' ) {
 	    ff_post_error( _("Not a plate file"), _("This does not seem to be a plate file\nExpected one of 'voc[]z'"));
+            free(spiros);
 return;
 	}
 	if ( cnt>=max )
@@ -251,6 +253,7 @@ return;
 	} else {
 	    if ( fscanf(plate,"%lg %lg )", &spiros[cnt].x, &spiros[cnt].y)!=2 ) {
 		ff_post_error( _("Not a plate file"), _("This does not seem to be a plate file\nExpected two real numbers"));
+                free(spiros);
 return;
 	    }
 	    ++cnt;

commit 015f017e9019af6db406a1eaceb9bc3f4db398b8

Fix autosave recovery
Regression from #3593

fontforge/autosave.c
@@ -28,7 +28,6 @@
 #include "autosave.h"
 
 #include "baseviews.h"
-#include "splineutil.h"
 #include "fontforgevw.h"
 #include "sfd.h"
 /*#include "ustring.h"*/
@@ -113,7 +112,6 @@ return( false );
 	    fprintf( stderr, " Done\n" );
 	}
     }
-    SplineFontFree(sf);
     free(recoverdir);
     closedir(dir);
 return( any );