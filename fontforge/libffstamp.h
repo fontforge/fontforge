80893ae604dd9ca010ca29da313c7eda40b307ee
Jeremy Tan <jtanx@outlook.com>
2019-06-08 11:43:23 +1000

    Remove libffstamp.h

--- fontforge/libffstamp.h
+++
@@ -1,6 +0,0 @@
-/* This file was generated using stamper.c to create the next version release.          */
-/* If you need to update this to the next release version, see fontforge/GNUmakefile.in */
-
-#define LibFF_ModTime		FONTFORGE_MODTIME_RAW	/* Seconds since 1970 (standard unix time) */
-#define LibFF_ModTime_Str	FONTFORGE_MODTIME_STR
-#define LibFF_VersionDate	FONTFORGE_VERSIONDATE_RAW	/* Year, month, day */
