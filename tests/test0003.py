776b80d6a790770ce90ba4b37fd36697755344db
Fredrick Brennan <copypaste@kittens.ph>
2020-02-12 15:21:43 +0800

    Add xBoundsAtY, yBoundsAtX to fontforge.glyph object (Python)
    This function also makes it so an optional layer argument can be passed
    to the `boundingBox` function of `fontforge.glyph` objects.
    
    New tests are added, for all of our sanity. This is the first time I've
    added new tests (after years of contributions, pretty scary right? haha).

---
+++ tests/test0003.py
@@ -0,0 +1,16 @@
+﻿#Needs: fonts/BoxesAndBounds.py
+
+import sys, fontforge
+
+font = fontforge.open(sys.argv[1])
+
+expected = (63.0, 57.0, 315.0, 309.0)
+assert font[0].boundingBox() == expected
+assert font[1].boundingBox(layer=1) == expected
+nullbb = (0.0, 0.0, 0.0, 0.0)
+assert font[1].boundingBox(layer=0) == nullbb
+
+expected = (63.0, 57.0, 315.0, 711.0)
+assert font[2].boundingBox() == expected
+expected = (63.0, 459.0, 315.0, 711.0)
+assert font[2].boundingBox(layer=2) == expected
