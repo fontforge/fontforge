46704cc75744965c7d11a434332f8e4a81d42bfc
Fredrick Brennan <copypaste@kittens.ph>
2020-02-14 19:32:13 +0800

    Add scriptFromUnicode tests

---
+++ tests/test0004.py
@@ -0,0 +1,16 @@
+# scriptFromUnicode tests…
+
+import sys, fontforge
+
+assert fontforge.scriptFromUnicode(ord('ᜈ')) == "tagl"
+assert fontforge.scriptFromUnicode(ord('Q')) == "latn"
+assert fontforge.scriptFromUnicode(ord('θ')) == "grek"
+assert fontforge.scriptFromUnicode(ord('Щ')) == "cyrl"
+assert fontforge.scriptFromUnicode(ord('ツ')) == "kana"
+assert fontforge.scriptFromUnicode(ord('ﬡ')) == "hebr"
+
+try:
+    fontforge.scriptFromUnicode("derp")
+    assert False
+except TypeError:
+    pass
