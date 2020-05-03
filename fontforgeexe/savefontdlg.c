fontforge/fontforge

commit 4b19eb9b30637cc6d92801a6cc6eaeaa67a0343e

Add an option to suppress the FFTM table

fontforgeexe/savefontdlg.c
@@ -99,6 +99,7 @@ static int nfnt_warned = false, post_warned = false;
 #define CID_TTF_DummyDSIG	1117
 #define CID_NativeKern		1118
 #define CID_TTF_OldKernMappedOnly 1119
+#define CID_TTF_FFTMTable	1120
 
 struct gfc_data {
     int done;
@@ -345,6 +346,8 @@ return( false );
 		    d->sfnt_flags |= ttf_flag_pfed_guides;
 		if ( GGadgetIsChecked(GWidgetGetControl(gw,CID_TTF_PfEdLayers)) )
 		    d->sfnt_flags |= ttf_flag_pfed_layers;
+		if ( !GGadgetIsChecked(GWidgetGetControl(gw,CID_TTF_FFTMTable)) )
+		    d->sfnt_flags |= ttf_flag_noFFTMtable;
 		if ( GGadgetIsChecked(GWidgetGetControl(gw,CID_TTF_TeXTable)) )
 		    d->sfnt_flags |= ttf_flag_TeXtable;
 		if ( GGadgetIsChecked(GWidgetGetControl(gw,CID_TTF_GlyphMap)) )
@@ -402,6 +405,8 @@ return( false );
 		    d->psotb_flags |= ttf_flag_pfed_guides;
 		if ( GGadgetIsChecked(GWidgetGetControl(gw,CID_TTF_PfEdLayers)) )
 		    d->psotb_flags |= ttf_flag_pfed_layers;
+		if ( !GGadgetIsChecked(GWidgetGetControl(gw,CID_TTF_FFTMTable)) )
+		    d->psotb_flags |= ttf_flag_noFFTMtable;
 		if ( GGadgetIsChecked(GWidgetGetControl(gw,CID_TTF_TeXTable)) )
 		    d->psotb_flags |= ttf_flag_TeXtable;
 		if ( GGadgetIsChecked(GWidgetGetControl(gw,CID_TTF_GlyphMap)) )
@@ -463,6 +468,8 @@ static void OptSetDefaults(GWindow gw,struct gfc_data *d,int which,int iscid) {
     GGadgetSetChecked(GWidgetGetControl(gw,CID_TTF_PfEdLookups),flags&ttf_flag_pfed_lookupnames);
     GGadgetSetChecked(GWidgetGetControl(gw,CID_TTF_PfEdGuides),flags&ttf_flag_pfed_guides);
     GGadgetSetChecked(GWidgetGetControl(gw,CID_TTF_PfEdLayers),flags&ttf_flag_pfed_layers);
+    GGadgetSetChecked(GWidgetGetControl(gw,CID_TTF_FFTMTable),
+	    (flags&~ttf_flag_noFFTMtable) && which!=0);
     GGadgetSetChecked(GWidgetGetControl(gw,CID_TTF_TeXTable),flags&ttf_flag_TeXtable);
     GGadgetSetChecked(GWidgetGetControl(gw,CID_TTF_GlyphMap),flags&ttf_flag_glyphmap);
     GGadgetSetChecked(GWidgetGetControl(gw,CID_TTF_OldKern),
@@ -500,6 +507,7 @@ static void OptSetDefaults(GWindow gw,struct gfc_data *d,int which,int iscid) {
     GGadgetSetEnabled(GWidgetGetControl(gw,CID_TTF_PfEdGuides),which!=0);
     GGadgetSetEnabled(GWidgetGetControl(gw,CID_TTF_PfEdColors),which!=0);
     GGadgetSetEnabled(GWidgetGetControl(gw,CID_TTF_PfEdLayers),which!=0);
+    GGadgetSetEnabled(GWidgetGetControl(gw,CID_TTF_FFTMTable),which!=0);
     GGadgetSetEnabled(GWidgetGetControl(gw,CID_TTF_TeXTable),which!=0);
     GGadgetSetEnabled(GWidgetGetControl(gw,CID_TTF_GlyphMap),which!=0);
     GGadgetSetEnabled(GWidgetGetControl(gw,CID_TTF_OFM),which!=0);
@@ -517,10 +525,10 @@ static void SaveOptionsDlg(struct gfc_data *d,int which,int iscid) {
     int k,fontlog_k,group,group2;
     GWindow gw;
     GWindowAttrs wattrs;
-    GGadgetCreateData gcd[35];
-    GTextInfo label[35];
+    GGadgetCreateData gcd[36];
+    GTextInfo label[36];
     GRect pos;
-    GGadgetCreateData *hvarray1[21], *hvarray2[42], *hvarray3[8], *harray[7], *varray[11];
+    GGadgetCreateData *hvarray1[21], *hvarray2[46], *hvarray3[8], *harray[7], *varray[11];
     GGadgetCreateData boxes[6];
 
     d->sod_done = false;
@@ -822,15 +830,25 @@ static void SaveOptionsDlg(struct gfc_data *d,int which,int iscid) {
 
     gcd[k].gd.pos.x = gcd[k-3].gd.pos.x; gcd[k].gd.pos.y = gcd[k-5].gd.pos.y;
     gcd[k].gd.flags = gg_visible;
+    label[k].text = (unichar_t *) _("FFTM Table");
+    label[k].text_is_1byte = true;
+    gcd[k].gd.popup_msg = _("The FFTM table is an extension to the TrueType format\nand contains a series of timestamps defined by FontForge\n");
+    gcd[k].gd.label = &label[k];
+    gcd[k].gd.cid = CID_TTF_FFTMTable;
+    gcd[k++].creator = GCheckBoxCreate;
+    hvarray2[32] = &gcd[k-1]; hvarray2[33] = GCD_ColSpan; hvarray2[34] = NULL;
+
+    gcd[k].gd.pos.x = gcd[k-1].gd.pos.x; gcd[k].gd.pos.y = gcd[k-1].gd.pos.y;
+    gcd[k].gd.flags = gg_visible;
     label[k].text = (unichar_t *) _("TeX Table");
     label[k].text_is_1byte = true;
     gcd[k].gd.popup_msg = _("The TeX table is an extension to the TrueType format\nand the various data you would expect to find in\na tfm file (that isn't already stored elsewhere\nin the ttf file)\n");
     gcd[k].gd.label = &label[k];
     gcd[k].gd.cid = CID_TTF_TeXTable;
     gcd[k++].creator = GCheckBoxCreate;
-    hvarray2[32] = &gcd[k-1]; hvarray2[33] = GCD_ColSpan; hvarray2[34] = NULL;
-    hvarray2[37] = GCD_Glue; hvarray2[38] = GCD_Glue; hvarray2[39] = NULL;
-    hvarray2[40] = NULL;
+    hvarray2[37] = &gcd[k-1]; hvarray2[38] = GCD_ColSpan; hvarray2[39] = NULL;
+    hvarray2[41] = GCD_Glue; hvarray2[42] = GCD_Glue; hvarray2[43] = NULL;
+    hvarray2[44] = NULL;
 
     boxes[3].gd.flags = gg_enabled|gg_visible;
     boxes[3].gd.u.boxelements = hvarray2;

fontforge/python.c
@@ -16668,6 +16668,7 @@ struct flaglist gen_flags[] = {
     { "symbol", fm_flag_symbol },
     { "dummy-dsig", fm_flag_dummyDSIG },
     { "dummy-DSIG", fm_flag_dummyDSIG },
+    { "no-FFTM-table", fm_flag_nofftm },
     { "tfm", fm_flag_tfm },
     { "no-flex", fm_flag_noflex },
     { "no-hints", fm_flag_nottfhints },

fontforge/savefont.c
@@ -1185,6 +1185,7 @@ int GenerateScript(SplineFont *sf,char *filename,const char *bitmaptype, int fmf
 		    old_sfnt_flags |= ttf_flag_oldkern;
 		if ( fmflags&fm_flag_symbol ) old_sfnt_flags |= ttf_flag_symbol;
 		if ( fmflags&fm_flag_dummyDSIG ) old_sfnt_flags |= ttf_flag_dummyDSIG;
+		if ( fmflags&fm_flag_nofftm ) old_sfnt_flags |= ttf_flag_noFFTMtable;
 		if ( fmflags&fm_flag_pfed_lookups ) old_sfnt_flags |= ttf_flag_pfed_lookupnames;
 		if ( fmflags&fm_flag_pfed_guides ) old_sfnt_flags |= ttf_flag_pfed_guides;
 		if ( fmflags&fm_flag_pfed_layers ) old_sfnt_flags |= ttf_flag_pfed_layers;
@@ -1229,6 +1230,7 @@ int GenerateScript(SplineFont *sf,char *filename,const char *bitmaptype, int fmf
 		old_sfnt_flags |= ttf_flag_oldkern;
 	    if ( fmflags&fm_flag_symbol ) old_sfnt_flags |= ttf_flag_symbol;
 	    if ( fmflags&fm_flag_dummyDSIG ) old_sfnt_flags |= ttf_flag_dummyDSIG;
+	    if ( fmflags&fm_flag_nofftm ) old_sfnt_flags |= ttf_flag_noFFTMtable;
 	    if ( fmflags&fm_flag_pfed_lookups ) old_sfnt_flags |= ttf_flag_pfed_lookupnames;
 	    if ( fmflags&fm_flag_pfed_guides ) old_sfnt_flags |= ttf_flag_pfed_guides;
 	    if ( fmflags&fm_flag_pfed_layers ) old_sfnt_flags |= ttf_flag_pfed_layers;

fontforge/savefont.h
@@ -47,7 +47,7 @@ enum fm_flags { fm_flag_afm = 0x1,
                 // fm_flag_??? = 0x1000,
                 fm_flag_symbol = 0x2000,
                 fm_flag_dummyDSIG = 0x4000,
-                // fm_flag_??? = 0x8000,
+                fm_flag_nofftm = 0x8000,
                 fm_flag_tfm = 0x10000,
                 fm_flag_nohintsubs = 0x20000,
                 fm_flag_noflex = 0x40000,

fontforge/splinefont.h
@@ -2039,6 +2039,7 @@ enum ttf_flags {
     ttf_flag_TeXtable          = 1 <<  7,
     ttf_flag_ofm               = 1 <<  8,
     ttf_flag_oldkern           = 1 <<  9, // never set in conjunction with applemode
+    ttf_flag_noFFTMtable       = 1 << 10,
     ttf_flag_pfed_lookupnames  = 1 << 11,
     ttf_flag_pfed_guides       = 1 << 12,
     ttf_flag_pfed_layers       = 1 << 13,

fontforge/ttfspecial.c
@@ -2232,6 +2232,9 @@ return;
 int ttf_fftm_dump(SplineFont *sf,struct alltabs *at) {
     int32 results[2];
 
+    if ( at->gi.flags & ttf_flag_noFFTMtable )
+	return false;
+
     at->fftmf = GFileTmpfile();
 
     putlong(at->fftmf,0x00000001);	/* Version */

commit 69a628c59e26aec1daad4cee383067b88432d693

Fix UI segmentation fault introduced in #4309

fontforgeexe/savefontdlg.c
@@ -847,8 +847,8 @@ static void SaveOptionsDlg(struct gfc_data *d,int which,int iscid) {
     gcd[k].gd.cid = CID_TTF_TeXTable;
     gcd[k++].creator = GCheckBoxCreate;
     hvarray2[37] = &gcd[k-1]; hvarray2[38] = GCD_ColSpan; hvarray2[39] = NULL;
-    hvarray2[41] = GCD_Glue; hvarray2[42] = GCD_Glue; hvarray2[43] = NULL;
-    hvarray2[44] = NULL;
+    hvarray2[40] = GCD_Glue; hvarray2[41] = GCD_Glue; hvarray2[42] = NULL;
+    hvarray2[43] = NULL;
 
     boxes[3].gd.flags = gg_enabled|gg_visible;
     boxes[3].gd.u.boxelements = hvarray2;