dcfba4c2b76b0319a59a39946a9fe18f157fd61b
yg8ijvjvjv <68180132+yg8ijvjvjv@users.noreply.github.com>
2020-10-03 18:00:04 +0200

    moved pointoncurve outside the function
    moved pointoncurve outside the function due to a function incompatibility.

--- fontforge/bezctx_ff.c
+++ fontforge/bezctx_ff.c
@@ -92,8 +92,9 @@ static void bezctx_ff_lineto(bezctx *z, double x, double y) {
 /* This could create a quadratic spline, except FontForge only is prepared */
 /* to deal with cubics, so convert the quadratic into the equivalent cubic */
 /* If NewTrueTypeModeEnabled, FontForge is prepared for TrueType. */
+int pointoncurve;
 static void
-bezctx_ff_quadto(bezctx *z, double xm, double ym, double x3, double y3, int pointoncurve) {
+bezctx_ff_quadto(bezctx *z, double xm, double ym, double x3, double y3) {
     if(NewTrueTypeModeEnabled){
     
 	    bezctx_ff *bc = (bezctx_ff *)z;
