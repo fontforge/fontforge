2d3d36148e8b5b93dcb0983d498204ab281c431e
Hin-Tak Leung <htl10@users.sourceforge.net>
2020-12-14 18:44:25 +0000

    replace comparison to True/False with direct boolean test

--- tests/test1016.py
+++ tests/test1016.py
@@ -7,11 +7,11 @@
 import sys, fontforge
 font = fontforge.open( sys.argv[1] )
 font.selection.all()
-if ( font.isWorthOutputting() != True ):
+if ( !font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after selection.all")
 
 font.selection.none()
-if ( font.isWorthOutputting() != False ):
+if ( font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after selection.none")
 
 font.reencode("ucs4")
@@ -24,31 +24,31 @@ else:
     raise ValueError("Unexpected Error")
 
 font.selection.select(256)
-if ( font.isWorthOutputting() != False ):
+if ( font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after selecting unused slot")
 
 font.selection.all()
-if ( font.isWorthOutputting() != True ):
+if ( !font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after 2nd selection.all")
 
 font.selection.none()
-if ( font.isWorthOutputting() != False ):
+if ( font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after 2nd selection.none")
 
 font.selection.select(32)
-if ( font.isWorthOutputting() != False ):
+if ( font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after 2nd unused slot")
 
 font.selection.select("accidentals.sharp")
-if ( font.isWorthOutputting() != True ):
+if ( !font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after used named slot")
 
 font.selection.none()
-if ( font.isWorthOutputting() != False ):
+if ( font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after 3rd selection.nome")
 
 font.selection.select(33)
-if ( font.isWorthOutputting() != False ):
+if ( font.isWorthOutputting() ):
     raise ValueError("Wrong return from fontforge.fonts after 3rd unused slot")
 
 print("font.isWorthOutputting tests done success")
