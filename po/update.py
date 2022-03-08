432e3bdde6d1c6d410f0ed188ec9df1ccb074bc4
Jeremy Tan <jtanx@outlook.com>
2022-03-08 20:36:02 +1100

    Update pofiles with latest potfile, sort and only keep translated strings

---
+++ po/update.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import pathlib
+import subprocess
+import sys
+
+
+def update(potfile, pofile):
+    header = []
+    with open(pofile, encoding="utf-8") as fp:
+        for l in fp:
+            l = l.strip()
+            if l.startswith("#"):
+                header.append(l)
+            else:
+                break
+
+    print(f"Updating {pofile} with {potfile}...")
+    subprocess.check_call(
+        ["msgmerge", "--for-msgfmt", "-s", pofile, potfile, "-o", pofile]
+    )
+
+    updated_trans = pathlib.Path("updates") / pathlib.Path(pofile).name
+    if updated_trans.exists():
+        print(f"Updating {pofile} with {updated_trans}...")
+        subprocess.check_call(
+            ["msgmerge", "--for-msgfmt", "-s", str(updated_trans), pofile, "-o", pofile]
+        )
+
+    print(f"Reappending header to {pofile}...")
+    with open(pofile, encoding="utf-8") as fp:
+        data = fp.read()
+    with open(pofile, "w", encoding="utf-8") as fp:
+        print("\n".join(header), file=fp)
+        fp.write(data)
+
+
+def update_all(potfile):
+    for f in pathlib.Path().glob("*.po"):
+        update(potfile, str(f))
+
+
+if __name__ == "__main__":
+    update_all(sys.argv[1])
