2181db0ec19de41e42329b90a34347e64b267ca0
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-10-07 01:16:20 +0300

    Removed debug prints

--- tests/pyhook_smoketest.py
+++ tests/pyhook_smoketest.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import sys
-
-print(sys.path)
 import fontforge
 import psMat
 import pickle
-import sysconfig
-
-print(sysconfig.get_paths())
-if hasattr(sysconfig, "_get_preferred_schemes"):
-    print(sysconfig._get_preferred_schemes())
 
 print(fontforge.__version__, fontforge.version())
 
