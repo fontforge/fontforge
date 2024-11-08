7773703156987ffd41a5d86618a1ded70a5d06ad
наб <nabijaczleweli@nabijaczleweli.xyz>
2024-11-08 19:05:58 +0100

    tottfgpos: remove unused variable
    [174/280] Building C object fontforge/CMakeFiles/fontforge.dir/stemdb.c.o
    /home/nabijaczleweli/uwu/fontforge/fontforge/stemdb.c:2366:32: warning: comparison of address of 'other->to->next' not equal to a null pointer is always true [-Wtautological-pointer-compare]
     2366 |         topd->colinear && &other->to->next != NULL ) {
          |                            ~~~~~~~~~~~^~~~    ~~~~
    /home/nabijaczleweli/uwu/fontforge/fontforge/stemdb.c:2382:36: warning: comparison of address of 'other->from->prev' not equal to a null pointer is always true [-Wtautological-pointer-compare]
     2382 |         frompd->colinear && &other->from->prev != NULL ) {
          |                              ~~~~~~~~~~~~~^~~~    ~~~~
    2 warnings generated.
    [175/280] Building C object fontforge/CMakeFiles/fontforge.dir/tottfgpos.c.o
    /home/nabijaczleweli/uwu/fontforge/fontforge/tottfgpos.c:711:6: warning: variable 'glyph_cnt' set but not used [-Wunused-but-set-variable]
      711 |         int glyph_cnt = 0;
          |             ^
    1 warning generated.

--- fontforge/tottfgpos.c
+++ fontforge/tottfgpos.c
@@ -708,7 +708,6 @@ static void dumpcoveragetable(FILE *gpos,SplineChar **glyphs) {
 
 	// We will not emit glyphs with -1 identifiers.
 	// We count the valid glyphs and the ranges.
-	int glyph_cnt = 0;
 	for (i=0; glyphs[i]!=NULL; i++) {
 		if (i > 0 && glyphs[i]->ttf_glyph <= glyphs[i-1]->ttf_glyph) {
 			LogError(_("Glyphs must be ordered when creating coverage table"));
@@ -716,7 +715,6 @@ static void dumpcoveragetable(FILE *gpos,SplineChar **glyphs) {
 		if (glyphs[i]->ttf_glyph < 0) {
 			LogError(_("-1 glyph index in dumpcoveragetable.\n"));
 		} else {
-			glyph_cnt++;
 			// On the first validly TrueType-indexed glyph or at the start of any discontinuity, start a new range.
 			if (range_cnt == 0 || glyphs[i]->ttf_glyph > last + 1)
 				range_cnt++;
