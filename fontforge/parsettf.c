e9fa9a3de7e136b9bc8df10878c01d65d86f0ea7
Maxim Iorsh <iorsh@users.sourceforge.net>
2025-06-13 00:45:48 +0300

    Add comment

--- fontforge/parsettf.c
+++ fontforge/parsettf.c
@@ -6305,6 +6305,10 @@ static SplineFont *SFFillFromTTF(struct ttfinfo *info) {
 			info->bad_cff = true;
 		    }
 		    if ( info->isFixedPitch ) {
+			/* Modern Unicode fonts tend to have lots of zero-width
+			   characters like diacritics or control chars, e.g. LRM.
+			   These characters don't have an advance width and
+			   shouldn't affect pitch computation. */
 			bool valid = (sc->width==0 || sc->width==info->advanceWidthMax || sc->width==info->advanceWidthMax / 2);
 			if (!valid) {
 			    LogError(_("The advance width of %s (%d) does not match the font's advanceWidthMax (%d) and this is a fixed pitch font\n"),
