b0aa0d2732dfe9971c83339940c40c0665adc0d1
Maxim Iorsh <iorsh@users.sourceforge.net>
2025-10-12 23:18:16 +0300

    Class LanguageListDlg

---
+++ fontforgeexe/gtk/language_list.cpp
@@ -0,0 +1,72 @@
+/* Copyright 2023 Joey Sabey <github.com/Omnikron13>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "language_list.hpp"
+
+#include "utils.hpp"
+#include "intl.h"
+
+namespace ff::dlg {
+
+LanguageListDlg::LanguageListDlg(GWindow parent,
+                                 const ff::dlg::LanguageRecords& lang_recs)
+    : Dialog(parent), list_(1, false, Gtk::SELECTION_MULTIPLE) {
+    set_title(_("Language List"));
+
+    auto scrolled_window = Gtk::make_managed<Gtk::ScrolledWindow>();
+    scrolled_window->set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC);
+
+    auto search_entry = Gtk::make_managed<Gtk::SearchEntry>();
+
+    for (const auto& [name, tag] : lang_recs) {
+        list_.append(name);
+    }
+    list_.set_headers_visible(false);
+    list_.set_search_entry(*search_entry);
+
+    // Show initially roughtly 8 rows.
+    property_default_height() = 40 * ui_font_eX_size();
+
+    scrolled_window->add(list_);
+
+    get_content_area()->pack_start(*search_entry, Gtk::PACK_SHRINK);
+    get_content_area()->pack_start(*scrolled_window, Gtk::PACK_EXPAND_WIDGET);
+
+    show_all();
+}
+
+int LanguageListDlg::show(GWindow parent,
+                          const ff::dlg::LanguageRecords& lang_recs) {
+    LanguageListDlg dialog(parent, lang_recs);
+    dialog.signal_response().connect([&](int response_id) {
+        // Ok/Add returns the entered integer
+        if (response_id == Gtk::RESPONSE_OK) {
+            // i = dialog.get_value();
+        }
+        // Cancel/close returns 0, which is semantically equivalent to the
+        // user actually entering 0
+        else if (response_id == Gtk::RESPONSE_CANCEL ||
+                 response_id == Gtk::RESPONSE_DELETE_EVENT ||
+                 response_id == Gtk::RESPONSE_CLOSE) {
+            // i = 0;
+        }
+        // Any other response, which shouldn't really be possible, returns
+        // -1 to indicate an error
+    });
+    dialog.run();
+    return 0;
+}
+
+}  // namespace ff::dlg
---
+++ fontforgeexe/gtk/language_list.hpp
@@ -0,0 +1,36 @@
+/* Copyright 2023 Joey Sabey <github.com/Omnikron13>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+#pragma once
+
+#include "dialog.hpp"
+#include "tag.hpp"
+
+namespace ff::dlg {
+
+using LanguageRecords = std::vector<std::pair<std::string /*name*/, ff::Tag>>;
+
+class LanguageListDlg final : public Dialog {
+ private:
+    Gtk::ListViewText list_;
+
+    LanguageListDlg(GWindow parent, const ff::dlg::LanguageRecords& lang_recs);
+
+ public:
+    // Show the dialog and return either the entered integer, 0 if the dialog
+    // was cancelled/closed, or -1 if something strange happened
+    static int show(GWindow parent, const ff::dlg::LanguageRecords& lang_recs);
+};
+
+}  // namespace ff::dlg
